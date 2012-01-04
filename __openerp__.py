8f44bbb054a982068700b660d7a73c5f4f387671
Christophe CHAUVET <christophe.chauvet@syleam.fr>
2012-01-04 10:56:56 +0100

    [FIX] Version number must be in 2 positions in 6.0

--- __openerp__.py
+++ __openerp__.py
@@ -24,7 +24,7 @@
 
 {
     'name': 'Stock Scanner',
-    'version': '0.1.0',
+    'version': '0.1',
     'category': 'Generic Modules/Inventory Control',
     'description': """Allows managing barcode readers with simple scenarios""",
     'author': 'SYLEAM Info Services',
