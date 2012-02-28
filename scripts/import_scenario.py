31824a5024694629c9d6e9127bcd8252e0069b17
Sylvain Garancher <sylvain.garancher@syleam.fr>
2012-02-28 11:58:21 +0100

    [IMP] Add an argument to import recursively scenarios from a directory

--- scripts/import_scenario.py
+++ scripts/import_scenario.py
@@ -31,15 +31,21 @@ from optparse import OptionGroup
 from lxml.etree import parse
 from StringIO import StringIO
 
+from glob import glob
 import logging
 import sys
+import os
 
 parser = GetParser('Export scenario', '0.1')
 group = OptionGroup(parser, "Object arguments",
         "Application Options")
 group.add_option('', '--directory', dest='directory',
                  default='.',
-                 help='directory where the script will search for the scenario files')
+                 help='directory where the script will search for the scenario files (default \'.\')')
+group.add_option('-r', '--recursive', dest='recursive',
+                 action='store_true',
+                 default=False,
+                 help='Recursively imports scenarios from the supplied directory')
 group.add_option('-v', '--verbose', dest='verbose',
                  action='store_true',
                  default=False,
@@ -76,117 +82,129 @@ except Exception, e:
 
 resid = {}
 
-# extract scenario
-scenario_obj = Object(cnx, 'scanner.scenario')
-model_obj = Object(cnx, 'ir.model')
-warehouse_obj = Object(cnx, 'stock.warehouse')
-step_obj = Object(cnx, 'scanner.scenario.step')
-trans_obj = Object(cnx, 'scanner.scenario.transition')
-xml_file = open('%s/scenario.xml' % opts.directory, 'r')
-logger.info('Scenario file found, process reading!')
-scenario_xml = xml_file.read()
-xml_file.close()
-xml_doc = StringIO(scenario_xml)
-root = parse(xml_doc).getroot()
+directories = []
+if glob(opts.directory + '/scenario.xml'):
+    directories.append(opts.directory)
+if opts.recursive:
+    for root, dirs, files in os.walk(opts.directory):
+        for directory in dirs:
+            if glob('%s/%s/scenario.xml' % (root, directory)):
+                directories.append('%s/%s' % (root, directory))
 
-step = []
-transition = []
-scen_vals = {}
+for directory in directories:
+    logger.info('Importing from : %s' % directory)
 
-# parse of the scenario
-steps_number = 0
-transitions_number = 0
-for node in root.getchildren():
-    #the node of the Step and Transition are put in other list
-    if node.tag == 'Step':
-        steps_number += 1
-        step.append(node)
-    elif node.tag == 'Transition':
-        transitions_number += 1
-        transition.append(node)
-    elif node.tag == 'warehouse_ids':
-        if not 'warehouse_ids' in scen_vals:
-            scen_vals['warehouse_ids'] = []
-        warehouse_ids = warehouse_obj.search([('name', '=', node.text)], 0, None, None, {'active_test': False})
-        if warehouse_ids:
-            scen_vals['warehouse_ids'].append((4, warehouse_ids[0]))
-    elif node.tag == 'shared_custom':
-        scen_vals[node.tag] = eval(node.text) or False
-    else:
-        scen_vals[node.tag] = node.text or False
-logger.info('%d steps found' % steps_number)
-logger.info('%d transitions found' % transitions_number)
+    # extract scenario
+    scenario_obj = Object(cnx, 'scanner.scenario')
+    model_obj = Object(cnx, 'ir.model')
+    warehouse_obj = Object(cnx, 'stock.warehouse')
+    step_obj = Object(cnx, 'scanner.scenario.step')
+    trans_obj = Object(cnx, 'scanner.scenario.transition')
+    xml_file = open('%s/scenario.xml' % directory, 'r')
+    logger.info('Scenario file found, process reading!')
+    scenario_xml = xml_file.read()
+    xml_file.close()
+    xml_doc = StringIO(scenario_xml)
+    root = parse(xml_doc).getroot()
 
-if scen_vals['model_id']:
-    logger.info('Search model: %s' % scen_vals['model_id'])
-    scen_vals['model_id'] = model_obj.search([('model', '=', scen_vals['model_id'])], 0, None, None, {'active_test': False}) or False
+    step = []
+    transition = []
+    scen_vals = {}
+
+    # parse of the scenario
+    steps_number = 0
+    transitions_number = 0
+    for node in root.getchildren():
+        #the node of the Step and Transition are put in other list
+        if node.tag == 'Step':
+            steps_number += 1
+            step.append(node)
+        elif node.tag == 'Transition':
+            transitions_number += 1
+            transition.append(node)
+        elif node.tag == 'warehouse_ids':
+            if not 'warehouse_ids' in scen_vals:
+                scen_vals['warehouse_ids'] = []
+            warehouse_ids = warehouse_obj.search([('name', '=', node.text)], 0, None, None, {'active_test': False})
+            if warehouse_ids:
+                scen_vals['warehouse_ids'].append((4, warehouse_ids[0]))
+        elif node.tag == 'shared_custom':
+            scen_vals[node.tag] = eval(node.text) or False
+        else:
+            scen_vals[node.tag] = node.text or False
+    logger.info('%d steps found' % steps_number)
+    logger.info('%d transitions found' % transitions_number)
+
     if scen_vals['model_id']:
-        logger.info('Model found')
-        scen_vals['model_id'] = scen_vals['model_id'][0]
-    else:
-        logger.error('Model not found')
-        sys.exit(1)
+        logger.info('Search model: %s' % scen_vals['model_id'])
+        scen_vals['model_id'] = model_obj.search([('model', '=', scen_vals['model_id'])], 0, None, None, {'active_test': False}) or False
+        if scen_vals['model_id']:
+            logger.info('Model found')
+            scen_vals['model_id'] = scen_vals['model_id'][0]
+        else:
+            logger.error('Model not found')
+            sys.exit(1)
 
-if 'parent_id' in scen_vals and scen_vals['parent_id']:
-    logger.info('Search parent: %s' % scen_vals['parent_id'])
-    scen_vals['parent_id'] = scenario_obj.search([('reference_res_id', '=', scen_vals['parent_id'])], 0, None, None, {'active_test': False}) or False
-    if scen_vals['parent_id']:
-        logger.info('Parent found')
-        scen_vals['parent_id'] = scen_vals['parent_id'][0]
-    else:
-        logger.error('Parent not found')
-        sys.exit(1)
+    if 'parent_id' in scen_vals and scen_vals['parent_id']:
+        logger.info('Search parent: %s' % scen_vals['parent_id'])
+        scen_vals['parent_id'] = scenario_obj.search([('reference_res_id', '=', scen_vals['parent_id'])], 0, None, None, {'active_test': False}) or False
+        if scen_vals['parent_id']:
+            logger.info('Parent found')
+            scen_vals['parent_id'] = scen_vals['parent_id'][0]
+        else:
+            logger.error('Parent not found')
+            sys.exit(1)
 
-# create or update
-scenario_ids = scenario_obj.search([('reference_res_id', '=', scen_vals['reference_res_id'])], 0, None, None, {'active_test': False})
-if scenario_ids:
-    logger.info('Scenario exists, update it')
-    del scen_vals['reference_res_id']
-    scenario_obj.write(scenario_ids, scen_vals)
-    scenario_id = scenario_ids[0]
-else:
-    logger.info('Scenario not exists, create it')
-    scenario_id = scenario_obj.create(scen_vals)
-
-#parse step
-logger.info('Update steps')
-resid = {}
-for node in step:
-    step_vals = {}
-    for key, item in node.items():
-        if item == 'False':
-            item = False
-        step_vals[key] = item
-    # get scenario id
-    step_vals['scenario_id'] = scenario_id
-    # get python src
-    python_code = open('%s/%s.py' % (opts.directory, step_vals['reference_res_id']), 'r')
-    step_vals['python_code'] = python_code.read()
-    python_code.close()
     # create or update
-    step_ids = step_obj.search([('reference_res_id', '=', step_vals['reference_res_id'])], 0, None, None, {'active_test': False})
-    if step_ids:
-        resid[step_vals['reference_res_id']] = step_ids[0]
-        del step_vals['reference_res_id']
-        step_obj.write(step_ids, step_vals)
+    scenario_ids = scenario_obj.search([('reference_res_id', '=', scen_vals['reference_res_id'])], 0, None, None, {'active_test': False})
+    if scenario_ids:
+        logger.info('Scenario exists, update it')
+        del scen_vals['reference_res_id']
+        scenario_obj.write(scenario_ids, scen_vals)
+        scenario_id = scenario_ids[0]
     else:
-        resid[step_vals['reference_res_id']] = step_obj.create(step_vals)
+        logger.info('Scenario not exists, create it')
+        scenario_id = scenario_obj.create(scen_vals)
 
-#parse transition
-logger.info('Update transitions')
-for node in transition:
-    trans_vals = {}
-    for key, item in node.items():
-        if key in ['to_id', 'from_id']:
-            item = resid[item]
-        trans_vals[key] = item
-    # create or update
-    trans_ids = trans_obj.search([('reference_res_id', '=', trans_vals['reference_res_id'])], 0, None, None, {'active_test': False})
-    if trans_ids:
-        del trans_vals['reference_res_id']
-        trans_obj.write(trans_ids, trans_vals)
-    else:
-        trans_obj.create(trans_vals)
+    #parse step
+    logger.info('Update steps')
+    resid = {}
+    for node in step:
+        step_vals = {}
+        for key, item in node.items():
+            if item == 'False':
+                item = False
+            step_vals[key] = item
+        # get scenario id
+        step_vals['scenario_id'] = scenario_id
+        # get python src
+        python_code = open('%s/%s.py' % (directory, step_vals['reference_res_id']), 'r')
+        step_vals['python_code'] = python_code.read()
+        python_code.close()
+        # create or update
+        step_ids = step_obj.search([('reference_res_id', '=', step_vals['reference_res_id'])], 0, None, None, {'active_test': False})
+        if step_ids:
+            resid[step_vals['reference_res_id']] = step_ids[0]
+            del step_vals['reference_res_id']
+            step_obj.write(step_ids, step_vals)
+        else:
+            resid[step_vals['reference_res_id']] = step_obj.create(step_vals)
+
+    #parse transition
+    logger.info('Update transitions')
+    for node in transition:
+        trans_vals = {}
+        for key, item in node.items():
+            if key in ['to_id', 'from_id']:
+                item = resid[item]
+            trans_vals[key] = item
+        # create or update
+        trans_ids = trans_obj.search([('reference_res_id', '=', trans_vals['reference_res_id'])], 0, None, None, {'active_test': False})
+        if trans_ids:
+            del trans_vals['reference_res_id']
+            trans_obj.write(trans_ids, trans_vals)
+        else:
+            trans_obj.create(trans_vals)
 
 logger.info('Import scenario done!')
 
