eaec27041bf48b8d11a31546ec3f21e80ee737ab
Sylvain GARANCHER <sylvain.garancher@syleam.fr>
2012-11-29 07:35:31 +0100

    [IMP] Added explanation for the steps return types

--- demo/Tutorial/Sentinel/9e4ea92e-399b-11e2-b3d4-74de2b37388c.py
+++ demo/Tutorial/Sentinel/9e4ea92e-399b-11e2-b3d4-74de2b37388c.py
@@ -6,5 +6,21 @@
 
 act = 'F'
 res = [
-    'This scenario is finished !',
+    'Return values of a step',
+    '',
+    'All steps must return three values :',
+    '- act : Type of step',
+    '- res : Content to display',
+    '- val : Default value',
+    '',
+    'The \'act\' variable must be a single character string.',
+    '',
+    'The \'res\' variable can contain multiple values :',
+    '- A list of strings (all step types). Each string corresponds to a line of text to display. The long lines will be splitted automatically to fit on screen.',
+    '- A list of tuples of two strings (list steps). The first value of each tuple is the returned value, the second value is the displayed value.',
+    '- A dict of strings (list step). The key is the returned value, the second value is the displayed value.',
+    '',
+    'The \'val\' variable can contain multiple values :',
+    '- A single value (all step types). The type of the value depends on the type of step (float, integer, boolean...).',
+    '- A dict (text input step). The key \'default\' is the default value, the key \'size\' is the maximum size before automatic validation.',
 ]
