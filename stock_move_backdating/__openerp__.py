1a680ac75a3889306d8625b6281a19ab78dc4cc5
Alex Comba <alex.comba@agilebg.com>
2013-04-22 09:55:38 +0200

    [ADD] stock_move_backdating

---
+++ stock_move_backdating/__openerp__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2012+ BREMSKERL-REIBBELAGWERKE EMMERLING GmbH & Co. KG
+#    Author Marco Dieckhoff
+#    Copyright (C) 2013 Agile Business Group sagl (<http://www.agilebg.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Allows back-dating of stock moves",
+    "version": "1.0",
+    'author' : 'Marco Dieckhoff, BREMSKERL', 'Agile Business Group',
+    "category": "Stock Logistics",
+    'website' : 'www.bremskerl.com', 'www.agilebg.com',
+    "depends": ["stock"],
+    "description": "Allows back-dating of stock moves",
+    'data': ["view/stock_view.xml"],
+    'demo': [],
+    'installable': True,
+}
