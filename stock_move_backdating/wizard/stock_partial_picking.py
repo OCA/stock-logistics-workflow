3babb4b3e64d9e3ccc238b17ed9db9180ba81246
Alex Comba <alex.comba@agilebg.com>
2013-05-27 16:25:59 +0200

    [FIX] stock_partial_picking_line

--- stock_move_backdating/wizard/stock_partial_picking.py
+++ stock_move_backdating/wizard/stock_partial_picking.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from tools.translate import _
 
 class stock_partial_picking_line(osv.TransientModel):
-
+    _inherit = 'stock.partial.picking.line'
     _name = "stock.partial.picking.line"
     _columns = {
     	'date_backdating': fields.related('move_id', 'date_backdating', type="datetime", string="Actual Movement Date")
