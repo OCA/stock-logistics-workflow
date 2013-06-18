fa102e98760ed3c14bca0e5572562c2ed4c78875
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-06-18 15:54:22 +0200

    [MRG] merged from the main branch (lp:ifrc-openerp/trunk-generic) (/home/jgrandguillaume/c2c/ProjetsEC/IFRC/Code/openerp_70_ifrc_core_dev/parts/generic-addons rev 8.1.9)

--- __openerp__.py
+++ __openerp__.py
@@ -39,6 +39,7 @@ A transport plan represent a kind of "contract" with your carrier.
               ],
  "demo": [],
  "data": ["data/tranport_plan_sequence.xml",
+          "data/tranport_mode_data.xml",
           "view/transport_plan.xml",
           "view/transport_mode.xml",
           "security/ir.model.access.csv",
