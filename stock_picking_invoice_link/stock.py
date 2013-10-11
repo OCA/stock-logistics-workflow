47e11d20030c540dd4db7de1b21961954aac4447
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2013-10-11 08:57:03 +0200

    [ADD] handling _invoice_line_hook

--- stock_picking_invoice_link/stock.py
+++ stock_picking_invoice_link/stock.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    Copyright (C) 2013 Agile Business Group sagl (<http://www.agilebg.com>)
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -21,28 +21,50 @@
 from openerp.osv import fields, orm
 from openerp.tools.translate import _
 
+
+class stock_move(orm.Model):
+    _inherit = "stock.move"
+
+    _columns = {
+        'invoice_line_id': fields.many2one(
+            'account.invoice.line', 'Invoice line', readonly=True),
+    }
+
+
 class stock_picking(orm.Model):
     _inherit = "stock.picking"
-    
+
     _columns = {
-        'invoice_id': fields.many2one('account.invoice', 'Invoice', readonly=True),
-        }
-        
+        'invoice_id': fields.many2one(
+            'account.invoice', 'Invoice', readonly=True),
+    }
+
     def _invoice_hook(self, cr, uid, picking, invoice_id):
-        res = super(stock_picking,self)._invoice_hook(cr, uid, picking, invoice_id)
+        res = super(stock_picking, self)._invoice_hook(
+            cr, uid, picking, invoice_id)
         picking.write({'invoice_id': invoice_id})
         return res
 
+    def _invoice_line_hook(self, cr, uid, move_line, invoice_line_id):
+        res = super(stock_picking, self)._invoice_line_hook(
+            cr, uid, move_line, invoice_line_id)
+        move_line.write({'invoice_line_id': invoice_line_id})
+        return res
+
+
 class stock_picking_out(orm.Model):
     _inherit = "stock.picking.out"
-    
+
     _columns = {
-        'invoice_id': fields.many2one('account.invoice', 'Invoice', readonly=True),
-        }
+        'invoice_id': fields.many2one(
+            'account.invoice', 'Invoice', readonly=True),
+    }
+
 
 class stock_picking_in(orm.Model):
     _inherit = "stock.picking.in"
-    
+
     _columns = {
-        'invoice_id': fields.many2one('account.invoice', 'Invoice', readonly=True),
-        }
+        'invoice_id': fields.many2one(
+            'account.invoice', 'Invoice', readonly=True),
+    }
