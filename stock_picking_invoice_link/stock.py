9f31739e77d5584eac0b13abfa11cb2ed0134580
Alexis de Lattre <alexis@via.ecp.fr>
2013-10-28 16:18:37 +0100

    Add one2many from invoice to pickings and display it in the invoice view. Minor coding style improvements.

--- stock_picking_invoice_link/stock.py
+++ stock_picking_invoice_link/stock.py
@@ -19,7 +19,6 @@
 ##############################################################################
 
 from openerp.osv import fields, orm
-from openerp.tools.translate import _
 
 
 class stock_move(orm.Model):
@@ -34,9 +33,24 @@ class stock_move(orm.Model):
 class stock_picking(orm.Model):
     _inherit = "stock.picking"
 
+    def _get_invoice_view_xmlid(self, cr, uid, ids, name, arg, context=None):
+        res = {}
+        for pick in self.browse(cr, uid, ids, context=context):
+            if pick.invoice_id:
+                if pick.invoice_id.type in ('in_invoice', 'in_refund'):
+                    res[pick.id] = 'account.invoice_supplier_form'
+                else:
+                    res[pick.id] = 'account.invoice_form'
+            else:
+                res[pick.id] = False
+        return res
+
     _columns = {
         'invoice_id': fields.many2one(
             'account.invoice', 'Invoice', readonly=True),
+        'invoice_view_xmlid': fields.function(
+            _get_invoice_view_xmlid, type='char', string="Invoice View XMLID",
+            readonly=True),
     }
 
     def _invoice_hook(self, cr, uid, picking, invoice_id):
@@ -55,16 +69,53 @@ class stock_picking(orm.Model):
 class stock_picking_out(orm.Model):
     _inherit = "stock.picking.out"
 
+    def _out_get_invoice_view_xmlid(
+            self, cr, uid, ids, name, arg, context=None):
+        return self.pool['stock.picking']._get_invoice_view_xmlid(
+            cr, uid, ids, name, arg, context=context)
+
     _columns = {
         'invoice_id': fields.many2one(
             'account.invoice', 'Invoice', readonly=True),
+        'invoice_view_xmlid': fields.function(
+            _out_get_invoice_view_xmlid, type='char',
+            string="Invoice View XMLID", readonly=True),
     }
 
 
 class stock_picking_in(orm.Model):
     _inherit = "stock.picking.in"
 
+    def _in_get_invoice_view_xmlid(
+            self, cr, uid, ids, name, arg, context=None):
+        return self.pool['stock.picking']._get_invoice_view_xmlid(
+            cr, uid, ids, name, arg, context=context)
+
     _columns = {
         'invoice_id': fields.many2one(
             'account.invoice', 'Invoice', readonly=True),
+        'invoice_view_xmlid': fields.function(
+            _in_get_invoice_view_xmlid, type='char',
+            string="Invoice View XMLID", readonly=True),
     }
+
+
+class account_invoice(orm.Model):
+    _inherit = "account.invoice"
+
+    _columns = {
+        'picking_ids': fields.one2many(
+            'stock.picking', 'invoice_id', 'Related Pickings', readonly=True,
+            help="Related pickings (only when the invoice has been generated from the picking)."),
+    }
+
+
+class account_invoice_line(orm.Model):
+    _inherit = "account.invoice.line"
+
+    _columns = {
+        'move_line_ids': fields.one2many(
+            'stock.move', 'invoice_line_id', 'Related Stock Moves',
+            readonly=True,
+            help="Related stock moves (only when the invoice has been generated from the picking)."),
+        }
