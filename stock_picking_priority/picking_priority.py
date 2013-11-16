e66f1afbba2783791d171ec8e89e18f3346a504d
unknown <alexandre.fayolle@camptocamp.com>
2013-11-16 18:44:01 +0100

    [ADD] stock_picking_priority
    this module adds a priority attribute on stock pickings and allows to recompute the availability of pickings taking this priority into account from a menu in Warehouse / Schedulers

---
+++ stock_picking_priority/picking_priority.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Alexandre Fayolle
+#    Copyright 2013 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+
+
+from openerp.osv import orm, fields
+from openerp import netsvc
+
+class StockPicking(orm.Model):
+    _inherit = "stock.picking"
+    _columns = {
+        'priority': fields.selection([('0', 'Normal'),
+                                      ('1', 'Urgent'),
+                                      ('2', 'Very Urgent')],
+                                     'Priority',
+                                     required=True,
+                                     help='The priority of the picking'),
+        }
+    _defaults = {
+        'priority': '0',
+        }
+    def retry_assign_all(self, cr, uid, ids, context=None):
+        domain = [('type', '!=', 'in'),
+                  ('state', 'in', ('confirmed', 'assigned'))]
+        if ids:
+            domain += [('ids', 'in', ids)]
+        picking_ids = self.search(cr, uid, domain, order='priority desc, min_date', context=context)
+        cancelled_ids = self.cancel_assign(cr, uid, picking_ids, context)
+        assigned_ids = self.action_assign(cr, uid, picking_ids, context)
+        return cancelled_ids, assigned_ids
+
+class StockPickingOut(orm.Model):
+    _inherit = 'stock.picking.out'
+    _columns = {
+        'priority': fields.selection([('0', 'Normal'),
+                                      ('1', 'Urgent'),
+                                      ('2', 'Very Urgent')],
+                                     'Priority',
+                                     required=True,
+                                     help='The priority of the picking'),
+        }
+    _defaults = {
+        'priority': '0',
+        }
+
+    def retry_assign_all(self, cr, uid, ids, context=None):
+        return self.pool.get('stock.picking').retry_assign_all(cr, uid, ids, context=context)
+
+class StockPickingIn(orm.Model):
+    _inherit = 'stock.picking.in'
+    _columns = {
+        'priority': fields.selection([('0', 'Normal'),
+                                      ('1', 'Urgent'),
+                                      ('2', 'Very Urgent')],
+                                     'Priority',
+                                     required=True,
+                                     help='The priority of the picking'),
+        }
+    _defaults = {
+        'priority': '0',
+        }
+
+    def retry_assign_all(self, cr, uid, ids, context=None):
+        return self.pool.get('stock.picking').retry_assign_all(cr, uid, ids, context=context)
+
+
+
+
+class StockPickingRetryAvailability(orm.TransientModel):
+    _name = "stock.picking.retry.availability"
+    _columns = {
+        }
+
+    def action_retry_assign(self, cr, uid, ids, context=None):
+        pick_obj = self.pool['stock.picking']
+
+        pick_obj.retry_assign_all(cr, uid, [], context=context)
+
+        return {'type': 'ir.actions.act_window_close'}
