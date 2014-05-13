f5569483c59170b34b210aa49055758d4cc34749
Alex Comba <alex.comba@agilebg.com>
2014-05-13 17:15:48 +0200

    [ADD] module picking_line_description

---
+++ picking_line_description/stock.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2014 Agile Business Group sagl
+#    (<http://www.agilebg.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import orm, fields
+
+
+class stock_move(orm.Model):
+    _inherit = "stock.move"
+
+    _columns = {
+        'name': fields.text('Description'),
+    }
+
+    def onchange_product_id(
+            self, cr, uid, ids, prod_id=False, loc_id=False,
+            loc_dest_id=False, partner_id=False
+    ):
+        res = super(stock_move, self).onchange_product_id(
+            cr, uid, ids,  prod_id=prod_id, loc_id=loc_id,
+            loc_dest_id=loc_dest_id, partner_id=partner_id
+        )
+        if prod_id:
+            user = self.pool.get('res.users').browse(cr, uid, uid)
+            user_groups = [g.id for g in user.groups_id]
+            ref = self.pool.get('ir.model.data').get_object_reference(
+                cr, uid, 'picking_line_description',
+                'group_use_product_description_per_picking_line'
+            )
+            if ref and len(ref) > 1 and ref[1]:
+                group_id = ref[1]
+                if group_id in user_groups:
+                    product_obj = self.pool.get('product.product')
+                    product = product_obj.browse(cr, uid, prod_id)
+                    if (
+                        product
+                        and product.description
+                        and 'value' in res
+                    ):
+                        res['value']['name'] = product.description
+        return res
