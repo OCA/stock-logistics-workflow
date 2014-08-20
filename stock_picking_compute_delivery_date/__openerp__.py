85232648d2222e0b2e2f65129c8d258e88f70bde
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-08-20 18:40:00 +0200

    WIP initial tests for compute_delivery_date

---
+++ stock_picking_compute_delivery_date/__openerp__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Leonardo Pistone
+#    Copyright 2014 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{"name": "Stock Picking Compute Delivery Date",
+ "version": "1.0",
+ "author": "Camptocamp",
+ "category": 'Warehouse Management',
+ "website": "http://camptocamp.com",
+ "description": """
+Stock Picking Compute Delivery Date
+===================================
+
+Features:
+
+*
+
+    """,
+ "complexity": "normal",
+ "depends": ["stock"],
+ "data": [],
+ "test": [
+     'test/setup_user.yml',
+     'test/test_picking_out.yml',
+ ],
+ "installable": True,
+ }
