3d4c7a99d530be27f2c81f16dd0ace8ab6e3364a
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-09-04 15:16:57 +0200

    add wizard for all products

--- stock_picking_compute_delivery_date/wizard/by_product.py
+++ stock_picking_compute_delivery_date/wizard/by_product.py
@@ -1,9 +1,9 @@
 from openerp.osv import orm
 
 
-class ComputeDateWizard(orm.TransientModel):
+class ComputeDeliveryDateByProductWizard(orm.TransientModel):
 
-    _name = 'compute.date.wizard'
+    _name = 'compute.delivery.date.by.product.wizard'
 
     def do_compute(self, cr, uid, ids, context=None):
         pick_obj = self.pool['stock.picking.out']
@@ -13,5 +13,16 @@ class ComputeDateWizard(orm.TransientModel):
         for product in product_obj.browse(cr, uid, product_ids,
                                           context=context):
             pick_obj.compute_delivery_dates(cr, uid, product, context=context)
+
+        return True
+
+
+class ComputeAllDeliveryDatesWizard(orm.TransientModel):
+
+    _name = 'compute.all.delivery.dates.wizard'
+
+    def do_compute(self, cr, uid, ids, context=None):
+        pick_obj = self.pool['stock.picking.out']
+        pick_obj.compute_all_delivery_dates(cr, uid, context=context)
 
         return True
