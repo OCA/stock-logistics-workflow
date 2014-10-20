d4b179db3b5b68b472e448ff6713c7244b29ec8a
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2014-10-20 16:26:56 +0200

    [PEP8]

--- product_customer_code_picking/stock_picking.py
+++ product_customer_code_picking/stock_picking.py
@@ -35,9 +35,10 @@ class stock_move(orm.Model):
                 move.picking_id.partner_id.commercial_partner_id.id)
             product = move.product_id
             if product and main_partner_id:
-                code_ids = product_customer_code_obj.search(cr, uid, [
-                    ('product_id', '=', product.id),
-                    ('partner_id', '=', main_partner_id),
+                code_ids = product_customer_code_obj.search(
+                    cr, uid, [
+                        ('product_id', '=', product.id),
+                        ('partner_id', '=', main_partner_id),
                     ], limit=1, context=context)
                 if code_ids:
                     data = product_customer_code_obj.read(
