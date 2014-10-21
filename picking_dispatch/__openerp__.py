6f65ab18b20c0f2d769d525d698fe6699d7b0f64
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2014-10-21 17:38:44 +0200

    move description in README.rst

--- picking_dispatch/__openerp__.py
+++ picking_dispatch/__openerp__.py
@@ -26,43 +26,6 @@
  'complexity': "normal",  # easy, normal, expert
  'depends': ['stock',
              ],
- 'description': """
-Picking dispatch
-================
-
-This module allows you to group various picking into a dispatch order
-(picking wave), having all the related moves in it and assigned to a warehouse
-keeper.
-
-Wave picking is an application of short interval scheduling, to assign orders
-into groupings (waves) and release them together so as to allow management to
-coordinate the several parallel and sequential activities required to complete
-the work.
-
-The individual orders in the wave are dependent on the criteria used to make
-the selection. To organize the sequence of orders and assignment to waves,
-consistent with routing, loading and planned departure times of shipping
-vehicles or production requirements, etc., to reduce the space required for
-shipping dock handling to assemble orders and load; and To assign staff to
-each wave and function within a wave, with the expectation that all the work
-assigned to each wave will be completed within the wave period.
-
-From there, you can perform various operation :
-
- * Check availability of related moves and picking
- * Organize your deliveries by grouping on interesting matter (e.g. by
-     transporter, by type of goods, etc..)
- * Make partial delivery
- * Print a report to pick the proper goods at once
-
-Contributors
-------------
-
-* Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
-* Guewen Baconnier <guewen.baconnier@camptocamp.com>
-* Yannick Vaucher <yannick.vaucher@camptocamp.com>
-
- """,
  'website': 'http://www.camptocamp.com/',
  'data': ['picking_dispatch_view.xml',
           'picking_dispatch_sequence.xml',
