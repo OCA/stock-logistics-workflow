c7b81226ec599af2f689818444230c24ab014349
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2014-10-22 11:59:47 +0200

    port model picking.dispatch to API 8.0

--- picking_dispatch/picking_dispatch.py
+++ picking_dispatch/picking_dispatch.py
@@ -20,14 +20,14 @@
 ##############################################################################
 import logging
 from datetime import datetime
-from openerp.osv import orm, fields
+from openerp import models, fields, api
 from openerp.osv.osv import except_osv
 from openerp.tools.translate import _
 
 _logger = logging.getLogger(__name__)
 
 
-class PickingDispatch(orm.Model):
+class PickingDispatch(models.Model):
 
     """New object that contain stock moves selected from stock.picking by the
     warehouse manager so that the warehouseman can go and bring the product
@@ -41,82 +41,86 @@ class PickingDispatch(orm.Model):
     _name = 'picking.dispatch'
     _description = "Dispatch Picking Order"
 
-    def _get_related_picking(self, cr, uid, ids, field_names, arg=None,
-                             context=None):
-        result = {}
-        if not ids:
-            return result
-        for dispatch_id in ids:
-            result[dispatch_id] = []
+    @api.multi
+    def _get_related_picking(self):
+        cr = self.env.cr
+        picking_ids = {}
+        for dispatch in self:
+            picking_ids[dispatch.id] = []
         sql = ("SELECT DISTINCT sm.dispatch_id, sm.picking_id "
                "FROM stock_move sm "
                "WHERE sm.dispatch_id in %s AND sm.picking_id is NOT NULL")
-        cr.execute(sql, (tuple(ids),))
+        cr.execute(sql, (tuple(self.ids),))
         res = cr.fetchall()
         for line in res:
-            result[line[0]].append(line[1])
-        return result
+            picking_ids[line[0]].append(line[1])
+        for dispatch in self:
+            # Must set None or will try to access it before being computed
+            dispatch.related_picking_ids = None
+            dispatch.related_picking_ids = [(6, 0, picking_ids[dispatch.id])]
 
-    _columns = {
-        'name': fields.char(
-            'Name', size=96, required=True, select=True,
-            states={'draft': [('readonly', False)]}, unique=True,
-            help='Name of the picking dispatch'),
-        'date': fields.date(
-            'Date', required=True, readonly=True, select=True,
-            states={'draft': [('readonly', False)],
-                    'assigned': [('readonly', False)]},
-            help='date on which the picking dispatched is to be processed'),
-        'picker_id': fields.many2one(
-            'res.users', 'Picker', readonly=True,
-            states={'draft': [('readonly', False)],
-                    'assigned': [('readonly', False),
-                                 ('required', True)]},
-            select=True,
-            help='the user to which the pickings are assigned'),
-        'move_ids': fields.one2many('stock.move', 'dispatch_id', 'Moves',
-                                    states={'draft': [('readonly', False)]},
-                                    readonly=True,
-                                    help='the list of moves to be processed'),
-        'notes': fields.text('Notes', help='free form remarks'),
-        'backorder_id': fields.many2one(
-            'picking.dispatch', 'Back Order of',
-            help='if this dispatch was split, this links to the dispatch '
-            'order containing the other part which was processed',
-            select=True),
-        'state': fields.selection(
-            [
-                ('draft', 'Draft'),
-                ('assigned', 'Assigned'),
-                ('progress', 'In Progress'),
-                ('done', 'Done'),
-                ('cancel', 'Cancelled'),
-            ], 'Dispatch State', readonly=True, select=True,
-            help='the state of the picking. '
-            'Workflow is draft -> assigned -> progress -> done or cancel'),
-        'related_picking_ids': fields.function(
-            _get_related_picking, method=True, type='one2many',
-            relation='stock.picking', string='Related Dispatch Picking'),
-        'company_id': fields.many2one('res.company', 'Company',
-                                      required=True),
-    }
+    @api.model
+    def _default_company(self):
+        company_obj = self.env['res.company']
+        return company_obj._company_default_get('picking.dispatch')
 
-    def _default_company(self, cr, uid, context=None):
-        company_obj = self.pool.get('res.company')
-        return company_obj._company_default_get(cr, uid,
-                                                'picking.dispatch',
-                                                context=context)
+    name = fields.Char(
+        'Name',
+        required=True, select=True,
+        copy=False, unique=True,
+        states={'draft': [('readonly', False)]},
+        default=lambda self: self.env['ir.sequence'].get('picking.dispatch'),
+        help='Name of the picking dispatch')
+    date = fields.Date(
+        'Date',
+        required=True, readonly=True, select=True,
+        states={'draft': [('readonly', False)],
+                'assigned': [('readonly', False)]},
+        default=fields.Date.context_today,
+        help='date on which the picking dispatched is to be processed')
+    picker_id = fields.Many2one(
+        'res.users', 'Picker',
+        readonly=True, select=True,
+        states={'draft': [('readonly', False)],
+                'assigned': [('readonly', False),
+                             ('required', True)]},
+        help='the user to which the pickings are assigned')
+    move_ids = fields.One2many(
+        'stock.move', 'dispatch_id', 'Moves',
+        readonly=True, copy=False,
+        states={'draft': [('readonly', False)]},
+        help='the list of moves to be processed')
+    notes = fields.Text('Notes', help='free form remarks')
+    backorder_id = fields.Many2one(
+        'picking.dispatch', 'Back Order of',
+        help='if this dispatch was split, this links to the dispatch '
+        'order containing the other part which was processed',
+        select=True)
+    state = fields.Selection(
+        [
+            ('draft', 'Draft'),
+            ('assigned', 'Assigned'),
+            ('progress', 'In Progress'),
+            ('done', 'Done'),
+            ('cancel', 'Cancelled'),
+        ], 'Dispatch State',
+        readonly=True, select=True, copy=False,
+        default='draft',
+        help='the state of the picking. '
+        'Workflow is draft -> assigned -> progress -> done or cancel')
+    related_picking_ids = fields.One2many(
+        'stock.picking',
+        readonly=True,
+        string='Related Dispatch Picking',
+        compute='_get_related_picking')
+    company_id = fields.Many2one(
+        'res.company', 'Company',
+        required=True,
+        default=_default_company)
 
-    _defaults = {
-        'name': lambda obj, cr, uid, ctxt: obj.pool.get('ir.sequence').get(
-            cr, uid, 'picking.dispatch'),
-        'date': fields.date.context_today,
-        'state': 'draft',
-        'company_id': _default_company,
-    }
-
-    def _check_picker_assigned(self, cr, uid, ids, context=None):
-        for dispatch in self.browse(cr, uid, ids, context=context):
+    @api.multi
+    def _check_picker_assigned(self):
+        for dispatch in self:
             if (dispatch.state in ('assigned', 'progress', 'done') and
                     not dispatch.picker_id):
                 return False
@@ -126,89 +130,74 @@ class PickingDispatch(orm.Model):
         (_check_picker_assigned, 'Please select a picker.', ['picker_id'])
     ]
 
-    def copy(self, cr, uid, id, default=None, context=None):
-        if default is None:
-            default = {}
-        default = default.copy()
-        if 'name' not in default:
-            default['name'] = self.pool.get(
-                'ir.sequence').get(cr, uid, 'picking.dispatch')
-        default['move_ids'] = []
-        default['related_picking_ids'] = []
-        res = super(PickingDispatch, self).copy(cr, uid, id, default, context)
-        return res
-
-    def action_assign(self, cr, uid, ids, context=None):
-        _logger.debug('set state to assigned for picking.dispatch %s', ids)
-        self.write(cr, uid, ids, {'state': 'assigned'}, context)
+    @api.multi
+    def action_assign(self):
+        _logger.debug('set state to assigned for picking.dispatch %s', self)
+        self.write({'state': 'assigned'})
         return True
 
-    def action_progress(self, cr, uid, ids, context=None):
-        self.assert_start_ok(cr, uid, ids, context)
-        _logger.debug('set state to progress for picking.dispatch %s', ids)
-        self.write(cr, uid, ids, {'state': 'progress'}, context)
+    @api.multi
+    def action_progress(self):
+        self.assert_start_ok()
+        _logger.debug('set state to progress for picking.dispatch %s', self)
+        self.write({'state': 'progress'})
         return True
 
-    def action_done(self, cr, uid, ids, context=None):
-        if not ids:
-            return True
-        move_obj = self.pool.get('stock.move')
-        move_ids = move_obj.search(
-            cr, uid, [('dispatch_id', 'in', ids)], context=context)
-        return move_obj.action_partial_move(cr, uid, move_ids, context)
+    @api.multi
+    def action_done(self):
+        domain = [('dispatch_id', 'in', self.ids)]
+        moves = self.env['stock.move'].search(domain)
+        return moves.action_partial_move()
 
-    def check_finished(self, cr, uid, ids, context):
+    @api.multi
+    def check_finished(self):
         """set the dispatch to finished if all the moves are finished"""
-        if not ids:
-            return True
-        finished = []
-        for dispatch in self.browse(cr, uid, ids, context=context):
-            if all(
-                move.state in ('cancel', 'done') for move in dispatch.move_ids
-            ):
-                finished.append(dispatch.id)
+        finished = self.browse()
+        for dispatch in self:
+            if all(move.state in ('cancel', 'done')
+                   for move in dispatch.move_ids):
+                finished |= dispatch
         _logger.debug('set state to done for picking.dispatch %s', finished)
-        self.write(cr, uid, finished, {'state': 'done'}, context)
+        finished.write({'state': 'done'})
         return finished
 
-    def action_cancel(self, cr, uid, ids, context=None):
-        move_obj = self.pool.get('stock.move')
-        move_ids = move_obj.search(
-            cr, uid, [('dispatch_id', 'in', ids)], context=context)
-        res = move_obj.action_cancel(cr, uid, move_ids, context)
+    @api.multi
+    def action_cancel(self):
+        domain = [('dispatch_id', 'in', self.ids)]
+        moves = self.env['stock.move'].search(domain)
+        res = moves.action_cancel()
         # If dispatch is empty, we still need to cancel it !
-        if not move_ids:
-            _logger.debug('set state to cancel for picking.dispatch %s', ids)
-            self.write(cr, uid, ids, {'state': 'cancel'}, context)
+        if not moves:
+            _logger.debug('set state to cancel for picking.dispatch %s', self)
+            self.write({'state': 'cancel'})
         return res
 
-    def assert_start_ok(self, cr, uid, ids, context=None):
+    @api.multi
+    def assert_start_ok(self):
         now = datetime.now().date()
-        for obj in self.browse(cr, uid, ids, context):
+        for obj in self:
             date = datetime.strptime(obj.date, '%Y-%m-%d').date()
             if now < date:
                 raise except_osv(
                     _('Error'),
                     _('This dispatch cannot be processed until %s') % obj.date)
 
-    def action_assign_moves(self, cr, uid, ids, context=None):
-        move_obj = self.pool['stock.move']
-        move_ids = move_obj.search(cr, uid,
-                                   [('dispatch_id', 'in', ids)],
-                                   context=context)
-        move_obj.action_assign(cr, uid, move_ids)
+    @api.multi
+    def action_assign_moves(self):
+        for rec in self:
+            rec.move_ids.action_assign()
         return True
 
-    def check_assign_all(self, cr, uid, ids=None, domain=None, context=None):
+    @api.multi
+    def check_assign_all(self, domain=None):
         """ Try to assign moves of a selection of dispatches
 
         Called from a cron
         """
-        if isinstance(ids, (int, long)):
-            ids = [ids]
-        if ids is None:
+        pickings = self
+        if not pickings:
             if domain is None:
                 domain = [('state', 'in', ('draft', 'assigned'))]
-            ids = self.search(cr, uid, domain, context=context)
-        self.action_assign_moves(cr, uid, ids, context=context)
+            pickings = self.search(domain)
+        pickings.action_assign_moves()
         return True
