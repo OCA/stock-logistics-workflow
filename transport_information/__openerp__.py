5ff343bdaac4b25fd53b624fdc0a5cc049a40fc9
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-10-23 18:34:12 +0200

    add transport mode and vehicle, models and views

---
+++ transport_information/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright 2014 Camptocamp SA
+#    Author: Leonardo Pistone
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{"name": "Transport Information",
+ "summary": "Transport Information",
+ "version": "0.1",
+ "author": "Camptocamp",
+ "category": "Purchase Management",
+ "license": "AGPL-3",
+ 'complexity': "easy",
+ "description": """
+Transport Information
+=====================
+""",
+ "depends": ["purchase",
+             ],
+ "data": ["view/transport_mode.xml",
+          "view/transport_vehicle.xml",
+          "security/ir.model.access.csv",
+          ],
+ "installable": True,
+ }
---
+++ transport_information/model/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+import transport_mode
+import transport_vehicle
