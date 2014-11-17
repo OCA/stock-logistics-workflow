0395d294b04a788c4b0658978e75d2112341cee3
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2014-11-17 10:41:10 +0100

    pylint fix: relative imports

--- picking_dispatch/report/__init__.py
+++ picking_dispatch/report/__init__.py
@@ -26,4 +26,4 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 ##############################################################################
-import dispatch_report
+from . import dispatch_report
