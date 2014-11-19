444848bd68006b091714969d990064bdd8fb98fc
Alexis de Lattre <alexis@via.ecp.fr>
2014-11-19 22:44:14 +0100

    Move AUTHORS.txt in the module description

--- stock_picking_invoice_link/__openerp__.py
+++ stock_picking_invoice_link/__openerp__.py
@@ -28,6 +28,11 @@ This module adds a link between pickings and generated invoices.
 So that user can easly reach the invoice related to the picking
 and see the pickings related to the invoice. This only applies
 on the invoices generated from pickings.
+
+Authors
+=======
+* Lorenzo Battistini <lorenzo.battistini@agilebg.com>
+* Alexis de Lattre <alexis.delattre@akretion.com>
 """,
     'author': 'Agile Business Group',
     'website': 'http://www.agilebg.com',
