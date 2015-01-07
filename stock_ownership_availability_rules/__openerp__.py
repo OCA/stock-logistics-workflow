5568b8f5ff418524cf2732b297e5150a1a606461
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2015-01-07 10:54:30 +0100

    add module summary

--- stock_ownership_availability_rules/__openerp__.py
+++ stock_ownership_availability_rules/__openerp__.py
@@ -15,7 +15,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 {'name': 'Stock Ownership Availability Rules',
- 'summary': 'XXX',
+ 'summary': 'Enforce ownership on stock availability',
  'version': '0.1',
  'author': 'Camptocamp',
  'category': 'Purchase Management',
