38eb65ca9980325b41e756b01298d60dd859d81b
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2015-01-12 12:07:22 +0100

    Add missing license

--- stock_shipment_management/__openerp__.py
+++ stock_shipment_management/__openerp__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-##############################################################################
 #
-#    Author:  Joël Grand-Guillaume
-#    Copyright 2013 Camptocamp SA
+#
+#    Authors: Joël Grand-Guillaume, Yannick Vaucher
+#    Copyright 2013-2015 Camptocamp SA
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,8 +17,7 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-##############################################################################
-
+#
 {"name": "Shipment Management",
  "version": "0.1",
  "author": "Camptocamp",
