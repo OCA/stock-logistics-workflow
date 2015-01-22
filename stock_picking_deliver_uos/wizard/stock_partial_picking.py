50feaefd29d30a0dc18119c02ae21aa9dd18453f
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2015-01-22 11:29:01 +0100

    [FIX] total delivery case

--- stock_picking_deliver_uos/wizard/stock_partial_picking.py
+++ stock_picking_deliver_uos/wizard/stock_partial_picking.py
@@ -62,27 +62,31 @@ class StockPartialPicking(orm.TransientModel):
     def do_partial(self, cr, uid, ids, context=None):
         res = super(StockPartialPicking, self).do_partial(
             cr, uid, ids, context=context)
-        wizard = self.browse(cr, uid, ids[0], context=context)
-        picking_pool = self.pool['stock.picking']
-        delivered_picking_id = res['res_id']
-        open_picking_id = res['context']['active_id']
-        delivered_picking = picking_pool.browse(
-            cr, uid, delivered_picking_id, context=context)
-        for delivered_line in delivered_picking.move_lines:
-            for wizard_line in wizard.move_ids:
-                if (
-                    wizard_line.product_id.id == delivered_line.product_id.id
-                    and
-                    wizard_line.quantity == delivered_line.product_qty
-                    and
-                    wizard_line.product_uom.id == delivered_line.product_uom.id
-                ):
-                    delivered_line.write(
-                        {'product_uos_qty': wizard_line.product_uos_qty},
-                        context=context)
-                    wizard_line.move_id.write(
-                        {'product_uos_qty': wizard_line.product_uos_qty * (
-                            wizard_line.move_id.product_qty
-                            / delivered_line.product_qty)}, context=context)
-                    break
+        if (
+            res.get('res_id') and res.get('context')
+            and res['context'].get('active_id')
+        ):
+            wizard = self.browse(cr, uid, ids[0], context=context)
+            picking_pool = self.pool['stock.picking']
+            delivered_picking_id = res['res_id']
+            open_picking_id = res['context']['active_id']
+            delivered_picking = picking_pool.browse(
+                cr, uid, delivered_picking_id, context=context)
+            for delivered_line in delivered_picking.move_lines:
+                for wizard_line in wizard.move_ids:
+                    if (
+                        wizard_line.product_id.id == delivered_line.product_id.id
+                        and
+                        wizard_line.quantity == delivered_line.product_qty
+                        and
+                        wizard_line.product_uom.id == delivered_line.product_uom.id
+                    ):
+                        delivered_line.write(
+                            {'product_uos_qty': wizard_line.product_uos_qty},
+                            context=context)
+                        wizard_line.move_id.write(
+                            {'product_uos_qty': wizard_line.product_uos_qty * (
+                                wizard_line.move_id.product_qty
+                                / delivered_line.product_qty)}, context=context)
+                        break
         return res
