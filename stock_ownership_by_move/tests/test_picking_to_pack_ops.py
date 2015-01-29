4ab7cb44840b14619d949226ae2653d0f108541c
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2015-01-29 11:46:54 +0100

    refactor test with factories

--- stock_ownership_by_move/tests/test_picking_to_pack_ops.py
+++ stock_ownership_by_move/tests/test_picking_to_pack_ops.py
@@ -19,23 +19,35 @@ from openerp.tests.common import TransactionCase
 class TestPickingToPackOps(TransactionCase):
 
     def test_one_move_takes_owner_from_move(self):
-        result = self.picking._prepare_pack_ops(self.picking, [],
-                                                {self.product: 10})
+        pick = self._picking_factory(owner=self.partner1)
+        pick.move_lines = self._move_factory(product=self.product1,
+                                             owner=self.partner2)
+
+        result = pick._prepare_pack_ops(pick, [], {self.product1: 5.0})
+
         self.assertEqual(1, len(result))
         self.assertEqual(self.partner2.id, result[0]['owner_id'])
 
+    def _picking_factory(self, owner):
+        return self.env['stock.picking'].create({
+            'picking_type_id': self.env.ref('stock.picking_type_in').id,
+            'owner_id': owner.id,
+        })
+
+    def _move_factory(self, product, owner, qty=5.0):
+        return self.env['stock.move'].create({
+            'name': '/',
+            'restrict_partner_id': owner.id,
+            'product_id': product.id,
+            'product_uom_qty': qty,
+            'product_uom': product.uom_id.id,
+            'location_id': self.env.ref('stock.stock_location_suppliers').id,
+            'location_dest_id': self.env.ref('stock.stock_location_stock').id,
+        })
+
     def setUp(self):
         super(TestPickingToPackOps, self).setUp()
-        self.product = self.env.ref('product.product_product_36')
+        self.product1 = self.env.ref('product.product_product_33')
+        self.product2 = self.env.ref('product.product_product_36')
         self.partner1 = self.env.ref('base.res_partner_1')
         self.partner2 = self.env.ref('base.res_partner_2')
-
-        self.picking = self.env['stock.picking'].new({
-            'picking_type_id': self.env.ref('stock.picking_type_in').id,
-            'owner_id': self.partner1.id,  # we do not want to use this one
-            'move_lines': [(0, 0, {
-                'product_id': self.product.id,
-                'product_qty': 10,
-                'restrict_partner_id': self.partner2.id,  # we want this!
-            })],
-        })
