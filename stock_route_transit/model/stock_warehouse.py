6c42ce6f930e7bed7c8d848c9b013a78f4fdbdd3
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2015-02-10 09:01:14 +0100

    flake8 fixes

--- stock_route_transit/model/stock_warehouse.py
+++ stock_route_transit/model/stock_warehouse.py
@@ -374,10 +374,12 @@ class StockWarehouse(orm.Model):
 
         wh = warehouse
         picking_type_writes = [
-            (wh.in_type_id.id, {'default_location_dest_id': input_loc.id,
-                                'default_location_src_id': input_from_loc.id}),
-            (wh.out_type_id.id, {'default_location_src_id': output_loc.id,
-                                 'default_location_dest_id': output_to_loc.id}),
+            (wh.in_type_id.id,
+             {'default_location_dest_id': input_loc.id,
+              'default_location_src_id': input_from_loc.id}),
+            (wh.out_type_id.id,
+             {'default_location_src_id': output_loc.id,
+              'default_location_dest_id': output_to_loc.id}),
             (wh.pick_type_id.id,
              {'active': new_delivery_step
               not in ('ship_only', 'ship_transit')}),
@@ -454,8 +456,11 @@ class StockWarehouse(orm.Model):
             input_loc = warehouse.wh_input_stock_loc_id
             if warehouse.reception_steps.endswith('one_step'):
                 input_loc = warehouse.lot_stock_id
-            warehouse.in_type_id.write({'default_location_dest_id': input_loc.id,
-                                        'default_location_src_id': warehouse.wh_transit_in_loc_id.id})
+            warehouse.in_type_id.write(
+                {'default_location_dest_id': input_loc.id,
+                 'default_location_src_id': warehouse.wh_transit_in_loc_id.id,
+                 }
+                )
         pick_type_val = {'name': _('Customer reception'),
                          'warehouse_id': warehouse.id,
                          'code': 'outgoing',
@@ -472,8 +477,12 @@ class StockWarehouse(orm.Model):
             output_loc = warehouse.wh_output_stock_loc_id
             if warehouse.delivery_steps == 'ship_transit':
                 output_loc = warehouse.lot_stock_id
-            warehouse.out_type_id.write({'default_location_src_id': output_loc.id,
-                                         'default_location_dest_id':warehouse.wh_transit_out_loc_id.id})
+            transit_out_loc = warehouse.wh_transit_out_loc_id
+            warehouse.out_type_id.write(
+                {'default_location_src_id': output_loc.id,
+                 'default_location_dest_id': transit_out_loc.id,
+                 }
+                )
         self.write(cr, uid, warehouse.id,
                    {'transit_in_type_id': in_transit_type,
                     'transit_out_type_id': out_transit_type,
