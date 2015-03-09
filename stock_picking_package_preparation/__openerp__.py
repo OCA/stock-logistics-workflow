6a5f93a726059f07a8695ae060aaad144e1c55f2
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2015-03-09 14:56:41 +0100

    Add Odoo Community Association (OCA) as author

--- stock_picking_package_preparation/__openerp__.py
+++ stock_picking_package_preparation/__openerp__.py
@@ -21,7 +21,7 @@
 
 {'name': 'Stock Picking Package Preparation',
  'version': '1.0',
- 'author': 'Camptocamp',
+ 'author': 'Camptocamp,Odoo Community Association (OCA)',
  'maintainer': 'Camptocamp',
  'license': 'AGPL-3',
  'category': 'Warehouse Management',
