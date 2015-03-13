072b92d4a9178ac4cdc5e3f6089b72d9c0c8c736
alfredo <alfredo@alfredo-Aspire-5738.(none)>
2015-03-13 13:37:18 +0100

    [IMP] stock_picking_package_info: Remove compute in "packages" field of stock.picking [IMP] stock_picking_wave_package_info: Remove compute in "packages" field of stock.picking

--- wizard/stock_transfer_details.py
+++ wizard/stock_transfer_details.py
@@ -1,30 +1,19 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-TODAY OpenERP S.A. <http://www.odoo.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+# For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
-
 from openerp import models, api
 from datetime import datetime
 
 
 class StockTransferDetails(models.TransientModel):
     _inherit = 'stock.transfer_details'
+
+    @api.one
+    def do_detailed_transfer(self):
+        result = super(StockTransferDetails, self).do_detailed_transfer()
+        self.picking_id._catch_operations()
+        return result
 
     @api.one
     def do_record(self):
@@ -52,4 +41,5 @@ class StockTransferDetails(models.TransientModel):
                         operation_obj.create(pack_datas)
                     else:
                         prod.packop_id.write(pack_datas)
+        self.picking_id._catch_operations()
         return True
