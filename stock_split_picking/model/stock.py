1c5fcd1e892b136b723ef84a5ae67243a0f5fb71
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2015-03-24 17:41:00 +0100

    Port to 8.0

--- stock_split_picking/model/stock.py
+++ stock_split_picking/model/stock.py
@@ -20,183 +20,28 @@
 ##############################################################################
 """Adds a split button on stock picking out to enable partial picking without
    passing backorder state to done"""
-from openerp.osv import orm
-from openerp import netsvc
+from openerp import models, api, _
 
 
-class stock_picking(orm.Model):
-    """Adds picking split without done state.
-    Long piece of code has to be copied from OpenERP
-    as there is no hook..."""
+class stock_picking(models.Model):
+    """Adds picking split without done state."""
 
     _inherit = "stock.picking"
 
-    def split_process(self, cr, uid, ids, context=None):
+    @api.multi
+    def split_process(self):
         """Use to trigger the wizard from button with
            correct context"""
-        if context is None:
-            context = {}
-        #  Open the partial picking wizard
-        ctx = context.copy()
-        ctx.update({
+        ctx = {
             'active_model': self._name,
-            'active_ids': ids,
-            'active_id': ids[0] if ids else False,
-            'partial_no_confirm': True,
-        })
-        return {
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': 'stock.partial.picking',
-            'type': 'ir.actions.act_window',
-            'target': 'new',
-            'context': ctx,
-            'nodestroy': True,
+            'active_ids': self.ids,
+            'active_id': len(self.ids) and self.ids[0] or False,
+            'do_only_split': True,
         }
 
-    def do_partial(self, cr, uid, ids, partial_datas, context=None):
-        """ Makes partial picking and moves done.
-
-        Inherited to allow the use of do_partial_no_confirm()
-        instead of do_partial(), switch is done with
-        a 'partial_no_confirm' key in the context.
-
-        @param partial_datas : Dictionary containing details of partial picking
-                          like partner_id, address_id, delivery_date,
-                          delivery moves with product_id, product_qty, uom
-        @return: Dictionary of values
-        """
-        if context is None:
-            context = {}
-        if context.get('partial_no_confirm'):
-            return self.do_partial_no_confirm(
-                cr, uid, ids, partial_datas, context=context)
-        else:
-            return super(stock_picking, self).do_partial(
-                cr, uid, ids, partial_datas, context=context)
-
-    def do_partial_no_confirm(self, cr, uid, ids, partial_datas,
-                              context=None):
-        """ Makes partial picking and moves but does not put them in done state.
-        Code is directly taken form OpenERP source code and not refactored.
-        @param partial_datas : Dictionary containing details of partial picking
-                          like partner_id, partner_id, delivery_date,
-                          delivery moves with product_id, product_qty, uom
-        @return: Dictionary of values
-        """
-        if context is None:
-            context = {}
-        else:
-            context = dict(context)
-        res = {}
-        move_obj = self.pool.get('stock.move')
-        uom_obj = self.pool.get('product.uom')
-        sequence_obj = self.pool.get('ir.sequence')
-        wf_service = netsvc.LocalService("workflow")
-        for pick in self.browse(cr, uid, ids, context=context):
-            new_picking = None
-            complete, too_many, too_few = [], [], []
-
-            move_product_qty, prodlot_ids = {}, {}
-            product_uoms, partial_qty = {}, {}
-
-            for move in pick.move_lines:
-                if move.state in ('done', 'cancel'):
-                    continue
-                partial_data = partial_datas.get('move%s' % (move.id), {})
-                product_qty = partial_data.get('product_qty', 0.0)
-                move_product_qty[move.id] = product_qty
-                product_uom = partial_data.get('product_uom', False)
-                prodlot_id = partial_data.get('prodlot_id')
-                prodlot_ids[move.id] = prodlot_id
-                product_uoms[move.id] = product_uom
-                partial_qty[move.id] = uom_obj._compute_qty(
-                    cr, uid, product_uoms[move.id],
-                    product_qty, move.product_uom.id)
-                if move.product_qty == partial_qty[move.id]:
-                    complete.append(move)
-                elif move.product_qty > partial_qty[move.id]:
-                    too_few.append(move)
-                else:
-                    too_many.append(move)
-            # We remove section of code that manage incoming picking
-            # please refere to original code if needed
-            for move in too_few:
-                product_qty = move_product_qty[move.id]
-                if not new_picking:
-                    new_picking_name = pick.name
-                    self.write(cr, uid, [pick.id], {
-                        'name': sequence_obj.get(
-                            cr, uid, 'stock.picking.%s' % pick.type),
-                    })
-                    new_picking = self.copy(cr, uid, pick.id,
-                                            {
-                                                'name': new_picking_name,
-                                                'move_lines': [],
-                                                'state': 'draft',
-                                            })
-                if product_qty != 0:
-                    defaults = {
-                        'product_qty': product_qty,
-                        # TODO: put correct uos_qty
-                        'product_uos_qty': product_qty,
-                        'picking_id': new_picking,
-                        'state': 'assigned',
-                        'move_dest_id': False,
-                        'price_unit': move.price_unit,
-                        'product_uom': product_uoms[move.id]
-                    }
-                    prodlot_id = prodlot_ids[move.id]
-                    if prodlot_id:
-                        defaults.update(prodlot_id=prodlot_id)
-                    move_obj.copy(cr, uid, move.id, defaults)
-                move_obj.write(cr, uid, [move.id],
-                               {
-                    'product_qty': move.product_qty - partial_qty[move.id],
-                    # TODO: put correct uos_qty
-                    'product_uos_qty': move.product_qty - partial_qty[move.id],
-                    'prodlot_id': False,
-                    'tracking_id': False,
-                })
-
-            if new_picking:
-                move_obj.write(
-                    cr, uid, [c.id for c in complete], {
-                        'picking_id': new_picking
-                    })
-            for move in complete:
-                defaults = {'product_uom': product_uoms[move.id],
-                            'product_qty': move_product_qty[move.id]}
-                if prodlot_ids.get(move.id):
-                    defaults.update({'prodlot_id': prodlot_ids[move.id]})
-                move_obj.write(cr, uid, [move.id], defaults)
-            for move in too_many:
-                product_qty = move_product_qty[move.id]
-                defaults = {
-                    'product_qty': product_qty,
-                    # TODO: put correct uos_qty
-                    'product_uos_qty': product_qty,
-                    'product_uom': product_uoms[move.id]
-                }
-                prodlot_id = prodlot_ids.get(move.id)
-                if prodlot_ids.get(move.id):
-                    defaults.update(prodlot_id=prodlot_id)
-                if new_picking:
-                    defaults.update(picking_id=new_picking)
-                move_obj.write(cr, uid, [move.id], defaults)
-
-            # At first we confirm the new picking (if necessary)
-            # We have removed here confirmation workflow calls
-            if new_picking:
-                wf_service.trg_validate(
-                    uid, 'stock.picking', new_picking, 'button_confirm', cr)
-                self.write(cr, uid, [pick.id], {'backorder_id': new_picking})
-                delivered_pack_id = new_picking
-            else:
-                delivered_pack_id = pick.id
-
-            delivered_pack = self.browse(
-                cr, uid, delivered_pack_id, context=context)
-            res[pick.id] = {'delivered_picking': delivered_pack.id or False}
-
-        return res
+        wiz = self.env['stock.transfer_details'].with_context(**ctx).create(
+            {'picking_id': len(self.ids) and self.ids[0] or False,
+             })
+        view_dict = wiz.wizard_view()
+        view_dict['name'] =  _('Enter quantities to split')
+        return view_dict
