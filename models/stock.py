e41aeb547b6ef81f7499e8817283b487a92ab77b
alfredo <alfredo@alfredo-Aspire-5738.(none)>
2015-04-23 15:00:32 +0200

    [FIX] stock_picking_package_info, stock_picking_wave_package_info: Fix error loading packages. [IMP] stock_picking_wave_management: Select pickigns that are not assigned to a picking wave.

--- models/stock.py
+++ models/stock.py
@@ -36,12 +36,13 @@ class StockPicking(models.Model):
     @api.one
     def _delete_packages_information(self):
         self.pack_operation_ids.unlink()
-        self.packages.unlink()
+        self.packages= [(6, 0, [])]
         self.packages_info.unlink()
         self.package_totals.unlink()
         return True
 
     def _catch_operations(self):
+        self.packages= [(6, 0, [])]
         self.packages = [
             operation.result_package_id.id for operation in
             self.pack_operation_ids if operation.result_package_id]
