d3c37da996165b668d7e7f798a7f2c0eaef2ca05
eLBati <lorenzo.battistini@agilebg.com>
2015-09-10 12:55:29 +0200

    FIX when making a partial transfer with picking wizard, update details quantity of pack preparation accordingly

--- stock_picking_package_preparation_line/model/stock.py
+++ stock_picking_package_preparation_line/model/stock.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Author: Francesco Apruzzese
-#    Copyright 2015 Apulia Software srl
 #    Copyright 2015 Lorenzo Battistini - Agile Business Group
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -20,22 +18,22 @@
 #
 ##############################################################################
 
-{
-    'name': 'Stock Picking Package Preparation Line',
-    'version': '1.0',
-    'author': 'Apulia Software srl,Odoo Community Association (OCA)',
-    'maintainer': 'Apulia Software srl',
-    'license': 'AGPL-3',
-    'category': 'Warehouse Management',
-    'depends': [
-        'stock_picking_package_preparation',
-    ],
-    'website': 'http://www.apuliasoftware.it',
-    'data': [
-        'view/stock_picking_package_preparation_line.xml',
-        'security/ir.model.access.csv',
-    ],
-    'test': [],
-    'installable': True,
-    'auto_install': False,
-}
+from openerp import models, api
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    @api.multi
+    def write(self, values):
+        res = super(StockMove, self).write(values)
+        if 'product_uom_qty' in values:
+            for move in self:
+                prep_line_model = self.env[
+                    'stock.picking.package.preparation.line'
+                    ]
+                prep_lines = prep_line_model.search([
+                    ('move_id', '=', move.id),
+                    ])
+                prep_lines.product_uom_qty = values['product_uom_qty']
+        return res
