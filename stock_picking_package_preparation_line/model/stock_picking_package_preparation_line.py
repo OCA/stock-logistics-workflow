d63753d54ba14e7e639ced48b9c43cbf81285e96
eLBati <lorenzo.battistini@agilebg.com>
2015-09-10 12:12:54 +0200

    FIX changing quantity for a 'ready' picking, the system will not generate a back order, but will just deliver the new quantity

--- stock_picking_package_preparation_line/model/stock_picking_package_preparation_line.py
+++ stock_picking_package_preparation_line/model/stock_picking_package_preparation_line.py
@@ -36,16 +36,23 @@ class StockPickingPackagePreparationLine(models.Model):
                               ondelete='cascade')
     product_id = fields.Many2one('product.product', string='Product')
     product_uom_qty = fields.Float(
-        digits_compute=dp.get_precision('Product Unit of Measure'))
+        digits_compute=dp.get_precision('Product Unit of Measure'),
+        help="If you change this quantity for a 'ready' picking, the system "
+             "will not generate a back order, but will just deliver the new "
+             "quantity")
     product_uom = fields.Many2one('product.uom')
     sequence = fields.Integer()
     note = fields.Text()
 
     @api.multi
     def write(self, values):
-        res = super(StockPickingPackagePreparationLine, self).write(values)
+        super(StockPickingPackagePreparationLine, self).write(values)
         if 'product_uom_qty' in values and self.move_id:
-            self.move_id.product_uom_qty = values['product_uom_qty']
+            if self.move_id.product_uom_qty != values['product_uom_qty']:
+                self.move_id.product_uom_qty = values['product_uom_qty']
+                # perform a new reservation with the new quantity
+                self.move_id.picking_id.do_unreserve()
+                self.move_id.picking_id.action_assign()
 
     @api.onchange('product_id')
     def _onchange_product_id(self):
@@ -173,14 +180,14 @@ class StockPickingPackagePreparation(models.Model):
                 # ----- Show an error if a picking is not confirmed
                 if picking.state != 'confirmed':
                     raise exceptions.Warning(
-                        _('Impossibile to create confirmed picking. \
-Please Check products availability!'))
+                        _('Impossible to create confirmed picking. '
+                          'Please Check products availability!'))
                 picking.action_assign()
                 # ----- Show an error if a picking is not assigned
                 if picking.state != 'assigned':
                     raise exceptions.Warning(
-                        _('Impossibile to create confirmed picking. \
-Please Check products availability!'))
+                        _('Impossible to create confirmed picking. '
+                          'Please Check products availability!'))
                 # ----- Add the relation between the new picking
                 #       and PackagePreparation
                 package.picking_ids = [(4, picking.id)]
