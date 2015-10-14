e736526e499c98f20b548d551a64b4a7b7ae3858
oihane <oihanecruce@gmail.com>
2015-10-14 18:43:29 +0200

    [IMP] stock_picking_package_info: Update to api.multi

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -40,51 +40,56 @@ class StockPickingPackageTotal(models.Model):
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    @api.one
+    @api.multi
     @api.depends('pack_operation_ids', 'pack_operation_ids.result_package_id')
     def _calc_picking_packages(self):
-        self.packages = self.pack_operation_ids.mapped('result_package_id')
+        for record in self:
+            record.packages = record.pack_operation_ids.mapped(
+                'result_package_id')
 
-    @api.one
+    @api.multi
     @api.depends('packages', 'packages.ul_id')
     def _calc_picking_packages_info(self):
         pack_weight = self.env['stock.picking.package.weight.lot']
         pack_weight_obj = self.env['stock.picking.package.weight.lot']
         pack_total = self.env['stock.picking.package.total']
         pack_total_obj = self.env['stock.picking.package.total']
-        sequence = 0
-        for package in self.packages:
-            sequence += 1
-            package_operations = self.pack_operation_ids.filtered(
-                lambda r: r.result_package_id == package)
-            total_weight = 0.0
-            for pack_operation in package_operations:
-                total_weight += (pack_operation.product_qty *
-                                 pack_operation.product_id.weight)
-            vals = {
-                'picking': self.id,
-                'sequence': sequence,
-                'package': package.id,
-                'lots': [(6, 0, (package.quant_ids.mapped('lot_id').ids or
-                                 package_operations.mapped('lot_id').ids))],
-                'net_weight': total_weight,
-                'gross_weight': total_weight + package.empty_weight,
-            }
-            pack_weight += pack_weight_obj.create(vals)
-        if self.packages:
-            for product_ul in self.env['product.ul'].search([]):
-                cont = len(self.packages.filtered(
-                    lambda x: x.ul_id.id == product_ul.id))
-                if cont:
-                    vals = {
-                        'picking': self.id,
-                        'ul': product_ul.id,
-                        'quantity': cont,
-                    }
-                    pack_total += pack_total_obj.create(vals)
-        self.packages_info = pack_weight
-        self.package_totals = pack_total
-        self.num_packages = sum(x.quantity for x in self.package_totals)
+        for record in self:
+            sequence = 0
+            for package in record.packages:
+                sequence += 1
+                package_operations = record.pack_operation_ids.filtered(
+                    lambda r: r.result_package_id == package)
+                total_weight = 0.0
+                for pack_operation in package_operations:
+                    total_weight += (pack_operation.product_qty *
+                                     pack_operation.product_id.weight)
+                vals = {
+                    'picking': record.id,
+                    'sequence': sequence,
+                    'package': package.id,
+                    'lots': [(6, 0,
+                              (package.quant_ids.mapped('lot_id').ids or
+                               package_operations.mapped('lot_id').ids))],
+                    'net_weight': total_weight,
+                    'gross_weight': total_weight + package.empty_weight,
+                }
+                pack_weight += pack_weight_obj.create(vals)
+            if record.packages:
+                for product_ul in self.env['product.ul'].search([]):
+                    cont = len(record.packages.filtered(
+                        lambda x: x.ul_id.id == product_ul.id))
+                    if cont:
+                        vals = {
+                            'picking': record.id,
+                            'ul': product_ul.id,
+                            'quantity': cont,
+                        }
+                        pack_total += pack_total_obj.create(vals)
+            record.packages_info = pack_weight
+            record.package_totals = pack_total
+            record.num_packages = sum(x.quantity for x in
+                                      record.package_totals)
 
     packages = fields.Many2many(
         comodel_name='stock.quant.package', string='Packages',
--- models/stock_quant.py
+++ models/stock_quant.py
@@ -9,13 +9,14 @@ from openerp import models, fields, api
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
-    @api.one
+    @api.multi
     @api.depends('product_id', 'qty', 'product_id.weight',
                  'product_id.weight_net', 'product_id.volume')
-    def _calculate_totals(self):
-        self.total_weight = self.product_id.weight * self.qty
-        self.total_weight_net = self.product_id.weight_net * self.qty
-        self.total_volume = self.product_id.volume * self.qty
+    def _compute_totals(self):
+        for record in self:
+            record.total_weight = record.product_id.weight * record.qty
+            record.total_weight_net = record.product_id.weight_net * record.qty
+            record.total_volume = record.product_id.volume * record.qty
 
     weight = fields.Float(string='Weight', related='product_id.weight')
     weight_net = fields.Float(
@@ -23,42 +24,48 @@ class StockQuant(models.Model):
     volume = fields.Float(
         string='Volume', related='product_id.volume')
     total_weight = fields.Float(
-        string='Total weight', compute='_calculate_totals')
+        string='Total weight', compute='_compute_totals')
     total_weight_net = fields.Float(
-        string='Total Net weight', compute='_calculate_totals')
+        string='Total Net weight', compute='_compute_totals')
     total_volume = fields.Float(
-        string='Total Volume', compute='_calculate_totals')
+        string='Total Volume', compute='_compute_totals')
 
 
 class StockQuantPackage(models.Model):
     _inherit = 'stock.quant.package'
 
-    @api.one
+    @api.multi
     @api.depends('height', 'width', 'length', 'quant_ids',
                  'quant_ids.total_weight', 'quant_ids.total_weight_net',
                  'quant_ids.total_volume', 'children_ids',
                  'children_ids.total_weight', 'children_ids.empty_weight',
                  'children_ids.total_weight_net', 'children_ids.total_volume')
-    def _calculate_totals(self):
-        self.total_weight = sum(x.total_weight for x in self.quant_ids)
-        self.total_weight_net = sum(x.total_weight_net for x in self.quant_ids)
-        self.total_volume = sum(x.total_volume for x in self.quant_ids)
-        self.total_estim_weight = (
-            self.total_weight + self.empty_weight +
-            sum(x.total_weight + x.empty_weight for x in self.children_ids))
-        self.real_weight = self.total_estim_weight
-        self.total_estim_weight_net = (
-            self.total_weight_net + self.empty_weight +
-            sum(x.total_weight_net + x.empty_weight for x in
-                self.children_ids))
-        self.tvolume_charge = (
-            self.total_volume + sum(x.total_volume for x in self.children_ids))
-        self.permitted_volume = self.height * self.width * self.length
+    def _compute_totals(self):
+        for record in self:
+            record.total_weight = sum(x.total_weight for x in record.quant_ids)
+            record.total_weight_net = sum(x.total_weight_net for x in
+                                          record.quant_ids)
+            record.total_volume = sum(x.total_volume for x in record.quant_ids)
+            record.total_estim_weight = (
+                record.total_weight + record.empty_weight +
+                sum(x.total_weight + x.empty_weight for x in
+                    record.children_ids))
+            record.real_weight = record.total_estim_weight
+            record.total_estim_weight_net = (
+                record.total_weight_net + record.empty_weight +
+                sum(x.total_weight_net + x.empty_weight for x in
+                    record.children_ids))
+            record.tvolume_charge = (
+                record.total_volume + sum(x.total_volume for x in
+                                          record.children_ids))
+            record.permitted_volume = (
+                record.height * record.width * record.length)
 
-    @api.one
-    def _get_pickings(self):
-        self.pickings = self.env['stock.pack.operation'].search(
-            [('result_package_id', '=', self.id)]).mapped('picking_id')
+    @api.multi
+    def _compute_pickings(self):
+        for record in self:
+            record.pickings = self.env['stock.pack.operation'].search(
+                [('result_package_id', '=', record.id)]).mapped('picking_id')
 
     height = fields.Float(string='Height', help='The height of the package')
     width = fields.Float(string='Width', help='The width of the package')
@@ -66,30 +73,31 @@ class StockQuantPackage(models.Model):
     empty_weight = fields.Float(string='Empty Package Weight')
     real_weight = fields.Float(string='Real Weight')
     total_weight = fields.Float(
-        string='Total Weight', compute='_calculate_totals')
+        string='Total Weight', compute='_compute_totals')
     total_weight_net = fields.Float(
-        string='Total Net Weight', compute='_calculate_totals')
+        string='Total Net Weight', compute='_compute_totals')
     total_volume = fields.Float(
-        string='Total Volume', compute='_calculate_totals')
+        string='Total Volume', compute='_compute_totals')
     total_estim_weight = fields.Float(
-        string='Total Estimated Weight', compute='_calculate_totals')
+        string='Total Estimated Weight', compute='_compute_totals')
     total_estim_weight_net = fields.Float(
-        string='Total Estimated Net Weight', compute='_calculate_totals')
+        string='Total Estimated Net Weight', compute='_compute_totals')
     permitted_volume = fields.Float(
-        string='Permitted volume', compute='_calculate_totals')
+        string='Permitted volume', compute='_compute_totals')
     tvolume_charge = fields.Float(
-        string='Total volume charge', compute='_calculate_totals')
+        string='Total volume charge', compute='_compute_totals')
     pickings = fields.Many2many(
         comodel_name='stock.picking', string='Pickings',
-        compute='_get_pickings')
+        compute='_compute_pickings')
 
-    @api.one
+    @api.multi
     @api.onchange('ul_id')
     def onchange_ul_id(self):
-        self.height = self.ul_id.height
-        self.width = self.ul_id.width
-        self.length = self.ul_id.length
-        self.empty_weight = self.ul_id.weight
+        for record in self:
+            record.height = record.ul_id.height
+            record.width = record.ul_id.width
+            record.length = record.ul_id.length
+            record.empty_weight = record.ul_id.weight
 
     @api.model
     def create(self, values):
