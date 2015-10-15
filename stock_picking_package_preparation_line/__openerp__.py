7ac7307efddfd755d164f1564b59042d9bef44ac
Francesco Apruzzese <cescoap@gmail.com>
2015-10-15 14:16:05 +0200

    [FIX] Version number

--- stock_picking_package_preparation_line/__openerp__.py
+++ stock_picking_package_preparation_line/__openerp__.py
@@ -22,7 +22,7 @@
 
 {
     'name': 'Stock Picking Package Preparation Line',
-    'version': '1.0',
+    'version': '8.0.0',
     'author': 'Apulia Software srl,Odoo Community Association (OCA)',
     'maintainer': 'Apulia Software srl',
     'license': 'AGPL-3',
