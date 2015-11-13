1442340f3f9958ce0d274bdecf4d332178cd06d3
jbeficent <jordi.ballester@eficent.com>
2015-11-13 11:28:35 +0100

    [FIX] various fixes

--- stock_picking_partial_invoicing/model/account.py
+++ stock_picking_partial_invoicing/model/account.py
@@ -18,9 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
-from openerp import models, api, fields, exceptions, _
-
+from openerp import models, api, fields
 
 
 class AccountInvoice(models.Model):
@@ -29,20 +27,8 @@ class AccountInvoice(models.Model):
 
     @api.multi
     def unlink(self):
-        cr = self.env.cr
         for invoice in self:
-            for line in invoice.invoice_line:
-                move_obj = self.env['stock.move']
-                cr.execute("""
-                SELECT move_id
-                FROM stock_move_invoice_rel
-                WHERE invoice_line_id = %s
-                """, (line.id, ))
-                res = cr.fetchone()
-                if res:
-                    move_id = res[0]
-                    move = move_obj.browse(move_id)
-                    move.write({'invoice_state': '2binvoiced'})
+            invoice.invoice_line.update_stock_moves()
         return super(AccountInvoice, self).unlink()
 
 
@@ -50,51 +36,24 @@ class AccountInvoiceLine(models.Model):
 
     _inherit = 'account.invoice.line'
 
-    @api.multi
-    def unlink(self):
-        cr = self.env.cr
-        for line in self:
-            if 'stock_invoice_onshipping' in self.env.context:
-                continue
-            move_obj = self.env['stock.move']
-            cr.execute("""
-            SELECT move_id
-            FROM stock_move_invoice_rel
-            WHERE invoice_line_id = %s
-            """, (line.id, ))
-            res = cr.fetchone()
-            if res:
-                move_id = res[0]
-                move = move_obj.browse(move_id)
-                move.write({'invoice_state': '2binvoiced'})
-        return super(AccountInvoiceLine, self).unlink()
+    stock_moves = fields.Many2many('stock.move',
+                                      'stock_move_invoice_rel',
+                                      'invoice_line_id', 'move_id',
+                                      'Stock Moves', readonly=True)
 
     @api.multi
-    def write(self, vals):
-        cr = self.env.cr
-        res = super(AccountInvoiceLine, self).write(vals)
-        if 'quantity' in vals:
-            for line in self:
-                move_obj = self.env['stock.move']
-                cr.execute("""
-                SELECT move_id
-                FROM stock_move_invoice_rel
-                WHERE invoice_line_id = %s
-                """, (line.id, ))
-                res = cr.fetchone()
-                if res:
-                    move_id = res[0]
-                    move = move_obj.browse(move_id)
-                    product_qty = move.product_uos_qty or move.product_qty
-                    if move.invoiced_qty > product_qty:
-                        raise exceptions.Warning(
-                            _('Error!'),
-                            _("""You are trying to invoice more quantity
-                            than what has actually been received or
-                            delivered."""))
-                    if move.invoiced_qty != product_qty:
-                        invoice_state = '2binvoiced'
-                    else:
-                        invoice_state = 'invoiced'
-                    move.write({'invoice_state': invoice_state})
-        return res
+    def update_stock_moves(self):
+        if 'stock_invoice_onshipping' in self.env.context:
+            return False
+        for line in self:
+            move_obj = self.env['stock.move']
+            moves = move_obj.search([('invoice_lines', '=', line.id)])
+            if moves:
+                move = moves[0]
+                move.write({'invoice_state': '2binvoiced'})
+        return True
+
+    @api.multi
+    def unlink(self):
+        self.update_stock_moves()
+        return super(AccountInvoiceLine, self).unlink()
