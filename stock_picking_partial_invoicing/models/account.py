61df27aa792c83378e02737bfd73d81e124fd990
jbeficent <jordi.ballester@eficent.com>
2015-11-13 14:04:25 +0100

    [FIX] adjusted as per v8

--- stock_picking_partial_invoicing/models/account.py
+++ stock_picking_partial_invoicing/models/account.py
@@ -18,7 +18,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
 from openerp.osv import orm, fields
 
 
@@ -29,24 +28,11 @@ class AccountInvoice(orm.Model):
     def unlink(self, cr, uid, ids, context=None):
         if context is None:
             context = {}
+        inv_line_obj = self.pool['account.invoice.line']
         for invoice in self.browse(cr, uid, ids, context=context):
-            for line in invoice.invoice_line:
-                move_obj = self.pool['stock.move']
-                picking_obj = self.pool['stock.picking']
-                cr.execute("""
-                SELECT move_id
-                FROM stock_move_invoice_rel
-                WHERE invoice_line_id = %s
-                """, (line.id, ))
-                res = cr.fetchone()
-                if res:
-                    move_id = res[0]
-                    move = move_obj.browse(cr, uid, move_id, context=context)
-                    if move.picking_id and move.picking_id.state != \
-                            '2binvoiced':
-                        picking_obj.write(cr, uid,  [move.picking_id.id],
-                                          {'invoice_state': '2binvoiced'},
-                                          context=context)
+            line_ids = [line.id for line in invoice.invoice_line]
+            inv_line_obj.update_stock_moves(cr, uid, line_ids,
+                                            context=context)
         return super(AccountInvoice, self).unlink(cr, uid, ids,
                                                   context=context)
 
@@ -61,54 +47,20 @@ class AccountInvoiceLine(orm.Model):
                                         'Stock Moves', readonly=True)
     }
 
-    def unlink(self, cr, uid, ids, context=None):
+    def update_stock_moves(self, cr, uid, ids, context=None):
         if context is None:
             context = {}
         for line in self.browse(cr, uid, ids, context=context):
             if 'stock_invoice_onshipping' in context:
                 continue
-            move_obj = self.pool['stock.move']
             picking_obj = self.pool['stock.picking']
-            cr.execute("""
-            SELECT move_id
-            FROM stock_move_invoice_rel
-            WHERE invoice_line_id = %s
-            """, (line.id, ))
-            res = cr.fetchone()
-            if res:
-                move_id = res[0]
-                move = move_obj.browse(cr, uid, move_id, context=context)
+            for move in line.stock_moves:
                 if move.picking_id and move.picking_id.state != '2binvoiced':
                     picking_obj.write(cr, uid,  [move.picking_id.id],
                                       {'invoice_state': '2binvoiced'},
                                       context=context)
+
+    def unlink(self, cr, uid, ids, context=None):
+        self.update_stock_moves(cr, uid, ids, context=context)
         return super(AccountInvoiceLine, self).unlink(cr, uid, ids,
                                                       context=context)
-
-    def write(self, cr, uid, ids, vals, context=None):
-        if context is None:
-            context = {}
-        if not isinstance(ids, list):
-            ids = [ids]
-        res = super(AccountInvoiceLine, self).write(cr, uid, ids, vals,
-                                                    context=context)
-        if 'quantity' in vals:
-            for line in self.browse(cr, uid, ids, context=context):
-                picking_obj = self.pool['stock.picking']
-                for move in line.stock_moves:
-                    product_qty = move.product_uos_qty or move.product_qty
-                    if move.picking_id:
-                        if move.invoiced_qty > product_qty:
-                            raise orm.except_orm(
-                                'Error!',
-                                'You are trying to invoice more quantity '
-                                'than what has actually been received or '
-                                'delivered.')
-                        if move.invoiced_qty != product_qty:
-                            invoice_state = '2binvoiced'
-                        else:
-                            invoice_state = 'invoiced'
-                        picking_obj.write(cr, uid, [move.picking_id.id],
-                                          {'invoice_state': invoice_state},
-                                          context=context)
-        return res
