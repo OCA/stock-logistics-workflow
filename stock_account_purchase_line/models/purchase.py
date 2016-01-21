3c80733a845fa24d314cf3c593b341f2a4761bb2
jbeficent <jordi.ballester@eficent.com>
2016-01-21 02:40:51 +0100

    [ADD] stock_account_purchase_line

---
+++ stock_account_purchase_line/models/purchase.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent Business and IT Consulting Services S.L. (www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from openerp.osv import fields, orm
+
+
+class PurchaseOrder(orm.Model):
+
+    _inherit = 'purchase.order'
+
+    def _prepare_inv_line(self, cr, uid, account_id, order_line, context=None):
+        res = super(PurchaseOrder, self)._prepare_inv_line(
+                cr, uid, account_id, order_line, context=context)
+        res['purchase_line_id'] = order_line.id
+        return res
---
+++ stock_account_purchase_line/models/stock.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent Business and IT Consulting Services S.L. (www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from openerp.osv import fields, orm
+
+
+class StockMove(orm.Model):
+
+    _inherit = 'stock.move'
+
+    def _get_invoice_line_vals(self, cr, uid, move, partner, inv_type,
+                               context=None):
+        res = super(StockMove, self)._get_invoice_line_vals(cr, uid,
+                                                            move, partner,
+                                                            inv_type,
+                                                            context=context)
+        if move.purchase_line_id:
+            res['purchase_line_od'] = move.purchase_line_id.id
+        return res
+
+
+class StockQuant(orm.Model):
+    _inherit = "stock.quant"
+
+    def _prepare_account_move_line(self, cr, uid, move, qty, cost,
+                                   credit_account_id, debit_account_id,
+                                   context=None):
+        res = super(StockQuant, self)._prepare_account_move_line(
+            cr, uid, move, qty, cost, credit_account_id, debit_account_id,
+            context)
+        for line in res:
+            line[2]['purchase_line_id'] = move.purchase_line_id.id
+        return res
