172da01023048ae2cbd7618a1bba8353d957db6e
Laetitia Gangloff <laetitia.gangloff@acsone.eu>
2016-01-22 18:33:29 +0100

    stock_ownership_availability_rules : set partner on inventory line when update quantity

---
+++ stock_ownership_availability_rules/model/stock_change_product_qty.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2016  Laetitia Gangloff, Acsone SA/NV (http://www.acsone.eu)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, models
+
+
+class StockChangeProductQty(models.TransientModel):
+    _inherit = "stock.change.product.qty"
+
+    @api.model
+    def _finalize_inventory_line(self, data):
+        line_data = super(StockChangeProductQty,
+                          self)._finalize_inventory_line(data)
+
+        Company = self.env['res.company']
+        location = data.location_id
+        line_data['partner_id'] = (
+            location.partner_id.id or
+            location.company_id.partner_id.id or
+            Company.browse(
+                Company._company_default_get('stock.quant')
+            ).partner_id.id
+        )
+
+        return line_data
