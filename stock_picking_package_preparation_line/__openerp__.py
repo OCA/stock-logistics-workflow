35f88699d7c8c0f105d71dc09ceddc1806cda157
eLBati <lorenzo.battistini@agilebg.com>
2016-02-02 14:28:23 +0100

    copying stock_picking_package_preparation_line from 8.0

---
+++ stock_picking_package_preparation_line/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Francesco Apruzzese
+#    Copyright 2015 Apulia Software srl
+#    Copyright 2015 Lorenzo Battistini - Agile Business Group
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Stock Picking Package Preparation Line',
+    'version': '8.0.1.0.0',
+    'author': 'Apulia Software srl,Odoo Community Association (OCA)',
+    'maintainer': 'Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'category': 'Warehouse Management',
+    'depends': [
+        'product',
+        'stock_picking_package_preparation',
+    ],
+    'website': 'http://www.apuliasoftware.it',
+    'data': [
+        'views/stock_picking_package_preparation_line.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
