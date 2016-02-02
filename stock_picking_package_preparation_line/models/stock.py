562133e4a52c1c130806a00e8858a44e7a90b7aa
eLBati <lorenzo.battistini@agilebg.com>
2016-02-02 14:28:08 +0100

    porting last changes from 8.0
    Removing parts related to product.ul as this model does not exist anymore We can only compute net weight, because package weight does not exist
    
    remove lot from operation tree, as it is a o2m now
    
    using new method check_tracking
    
    FIX tests
    
    Adding my name as I ported modules to 9
    
    udpate module version
    
    stock_picking_package_preparation: IMP README and use short copyright header
    
    Moving pack preparation under 'inventory control' as 'operations' is only available in debug mode
    
    Porting translations from 8.0
    
    stock_picking_package_preparation_line: porting translations form 8.0
    
    IMP stock_picking_package_preparation: avoid @api.one
    
    IMP stock_picking_package_preparation: use odoo noupdate
    
    IMP stock_picking_package_preparation: use odoo noupdate
    FIX PEP8
    
    FIX stock_picking_package_preparation: compatibility with delivery and its ensure_one
    
    copying stock_picking_package_preparation_line from 8.0
    
    adapting to new values format from write method
    
    FIX correctly handle write tuples
    
    location_id and location_dest_id are mandatory in picking
    
    using notebook as position based on string is not allowed
    
    FIX tests
    
    Adding my name as I ported modules to 9
    
    udpate module version
    
    stock_picking_package_preparation: IMP README and use short copyright header

---
+++ stock_picking_package_preparation_line/models/stock.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright 2015 Lorenzo Battistini - Agile Business Group
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, api
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    def get_packs(self):
+        pack_line_model = self.env['stock.picking.package.preparation.line']
+        pack_lines = pack_line_model.search([
+            ('move_id', '=', self.id),
+            ])
+        return pack_lines.mapped('package_preparation_id')
+
+    @api.multi
+    def write(self, values):
+        res = super(StockMove, self).write(values)
+        if not self.env.context.get('skip_update_line_ids', False):
+            pack_to_update = self.env['stock.picking.package.preparation']
+            for move in self:
+                pack_to_update |= move.get_packs()
+            if pack_to_update:
+                pack_to_update._update_line_ids()
+        return res
+
+    @api.multi
+    def unlink(self):
+        pack_to_update = self.env['stock.picking.package.preparation']
+        for move in self:
+            pack_to_update |= move.get_packs()
+        res = super(StockMove, self).unlink()
+        if pack_to_update:
+            pack_to_update._update_line_ids()
+        return res
