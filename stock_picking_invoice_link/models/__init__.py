c950aa7e9e0262a42a819920f7d1dbbbf55e8ba2
oihane <oihanecruce@gmail.com>
2016-02-10 11:25:45 +0100

    stock_picking_invoice_link: added new functionality * When an invoice is cancelled its pickings go back to "to2invoiced" state * If a cancelled invoice goes back again to draft, its pickings are marked as invoiced to avoid invoicing twice * Not able to erase an invoice with pickings not cancelled * Tests
    stock_picking_invoice_link: unlink improved * enable erasing when invoice is in state 'draft' or 'cancel' * if invoice is in state 'draft', not cancelled picking will be invoiceable

--- stock_picking_invoice_link/models/__init__.py
+++ stock_picking_invoice_link/models/__init__.py
@@ -1,6 +1,5 @@
+# -*- coding: utf-8 -*-
+# © 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from . import account_move
-from . import stock_move
-from . import stock_picking
-from . import sale_order
+from . import stock
