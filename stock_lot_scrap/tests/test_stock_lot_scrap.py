96492701c784ab118825dbb5ca8de84c178f65f0
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-04-22 17:51:03 +0200

    stock_lot_scrap: Inject button in view + auto-assign moves

--- stock_lot_scrap/tests/test_stock_lot_scrap.py
+++ stock_lot_scrap/tests/test_stock_lot_scrap.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # © 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
+# © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp.exceptions import Warning as UserError
@@ -9,12 +10,13 @@ from openerp.tests.common import TransactionCase
 class TestStockLotScrap(TransactionCase):
     def setUp(self):
         super(TestStockLotScrap, self).setUp()
-        self.product = self.env.ref('product.product_product_36')
-        self.lot010 = self.env['stock.production.lot'].create(
-            {
-                'name': "0000010",
-                'product_id': self.product.id
-            })
+        self.product = self.env['product.product'].create({
+            'name': 'Test product',
+        })
+        self.lot010 = self.env['stock.production.lot'].create({
+            'name': "0000010",
+            'product_id': self.product.id
+        })
         self.quant1 = self.env['stock.quant'].create({
             'qty': 5000.0,
             'location_id': self.env.ref('stock.stock_location_stock').id,
@@ -34,9 +36,9 @@ class TestStockLotScrap(TransactionCase):
         picking = self.env['stock.picking'].browse(res['res_id'])
         self.assertEqual(len(picking.move_lines), 2)
         self.assertAlmostEqual(
-            sum(picking.move_lines.mapped('product_uos_qty')),
-            5000.0 + 325.0
-        )
+            sum(picking.move_lines.mapped('product_uos_qty')), 5325)
+        self.assertTrue(
+            all(picking.move_lines.mapped(lambda x: x.state == 'assigned')))
 
     def test_warning(self):
         product_new = self.product.create({
