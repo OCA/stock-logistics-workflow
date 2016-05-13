f51f0e7d000a239b51a1dd95db310690548f0828
eLBati <lorenzo.battistini@agilebg.com>
2016-05-13 17:05:02 +0200

    Porting translations from 8.0
    stock_picking_package_preparation_line: porting translations form 8.0
    
    FIX correctly handle lots on preparation lines
    
    IMP stock_picking_package_preparation: use odoo noupdate
    
    IMP stock_picking_package_preparation: use odoo noupdate
    FIX PEP8
    
    REF stock_picking_package_preparation_line: default_get with new API
    
    stock_picking_package_preparation_line: FIX headers, file and model names
    
    stock_picking_package_preparation_line: FIX ensure_one and required field
    
    stock_picking_package_preparation_line: rename product_uom_id
    
    FIX stock_picking_package_preparation_line : Field `product_uom` does not exist
    
    FIX stock_picking_package_preparation_line: AttributeError: 'stock.move' object has no attribute 'product_uom_id'
    
    IMP stock_picking_package_preparation_line comments

--- stock_picking_package_preparation_line/models/stock_move.py
+++ stock_picking_package_preparation_line/models/stock_move.py
@@ -1,22 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
 #    Copyright 2015 Lorenzo Battistini - Agile Business Group
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import models, api
 
@@ -25,6 +9,7 @@ class StockMove(models.Model):
     _inherit = 'stock.move'
 
     def get_packs(self):
+        self.ensure_one()
         pack_line_model = self.env['stock.picking.package.preparation.line']
         pack_lines = pack_line_model.search([
             ('move_id', '=', self.id),
