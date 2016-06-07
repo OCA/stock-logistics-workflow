f7cec54175d6f7987c4a8748d77736249acd84e3
archetipo <alessio.gerace@gmail.com>
2016-06-07 10:59:44 +0200

    module manifest and minor fix

--- stock_disable_force_availability_button/__openerp__.py
+++ stock_disable_force_availability_button/__openerp__.py
@@ -1,42 +1,23 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see http://www.gnu.org/licenses/.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Disable force availability button",
-    "version": "1.0",
-    "depends": [
-        "stock",
-    ],
+    "version": "8.0.1.0.0",
+    "depends": ["stock"],
     "author": "OdooMRP team,"
               "AvanzOSC,"
-              "Serv. Tecnol. Avanzados - Pedro M. Baeza,"
-              "Odoo Community Association (OCA)",
+              "Tecnativa - Pedro M. Baeza,"
+              "Odoo Community Association (OCA),"
+              "Agile Business Group",
     "website": "http://www.odoomrp.com",
-    "contributors": [
-        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
-        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
-        "Ana Juaristi <anajuaristi@avanzosc.es>"
-    ],
-    "category": "Custom Module",
-    "summary": "",
+    "category": "Warehouse Management",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
     "data": [
         "security/stock_disable_force_availability_button_security.xml",
         "views/stock_view.xml",
     ],
-    "installable": True,
+
 }
