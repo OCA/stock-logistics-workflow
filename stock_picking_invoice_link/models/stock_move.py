a311174d72a5c0702bd46ede0de91d78b70d9d7c
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-06-08 13:17:49 +0200

    stock_picking_invoice_link: Many2many instead of many2one for invoice references
    For dropshipping pickings with module stock_dropshipping_dual_invoice, you generate
    two invoices for the same picking, so the many2one only reflects the last generated
    invoice. With this change, now both are linked.

--- stock_picking_invoice_link/models/stock_move.py
+++ stock_picking_invoice_link/models/stock_move.py
@@ -1,37 +1,33 @@
-# Copyright 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
-# Copyright 2015-2016 AvanzOSC
-# Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
-# Copyright 2022 Antony Herrera - LooErp
+# -*- coding: utf-8 -*-
+# © 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
+# © 2015-2016 AvanzOSC
+# © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import _, fields, models
-from odoo.exceptions import UserError
+from openerp import api, fields, models
 
 
 class StockMove(models.Model):
     _inherit = "stock.move"
 
     invoice_line_ids = fields.Many2many(
-        comodel_name="account.move.line",
-        relation="stock_move_invoice_line_rel",
-        column1="move_id",
-        column2="invoice_line_id",
-        string="Invoice Line",
-        copy=False,
-        readonly=True,
-    )
+        comodel_name='account.invoice.line', string='Invoice Lines',
+        copy=False, readonly=True)
+    # Provide this field for backwards compatibility
+    invoice_line_id = fields.Many2one(
+        comodel_name='account.invoice.line', string='Invoice Line',
+        compute="_compute_invoice_line_id")
 
-    def write(self, vals):
-        """
-        User can update any picking in done state, but if this picking already
-        invoiced the stock move done quantities can be different to invoice
-        line quantities. So to avoid this inconsistency you can not update any
-        stock move line in done state and have invoice lines linked.
-        """
-        if "product_uom_qty" in vals and not self.env.context.get(
-            "bypass_stock_move_update_restriction"
-        ):
-            for move in self:
-                if move.state == "done" and move.invoice_line_ids:
-                    raise UserError(_("You can not modify an invoiced stock move"))
-        return super().write(vals)
+    @api.multi
+    @api.depends('invoice_line_ids')
+    def _compute_invoice_line_id(self):
+        for move in self:
+            move.invoice_line_id = move.invoice_line_ids[:1]
+
+    @api.model
+    def _create_invoice_line_from_vals(self, move, invoice_line_vals):
+        inv_line_id = super(StockMove, self)._create_invoice_line_from_vals(
+            move, invoice_line_vals)
+        move.invoice_line_ids = [(4, inv_line_id)]
+        move.picking_id.invoice_ids = [(4, invoice_line_vals['invoice_id'])]
+        return inv_line_id
