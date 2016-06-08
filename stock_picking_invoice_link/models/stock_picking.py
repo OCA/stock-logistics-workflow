43501003ef465250ef5561b0f33246bf55ff5aea
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-06-08 13:17:49 +0200

    stock_picking_invoice_link: Many2many instead of many2one for invoice references
    For dropshipping pickings with module stock_dropshipping_dual_invoice, you generate
    two invoices for the same picking, so the many2one only reflects the last generated
    invoice. With this change, now both are linked.

--- stock_picking_invoice_link/models/stock_picking.py
+++ stock_picking_invoice_link/models/stock_picking.py
@@ -1,42 +1,25 @@
-# Copyright 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
-# Copyright 2015-2016 AvanzOSC
-# Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
-# Copyright 2017 Jacques-Etienne Baudoux <je@bcim.be>
-# Copyright 2022 Antony Herrera - LooErp
+# -*- coding: utf-8 -*-
+# © 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
+# © 2015-2016 AvanzOSC
+# © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, fields, models
+from openerp import api, fields, models
 
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     invoice_ids = fields.Many2many(
-        comodel_name="account.move", copy=False, string="Invoices", readonly=True
-    )
-    invoice_count = fields.Integer(
-        string="Invoices Orders", compute="_compute_invoice_ids"
-    )
+        comodel_name='account.invoice', copy=False, string='Invoices',
+        readonly=True)
+    # Provide this field for backwards compatibility
+    invoice_id = fields.Many2one(
+        comodel_name='account.invoice', string='Invoice',
+        compute="_compute_invoice_id")
 
-    def action_view_invoice(self):
-        """This function returns an action that display existing invoices
-        of given stock pickings.
-        It can either be a in a list or in a form view, if there is only
-        one invoice to show.
-        """
-        self.ensure_one()
-        form_view_name = "account.view_move_form"
-        action = self.env.ref("account.action_move_out_invoice_type")
-        result = action.read()[0]
-        if len(self.invoice_ids) > 1:
-            result["domain"] = "[('id', 'in', %s)]" % self.invoice_ids.ids
-        else:
-            form_view = self.env.ref(form_view_name)
-            result["views"] = [(form_view.id, "form")]
-            result["res_id"] = self.invoice_ids.id
-        return result
-
-    @api.depends("invoice_ids")
-    def _compute_invoice_ids(self):
-        for order in self:
-            order.invoice_count = len(order.invoice_ids)
+    @api.multi
+    @api.depends('invoice_ids')
+    def _compute_invoice_id(self):
+        for move in self:
+            move.invoice_id = move.invoice_ids[:1]
