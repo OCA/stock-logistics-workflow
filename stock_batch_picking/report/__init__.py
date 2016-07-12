ab25102121f465719dfac7c609776028c79fbea1
Cyril Gaudin <cyril.gaudin@camptocamp.com>
2016-07-12 17:06:06 +0200

    stock_batch_picking

---
+++ stock_batch_picking/report/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# © 2012-2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import batch_report
---
+++ stock_batch_picking/report/batch_report.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2012-2014 Nicolas Bessi, Joel Grand-Guillaume, Alexandre Fayolle
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import logging
+from openerp import models
+
+from . print_batch import PrintBatch
+
+
+_logger = logging.getLogger(__name__)
+
+
+class ReportPrintBatchPicking(models.AbstractModel):
+    _name = 'report.stock_batch_picking.report_batch_picking'
+    _inherit = 'report.abstract_report'
+    _template = 'stock_batch_picking.report_batch_picking'
+    _wrapped_report_class = PrintBatch
