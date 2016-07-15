12e10568fbb1b8de4429a3bc1e57e6d4c954576d
eLBati <lorenzo.battistini@agilebg.com>
2016-07-15 14:57:40 +0200

    copying from 8.0 branch beacause
    $ git log refs/heads/8.0-extract --since=2015-10-14 --pretty=format:'%H' --reverse | while read line || [ -n "$line" ] ; do
    >   git cherry-pick $line
    > done
    error: Commit 698d7875cc5e3145a59a07bd3b1b3d40cf81822e is a merge but no -m option was given.
    fatal: cherry-pick failed
    [9.0-stock_picking_invoice_link fd53a50] OCA Transbot updated translations from Transifex
     Author: OCA Transbot <transbot@odoo-community.org>
     Date: Mon Oct 26 08:22:09 2015 -0400
     3 files changed, 214 insertions(+)
     create mode 100644 stock_picking_invoice_link/i18n/fr.po
     create mode 100644 stock_picking_invoice_link/i18n/it.po
     create mode 100644 stock_picking_invoice_link/i18n/sl.po
    [9.0-stock_picking_invoice_link 9a4358f] OCA Transbot updated translations from Transifex
     Author: OCA Transbot <transbot@odoo-community.org>
     Date: Sun Feb 21 01:45:13 2016 -0500
     1 file changed, 72 insertions(+)
     create mode 100644 stock_picking_invoice_link/i18n/es.po
    error: could not apply 1b8d487... [IMP] stock_picking_invoice_link: added new functionality
    hint: after resolving the conflicts, mark the corrected paths
    hint: with 'git add <paths>' or 'git rm <paths>'
    hint: and commit the result with 'git commit'
    error: cherry-pick is not possible because you have unmerged files.
    hint: Fix them up in the work tree, and then use 'git add/rm <file>'
    hint: as appropriate to mark resolution and make a commit.
    fatal: cherry-pick failed
    error: cherry-pick is not possible because you have unmerged files.
    hint: Fix them up in the work tree, and then use 'git add/rm <file>'
    hint: as appropriate to mark resolution and make a commit.
    fatal: cherry-pick failed
    error: cherry-pick is not possible because you have unmerged files.
    hint: Fix them up in the work tree, and then use 'git add/rm <file>'
    hint: as appropriate to mark resolution and make a commit.
    fatal: cherry-pick failed
    error: cherry-pick is not possible because you have unmerged files.
    hint: Fix them up in the work tree, and then use 'git add/rm <file>'
    hint: as appropriate to mark resolution and make a commit.
    fatal: cherry-pick failed
    error: cherry-pick is not possible because you have unmerged files.
    hint: Fix them up in the work tree, and then use 'git add/rm <file>'
    hint: as appropriate to mark resolution and make a commit.
    fatal: cherry-pick failed
    error: cherry-pick is not possible because you have unmerged files.
    hint: Fix them up in the work tree, and then use 'git add/rm <file>'
    hint: as appropriate to mark resolution and make a commit.
    fatal: cherry-pick failed
    error: cherry-pick is not possible because you have unmerged files.
    hint: Fix them up in the work tree, and then use 'git add/rm <file>'
    hint: as appropriate to mark resolution and make a commit.
    fatal: cherry-pick failed
    error: cherry-pick is not possible because you have unmerged files.
    hint: Fix them up in the work tree, and then use 'git add/rm <file>'
    hint: as appropriate to mark resolution and make a commit.
    fatal: cherry-pick failed
    error: cherry-pick is not possible because you have unmerged files.
    hint: Fix them up in the work tree, and then use 'git add/rm <file>'
    hint: as appropriate to mark resolution and make a commit.
    fatal: cherry-pick failed

---
+++ stock_picking_invoice_link/tests/test_stock_picking_invoice_link.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+# © 2016 Oihane Crucelaegui - AvanzOSC
+# © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from openerp.addons.stock.tests.common import TestStockCommon
+
+
+class TestStockPickingInvoiceLink(TestStockCommon):
+    def setUp(self):
+        super(TestStockPickingInvoiceLink, self).setUp()
+        self.partner = self.env['res.partner'].create({
+            'name': 'Test partner',
+        })
+        self.picking_in = self.PickingObj.create({
+            'partner_id': self.partner.id,
+            'picking_type_id': self.picking_type_in,
+            'invoice_state': '2binvoiced'})
+        self.MoveObj.create({
+            'name': self.productA.name,
+            'product_id': self.productA.id,
+            'product_uom_qty': 1,
+            'product_uom': self.productA.uom_id.id,
+            'picking_id': self.picking_in.id,
+            'location_id': self.supplier_location,
+            'location_dest_id': self.stock_location,
+            'invoice_state': '2binvoiced',
+        })
+        self.MoveObj.create({
+            'name': self.productB.name,
+            'product_id': self.productB.id,
+            'product_uom_qty': 1,
+            'product_uom': self.productB.uom_id.id,
+            'picking_id': self.picking_in.id,
+            'location_id': self.supplier_location,
+            'location_dest_id': self.stock_location,
+            'invoice_state': '2binvoiced',
+        })
+        self.MoveObj.create({
+            'name': self.productC.name,
+            'product_id': self.productC.id,
+            'product_uom_qty': 10,
+            'product_uom': self.productC.uom_id.id,
+            'picking_id': self.picking_in.id,
+            'location_id': self.supplier_location,
+            'location_dest_id': self.stock_location,
+            'invoice_state': '2binvoiced',
+        })
+        self.MoveObj.create({
+            'name': self.productD.name,
+            'product_id': self.productD.id,
+            'product_uom_qty': 10,
+            'product_uom': self.productD.uom_id.id,
+            'picking_id': self.picking_in.id,
+            'location_id': self.supplier_location,
+            'location_dest_id': self.stock_location,
+            'invoice_state': '2binvoiced',
+        })
+        self.MoveObj.create({
+            'name': self.productD.name,
+            'product_id': self.productD.id,
+            'product_uom_qty': 5,
+            'product_uom': self.productD.uom_id.id,
+            'picking_id': self.picking_in.id,
+            'location_id': self.supplier_location,
+            'location_dest_id': self.stock_location,
+            'invoice_state': '2binvoiced'})
+        context = {"active_model": 'stock.picking',
+                   "active_ids": [self.picking_in.id],
+                   "active_id": self.picking_in.id}
+        self.wizard = self.env['stock.invoice.onshipping'].with_context(
+            context).create({})
+
+    def test_invoice_create(self):
+        self.assertEqual(self.picking_in.invoice_state, '2binvoiced')
+        for move in self.picking_in.move_lines:
+            self.assertEqual(move.invoice_state, '2binvoiced')
+        self.wizard.open_invoice()
+        self.assertTrue(self.picking_in.invoice_ids)
+        invoice = self.picking_in.invoice_id
+        self.assertTrue(invoice)
+        self.assertEqual(self.picking_in.invoice_state, 'invoiced')
+        for move in self.picking_in.move_lines:
+            self.assertTrue(move.invoice_line_ids)
+            self.assertTrue(move.invoice_line_id)
+            self.assertEqual(move.invoice_state, 'invoiced')
+        invoice.action_cancel()
+        self.assertEqual(self.picking_in.invoice_state, '2binvoiced')
+        invoice.action_cancel_draft()
+        self.assertEqual(self.picking_in.invoice_state, 'invoiced')
+        self.assertEqual(
+            set(invoice.invoice_line.ids),
+            set(self.picking_in.move_lines.mapped('invoice_line_id').ids))
+        self.assertEqual(
+            set(self.picking_in.move_lines.ids),
+            set(invoice.invoice_line.mapped('move_line_ids').ids))
+
+    def test_invoice_unlink_draft(self):
+        self.wizard.open_invoice()
+        self.picking_in.invoice_ids.unlink()
+        self.assertEqual(self.picking_in.invoice_state, '2binvoiced')
+
+    def test_invoice_unlink_cancel(self):
+        self.wizard.open_invoice()
+        self.picking_in.invoice_ids.signal_workflow('invoice_cancel')
+        self.assertEqual(self.picking_in.invoice_state, '2binvoiced')
+
+    def test_invoice_unlink_not_draft_nor_cancel(self):
+        self.wizard.open_invoice()
+        self.assertEqual(self.picking_in.invoice_state, 'invoiced')
