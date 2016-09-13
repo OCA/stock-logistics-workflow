cc13bf68a0e11d6532f8979707ebc57d4ad2ff0d
Alexis de Lattre <alexis.delattre@akretion.com>
2016-09-13 22:17:01 +0200

    [IMP] stock_picking_invoice_link: Refactoring based on new 9.0 invoicing flow

--- stock_picking_invoice_link/tests/test_stock_picking_invoice_link.py
+++ stock_picking_invoice_link/tests/test_stock_picking_invoice_link.py
@@ -3,108 +3,22 @@
 # © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp.addons.stock.tests.common import TestStockCommon
+from openerp.addons.sale.tests.test_sale_common import TestSale
 
 
-class TestStockPickingInvoiceLink(TestStockCommon):
-    def setUp(self):
-        super(TestStockPickingInvoiceLink, self).setUp()
-        self.partner = self.env['res.partner'].create({
-            'name': 'Test partner',
-        })
-        self.picking_in = self.PickingObj.create({
+class TestStockPickingInvoiceLink(TestSale):
+    def test_00_sale_stock_invoice(self):
+        self.so = self.env['sale.order'].create({
             'partner_id': self.partner.id,
-            'picking_type_id': self.picking_type_in,
-            'invoice_state': '2binvoiced'})
-        self.MoveObj.create({
-            'name': self.productA.name,
-            'product_id': self.productA.id,
-            'product_uom_qty': 1,
-            'product_uom': self.productA.uom_id.id,
-            'picking_id': self.picking_in.id,
-            'location_id': self.supplier_location,
-            'location_dest_id': self.stock_location,
-            'invoice_state': '2binvoiced',
+            'partner_invoice_id': self.partner.id,
+            'partner_shipping_id': self.partner.id,
+            'order_line': [(0, 0, {
+                'name': p.name, 'product_id': p.id, 'product_uom_qty': 2,
+                'product_uom': p.uom_id.id, 'price_unit': p.list_price
+            }) for (_, p) in self.products.iteritems()],
         })
-        self.MoveObj.create({
-            'name': self.productB.name,
-            'product_id': self.productB.id,
-            'product_uom_qty': 1,
-            'product_uom': self.productB.uom_id.id,
-            'picking_id': self.picking_in.id,
-            'location_id': self.supplier_location,
-            'location_dest_id': self.stock_location,
-            'invoice_state': '2binvoiced',
-        })
-        self.MoveObj.create({
-            'name': self.productC.name,
-            'product_id': self.productC.id,
-            'product_uom_qty': 10,
-            'product_uom': self.productC.uom_id.id,
-            'picking_id': self.picking_in.id,
-            'location_id': self.supplier_location,
-            'location_dest_id': self.stock_location,
-            'invoice_state': '2binvoiced',
-        })
-        self.MoveObj.create({
-            'name': self.productD.name,
-            'product_id': self.productD.id,
-            'product_uom_qty': 10,
-            'product_uom': self.productD.uom_id.id,
-            'picking_id': self.picking_in.id,
-            'location_id': self.supplier_location,
-            'location_dest_id': self.stock_location,
-            'invoice_state': '2binvoiced',
-        })
-        self.MoveObj.create({
-            'name': self.productD.name,
-            'product_id': self.productD.id,
-            'product_uom_qty': 5,
-            'product_uom': self.productD.uom_id.id,
-            'picking_id': self.picking_in.id,
-            'location_id': self.supplier_location,
-            'location_dest_id': self.stock_location,
-            'invoice_state': '2binvoiced'})
-        context = {"active_model": 'stock.picking',
-                   "active_ids": [self.picking_in.id],
-                   "active_id": self.picking_in.id}
-        self.wizard = self.env['stock.invoice.onshipping'].with_context(
-            context).create({})
-
-    def test_invoice_create(self):
-        self.assertEqual(self.picking_in.invoice_state, '2binvoiced')
-        for move in self.picking_in.move_lines:
-            self.assertEqual(move.invoice_state, '2binvoiced')
-        self.wizard.open_invoice()
-        self.assertTrue(self.picking_in.invoice_ids)
-        invoice = self.picking_in.invoice_id
-        self.assertTrue(invoice)
-        self.assertEqual(self.picking_in.invoice_state, 'invoiced')
-        for move in self.picking_in.move_lines:
-            self.assertTrue(move.invoice_line_ids)
-            self.assertTrue(move.invoice_line_id)
-            self.assertEqual(move.invoice_state, 'invoiced')
-        invoice.action_cancel()
-        self.assertEqual(self.picking_in.invoice_state, '2binvoiced')
-        invoice.action_cancel_draft()
-        self.assertEqual(self.picking_in.invoice_state, 'invoiced')
-        self.assertEqual(
-            set(invoice.invoice_line.ids),
-            set(self.picking_in.move_lines.mapped('invoice_line_id').ids))
-        self.assertEqual(
-            set(self.picking_in.move_lines.ids),
-            set(invoice.invoice_line.mapped('move_line_ids').ids))
-
-    def test_invoice_unlink_draft(self):
-        self.wizard.open_invoice()
-        self.picking_in.invoice_ids.unlink()
-        self.assertEqual(self.picking_in.invoice_state, '2binvoiced')
-
-    def test_invoice_unlink_cancel(self):
-        self.wizard.open_invoice()
-        self.picking_in.invoice_ids.signal_workflow('invoice_cancel')
-        self.assertEqual(self.picking_in.invoice_state, '2binvoiced')
-
-    def test_invoice_unlink_not_draft_nor_cancel(self):
-        self.wizard.open_invoice()
-        self.assertEqual(self.picking_in.invoice_state, 'invoiced')
+        self.so.action_confirm()
+        self.so.action_invoice_create()
+        self.assertEqual(len(self.so.invoice_ids.picking_ids), 1)
+        self.assertEqual(self.so.picking_ids, self.so.invoice_ids.picking_ids)
+        self.assertEqual(self.so.invoice_ids, self.so.picking_ids.invoice_ids)
