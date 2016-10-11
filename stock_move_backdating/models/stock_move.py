6cc10234492c825304e7ce1bf2392a75e5112ced
Alex Comba <alex.comba@agilebg.com>
2016-10-11 22:19:22 +0200

    stock_move_backdating module (#252)
    * Apply some cosmetic changes according to OCA guidelines
    
    * Add date_backdating to stock.transfer.details and set date_done of stock.picking as the youngest date among moves
    
    * Update test accordingly to last changes
    
    * Update i18n/it.po accordingly to last changes
    
    * Apply some cosmetic changes: copyright and OCA url
    
    * [IMP] stock_move_backdating: decription
    
    * FIX stock_move_backdating: avoid to use future date anyway, if forced in web interface

--- stock_move_backdating/models/stock_move.py
+++ stock_move_backdating/models/stock_move.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-# ©2015 Agile Business Group (<http://www.agilebg.com>)
-# ©2015 BREMSKERL-REIBBELAGWERKE EMMERLING GmbH & Co. KG
+# Copyright 2015-2016 Agile Business Group (<http://www.agilebg.com>)
+# Copyright 2016 BREMSKERL-REIBBELAGWERKE EMMERLING GmbH & Co. KG
 #    Author Marco Dieckhoff
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import models, api
+from ..wizards.stock_transfer_details import check_date
 
 
 class StockMove(models.Model):
@@ -20,6 +21,12 @@ class StockMove(models.Model):
                 operation = move.linked_move_operation_ids[0]
                 move.date = operation.operation_id.date
                 if move.quant_ids:
+                    check_date(move.date)
                     move.quant_ids.sudo().write({'in_date': move.date})
-
+        pickings = self.mapped('picking_id').filtered(
+            lambda r: r.state == 'done')
+        for picking in pickings:
+            # set date_done as the youngest date among the moves
+            dates = picking.mapped('move_lines.date')
+            picking.write({'date_done': max(dates)})
         return result
