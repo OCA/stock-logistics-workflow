2a112333166a4da33ad2b170d5aa666e3a09ddc9
archetipo <alessio.gerace@gmail.com>
2016-10-19 12:55:26 +0200

    [MIG] stock_picking_package_preparation: Migrated to 10.0

--- stock_picking_package_preparation/__manifest__.py
+++ stock_picking_package_preparation/__manifest__.py
@@ -3,20 +3,19 @@
 # Copyright 2015 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-{'name': 'Stock Picking Package Preparation',
- 'version': '9.0.1.0.0',
- 'author': 'Camptocamp,Odoo Community Association (OCA)',
- 'maintainer': 'Camptocamp',
- 'license': 'AGPL-3',
- 'category': 'Warehouse Management',
- 'depends': ['stock',
-             ],
- 'website': 'http://www.camptocamp.com',
- 'data': ['view/stock_picking_package_preparation_view.xml',
-          'security/ir.model.access.csv',
-          'security/package_preparation_security.xml',
-          ],
- 'test': [],
- 'installable': True,
- 'auto_install': False,
+{
+    'name': 'Stock Picking Package Preparation',
+    'version': '10.0.1.0.0',
+    'author': 'Camptocamp,Odoo Community Association (OCA)',
+    'maintainer': 'Camptocamp',
+    'license': 'AGPL-3',
+    'category': 'Warehouse Management',
+    'depends': ['stock'],
+    'website': 'http://www.camptocamp.com',
+    'data': [
+        'views/stock_picking_package_preparation_view.xml',
+        'security/ir.model.access.csv',
+        'security/package_preparation_security.xml'],
+    'installable': True,
+    'auto_install': False,
  }
