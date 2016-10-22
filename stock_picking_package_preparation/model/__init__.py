2123f5dd515afd94f214ae6480f96064a1869ede
archetipo <alessio.gerace@gmail.com>
2016-10-22 11:36:30 +0200

    [RM] view and model useless folder

--- stock_picking_package_preparation/model/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Author: Guewen Baconnier
-# Copyright 2015 Camptocamp SA
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import stock_picking_package_preparation
