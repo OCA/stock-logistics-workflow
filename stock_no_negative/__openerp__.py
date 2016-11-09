e00ad8b36d095c729c45d5ef558d705e5d1a29d2
Noreddine Ben Jillali <nbj.microcom@gmail.com>
2016-11-09 11:44:57 -0500

    migrate stock_no_negative module

---
+++ stock_no_negative/__openerp__.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2014 Vauxoo - http://www.vauxoo.com/
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+#
+#    Coded by: Luis Torres (luis_t@vauxoo.com)
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+{
+    "name": "Stock check no negative",
+    "version": "1.0",
+    "author": "Vauxoo",
+    "category": "Generic Modules",
+    "description": """
+This module check that can not move a product with quantity available negative
+
+""",
+    "website": "http://www.vauxoo.com",
+    "license": "",
+    "depends": [
+        "stock"
+    ],
+    "demo": [],
+    "data": [
+        "view/product_view.xml"
+    ],
+    "test": [],
+    "js": [],
+    "css": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ stock_no_negative/model/product_template.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2015 Vauxoo - http://www.vauxoo.com/
+#    All Rights Reserved.
+#    info Vauxoo (info@vauxoo.com)
+#
+#    Coded by: Luis Torres (luis_t@vauxoo.com)
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+from openerp import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    check_no_negative = fields.Boolean(
+        string='Check no negative',
+        help='If this field is True can not move this'
+             ' product in negative quantity available in'
+             ' the internal location source')
