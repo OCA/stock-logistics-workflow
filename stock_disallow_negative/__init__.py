430f5ff0d188d524f77f340717eb0efb9dafb27a
Alexis de Lattre <alexis.delattre@akretion.com>
2016-11-17 22:46:17 +0100

    Add lot in error message
    Update to latest OCA conventions

--- stock_disallow_negative/__init__.py
+++ stock_disallow_negative/__init__.py
@@ -1,3 +1,3 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-from . import stock_disallow_negative
+from . import models
--- stock_disallow_negative/__openerp__.py
+++ stock_disallow_negative/__openerp__.py
@@ -1,35 +1,18 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Stock Disallow Negative module for Odoo
-#    Copyright (C) 2015 Akretion (http://www.akretion.com)
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# © 2015-2016 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
 {
     'name': 'Stock Disallow Negative',
-    'version': '0.1',
+    'version': '8.0.1.0.2',
     'category': 'Inventory, Logistic, Storage',
     'license': 'AGPL-3',
     'summary': 'Disallow negative stock levels by default',
     'author': 'Akretion,Odoo Community Association (OCA)',
     'website': 'http://www.akretion.com',
     'depends': ['stock'],
-    'data': ['product_view.xml'],
+    'data': ['views/product.xml'],
     'installable': True,
 }
