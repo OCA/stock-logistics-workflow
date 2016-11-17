b0c8ab57df89e906ca2c5b3940fd21829438dc29
Alexis de Lattre <alexis.delattre@akretion.com>
2016-11-17 23:28:10 +0100

    [MIG] stock_no_negative: Migration to 10.0

---
+++ stock_no_negative/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2015-2016 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+{
+    'name': 'Stock Disallow Negative',
+    'version': '8.0.1.0.2',
+    'category': 'Inventory, Logistic, Storage',
+    'license': 'AGPL-3',
+    'summary': 'Disallow negative stock levels by default',
+    'author': 'Akretion,Odoo Community Association (OCA)',
+    'website': 'http://www.akretion.com',
+    'depends': ['stock'],
+    'data': ['views/product.xml'],
+    'installable': True,
+}
