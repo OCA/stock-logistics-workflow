ad960cdc728a017b558eaf34a9e33c9b009e01bb
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-12-06 20:38:13 +0100

    [MIG] stock_lot_scrap: Migration to 9.0

--- stock_lot_scrap/tests/test_stock_lot_scrap.py
+++ stock_lot_scrap/tests/test_stock_lot_scrap.py
@@ -3,31 +3,34 @@
 # © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.exceptions import Warning as UserError
-from openerp.tests.common import TransactionCase
+from openerp.exceptions import UserError
+from openerp.tests import common
 
 
-class TestStockLotScrap(TransactionCase):
-    def setUp(self):
-        super(TestStockLotScrap, self).setUp()
-        self.product = self.env['product.product'].create({
+@common.at_install(False)
+@common.post_install(True)
+class TestStockLotScrap(common.SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestStockLotScrap, cls).setUpClass()
+        cls.product = cls.env['product.product'].create({
             'name': 'Test product',
         })
-        self.lot010 = self.env['stock.production.lot'].create({
+        cls.lot010 = cls.env['stock.production.lot'].create({
             'name': "0000010",
-            'product_id': self.product.id
+            'product_id': cls.product.id
         })
-        self.quant1 = self.env['stock.quant'].create({
+        cls.quant1 = cls.env['stock.quant'].create({
             'qty': 5000.0,
-            'location_id': self.env.ref('stock.stock_location_stock').id,
-            'product_id': self.product.id,
-            'lot_id': self.lot010.id,
+            'location_id': cls.env.ref('stock.stock_location_stock').id,
+            'product_id': cls.product.id,
+            'lot_id': cls.lot010.id,
         })
-        self.quant2 = self.env['stock.quant'].create({
+        cls.quant2 = cls.env['stock.quant'].create({
             'qty': 325.0,
-            'location_id': self.env.ref('stock.stock_location_stock').id,
-            'product_id': self.product.id,
-            'lot_id': self.lot010.id,
+            'location_id': cls.env.ref('stock.stock_location_stock').id,
+            'product_id': cls.product.id,
+            'lot_id': cls.lot010.id,
         })
 
     def test_picking_created(self):
@@ -36,18 +39,17 @@ class TestStockLotScrap(TransactionCase):
         picking = self.env['stock.picking'].browse(res['res_id'])
         self.assertEqual(len(picking.move_lines), 2)
         self.assertAlmostEqual(
-            sum(picking.move_lines.mapped('product_uos_qty')), 5325)
+            sum(picking.move_lines.mapped('product_qty')), 5325)
         self.assertTrue(
             all(picking.move_lines.mapped(lambda x: x.state == 'assigned')))
 
     def test_warning(self):
         product_new = self.product.create({
-            'name': 'Test product 040'
+            'name': 'Test product 040',
         })
-        self.lot040 = self.env['stock.production.lot'].create(
-            {
-                'name': "0000040",
-                'product_id': product_new.id
-            })
+        self.lot040 = self.env['stock.production.lot'].create({
+            'name': "0000040",
+            'product_id': product_new.id,
+        })
         with self.assertRaises(UserError):
             self.lot040.action_scrap_lot()
