e7cf4df403a6be7848fa3d70dda26f9a38924547
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-12-06 20:38:13 +0100

    [MIG] stock_lot_scrap: Migration to 9.0

--- stock_lot_scrap/models/stock_scrap_lot.py
+++ stock_lot_scrap/models/stock_scrap_lot.py
@@ -3,7 +3,6 @@
 # © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-
 from openerp import api, models, _
 from openerp.exceptions import Warning as UserError
 from lxml import etree
@@ -14,7 +13,7 @@ class StockProductionLot(models.Model):
 
     @api.model
     def fields_view_get(self, view_id=None, view_type='form', toolbar=False,
-                        submenu=False):
+                        submenu=False):  # pragma: no cover
         """Inject the button here to avoid conflicts with other modules
          that add a header element in the main view.
         """
@@ -41,11 +40,16 @@ class StockProductionLot(models.Model):
         return res
 
     @api.multi
-    def _prepare_picking_vals(self, warehouse):
+    def _prepare_picking_vals(self, quant, scrap_location_id):
+        warehouse = (
+            quant.history_ids[-1:].picking_id.picking_type_id.warehouse_id
+        )
         return {
             'origin': _('Lot: %s') % self.name,
             'picking_type_id': warehouse.int_type_id.id or self.env.ref(
                 'stock.picking_type_internal').id,
+            'location_id': quant.location_id.id,
+            'location_dest_id': scrap_location_id,
         }
 
     @api.multi
@@ -55,10 +59,8 @@ class StockProductionLot(models.Model):
         product = quant.product_id
         res = move_obj.onchange_product_id(
             prod_id=product.id, loc_id=quant.location_id.id,
-            loc_dest_id=scrap_location_id)['value']
-        res.update(move_obj.onchange_quantity(
-            product.id, quant.qty, res['product_uom'], res['product_uos']
-        )['value'])
+            loc_dest_id=scrap_location_id,
+        )['value']
         res.update({
             'product_id': product.id,
             'product_uom_qty': quant.qty,
@@ -71,32 +73,34 @@ class StockProductionLot(models.Model):
     def action_scrap_lot(self):
         self.ensure_one()
         quants = self.quant_ids.filtered(
-            lambda x: x.location_id.usage == 'internal')
+            lambda x: x.location_id.usage == 'internal',
+        )
         if not quants:
-            raise UserError(_("This lot doesn't contain any quant in internal "
-                            "location."))
+            raise UserError(
+                _("This lot doesn't contain any quant in internal location."),
+            )
         move_obj = self.env['stock.move']
         picking_obj = self.env['stock.picking']
         pickings = picking_obj.browse()
         scrap_location_id = self.env.ref('stock.stock_location_scrapped').id
-        warehouse_ids = []
+        location_ids = []
         for quant in quants.sorted(key=lambda x: x.history_ids[-1:].
                                    picking_id.picking_type_id.warehouse_id.id):
-            warehouse = quant.history_ids[-1:].picking_id.picking_type_id.\
-                warehouse_id
-            if warehouse.id not in warehouse_ids:
-                warehouse_ids.append(warehouse.id)
-                picking = picking_obj.create(self._prepare_picking_vals(
-                    warehouse))
+            if quant.location_id.id not in location_ids:
+                location_ids.append(quant.location_id.id)
+                picking = picking_obj.create(
+                    self._prepare_picking_vals(quant, scrap_location_id),
+                )
                 pickings |= picking
-            move = move_obj.create(self._prepare_move_vals(
-                picking, quant, scrap_location_id))
+            move = move_obj.create(
+                self._prepare_move_vals(picking, quant, scrap_location_id),
+            )
             quant.reservation_id = move.id
             move.action_confirm()
             move.action_assign()
         result = self.env.ref('stock.action_picking_tree').read()[0]
         result['context'] = self.env.context
-        if len(pickings) != 1:
+        if len(pickings) != 1:  # pragma: no cover
             result['domain'] = "[('id', 'in', %s)]" % pickings.ids
         else:
             res = self.env.ref('stock.view_picking_form', False)
