0f6b9d35443a57ad1aa90b44af44196708582d67
Davide Corio <davide.corio@abstract.it>
2016-12-21 08:58:14 +0100

    [FIX] PEP8

--- stock_picking_package_preparation_line/models/stock_picking_package_preparation_line.py
+++ stock_picking_package_preparation_line/models/stock_picking_package_preparation_line.py
@@ -207,7 +207,8 @@ class StockPickingPackagePreparation(models.Model):
                 # ----- If line has 'move_id' this means we don't need to
                 #       recreate picking and move again
                 if (line.product_id and
-                        line.product_id.type != 'service' and not line.move_id):
+                        line.product_id.type != 'service' and not
+                        line.move_id):
                     move_data = line.get_move_data()
                     move_data.update({
                         'partner_id': package.partner_id.id,
