606a649a45d047ca65f719707c6151ae606cd9a9
agaldona <ainaragaldona@avanzosc.es>
2016-12-28 13:29:28 +0100

    [IMP] stock_lock_lot, mrp_lock_lot: allow to move locked lot in specific locations

--- stock_lock_lot/models/__init__.py
+++ stock_lock_lot/models/__init__.py
@@ -7,3 +7,6 @@ from . import product_category
 from . import stock_production_lot
 from . import stock_quant
 from . import res_config
+from . import stock_location
+from . import stock_move
+from . import stock_inventory_line
