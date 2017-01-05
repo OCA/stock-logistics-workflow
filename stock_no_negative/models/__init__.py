48f5d130d5562a10addfe1bbbb576414efa2bfcf
darshan-serpent <darshan.patel.serpentcs@gmail.com>
2017-01-05 16:59:32 +0530

    [ADD] test cases

--- stock_no_negative/models/__init__.py
+++ stock_no_negative/models/__init__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# © 2015-2016 Akretion (http://www.akretion.com)
+# @author Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import product
 from . import stock_quant
