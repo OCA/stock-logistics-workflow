48321dfb38fdad333446e983829c4de040c1b4e8
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-01-15 11:33:12 +0100

    [ADD] stock_picking_transfer_lot_autoassign (#289)
    ===================================
    Auto-assignment of lots on pickings
    ===================================
    
    When working with lots, it's very uncomfortable to introduce the quantity,
    lot by lot, when transferring pickings from your warehouse (outgoing or
    internal).
    
    This module automatically assigns the reserved quantity as the done one, so
    that you only have to change it in case of divergence, but having the
    possibility of transferring directly.
    
    Configuration
    =============
    
    * Make sure you have selected the proper removal strategy on your product
      categories.
    * Configure the product on the page "Inventory", field "Tracking" with one of
      these values: "By Unique Serial Number" or "By Lots".
    
    Usage
    =====
    
    * Create an outgoing or an internal picking.
    * Include one product with lots and with enough stock.
    * Click on "Mark as Todo" button, and then on "Reserve".
    * Clicking on the icon with the three items bullet list on the "Operations"
      tab you will see that the quantities have been auto-assigned on the "Done"
      column.

---
+++ stock_picking_transfer_lot_autoassign/tests/test_stock_picking_transfer_lot_autoassign.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp.tests import common
+
+
+@common.at_install(False)
+@common.post_install(True)
+class TestStockPickingTransferLotAutoAssign(common.SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestStockPickingTransferLotAutoAssign, cls).setUpClass()
+        cls.partner = cls.env['res.partner'].create({'name': 'Test partner'})
+        cls.warehouse = cls.env['stock.warehouse'].search([], limit=1)
+        cls.picking_type = cls.env['stock.picking.type'].search([
+            ('warehouse_id', '=', cls.warehouse.id),
+            ('code', '=', 'outgoing'),
+        ], limit=1)
+        cls.picking = cls.env['stock.picking'].create({
+            'partner_id': cls.partner.id,
+            'picking_type_id': cls.picking_type.id,
+            'location_id': cls.picking_type.default_location_src_id.id,
+            'location_dest_id': cls.partner.property_stock_customer.id,
+        })
+        cls.Move = cls.env['stock.move']
+        cls.product = cls.env['product.product'].create({
+            'name': 'Test product',
+            'type': 'product',
+            'tracking': 'lot',
+        })
+        cls.lot1 = cls.env['stock.production.lot'].create({
+            'product_id': cls.product.id,
+            'name': 'Lot 1',
+        })
+        cls.quant1 = cls.env['stock.quant'].create({
+            'product_id': cls.product.id,
+            'location_id': cls.picking.location_id.id,
+            'qty': 6,
+            'lot_id': cls.lot1.id,
+        })
+        cls.lot2 = cls.env['stock.production.lot'].create({
+            'product_id': cls.product.id,
+            'name': 'Lot 2',
+        })
+        cls.quant2 = cls.env['stock.quant'].create({
+            'product_id': cls.product.id,
+            'location_id': cls.picking.location_id.id,
+            'qty': 10,
+            'lot_id': cls.lot2.id,
+        })
+        move_vals = cls.Move.onchange_product_id(
+            prod_id=cls.product.id,
+            loc_id=cls.picking.location_id.id,
+            loc_dest_id=cls.picking.location_dest_id.id,
+            partner_id=cls.picking.partner_id.id,
+        ).get('value', {})
+        move_vals.update({
+            'product_id': cls.product.id,
+            'picking_id': cls.picking.id,
+            'product_uom_qty': 10,
+        })
+        cls.move = cls.Move.create(move_vals)
+
+    def test_transfer(self):
+        self.picking.action_confirm()
+        self.picking.action_assign()
+        pack_ops = self.picking.pack_operation_ids
+        self.assertEqual(len(pack_ops), 1)
+        self.assertEqual(len(pack_ops.pack_lot_ids), 2)
+        self.assertEqual(pack_ops.pack_lot_ids[0].qty, 6)
+        self.assertEqual(pack_ops.pack_lot_ids[1].qty, 4)
+        self.assertEqual(pack_ops.qty_done, 10)
