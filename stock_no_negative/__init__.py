0822898478acf48953860f84e8b779e2866e22e5
sebalix <seb@usr-src.org>
2017-02-02 14:01:01 +0100

    [IMP] stock_no_negative : data migration 8.0 -> 9.0

--- stock_no_negative/__init__.py
+++ stock_no_negative/__init__.py
@@ -4,3 +4,25 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import models
+
+
+def pre_init_hook(cr):
+    query = """
+        SELECT column_name
+        FROM information_schema.columns
+        WHERE table_name='product_template'
+        AND column_name='check_no_negative';
+    """
+    cr.execute(query)
+    if cr.fetchall():
+        migrate_from_v8(cr)
+
+
+def migrate_from_v8(cr):
+    """Migration from the v8 version.
+    The column has a different name.
+    """
+    cr.execute("ALTER TABLE product_template "
+               "RENAME COLUMN check_no_negative TO allow_negative_stock")
+    cr.execute("UPDATE product_template "
+               "SET allow_negative_stock = NOT allow_negative_stock")
