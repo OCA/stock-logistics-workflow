32500f3361ebcae1c6eadb65d03653549ca77047
Sergio Teruel Albert <sergio.teruel@tecnativa.com>
2017-02-15 22:18:35 +0100

    [9.0][IMP] stock_deposit: Assign owner for reserved quants

--- stock_deposit/models/stock_quant.py
+++ stock_deposit/models/stock_quant.py
@@ -22,3 +22,21 @@ class StockQuant(models.Model):
             domain = self._block_deposit_quants(domain)
         return super(StockQuant, self).quants_get(
             qty, move, ops, domain, removal_strategy)
+
+    @api.multi
+    def write(self, vals):
+        if 'reservation_id' in vals:
+            for quant in self:
+                stock_move = self.env['stock.move'].browse(
+                    vals['reservation_id'])
+                if stock_move and stock_move.picking_id.owner_id:
+                    vals['owner_id'] = stock_move.picking_id.owner_id.id
+                elif quant.owner_id:
+                    location_id = vals.get('location_id', False)
+                    if not self.env['stock.location'].browse(
+                            location_id).deposit_location:
+                        # Remove owner only if the location to write is not a
+                        # deposit location
+                        vals['owner_id'] = False
+        res = super(StockQuant, self).write(vals)
+        return res
