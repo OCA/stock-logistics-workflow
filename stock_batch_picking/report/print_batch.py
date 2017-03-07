ab185aee4095c778a62470c0bbea617c10bbd50f
Edi Piqoni <piqoni@gmail.com>
2017-03-07 18:10:42 +0100

    remove pooler, time

--- stock_batch_picking/report/print_batch.py
+++ stock_batch_picking/report/print_batch.py
@@ -2,8 +2,6 @@
 # © 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import time
-from odoo import pooler
 from odoo.report import report_sxw
 
 from . batch_aggregation import BatchAggregation
@@ -13,12 +11,8 @@ class PrintBatch(report_sxw.rml_parse):
 
     def __init__(self, cursor, uid, name, context):
         super(PrintBatch, self).__init__(cursor, uid, name, context=context)
-        self.pool = pooler.get_pool(self.cr.dbname)
-        self.cursor = self.cr
-        self.uid = uid
         self.numeration_type = False
         self.localcontext.update({
-            'time': time,
             'get_location_datas': self._get_location_datas,
         })
 
@@ -31,15 +25,13 @@ class PrintBatch(report_sxw.rml_parse):
 
     def set_context(self, objects, data, ids, report_type=None):
         new_objects = []
-        location_obj = self.pool.get('stock.location')
+        location_obj = self.localcontext['user'].env['stock.location']
         for batch in objects:
             pack_operations = {}
             for op in batch.pack_operation_ids:
                 id1, id2 = op.location_id.id, op.location_dest_id.id
                 key_dict = dict(
-                    location_obj.name_get(
-                        self.cursor, self.uid, [id1, id2]
-                    )
+                    location_obj.browse([id1, id2]).name_get()
                 )
 
                 key = key_dict[id1], key_dict[id2]
