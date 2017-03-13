cfc9b3b7b08162461f3d466266ab7e635c05a91e
Sergio Teruel Albert <sergio.teruel@tecnativa.com>
2017-03-14 00:40:14 +0100

    [9.0][ADD] stock_picking_sale_order_link: Allow navigate from picking to sale order

---
+++ stock_picking_sale_order_link/models/stock_picking.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2017 Sergio Teruel <sergio.teruel@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, models
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    @api.multi
+    def action_view_sale_order(self):
+        """This function returns an action that display existing sales order
+        of given picking.
+        """
+        self.ensure_one()
+        action = self.env.ref('sale.action_orders').read()[0]
+        form = self.env.ref('sale.view_order_form')
+        action['views'] = [(form.id, 'form')]
+        action['res_id'] = self.sale_id.id
+        return action
