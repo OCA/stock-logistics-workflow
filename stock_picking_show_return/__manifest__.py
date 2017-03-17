f0e57d4250aecfdf70e0901d23d4c523a40ed8b2
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-03-17 12:54:18 +0100

    [MIG] stock_picking_show_return: Migration to 10.0

--- stock_picking_show_return/__manifest__.py
+++ stock_picking_show_return/__manifest__.py
@@ -1,44 +1,20 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-# OpenERP, Open Source Management Solution
-# Copyright (c) 2013 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
-#                    Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
-# $Id$
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2014-2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Show returns on stock pickings",
-    "version": "1.0",
-    "author": "Serv. Tecnol. Avanzados - Pedro M. Baeza,"
+    "version": "10.0.1.0.0",
+    "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
     "category": "Warehouse Management",
-    "description": """
-    Show in pickings one tab that contains returns made for that picking.
-    """,
-    "website": "www.serviciosbaeza.com",
+    "website": "https://www.tecnativa.com",
     "license": "AGPL-3",
     "depends": [
         "stock",
     ],
-    "demo": [],
     "data": [
-        'stock_picking_view.xml',
+        'views/stock_picking_view.xml',
     ],
-    "installable": False,
-    "active": False,
+    "installable": True,
 }
