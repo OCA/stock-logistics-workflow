5ff983329ca46d1999e1191f2628af80958861cd
fmdl <florent.mirieu@gmail.com>
2017-03-18 13:34:54 +0100

    Solve conflict

--- stock_picking_show_return/models/stock_picking.py
+++ stock_picking_show_return/models/stock_picking.py
@@ -1,80 +1,19 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-# OpenERP, Open Source Management Solution
-# Copyright (c) 2013 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
-#                    Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-from openerp.osv import fields, orm
+# Copyright 2014-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, fields, models
 
 
-class stock_picking(orm.Model):
+class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    def _get_return_ids(self, cr, uid, ids, name, arg, context=None):
-        result = {}
-        for picking in self.browse(cr, uid, ids, context=context):
-            for line in picking.move_lines:
-                return_ids = []
-                if line.state == 'done':
-                    for rec in line.move_history_ids2:
-                        if (
-                            rec.location_dest_id.id == line.location_id.id and
-                            rec.location_id.id == line.location_dest_id.id
-                        ):
-                            return_ids.append(rec.picking_id.id)
-                result[picking.id] = return_ids
-        return result
+    returned_ids = fields.Many2many(
+        comodel_name="stock.picking", compute="_compute_returned_ids",
+        string="Returned pickings")
 
-    _columns = {
-        'return_ids': fields.function(_get_return_ids,
-                                      relation='stock.picking',
-                                      string="Return pickings",
-                                      type='many2many'),
-    }
-
-
-class stock_picking_out(orm.Model):
-    _inherit = 'stock.picking.out'
-
-    def _get_return_ids(self, cr, uid, ids, name, arg, context=None):
-        return super(stock_picking_out, self)._get_return_ids(cr, uid, ids,
-                                                              name, arg,
-                                                              context=context)
-
-    _columns = {
-        'return_ids': fields.function(_get_return_ids,
-                                      relation='stock.picking',
-                                      string="Return pickings",
-                                      type='many2many'),
-    }
-
-
-class stock_picking_in(orm.Model):
-    _inherit = 'stock.picking.in'
-
-    def _get_return_ids(self, cr, uid, ids, name, arg, context=None):
-        return super(stock_picking_out, self)._get_return_ids(cr, uid, ids,
-                                                              name, arg,
-                                                              context=context)
-
-    _columns = {
-        'return_ids': fields.function(_get_return_ids,
-                                      relation='stock.picking',
-                                      string="Return pickings",
-                                      type='many2many'),
-    }
+    @api.multi
+    def _compute_returned_ids(self):
+        for picking in self:
+            picking.returned_ids = self.mapped(
+                'move_lines.returned_move_ids.picking_id')
