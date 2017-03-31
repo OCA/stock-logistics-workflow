930d4e4e6e87b4b8170b4dd580c51b33e8d20c30
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-03-31 16:24:14 -0300

    FIX link invoice view on pickings

--- stock_picking_invoice_link/__manifest__.py
+++ stock_picking_invoice_link/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Stock Picking Invoice Link',
-    'version': '9.0.1.0.0',
+    'version': '9.0.1.1.0',
     'category': 'Warehouse Management',
     'summary': 'Adds link between pickings and invoices',
     'author': 'Agile Business Group, '
