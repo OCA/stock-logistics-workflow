c20a2799a9b9d682495527b272da9fb544c8c83d
eLBati <lorenzo.battistini@agilebg.com>
2017-04-10 15:49:50 +0200

    [MIG] stock_picking_invoice_link: Migration to 10.0
    avoiding 'product.service_delivery' because not useful in tests and correctly handled as service by 10.0 tests

--- stock_picking_invoice_link/models/account_invoice.py
+++ stock_picking_invoice_link/models/account_invoice.py
@@ -5,7 +5,7 @@
 # © 2017 Jacques-Etienne Baudoux <je@bcim.be>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class AccountInvoice(models.Model):
