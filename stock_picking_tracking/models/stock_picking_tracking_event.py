78b9843f8d1e628df0a308937628431d617e8134
Dave Lasley <dave@laslabs.com>
2017-05-29 17:32:01 -0700

    [ADD][9.0] Create new module stock_picking_tracking (#239)

---
+++ stock_picking_tracking/models/stock_picking_tracking_event.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from openerp import models, fields, api
+
+
+class StockPickingTrackingEvent(models.Model):
+    _name = 'stock.picking.tracking.event'
+    _description = 'Stock Picking Tracking Event'
+    _order = "date_created desc"
+
+    group_id = fields.Many2one(
+        string='Tracking Group',
+        comodel_name='stock.picking.tracking.group',
+        required=True,
+    )
+    state = fields.Selection(
+        [
+            ('unknown', 'Unknown'),
+            ('pre_transit', 'Pre Transit'),
+            ('in_transit', 'In Transit'),
+            ('out_for_delivery', 'Out For Delivery'),
+            ('delivered', 'Delivered'),
+            ('available_for_pickup', 'Available For Pickup'),
+            ('return_to_sender', 'Return To Sender'),
+            ('fail', 'Failure'),
+            ('cancel', 'Cancelled'),
+            ('error', 'Error'),
+        ],
+        required=True,
+        default='unknown',
+        help="Status of the dispatch at time of event",
+    )
+    date_created = fields.Datetime(
+        string='Date',
+        readonly=True,
+        default=lambda s: fields.Datetime.now(),
+    )
+    location_id = fields.Many2one(
+        string='Location',
+        comodel_name='stock.picking.tracking.location',
+        required=True,
+    )
+    message = fields.Char(
+        required=True,
+        help="Description of the tracking event",
+    )
+    source = fields.Char(
+        help="Original source of the tracking event",
+    )
+
+    @api.multi
+    def name_get(self):
+        res = []
+        for rec_id in self:
+            name = '[{date}] {location} - {state}'.format(
+                date=rec_id.date_created,
+                location=rec_id.location_id.display_name,
+                state=rec_id.state.capitalize(),
+            )
+            res.append((rec_id.id, name))
+        return res
---
+++ stock_picking_tracking/tests/test_stock_picking_tracking_event.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from .common import TestHelper
+
+
+class TestStockPickingTrackingEvent(TestHelper):
+
+    def test_name_get(self):
+        """ It should return proper display_name and syntax """
+        rec_id = self.new_event()
+        expect = '[{date}] {location} - {state}'.format(
+            date=rec_id.date_created,
+            location=rec_id.location_id.display_name,
+            state=rec_id.state.capitalize(),
+        )
+        self.assertEqual(
+            expect, rec_id.name_get()[0][1],
+            'Did not get name w/ state. Expect %s, Got %s' % (
+                expect, rec_id.name_get()[0][1],
+            )
+        )
