6e646f7a443933901b5e5366ec483efd28f8db5c
sebalix <seb@usr-src.org>
2017-06-07 17:56:09 +0200

    [IMP] stock_picking_invoice_link: add a button on stock picking form to open related invoices

--- stock_picking_invoice_link/models/stock_picking.py
+++ stock_picking_invoice_link/models/stock_picking.py
@@ -17,9 +17,35 @@ class StockPicking(models.Model):
     invoice_id = fields.Many2one(
         comodel_name='account.invoice', string='Invoice',
         compute="_compute_invoice_id")
+    invoice_exists = fields.Boolean(
+        u"Invoiced", compute='_compute_invoice_exists')
 
     @api.multi
     @api.depends('invoice_ids')
     def _compute_invoice_id(self):
         for picking in self:
             picking.invoice_id = picking.invoice_ids[:1]
+
+    @api.multi
+    @api.depends('invoice_ids')
+    def _compute_invoice_exists(self):
+        for picking in self:
+            picking.invoice_exists = bool(picking.invoice_ids)
+
+    @api.multi
+    def action_view_invoice(self):
+        """This function returns an action that display existing invoices
+        of given stock pickings.
+        It can either be a in a list or in a form view, if there is only
+        one invoice to show.
+        """
+        self.ensure_one()
+        action = self.env.ref('account.action_invoice_tree1')
+        result = action.read()[0]
+        if len(self.invoice_ids) > 1:
+            result['domain'] = "[('id', 'in', %s)]" % self.invoice_ids.ids
+        else:
+            form_view = self.env.ref('account.invoice_form')
+            result['views'] = [(form_view.id, 'form')]
+            result['res_id'] = self.invoice_ids.id
+        return result
