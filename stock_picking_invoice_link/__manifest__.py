f2e2fdc48ab26aa510c603bf6526e4f83beed4a5
Jacques-Etienne Baudoux <je.baudoux@gmail.com>
2017-06-15 12:59:49 +0200

    [9.0] stock_picking_invoice_link: redo migration from 8.0 to 9.0 (#296)
    Previous migration did not keep the business functionality of the previous module. Link from invoice to picking must link only related delivery and not all deliveries in case of partial delivery

--- stock_picking_invoice_link/__manifest__.py
+++ stock_picking_invoice_link/__manifest__.py
@@ -2,15 +2,17 @@
 # © 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
 # © 2016 AvanzOSC (<http://www.avanzosc.es>)
 # © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# © 2017 Jacques-Etienne Baudoux <je@bcim.be>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     'name': 'Stock Picking Invoice Link',
-    'version': '9.0.1.1.0',
+    'version': '9.0.2.0.0',
     'category': 'Warehouse Management',
     'summary': 'Adds link between pickings and invoices',
     'author': 'Agile Business Group, '
               'Tecnativa, '
+              'BCIM sprl, '
               'Odoo Community Association (OCA)',
     'website': 'http://www.agilebg.com',
     'license': 'AGPL-3',
