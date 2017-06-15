32e9b18b7ec69f2d16f20562d1ec4266a422f670
Jacques-Etienne Baudoux <je.baudoux@gmail.com>
2017-06-15 12:59:49 +0200

    [9.0] stock_picking_invoice_link: redo migration from 8.0 to 9.0 (#296)
    Previous migration did not keep the business functionality of the previous module. Link from invoice to picking must link only related delivery and not all deliveries in case of partial delivery

--- stock_picking_invoice_link/models/stock_picking.py
+++ stock_picking_invoice_link/models/stock_picking.py
@@ -2,6 +2,7 @@
 # © 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
 # © 2015-2016 AvanzOSC
 # © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# © 2017 Jacques-Etienne Baudoux <je@bcim.be>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from openerp import api, fields, models
@@ -11,13 +12,15 @@ class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     invoice_ids = fields.Many2many(
-        comodel_name='account.invoice', string='Invoices',
-        compute="_compute_invoice_ids")
+        comodel_name='account.invoice', copy=False, string='Invoices',
+        readonly=True)
+    # Provide this field for backwards compatibility
+    invoice_id = fields.Many2one(
+        comodel_name='account.invoice', string='Invoice',
+        compute="_compute_invoice_id")
 
     @api.multi
-    def _compute_invoice_ids(self):
+    @api.depends('invoice_ids')
+    def _compute_invoice_id(self):
         for picking in self:
-            invoices = self.env['account.invoice']
-            for line in picking.move_lines:
-                invoices |= line.invoice_line_ids.mapped('invoice_id')
-            picking.invoice_ids = invoices
+            picking.invoice_id = picking.invoice_ids[:1]
