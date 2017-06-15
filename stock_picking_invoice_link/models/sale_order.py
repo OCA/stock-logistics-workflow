f2e6b8d4406d7d9d7eb0d7c86e0a16b00cb388fe
Jacques-Etienne Baudoux <je.baudoux@gmail.com>
2017-06-15 12:59:49 +0200

    [9.0] stock_picking_invoice_link: redo migration from 8.0 to 9.0 (#296)
    Previous migration did not keep the business functionality of the previous module. Link from invoice to picking must link only related delivery and not all deliveries in case of partial delivery

--- stock_picking_invoice_link/models/sale_order.py
+++ stock_picking_invoice_link/models/sale_order.py
@@ -1,45 +1,34 @@
-# Copyright 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
-# Copyright 2017 Jacques-Etienne Baudoux <je@bcim.be>
-# Copyright 2021 Tecnativa - João Marques
-# Copyright 2022 Antony Herrera - LooErp
+# -*- coding: utf-8 -*-
+# © 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
+# © 2017 Jacques-Etienne Baudoux <je@bcim.be>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import models
-from odoo.tools import float_compare
+
+from openerp import api, models
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    def get_stock_moves_link_invoice(self):
-        return self.mapped("move_ids").filtered(
-            lambda mv: (
-                mv.state == "done"
-                and not (
-                    any(
-                        inv.state != "cancel"
-                        for inv in mv.invoice_line_ids.mapped("move_id")
-                    )
-                )
-                and not mv.scrapped
-                and (
-                    mv.location_dest_id.usage == "customer"
-                    or (mv.location_id.usage == "customer" and mv.to_refund)
-                )
-            )
-        )
+    @api.multi
+    def invoice_line_create(self, invoice_id, qty):
+        self.mapped('procurement_ids') \
+            .mapped('move_ids') \
+            .filtered(
+                lambda x: x.state == 'done' and
+                not x.invoice_line_id and
+                not x.location_dest_id.scrap_location and
+                x.location_dest_id.usage == 'customer') \
+            .mapped('picking_id') \
+            .write({'invoice_ids': [(4, invoice_id)]})
+        return super(SaleOrderLine, self).invoice_line_create(invoice_id, qty)
 
-    def _prepare_invoice_line(self, **optional_values):
-        vals = super()._prepare_invoice_line(**optional_values)
-        stock_moves = self.get_stock_moves_link_invoice()
-        # Invoice returned moves marked as to_refund
-        if (
-            float_compare(
-                self.qty_to_invoice, 0.0, precision_rounding=self.currency_id.rounding
-            )
-            < 0
-        ):
-            stock_moves = stock_moves.filtered(
-                lambda m: m.to_refund and not m.invoice_line_ids
-            )
-        vals["move_line_ids"] = [(4, m.id) for m in stock_moves]
+    @api.multi
+    def _prepare_invoice_line(self, qty):
+        vals = super(SaleOrderLine, self)._prepare_invoice_line(qty)
+        move_ids = self.mapped('procurement_ids').mapped('move_ids').filtered(
+            lambda x: x.state == 'done' and
+            not x.invoice_line_id and
+            not x.location_dest_id.scrap_location and
+            x.location_dest_id.usage == 'customer').ids
+        vals['move_line_ids'] = [(6, 0, move_ids)]
         return vals
