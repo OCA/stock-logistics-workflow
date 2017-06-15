b9b546e91db6dcd3c36272d8283cab7797fac734
Jacques-Etienne Baudoux <je.baudoux@gmail.com>
2017-06-15 12:59:49 +0200

    [9.0] stock_picking_invoice_link: redo migration from 8.0 to 9.0 (#296)
    Previous migration did not keep the business functionality of the previous module. Link from invoice to picking must link only related delivery and not all deliveries in case of partial delivery

--- stock_picking_invoice_link/tests/test_stock_picking_invoice_link.py
+++ stock_picking_invoice_link/tests/test_stock_picking_invoice_link.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 # © 2016 Oihane Crucelaegui - AvanzOSC
 # © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# © 2017 Jacques-Etienne Baudoux <je@bcim.be>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from openerp.addons.sale.tests.test_sale_common import TestSale
 
 
 class TestStockPickingInvoiceLink(TestSale):
-    def test_00_sale_stock_invoice(self):
+    def test_00_sale_stock_invoice_link(self):
+        inv_obj = self.env['account.invoice']
         self.so = self.env['sale.order'].create({
             'partner_id': self.partner.id,
             'partner_invoice_id': self.partner.id,
@@ -16,9 +18,74 @@ class TestStockPickingInvoiceLink(TestSale):
                 'name': p.name, 'product_id': p.id, 'product_uom_qty': 2,
                 'product_uom': p.uom_id.id, 'price_unit': p.list_price
             }) for (_, p) in self.products.iteritems()],
+            'pricelist_id': self.env.ref('product.list0').id,
+            'picking_policy': 'direct',
         })
         self.so.action_confirm()
+        self.assertTrue(self.so.picking_ids,
+                        'Sale Stock: no picking created for '
+                        '"invoice on delivery" stockable products')
+
+        # invoice on order
         self.so.action_invoice_create()
-        self.assertEqual(len(self.so.invoice_ids.picking_ids), 1)
-        self.assertEqual(self.so.picking_ids, self.so.invoice_ids.picking_ids)
-        self.assertEqual(self.so.invoice_ids, self.so.picking_ids.invoice_ids)
+
+        # deliver partially
+        self.assertEqual(self.so.invoice_status, 'no',
+                         'Sale Stock: so invoice_status should be '
+                         '"nothing to invoice" after invoicing')
+        pick_1 = self.so.picking_ids.filtered(
+            lambda x: x.picking_type_code == 'outgoing' and
+            x.state in ('confirmed', 'partially_available'))
+        pick_1.force_assign()
+        pick_1.pack_operation_product_ids.write({'qty_done': 1})
+        wiz_act = pick_1.do_new_transfer()
+        wiz = self.env[wiz_act['res_model']].browse(wiz_act['res_id'])
+        wiz.process()
+        self.assertEqual(self.so.invoice_status, 'to invoice',
+                         'Sale Stock: so invoice_status should be '
+                         '"to invoice" after partial delivery')
+        inv_id = self.so.action_invoice_create()
+        inv_1 = inv_obj.browse(inv_id)
+
+        # complete the delivery
+        self.assertEqual(self.so.invoice_status, 'no',
+                         'Sale Stock: so invoice_status should be '
+                         '"nothing to invoice" after partial delivery '
+                         'and invoicing')
+        self.assertEqual(len(self.so.picking_ids), 2,
+                         'Sale Stock: number of pickings should be 2')
+        pick_2 = self.so.picking_ids.filtered(
+            lambda x: x.picking_type_code == 'outgoing' and
+            x.state in ('confirmed', 'partially_available'))
+        pick_2.force_assign()
+        pick_2.pack_operation_product_ids.write({'qty_done': 1})
+        self.assertIsNone(pick_2.do_new_transfer(),
+                          'Sale Stock: second picking should be '
+                          'final without need for a backorder')
+        self.assertEqual(self.so.invoice_status, 'to invoice',
+                         'Sale Stock: so invoice_status should be '
+                         '"to invoice" after complete delivery')
+        inv_id = self.so.action_invoice_create()
+        inv_2 = inv_obj.browse(inv_id)
+        self.assertEqual(self.so.invoice_status, 'invoiced',
+                         'Sale Stock: so invoice_status should be '
+                         '"fully invoiced" after complete delivery and '
+                         'invoicing')
+
+        # Check links
+        self.assertEqual(
+            inv_1.picking_ids, pick_1,
+            "Invoice 1 must link to only First Partial Delivery")
+        self.assertEqual(
+            inv_1.invoice_line_ids.mapped('move_line_ids'),
+            pick_1.move_lines.filtered(
+                lambda x: x.product_id.invoice_policy == "delivery"),
+            "Invoice 1 lines must link to only First Partial Delivery lines")
+        self.assertEqual(
+            inv_2.picking_ids, pick_2,
+            "Invoice 2 must link to only Second Delivery")
+        self.assertEqual(
+            inv_2.invoice_line_ids.mapped('move_line_ids'),
+            pick_2.move_lines.filtered(
+                lambda x: x.product_id.invoice_policy == "delivery"),
+            "Invoice 2 lines must link to only Second Delivery lines")
