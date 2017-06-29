8e30e37fc1d776b933ade6c77c6b1e100185e482
Darshan Patel <darshan.patel.serpentcs@gmail.com>
2017-06-29 18:30:59 +0530

    stock_picking_line_sequence v10

--- stock_picking_line_sequence/__init__.py
+++ stock_picking_line_sequence/__init__.py
@@ -4,4 +4,5 @@
 # Copyright 2017 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from . import stock
+from . import models
+from .init_hooks import post_init_hook
