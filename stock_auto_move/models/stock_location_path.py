84e6bd14e144e97512ee03f48279edaa41c5920a
Zakaria Makrelouf <z.makrelouf@gmail.com>
2017-09-05 09:01:53 +0100

    [IMP]stock_auto_move: auto move from push rules

---
+++ stock_auto_move/models/stock_location_path.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2014-2015 NDP Systèmes (<http://www.ndp-systemes.fr>)
+
+from odoo import api, fields, models
+
+
+class StockLocationPath(models.Model):
+
+    _inherit = 'stock.location.path'
+
+    auto_move = fields.Boolean(
+        help="If this option is selected, the generated moves will be "
+             "automatically processed as soon as the products are available. "
+             "This can be useful for situations with chained moves where we "
+             "do not want an operator action."
+    )
+
+    @api.model
+    def _prepare_move_copy_values(self, move_to_copy, new_date):
+        new_move_vals = super(StockLocationPath, self)._prepare_move_copy_values(
+            move_to_copy=move_to_copy, new_date=new_date)
+        new_move_vals.update({'auto_move': self.auto_move})
+        return new_move_vals
