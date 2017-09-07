3fbd9858a7d029ef434df24f691a53c0432500a5
cubells <vicent@vcubells.net>
2017-09-07 17:43:55 +0200

    [IMP] stock_picking_invoice_link: Review changes

--- stock_picking_invoice_link/models/stock_picking.py
+++ stock_picking_invoice_link/models/stock_picking.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# © 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
-# © 2015-2016 AvanzOSC
-# © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
-# © 2017 Jacques-Etienne Baudoux <je@bcim.be>
+# Copyright 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
+# Copyright 2015-2016 AvanzOSC
+# Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2017 Jacques-Etienne Baudoux <je@bcim.be>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import api, fields, models
@@ -24,3 +24,21 @@ class StockPicking(models.Model):
     def _compute_invoice_id(self):
         for picking in self:
             picking.invoice_id = picking.invoice_ids[:1]
+
+    @api.multi
+    def action_view_invoice(self):
+        """This function returns an action that display existing invoices
+        of given stock pickings.
+        It can either be a in a list or in a form view, if there is only
+        one invoice to show.
+        """
+        self.ensure_one()
+        action = self.env.ref('account.action_invoice_tree1')
+        result = action.read()[0]
+        if len(self.invoice_ids) > 1:
+            result['domain'] = "[('id', 'in', %s)]" % self.invoice_ids.ids
+        else:
+            form_view = self.env.ref('account.invoice_form')
+            result['views'] = [(form_view.id, 'form')]
+            result['res_id'] = self.invoice_ids.id
+        return result
