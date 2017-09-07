de0a7a5a5425901c033f08e1803c2c077937212d
cubells <vicent@vcubells.net>
2017-09-07 17:43:55 +0200

    [IMP] stock_picking_invoice_link: Review changes

--- stock_picking_invoice_link/__manifest__.py
+++ stock_picking_invoice_link/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-# © 2013-17 Agile Business Group (<http://www.agilebg.com>)
-# © 2016 AvanzOSC (<http://www.avanzosc.es>)
-# © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
-# © 2017 Jacques-Etienne Baudoux <je@bcim.be>
+# Copyright 2013-17 Agile Business Group (<http://www.agilebg.com>)
+# Copyright 2016 AvanzOSC (<http://www.avanzosc.es>)
+# Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2017 Jacques-Etienne Baudoux <je@bcim.be>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     'name': 'Stock Picking Invoice Link',
-    'version': '10.0.0.2.0',
+    'version': '10.0.1.0.0',
     'category': 'Warehouse Management',
     'summary': 'Adds link between pickings and invoices',
     'author': 'Agile Business Group, '
