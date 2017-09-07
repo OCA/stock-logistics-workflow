bce8bc92a8991f660a0bf07fc66d800310fb5b58
cubells <vicent@vcubells.net>
2017-09-07 17:43:55 +0200

    [IMP] stock_picking_invoice_link: Review changes

--- stock_picking_invoice_link/models/account_invoice.py
+++ stock_picking_invoice_link/models/account_invoice.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# © 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
-# © 2015-2016 AvanzOSC
-# © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
-# © 2017 Jacques-Etienne Baudoux <je@bcim.be>
+# Copyright 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
+# Copyright 2015-2016 AvanzOSC
+# Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2017 Jacques-Etienne Baudoux <je@bcim.be>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import fields, models
--- stock_picking_invoice_link/models/stock_move.py
+++ stock_picking_invoice_link/models/stock_move.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# © 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
-# © 2015-2016 AvanzOSC
-# © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
+# Copyright 2015-2016 AvanzOSC
+# Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import api, fields, models
