21572ea52f0fde5ba8043295510a55a226c7df14
Cédric Pigeon <cedric.pigeon@acsone.eu>
2017-09-18 14:04:29 +0200

    [MIG] stock_pack_operation_auto_fill: Migration to 10.0

--- stock_pack_operation_auto_fill/__manifest__.py
+++ stock_pack_operation_auto_fill/__manifest__.py
@@ -6,7 +6,7 @@
     'name': 'Stock Pack Operation Auto Fill',
     'summary': """
         Stock pack operation auto fill""",
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
     'website': 'https://acsone.eu/',
