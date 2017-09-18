b672d81af5bfb778907d72296c15c168a744a05c
Cédric Pigeon <cedric.pigeon@acsone.eu>
2017-09-18 14:04:29 +0200

    [MIG] stock_pack_operation_auto_fill: Migration to 10.0

--- stock_pack_operation_auto_fill/models/stock_picking.py
+++ stock_pack_operation_auto_fill/models/stock_picking.py
@@ -2,8 +2,8 @@
 # Copyright 2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, fields, models, _
-from openerp.exceptions import UserError
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 
 class StockPicking(models.Model):
