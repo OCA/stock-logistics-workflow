4fae44d8b2da98052ccff819dd374a54526da256
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-10-09 15:08:50 +0530

    [MIG] product_customer_code_picking v10

--- product_customer_code_picking/__manifest__.py
+++ product_customer_code_picking/__manifest__.py
@@ -1,49 +1,23 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2013 Agile Business Group sagl (<http://www.agilebg.com>)
-#    Author: Nicola Malcontenti <nicola.malcontenti@agilebg.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2013-2017 Agile Business Group sagl
+#     (<http://www.agilebg.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product Customer code for stock picking",
-    "version": "1.0",
+    "version": "10.0.1.0.0",
     "author": "Agile Business Group,Odoo Community Association (OCA)",
     "website": "http://www.agilebg.com",
-    "license": 'AGPL-3',
     "category": "Stock",
+    "summary": """This module makes the product customer code visible in
+                      the stock moves of a picking.""",
+    "license": "AGPL-3",
     "depends": [
-        'base',
-        'product',
-        'stock',
-        'product_customer_code'
+        "product",
+        "stock",
+        "product_supplierinfo_for_customer"
     ],
-    "description": """
-This module makes the product customer code visible in the stock moves
-of a picking.
-
-Contributors
-------------
-* Nicola Malcontenti <nicola.malcontenti@agilebg.com>
-* Alex Comba <alex.comba@agilebg.com>
-* Lorenzo Battistini <lorenzo.battistini@agilebg.com>
-""",
-    "demo": [],
     "data": [
-        'stock_picking_view.xml',
+        "views/stock_picking_view.xml",
     ],
-    "installable": False,
+    "installable": True,
 }
