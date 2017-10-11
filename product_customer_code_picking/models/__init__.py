8e40c4e72969cdfec990967524e9320c54bb8b8a
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-10-11 15:21:44 +0530

    [IMP] Changed file name

--- product_customer_code_picking/models/__init__.py
+++ product_customer_code_picking/models/__init__.py
@@ -2,4 +2,4 @@
 # Copyright 2013-2017 Agile Business Group sagl
 #     (<http://www.agilebg.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from . import stock_picking
+from . import stock_move
