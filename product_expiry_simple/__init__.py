7f1d9ceee35d78dc1c8dd5cc58819c92d9a8417d
Alan Ramos <alan.ramos@jarsa.com.mx>
2018-01-04 20:14:08 -0600

    make changes to migrate module to V11

--- product_expiry_simple/__init__.py
+++ product_expiry_simple/__init__.py
@@ -1,3 +1,5 @@
-# -*- coding: utf-8 -*-
+# © 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# © 2018 Jarsa Sistemas (Alan Ramos <alan.ramos@jarsa.com.mx>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import models
