7092fc932f1c5edd4bb1a83c2a7e4d5c9ce3d926
Alan Ramos <alan.ramos@jarsa.com.mx>
2018-01-04 20:14:08 -0600

    make changes to migrate module to V11

--- product_expiry_simple/models/stock_production_lot.py
+++ product_expiry_simple/models/stock_production_lot.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 # © 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# © 2018 Jarsa Sistemas (Alan Ramos <alan.ramos@jarsa.com.mx>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields, api
@@ -7,16 +7,15 @@ from odoo import models, fields, api
 
 class StockProductionLot(models.Model):
     _inherit = 'stock.production.lot'
-    _rec_name = 'display_name'
 
     expiry_date = fields.Date(string='Expiry Date')
     display_name = fields.Char(
-        compute='compute_display_name_field',
+        compute='_compute_display_name',
         string='Lot/Serial Number Display', store=True, readonly=True)
 
     @api.multi
     @api.depends('name', 'expiry_date')
-    def compute_display_name_field(self):
+    def _compute_display_name(self):
         for lot in self:
             dname = lot.name
             if lot.expiry_date:
