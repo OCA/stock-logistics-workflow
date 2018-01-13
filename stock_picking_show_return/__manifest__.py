861face76fdb6855fe022d8d32052620913a03ba
Florent de Labarre <florent.mirieu@gmail.com>
2018-01-13 22:52:21 +0100

    [MIG] Migrated stock_picking_show_return to v11.0

--- stock_picking_show_return/__manifest__.py
+++ stock_picking_show_return/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014-2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Show returns on stock pickings",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
     "category": "Warehouse Management",
