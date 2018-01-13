e00f7ada5a4683a0cc19170a5028890893a52545
Florent de Labarre <florent.mirieu@gmail.com>
2018-01-14 00:09:59 +0100

    MIG V11

--- stock_picking_show_backorder/__manifest__.py
+++ stock_picking_show_backorder/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015-17 Eficent Business and IT Consulting Services, S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Stock Picking Show Backorder',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Warehouse Management',
     'summary': "Provides a new field on stock pickings, allowing to display "
                "the corresponding backorders.",
