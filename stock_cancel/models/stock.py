753937757c4d27f8f3eff3867ef3e53aa422f0b0
Cristian Salamea <ovnicraft@gmail.com>
2018-01-25 04:57:11 -0500

    [IMP] some restrictions
    backorder, splits, recompute ops

--- stock_cancel/models/stock.py
+++ stock_cancel/models/stock.py
@@ -23,7 +23,16 @@
 #
 ##############################################################################
 
-from odoo import models, api, exceptions, _
+from odoo import api, models, exceptions, _
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    def _check_restrictions(self):
+        # Restrictions before remove quants
+        if self.returned_move_ids or self.split_from:
+            raise exceptions.UserError(_('Action not allowed. Move splited / with returned moves.'))  # noqa
 
 
 class StockPicking(models.Model):
@@ -36,22 +45,36 @@ class StockPicking(models.Model):
             [('ref', '=', self.name)])
         return bool(account_moves)
 
+    def _check_restrictions(self):
+        # Only allow with incoming operations
+        # backorder_id in picking
+        # returned_move_ids in stock.move
+        # split_from in stock.move
+        if not self.picking_type_id.code == 'incoming':
+            raise exceptions.UserError(_('Allowed only with incoming operations.'))  # noqa
+        if self.backorder_id:
+            raise exceptions.UserError(_('Not Allowed, picking has backorder.'))  # noqa
+
     @api.multi
     def action_revert_done(self):
         for picking in self:
+            picking._check_restrictions()
             if picking.has_valuation_moves():
-                raise exceptions.Warning(
+                raise exceptions.UserError(
                     _('Picking %s has valuation moves: '
                         'remove them first.')
                     % (picking.name))
             if picking.invoice_id:
-                raise exceptions.Warning(
+                raise exceptions.UserError(
                     _('Picking %s has invoices!') % (picking.name))
             picking.move_lines.write({'state': 'draft'})
             # remove quants done
             for move in picking.move_lines:
+                move._check_restrictions()
                 move.quant_ids.with_context(force_unlink=True).unlink()
             picking.state = 'draft'
+            picking.action_confirm()
+            picking.do_prepare_partial()
             picking.message_post(
                 _("The picking has been re-opened and set to draft state"))
         return
