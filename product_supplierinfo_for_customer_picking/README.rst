8c726ec548934e0b3e739dd18ce2c757e30afabd
aheficent <ahenriquez@eficent.com>
2018-03-20 17:28:51 +0100

    [UPT] rename module and minor fixes

--- product_supplierinfo_for_customer_picking/README.rst
+++ product_supplierinfo_for_customer_picking/README.rst
@@ -2,9 +2,10 @@
    :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
    :alt: License: AGPL-3
 
-====================================
-Product Customer Code For Stock Move
-====================================
+=========================================
+Product Supplierinfo For Customer Picking
+=========================================
+
 Based on product_supplierinfo_for_customer, this module loads in every Stock move the
 customer code defined in the product.
 
@@ -32,6 +33,7 @@ Contributors
 * Alex Comba <alex.comba@agilebg.com>
 * Lorenzo Battistini <lorenzo.battistini@agilebg.com>
 * Serpent Consulting Services Pvt. Ltd. <support@serpentcs.com>
+* Aaron Henriquez <ahenriquez@eficent.com>
 
 Maintainer
 ----------
--- product_supplierinfo_for_customer_picking/__manifest__.py
+++ product_supplierinfo_for_customer_picking/__manifest__.py
@@ -3,13 +3,14 @@
 #     (<http://www.agilebg.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Product Customer code for stock picking",
+    "name": "Product Supplierinfo For Customer Picking",
     "version": "10.0.1.0.0",
-    "author": "Agile Business Group,Odoo Community Association (OCA)",
-    "website": "http://www.agilebg.com",
+    "author": "Agile Business Group, "
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
     "category": "Stock",
-    "summary": """This module makes the product customer code visible in
-                      the stock moves of a picking.""",
+    "summary": "This module makes the product customer code visible "
+               "in the stock moves of a picking.",
     "license": "AGPL-3",
     "depends": [
         "product",
