c1b1a41c0db905c99a10ff5937217d8835a1f8b4
celm1990 <celm1990@hotmail.com>
2018-03-23 13:00:38 -0500

    IMP: migrate module to V11 (migration done by @Softdil)

---
+++ stock_picking_invoice_link/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from . import account_invoice
+from . import stock_move
+from . import stock_picking
+from . import sale_order
