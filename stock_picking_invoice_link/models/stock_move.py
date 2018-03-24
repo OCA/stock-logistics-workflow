a9c9f52ec4c93654c9b0f0b4ae10672ab7957aca
celm1990 <celm1990@hotmail.com>
2018-03-24 13:38:41 -0500

    FIX: flake8 'odoo.api' imported but unused

--- stock_picking_invoice_link/models/stock_move.py
+++ stock_picking_invoice_link/models/stock_move.py
@@ -4,7 +4,7 @@
 # Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class StockMove(models.Model):
