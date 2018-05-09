88d0ec429d511adbf76013e0cc4248d500c84123
Alex Comba <alex.comba@agilebg.com>
2018-05-09 12:22:57 +0200

    [MIG] stock_move_backdating: Migration to 10.0

--- stock_move_backdating/models/stock_move.py
+++ stock_move_backdating/models/stock_move.py
@@ -2,10 +2,11 @@
 # Copyright 2015-2016 Agile Business Group (<http://www.agilebg.com>)
 # Copyright 2016 BREMSKERL-REIBBELAGWERKE EMMERLING GmbH & Co. KG
 #    Author Marco Dieckhoff
+# Copyright 2018 Alex Comba - Agile Business Group
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api
-from ..wizards.stock_transfer_details import check_date
+from odoo import api, models
+from ..models.stock_pack_operation import check_date
 
 
 class StockMove(models.Model):
@@ -19,14 +20,9 @@ class StockMove(models.Model):
         for move in self:
             if move.linked_move_operation_ids:
                 operation = move.linked_move_operation_ids[0]
-                move.date = operation.operation_id.date
-                if move.quant_ids:
-                    check_date(move.date)
-                    move.quant_ids.sudo().write({'in_date': move.date})
-        pickings = self.mapped('picking_id').filtered(
-            lambda r: r.state == 'done')
-        for picking in pickings:
-            # set date_done as the youngest date among the moves
-            dates = picking.mapped('move_lines.date')
-            picking.write({'date_done': max(dates)})
+                if operation.operation_id.date_backdating:
+                    move.date = operation.operation_id.date_backdating
+                    if move.quant_ids:
+                        check_date(move.date)
+                        move.quant_ids.sudo().write({'in_date': move.date})
         return result
