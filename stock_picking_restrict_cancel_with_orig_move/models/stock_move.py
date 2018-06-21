b4a97d8329c1edc2ebb02299c086aeb1cf9490ff
Akim Juillerat <akim.juillerat@camptocamp.com>
2018-06-21 15:31:13 +0200

    Fix restriction module:
    - Abstract blocking objects
    - Add glue module with MRP workorders
    - Add glue module with Purchase orders

--- stock_picking_restrict_cancel_with_orig_move/models/stock_move.py
+++ stock_picking_restrict_cancel_with_orig_move/models/stock_move.py
@@ -5,6 +5,11 @@ from odoo import models, _
 from odoo.exceptions import UserError
 
 
+def format_link(object):
+    return '<a href=# data-oe-model=%s data-oe-id=%d>%s</a>' % (
+        object._name, object.id, object.name)
+
+
 class StockMove(models.Model):
 
     _inherit = 'stock.move'
@@ -24,12 +29,33 @@ class StockMove(models.Model):
             return super(StockMove, self.with_context(
                 bypass_check_state=True))._action_cancel()
         else:
-            pickings = self.env['stock.picking']
-            for move in self:
-                for orig_stock_move in move.move_orig_ids:
-                    pickings |= orig_stock_move.picking_id
+            blocking_moves = self.get_blocking_moves(orig_moves)
+            blocking_objects = self.identify_blocking_objects(blocking_moves)
+            error_objects = ''
+            for object_type, objects in blocking_objects.items():
+                error_objects += _('- %s : %s. \n' % (
+                    object_type, ','.join([format_link(o) for o in objects])))
+            # TODO Fix error message:
+            # We'd like the links to be clickable but they are interpreted as
+            # raw text
             raise UserError(
                 _("Cancelation of destination move is restricted if any "
                   "previous move is not canceled or done."
                   "Original moves are not canceled or done on the following "
-                  "pickings : %s") % ', '.join([p.name for p in pickings]))
+                  "objects : \n%s") % error_objects)
+
+    def get_blocking_moves(self, orig_moves):
+        not_canceled_or_done = orig_moves.filtered(
+            lambda m: m.state not in ('cancel', 'done')
+        )
+        not_in_self = orig_moves.filtered(
+            lambda m: m not in self
+        )
+        return not_in_self & not_canceled_or_done
+
+    def identify_blocking_objects(self, blocking_moves):
+        pickings = blocking_moves.mapped('picking_id')
+        if pickings:
+            return {'pickings': pickings}
+        else:
+            return {}
