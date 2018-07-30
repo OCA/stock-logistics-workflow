1e6c073e304578fce24b23dc015806ea9ad64085
Alvaro Marcos <alvaro.marcos@factorlibre.com>
2018-07-30 11:07:13 +0200

    Stock_picking_mass_action migration to v11

---
+++ stock_picking_mass_action/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2014 Camptocamp SA - Guewen Baconnier
+# Copyright 2018 Tecnativa - Vicent Cubells
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': 'Stock Picking Mass Action',
+    'version': '11.0.1.0.0',
+    'author': 'Camptocamp, '
+              'GRAP,'
+              'Tecnativa,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/stock-logistics-workflow/',
+    'license': 'AGPL-3',
+    'category': 'Warehouse Management',
+    'depends': [
+        'stock_account',
+    ],
+    'data': [
+        'wizard/mass_action_view.xml',
+        'data/ir_cron.xml',
+    ],
+    'installable': True,
+}
