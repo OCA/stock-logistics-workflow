5e44cd2c5ca5cd4e069d7e269605afffb3d86008
SimoRubi <simone.rubino@agilebg.com>
2018-09-04 15:34:11 +0200

    [MIG] stock_picking_package_preparation: Migration to 11.0
    manifest
    
    Substitute stock.pack.operation with stock.move.line, after https://github.com/odoo/odoo/commit/ddf2b21bdf38492ce075d4ae96bb9323c6ef59dd
    
    Removing children_ids after https://github.com/odoo/odoo/commit/809605bb8f16cf1398ff5070ec0d5bd568aef343
    
    product packaging gets a variant instead of a template after https://github.com/odoo/odoo/commit/796900feb76f7ca5084d6b5f90cb24d5448cc326
    
    get_content becomes get_contained_quants
    
    test little refactoring
    
    qty becomes quantity in stock.quant
    
    [FIX] result_package_id has ondelete=restrict, since https://github.com/odoo/odoo/commit/809605bb8f16cf1398ff5070ec0d5bd568aef343
    
    [FIX] update done qty only if it greater than 0

--- stock_picking_package_preparation/__init__.py
+++ stock_picking_package_preparation/__init__.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-# Author: Guewen Baconnier
-# Copyright 2015 Camptocamp SA
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from . import models
