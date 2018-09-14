41433994442301e58bbd989962a014fc89ed850c
Andrea Cometa <a.cometa@apuliasoftware.it>
2018-09-14 11:17:10 +0200

    add stock_no_extra_move by gurneyalex

---
+++ stock_no_extra_move/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Camptocamp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock no extra move",
+    "summary": "Prevent creation of extra moves in picking processing",
+    "version": "8.0.1.0.0",
+    "category": "Logistics",
+    "website": "https://camptocamp.com/",
+    "author": "Camptocamp",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        'specific_stock',
+    ],
+}
