418b168d20b876fa6e6040811bee0273079aff7a
sergio.teruel <sergio.teruel@tecnativa.com>
2018-09-24 13:31:07 +0200

    [11.0][MIG] stock_picking_sale_order_link: Migration to v11.0

--- stock_picking_sale_order_link/__manifest__.py
+++ stock_picking_sale_order_link/__manifest__.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
 # © 2017 Sergio Teruel <sergio.teruel@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     "name": "Stock Picking Sale Order Link",
     "summary": "Link between picking and sale order",
-    "version": "9.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Inventory",
-    "website": "http://www.tecnativa.com",
+    "website": "http://github.com/OCA/stock-logistics-workflow",
     "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
