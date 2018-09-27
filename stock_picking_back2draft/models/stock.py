a129b8c1dc0cf67f6ea90d9b18832f38c8eb9bdb
Iván Montagud <ivan@studio73.es>
2018-09-27 11:03:16 +0200

    [FIX] Import UserError directly

--- stock_picking_back2draft/models/stock.py
+++ stock_picking_back2draft/models/stock.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import _, models, api
-from odoo.exceptions import Warning as UserError
+from odoo.exceptions import UserError
 
 
 class StockMove(models.Model):
