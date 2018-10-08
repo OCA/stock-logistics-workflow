756d7d713dd68dce084716a55ae7e4d230c9870b
SimoRubi <simone.rubino@agilebg.com>
2018-10-08 14:17:14 +0200

    [MIG] stock_picking_package_preparation_line: Migration to 11.0

--- stock_picking_package_preparation_line/models/ir_config.py
+++ stock_picking_package_preparation_line/models/ir_config.py
@@ -1,28 +1,15 @@
-# -*- coding: utf-8 -*-
-#    Author: Francesco Apruzzese
-#    Copyright 2015 Apulia Software srl
-#    Copyright 2015 Lorenzo Battistini - Agile Business Group
-#    Copyright 2016 Alessio Gerace - Agile Business Group
+# Copyright 2015 Francesco Apruzzese - Apulia Software srl
+# Copyright 2015-2018 Lorenzo Battistini - Agile Business Group
+# Copyright 2016 Alessio Gerace - Agile Business Group
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api
+from odoo import models, fields
 
 
-class StockConfigSettings(models.TransientModel):
+class ResConfigSettings(models.TransientModel):
 
-    _inherit = 'stock.config.settings'
+    _inherit = 'res.config.settings'
 
     default_picking_type_for_package_preparation_id = fields.Many2one(
         'stock.picking.type',
         related='company_id.default_picking_type_for_package_preparation_id')
-
-    @api.model
-    def default_get(self, fields):
-        res = super(StockConfigSettings, self).default_get(fields)
-        if res:
-            company = self.env.user.company_id
-            res['default_picking_type_for_package_preparation_id'] = \
-                company.default_picking_type_for_package_preparation_id.id \
-                if company.default_picking_type_for_package_preparation_id \
-                else False
-        return res
