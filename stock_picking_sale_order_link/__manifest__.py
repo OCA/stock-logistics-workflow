67deb065f8cd3cb412361f0ec10904683a3cfb48
Santeri Valjakka <santeri.valjakka@avoin.systems>
2018-11-15 18:55:23 +0200

    [MIG] stock_picking_sale_order_link: Migration to 12.0

--- stock_picking_sale_order_link/__manifest__.py
+++ stock_picking_sale_order_link/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Picking Sale Order Link",
     "summary": "Link between picking and sale order",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Inventory",
     "website": "http://github.com/OCA/stock-logistics-workflow",
     "author": "Tecnativa, "
