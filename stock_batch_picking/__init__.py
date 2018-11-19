3f04013aa802577c0f2dd38052a410ea99077a3d
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-11-19 10:07:23 +0100

    [IMP] stock_batch_picking: README by fragments + remove copyright in init files

--- stock_batch_picking/__init__.py
+++ stock_batch_picking/__init__.py
@@ -1,4 +1,3 @@
-# Copyright 2012-2014 Alexandre Fayolle, Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from . import models
--- stock_batch_picking/models/__init__.py
+++ stock_batch_picking/models/__init__.py
@@ -1,4 +1,3 @@
-# Copyright 2016 Cyril Gaudin, Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from . import stock_batch_picking
