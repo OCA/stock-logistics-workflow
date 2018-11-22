0a66c5d5925ce9a3bdd833f4e8a9f464fd6789bd
Beñat Jimenez <benat.jimenez@eficent.com>
2018-11-22 09:26:43 +0100

    [MIG] Migration of module stock_logistics_workflow to version 12

--- stock_picking_show_backorder/__manifest__.py
+++ stock_picking_show_backorder/__manifest__.py
@@ -3,13 +3,13 @@
 
 {
     'name': 'Stock Picking Show Backorder',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Warehouse Management',
     'summary': "Provides a new field on stock pickings, allowing to display "
                "the corresponding backorders.",
     'author': "Eficent, "
               "Odoo Community Association (OCA)",
-    'website': 'http://www.eficent.com',
+    'website': 'https://github.com/OCA/stock-logistics-workflow',
     'depends': ['stock'],
     'data': [
         'views/stock_view.xml',
---
+++ stock_picking_show_backorder/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module provide a new field on stock pickings, allowing to see the
+backorders associated to a picking.
