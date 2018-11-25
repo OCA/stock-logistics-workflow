8031a72d24ce52d41d0b8ee8b7185820d09d451b
Ruchir Shukla <ruchir@bizzappdev.com>
2018-11-25 10:05:45 +0530

    [MIG]stock_picking_customer_ref: migration from 11 to 12

--- stock_picking_customer_ref/models/stock_picking.py
+++ stock_picking_customer_ref/models/stock_picking.py
@@ -1,23 +1,14 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 AvanzOSC - Alfredo de la Fuente
 # Copyright 2017 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    @api.depends('group_id', 'sale_id.client_order_ref')
-    def _compute_client_order_ref(self):
-        for picking in self:
-            if picking.group_id:
-                cond = [('procurement_group_id', '=', picking.group_id.id)]
-                sale = self.env['sale.order'].search(cond, limit=1)
-                if sale:
-                    picking.client_order_ref = sale.client_order_ref
-
-    client_order_ref = fields.Char(string="Sale Reference/Description",
-                                   compute="_compute_client_order_ref",
-                                   store=True)
+    client_order_ref = fields.Char(
+        string="Customer Reference",
+        related="sale_id.client_order_ref",
+        store=True)
