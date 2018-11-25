1a2d90c42fd98ed42463177eb673cde2707e9b27
Ruchir Shukla <ruchir@bizzappdev.com>
2018-11-25 10:05:45 +0530

    [MIG]stock_picking_customer_ref: migration from 11 to 12

--- stock_picking_customer_ref/__manifest__.py
+++ stock_picking_customer_ref/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Tecnativa - Pedro M. Baeza
 # Copyright 2015 AvanzOSC - Ana Juaristi
 # Copyright 2015 AvanzOSC - Mikel Arregi
@@ -6,13 +5,13 @@
 # Copyright 2017 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Stock Picking Customer Ref',
-    'version': '11.0.1.0.0',
+    'name': 'Stock Picking Customer Reference',
+    'version': '12.0.1.0.0',
     'category': 'Warehouse',
     'author': 'AvanzOSC,'
               'Tecnativa,'
               'Odoo Community Association (OCA)',
-    'website': 'https://odoo-community.org/',
+    'website': 'https://github.com/OCA/stock-logistics-workflow',
     'license': 'AGPL-3',
     'summary': 'This module displays the sale reference/description in the '
                'pickings',
--- stock_picking_customer_ref/tests/test_stock_picking_customer_ref.py
+++ stock_picking_customer_ref/tests/test_stock_picking_customer_ref.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 AvanzOSC - Alfredo de la Fuente
 # Copyright 2017 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
@@ -11,17 +10,22 @@ class TestStockPickingCustomerRef(common.SavepointCase):
     @classmethod
     def setUpClass(cls):
         super(TestStockPickingCustomerRef, cls).setUpClass()
+        cls.SaleOrderObj = cls.env['sale.order']
+
         cls.product = cls.env['product.product'].create({
             'name': 'Test stuff',
         })
         cls.partner = cls.env['res.partner'].create({
             'name': 'Mr. Odoo',
         })
-        cls.sale_order = cls.env['sale.order'].create({
+        cls.sale_order = cls.SaleOrderObj.create({
             'partner_id': cls.partner.id,
             'order_line': [(0, 0, {'product_id': cls.product.id})],
         })
         cls.sale_order.with_context(show_sale=True).action_confirm()
+        cls.picking_type_id = cls.env.ref('stock.picking_type_in')
+        cls.location_id = cls.env.ref('stock.stock_location_suppliers')
+        cls.location_dest_id = cls.env.ref('stock.stock_location_stock')
 
     def test_stock_picking_customer_ref(self):
         self.sale_order.client_order_ref = 'EDIT123456'
@@ -36,7 +40,7 @@ class TestStockPickingCustomerRef(common.SavepointCase):
                 'Order ref of picking not equal to client order ref in sale')
 
     def test_stock_picking_customer_ref_on_sale_creation(self):
-        sale_order = self.env['sale.order'].create({
+        sale_order = self.SaleOrderObj.create({
             'partner_id': self.partner.id,
             'client_order_ref': 'CREATE123456',
             'order_line': [(0, 0, {'product_id': self.product.id})],
@@ -46,3 +50,22 @@ class TestStockPickingCustomerRef(common.SavepointCase):
             self.assertEqual(
                 picking.client_order_ref, sale_order.client_order_ref,
                 'Order ref of picking not equal to client order ref in sale')
+
+    def test_stock_picking_customer_ref_without_sale(self):
+        stock_picking = self.env['stock.picking'].create({
+            'partner_id': self.partner.id,
+            'picking_type_id': self.picking_type_id.id,
+            'location_id': self.location_id.id,
+            'location_dest_id': self.location_dest_id.id,
+            'move_lines': [(0, 0, {
+                'product_id': self.product.id,
+                'name': self.product.name,
+                'product_uom': self.env.ref('uom.product_uom_unit').id,
+                'product_uom_qty': 50.0,
+                'picking_type_id': self.picking_type_id.id,
+                'location_id': self.location_id.id,
+                'location_dest_id': self.location_dest_id.id,
+            })]
+        })
+        self.assertFalse(
+            stock_picking.client_order_ref, 'Client Order Reference is None')
