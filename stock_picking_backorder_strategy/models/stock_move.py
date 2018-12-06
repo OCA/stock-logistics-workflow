b4227333a891d89055fc8523cee33c126748aa0a
mgosai <mgosai@ursainfosystems.com>
2018-12-06 10:51:24 -0500

    [MIG] stock_picking_backorder_strategy: Migration to 11.0

--- stock_picking_backorder_strategy/models/stock_move.py
+++ stock_picking_backorder_strategy/models/stock_move.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -6,10 +5,9 @@ from odoo import api, models
 
 
 class StockMove(models.Model):
-
     _inherit = 'stock.move'
 
     @api.multi
     def _cancel_remaining_quantities(self):
         to_cancel = self.filtered(lambda m: m.state not in ('done', 'cancel'))
-        to_cancel.action_cancel()
+        to_cancel._action_cancel()
