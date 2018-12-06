8750bb9eab28214f5796f2b280b965a26971d1d5
mgosai <mgosai@ursainfosystems.com>
2018-12-06 10:51:24 -0500

    [MIG] stock_picking_backorder_strategy: Migration to 11.0

--- stock_picking_backorder_strategy/__manifest__.py
+++ stock_picking_backorder_strategy/__manifest__.py
@@ -1,13 +1,18 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Picking backordering strategies',
-    'version': '10.0.0.1.0',
-    'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
+    'version': '11.0.1.0.0',
+    'development_status': 'Production/Stable',
+    'author': """ACSONE SA/NV,
+                 Odoo Community Association (OCA)""",
+    'maintainers': [
+        'rousseldenis',
+        'mgosai'
+    ],
     'category': 'Warehouse Management',
-    'website': 'http://www.acsone.eu',
+    'website': 'https://github.com/OCA/stock-logistics-workflow',
     'depends': [
         'stock',
     ],
--- stock_picking_backorder_strategy/readme/CONTRIBUTORS.rst
+++ stock_picking_backorder_strategy/readme/CONTRIBUTORS.rst
@@ -1,2 +1,3 @@
 * Laetitia Gangloff <laetitia.gangloff@acsone.eu>
 * Denis Roussel <denis.roussel@acsone.eu>
+* Mayank Gosai <mgosai@opensourceintegrators.com>
