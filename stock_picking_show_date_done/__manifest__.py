feea2777f5a4f4e874af587929c2f4f7581bc5c6
Lluís Rovira @PlanetaTIC <lrovira@planetatic.com>
2018-12-10 18:25:37 +0100

    Add stock_picking_show_date_done module

---
+++ stock_picking_show_date_done/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 PlanetaTIC - Lluís Rovira <lrovira@planetatic.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Stock Picking Show Date Done',
+    'summary': 'Show and group date_done of Stock Pickings',
+    'version': '10.0.1.0.0',
+    'category': 'Warehouse Management',
+    'author': 'PlanetaTIC',
+    'website': 'https://www.planetatic.com',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/stock_picking_view.xml',
+    ],
+}
