33179bf2ceec75b7154d5a2b5929d111daccc99c
Adrià Gil Sorribes <adria.gil@eficent.com>
2018-12-21 10:47:12 +0100

    fixup!

--- stock_picking_deactivate_immediate_transfer/__manifest__.py
+++ stock_picking_deactivate_immediate_transfer/__manifest__.py
@@ -3,17 +3,20 @@
 
 {
     'name': 'Stock Picking Deactivate Immediate Transfer',
-    'version': '11.0.1.0.0',
-    'category': 'Warehouse Management',
     'summary': "Deactivates immediate transfers"
                "sticking only to planned ones",
+    'version': '11.0.1.0.0',
+    'category': 'Warehouse Management',
     'author': "Eficent, "
               "Odoo Community Association (OCA)",
-    'website': 'http://www.eficent.com',
-    'depends': ['stock'],
+    'website': "http://www.github.com/OCA/stock-logistics-workflow",
+    'license': "AGPL-3",
+    'depends': [
+        'stock'
+    ],
     'data': [
         'views/stock_picking_views.xml',
     ],
     'installable': True,
-    'license': "AGPL-3",
+    'application': False,
 }
