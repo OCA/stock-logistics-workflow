9dffd7475236613ff74380ed71ca1e9a76f69760
Alexis de Lattre <alexis.delattre@akretion.com>
2019-01-21 17:03:41 +0100

    Update to recent OCA conventions

--- stock_cost_method_last/__manifest__.py
+++ stock_cost_method_last/__manifest__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2016-2019 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
@@ -7,9 +8,9 @@
     'version': '10.0.1.0.0',
     'category': 'Warehouse',
     'license': 'AGPL-3',
-    'summary': 'Add a new Costing Method *Last Price*',
+    'summary': "Add a new Costing Method 'Last Price'",
     'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'http://www.akretion.com',
+    'website': 'https://github.com/OCA/stock-logistics-workflow',
     'depends': ['stock_account'],
     'data': ['views/product.xml'],
     'installable': True,
--- stock_cost_method_last/models/stock_move.py
+++ stock_cost_method_last/models/stock_move.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2016-2019 Akretion (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, api
+from odoo import api, models
 
 
 class StockMove(models.Model):
