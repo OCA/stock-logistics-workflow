27d373697d7cce9da76ae9c7c966a5ad3d446526
Alexis de Lattre <alexis.delattre@akretion.com>
2019-01-21 17:03:41 +0100

    Update to recent OCA conventions

--- stock_cost_method_last/models/product.py
+++ stock_cost_method_last/models/product.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2016-2019 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
