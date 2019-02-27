b6c974023f87515f02fb98279548e64b772a21b7
sergiocorato <sergiocorato@gmail.com>
2019-02-27 23:23:54 +0100

    [FIX] revert commit

--- stock_picking_package_preparation_line/models/stock.py
+++ stock_picking_package_preparation_line/models/stock.py
@@ -34,8 +34,7 @@ class StockMove(models.Model):
     @api.multi
     def write(self, values):
         res = super(StockMove, self).write(values)
-        if not self.env.context.get('skip_update_line_ids', False) and \
-                'invoice_state' not in values:
+        if not self.env.context.get('skip_update_line_ids', False):
             pack_to_update = self.env['stock.picking.package.preparation']
             for move in self:
                 pack_to_update |= move.get_packs()
