23c61bce460b8f044358f500022a15a3d294fb50
Pierrick Brun <pierrick.brun@akretion.com>
2019-03-12 15:33:06 +0100

    [ADD] module stock_picking_quick

---
+++ stock_picking_quick/__manifest__.py
@@ -0,0 +1,15 @@
+# © 2019 Today Akretion
+# @author Pierrick Brun <pierrick.brun@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Quick Stock Picking",
+    "version": "12.0.1.0.0",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "license": "AGPL-3",
+    "category": "Stock",
+    "depends": ["base_product_mass_addition", "stock"],
+    "data": ["views/picking_view.xml", "views/product_view.xml"],
+    "installable": True,
+}
---
+++ stock_picking_quick/models/__init__.py
@@ -0,0 +1,2 @@
+from . import stock_picking
+from . import product_product
