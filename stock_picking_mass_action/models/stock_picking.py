40f83b2f486c614531b5faf24f967d9ffb9f397a
Carlos Dauden <carlos.dauden@tecnativa.com>
2019-05-14 21:10:54 +0200

    [IMP] stock_picking_mass_action: Fix and improve transfer method

--- stock_picking_mass_action/models/stock_picking.py
+++ stock_picking_mass_action/models/stock_picking.py
@@ -2,7 +2,7 @@
 # Copyright 2018 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api
+from odoo import _, api
 from odoo.models import Model
 
 
@@ -18,3 +18,20 @@ class StockPicking(Model):
                   ('state', '=', 'confirmed')]
         records = self.search(domain, order='scheduled_date')
         records.action_assign()
+
+    def action_immediate_transfer_wizard(self):
+        view = self.env.ref('stock.view_immediate_transfer')
+        wiz = self.env['stock.immediate.transfer'].create(
+            {'pick_ids': [(4, p.id) for p in self]})
+        return {
+            'name': _('Immediate Transfer?'),
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'stock.immediate.transfer',
+            'views': [(view.id, 'form')],
+            'view_id': view.id,
+            'target': 'new',
+            'res_id': wiz.id,
+            'context': self.env.context,
+        }
