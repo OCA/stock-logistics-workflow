81e6c9e1035f269ddc737de916ecbd3e35262c2f
Alexis de Lattre <alexis.delattre@akretion.com>
2019-06-06 20:02:20 +0200

    [MIG] product_expiry_simple from v11 to v12

--- product_expiry_simple/models/stock_production_lot.py
+++ product_expiry_simple/models/stock_production_lot.py
@@ -1,23 +1,35 @@
-# © 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
-# © 2018 Jarsa Sistemas (Alan Ramos <alan.ramos@jarsa.com.mx>)
+# Copyright 2017-2019 Akretion France (http://www.akretion.com/)
+# Copyright 2018 Jarsa Sistemas (Alan Ramos <alan.ramos@jarsa.com.mx>)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import api, fields, models, _
 
 
 class StockProductionLot(models.Model):
     _inherit = 'stock.production.lot'
 
     expiry_date = fields.Date(string='Expiry Date')
-    display_name = fields.Char(
-        compute='_compute_display_name',
-        string='Lot/Serial Number Display', store=True, readonly=True)
+    expired = fields.Boolean(compute='_compute_expired')
 
-    @api.multi
+    def _compute_expired(self):
+        today = fields.Date.context_today(self)
+        for lot in self:
+            expired = False
+            if lot.expiry_date and lot.expiry_date < today:
+                expired = True
+            lot.expired = expired
+
     @api.depends('name', 'expiry_date')
-    def _compute_display_name(self):
+    def name_get(self):
+        res = []
+        today = fields.Date.context_today(self)
         for lot in self:
             dname = lot.name
             if lot.expiry_date:
-                dname = '[%s] %s' % (lot.expiry_date, dname)
-            lot.display_name = dname
+                if lot.expiry_date < today:
+                    dname = _('[%s Expired] %s') % (lot.expiry_date, dname)
+                else:
+                    dname = '[%s] %s' % (lot.expiry_date, dname)
+            res.append((lot.id, dname))
+        return res
