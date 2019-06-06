701586149cb0b3f0cd95a8a8d6f0bb798500cc98
Alexis de Lattre <alexis.delattre@akretion.com>
2019-06-06 20:02:20 +0200

    [MIG] product_expiry_simple from v11 to v12

--- product_expiry_simple/models/stock_quant.py
+++ product_expiry_simple/models/stock_quant.py
@@ -1,18 +1,19 @@
-# © 2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
-# © 2018 Jarsa Sistemas (Alan Ramos <alan.ramos@jarsa.com.mx>)
+# Copyright 2017-2019 Akretion France (http://www.akretion.com/)
+# Copyright 2018-2019 Jarsa Sistemas (Alan Ramos <alan.ramos@jarsa.com.mx>)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
-    expiry_date = fields.Date(
-        related='lot_id.expiry_date', store=True, readonly=True)
+    expiry_date = fields.Date(related='lot_id.expiry_date', store=True)
+    expired = fields.Boolean(related='lot_id.expired')
 
     # method copy/pasted from the official product_expiry module
-    # © Odoo SA
+    # Copyright Odoo SA
     @api.model
     def _get_removal_strategy_order(self, removal_strategy):
         if removal_strategy == 'fefo':
