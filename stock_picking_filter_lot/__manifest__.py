d46b932e13e800a46d518401aa7a8a8c963c97ea
IsabelEsparza <isa_tkm_ris@live.com.mx>
2019-06-14 09:31:50 -0500

    [MIG]stock_picking_filter_lot: Migration to 12.0

--- stock_picking_filter_lot/__manifest__.py
+++ stock_picking_filter_lot/__manifest__.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Simone Rubino - Agile Business Group
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Stock picking filter lot",
     "summary": "In picking out lots' selection, "
                "filter lots based on their location",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Warehouse",
-    "website": "https://github.com/OCA/stock-logistics-workflow/tree/"
-               "11.0/stock_filter_lot",
+    "website": "https://github.com/OCA/stock-logistics-workflow"
+               "12.0/stock_filter_lot",
     "author": "Agile Business Group, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
