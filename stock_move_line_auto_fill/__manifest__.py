7a78edd87ee89081fb7ad9c505c7bcdfbb0a212a
Jaime Arroyo <jaime.arroyo@creublanca.es>
2019-06-21 12:01:49 +0200

    [12.0][MIG] stock_pack_operation_auto_fill

---
+++ stock_move_line_auto_fill/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2017 ACSONE SA/NV
+# Copyright 2018 Tecnativa - Sergio Teruel
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Stock Move Line Auto Fill',
+    'summary': "Stock Move Line auto fill",
+    'version': '12.0.2.0.1',
+    'license': 'AGPL-3',
+    'author': 'ACSONE SA/NV,'
+              'Tecnativa,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/stock-logistics-workflow/',
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/stock_picking.xml',
+        'views/stock_picking_type_views.xml',
+    ],
+}
