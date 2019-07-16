a03b5fb08ab217b75c72084203fdc2b38e125914
Adrià Gil Sorribes <adria.gil@eficent.com>
2019-07-16 10:09:04 +0200

    [11.0][ADD] stock_picking_purchase_order_link

---
+++ stock_picking_purchase_order_link/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2019 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Stock Picking Purchase Order Link',
+    'summary': """
+        Link between picking and purchase order""",
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Eficent Business and IT Consulting Services S.L.,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/stock-logistics-workflow',
+    "application": False,
+    "installable": True,
+    'depends': [
+        'purchase'
+    ],
+    'data': [
+        'views/stock_picking_view.xml',
+    ],
+}
---
+++ stock_picking_purchase_order_link/models/stock_picking.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    @api.multi
+    def action_view_purchase_order(self):
+        """This function returns an action that display existing purchase order
+        of given picking.
+        """
+        self.ensure_one()
+        action = self.env.ref('purchase.purchase_form_action').read()[0]
+        form = self.env.ref('purchase.purchase_order_form')
+        action['views'] = [(form.id, 'form')]
+        action['res_id'] = self.purchase_id.id
+        return action
