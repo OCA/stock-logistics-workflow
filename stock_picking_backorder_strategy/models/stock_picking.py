9cc7220317788fe76da1e51b6eb74801d3a3fe1c
Luis Escobar <lescobar@vauxoo.com>
2019-09-11 00:31:36 +0000

    [MIG] stock_picking_backorder_strategy: Migration to 12.0

--- stock_picking_backorder_strategy/models/stock_picking.py
+++ stock_picking_backorder_strategy/models/stock_picking.py
@@ -12,7 +12,7 @@ class StockPicking(models.Model):
     def _check_backorder(self):
         self.ensure_one()
         # If strategy == 'manual', let the normal process going on
-        if self.picking_type_id.backorder_strategy == 'manual':
+        if self.picking_type_id.backorder_strategy == "manual":
             return super(StockPicking, self)._check_backorder()
         return False
 
@@ -23,13 +23,15 @@ class StockPicking(models.Model):
         res = False
         # Do nothing with pickings 'no_create'
         pickings = self.filtered(
-            lambda p: p.picking_type_id.backorder_strategy != 'no_create')
+            lambda p: p.picking_type_id.backorder_strategy != "no_create"
+        )
         pickings_no_create = self - pickings
-        pickings_no_create.mapped('move_lines')._cancel_remaining_quantities()
+        pickings_no_create.mapped("move_lines")._cancel_remaining_quantities()
         res = super(StockPicking, pickings)._create_backorder(
-            backorder_moves=backorder_moves)
+            backorder_moves=backorder_moves
+        )
         to_cancel = res.filtered(
-            lambda b: b.backorder_id.picking_type_id.backorder_strategy ==
-            'cancel')
+            lambda b: b.backorder_id.picking_type_id.backorder_strategy == "cancel"
+        )
         to_cancel.action_cancel()
         return res
--- stock_picking_backorder_strategy/tests/test_backorder_strategy.py
+++ stock_picking_backorder_strategy/tests/test_backorder_strategy.py
@@ -5,35 +5,38 @@ from odoo.tests import common
 
 
 class TestBackorderStrategy(common.TransactionCase):
-
     def setUp(self):
         """ Create the picking
         """
         super(TestBackorderStrategy, self).setUp()
 
-        self.picking_obj = self.env['stock.picking']
-        move_obj = self.env['stock.move']
+        self.picking_obj = self.env["stock.picking"]
+        move_obj = self.env["stock.move"]
 
-        self.picking_type = self.env.ref('stock.picking_type_in')
+        self.picking_type = self.env.ref("stock.picking_type_in")
 
-        product = self.env.ref('product.product_product_13')
-        loc_supplier_id = self.env.ref('stock.stock_location_suppliers').id
-        loc_stock_id = self.env.ref('stock.stock_location_stock').id
+        product = self.env.ref("product.product_product_13")
+        loc_supplier_id = self.env.ref("stock.stock_location_suppliers").id
+        loc_stock_id = self.env.ref("stock.stock_location_stock").id
 
-        self.picking = self.picking_obj.create({
-            'picking_type_id': self.picking_type.id,
-            'location_id': loc_supplier_id,
-            'location_dest_id': loc_stock_id,
-        })
-        move_obj.create({
-            'name': '/',
-            'picking_id': self.picking.id,
-            'product_uom': product.uom_id.id,
-            'location_id': loc_supplier_id,
-            'location_dest_id': loc_stock_id,
-            'product_id': product.id,
-            'product_uom_qty': 2
-        })
+        self.picking = self.picking_obj.create(
+            {
+                "picking_type_id": self.picking_type.id,
+                "location_id": loc_supplier_id,
+                "location_dest_id": loc_stock_id,
+            }
+        )
+        move_obj.create(
+            {
+                "name": "/",
+                "picking_id": self.picking.id,
+                "product_uom": product.uom_id.id,
+                "location_id": loc_supplier_id,
+                "location_dest_id": loc_stock_id,
+                "product_id": product.id,
+                "product_uom_qty": 2,
+            }
+        )
         self.picking.action_confirm()
 
     def _process_picking(self):
@@ -47,10 +50,9 @@ class TestBackorderStrategy(common.TransactionCase):
             Receive picking
             Check backorder is created
         """
-        self.picking_type.backorder_strategy = 'create'
+        self.picking_type.backorder_strategy = "create"
         self._process_picking()
-        backorder = self.picking_obj.search(
-            [('backorder_id', '=', self.picking.id)])
+        backorder = self.picking_obj.search([("backorder_id", "=", self.picking.id)])
         if len(backorder):
             self.assertTrue(backorder)
 
@@ -60,20 +62,17 @@ class TestBackorderStrategy(common.TransactionCase):
             Check there is no backorder
             Check if there is one move done and one move cancelled
         """
-        self.picking_type.backorder_strategy = 'no_create'
+        self.picking_type.backorder_strategy = "no_create"
         self._process_picking()
-        backorder = self.picking_obj.search(
-            [('backorder_id', '=', self.picking.id)])
+        backorder = self.picking_obj.search([("backorder_id", "=", self.picking.id)])
         if len(backorder):
             self.assertFalse(backorder)
-            states = list(set(self.picking.move_lines.mapped('state')))
+            states = list(set(self.picking.move_lines.mapped("state")))
             self.assertEquals(
-                'done',
-                self.picking.state,
+                "done", self.picking.state,
             )
             self.assertListEqual(
-                ['cancel', 'done'],
-                states,
+                ["cancel", "done"], states,
             )
 
     def test_backorder_strategy_cancel(self):
@@ -81,25 +80,23 @@ class TestBackorderStrategy(common.TransactionCase):
             Receive picking
             Check the backorder state is cancel
         """
-        self.picking_type.backorder_strategy = 'cancel'
+        self.picking_type.backorder_strategy = "cancel"
         self._process_picking()
-        backorder = self.picking_obj.search(
-            [('backorder_id', '=', self.picking.id)])
+        backorder = self.picking_obj.search([("backorder_id", "=", self.picking.id)])
         if len(backorder):
             self.assertTrue(backorder)
-            self.assertEqual('cancel', backorder[0].state)
+            self.assertEqual("cancel", backorder[0].state)
 
     def test_backorder_strategy_manual(self):
         """ Set strategy for stock.picking_type_in to manual
             Receive picking
             Check the backorder wizard is launched
         """
-        self.assertEqual('manual', self.picking_type.backorder_strategy)
-        old_wizards = self.env['stock.backorder.confirmation'].search([])
+        self.assertEqual("manual", self.picking_type.backorder_strategy)
+        old_wizards = self.env["stock.backorder.confirmation"].search([])
         self._process_picking()
         if len(old_wizards):
-            backorder_obj = self.env['stock.backorder.confirmation']
-            new_wizards = backorder_obj.search([]) -\
-                old_wizards
+            backorder_obj = self.env["stock.backorder.confirmation"]
+            new_wizards = backorder_obj.search([]) - old_wizards
             if len(new_wizards):
                 self.assertTrue(new_wizards)
