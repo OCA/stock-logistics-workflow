5a14d47220c425227e7455708c2af89cd4b47771
Luis Escobar <lescobar@vauxoo.com>
2019-09-11 00:31:36 +0000

    [MIG] stock_picking_backorder_strategy: Migration to 12.0

--- stock_picking_backorder_strategy/models/stock_picking_type.py
+++ stock_picking_backorder_strategy/models/stock_picking_type.py
@@ -1,16 +1,16 @@
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models, _
+from odoo import fields, models
 
 
 class StockPickingType(models.Model):
     _inherit = 'stock.picking.type'
 
     backorder_strategy = fields.Selection(
-        [('manual', _('Manual')), ('create', _('Create')),
-         ('no_create', _('No create')),
-         ('cancel', _('Cancel'))],
+        [('manual', 'Manual'), ('create', 'Create'),
+         ('no_create', 'No create'),
+         ('cancel', 'Cancel')],
         default='manual',
         help="Define what to do with backorder",
         required=True
