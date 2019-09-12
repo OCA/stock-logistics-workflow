b6fa888ec74cdf5bd7d9a06df5f53c963444cf68
oca-travis <oca+oca-travis@odoo-community.org>
2019-09-12 16:36:47 +0000

    [UPD] Update stock_picking_backorder_strategy.pot

--- stock_picking_backorder_strategy/__manifest__.py
+++ stock_picking_backorder_strategy/__manifest__.py
@@ -2,24 +2,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Picking backordering strategies',
-    'version': '12.0.1.0.1',
-    'development_status': 'Production/Stable',
-    'author': """ACSONE SA/NV,
+    "name": "Picking backordering strategies",
+    "version": "12.0.1.0.1",
+    "development_status": "Production/Stable",
+    "author": """ACSONE SA/NV,
                  Odoo Community Association (OCA)""",
-    'maintainers': [
-        'rousseldenis',
-        'mgosai'
-    ],
-    'category': 'Warehouse Management',
-    'website': 'https://github.com/OCA/stock-logistics-workflow',
-    'depends': [
-        'stock',
-    ],
-    'data': [
-        'views/picking_type.xml',
-    ],
-    'license': 'AGPL-3',
-    'installable': True,
-    'application': False,
+    "maintainers": ["rousseldenis", "mgosai"],
+    "category": "Warehouse Management",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "depends": ["stock"],
+    "data": ["views/picking_type.xml"],
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
 }
