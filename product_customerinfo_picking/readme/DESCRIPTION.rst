e9688fc3ae3689f9ef167bb17161dab4f6b38bf7
mreficent <miquel.raich@eficent.com>
2019-10-23 15:10:52 +0200

    [MIG] product_supplierinfo_for_customer_picking: Migration to 12.0

---
+++ product_customerinfo_picking/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+Based on product_supplierinfo_for_customer, this module loads in every Stock move the
+customer code defined in the product.
--- product_customerinfo_picking/tests/__init__.py
+++ product_customerinfo_picking/tests/__init__.py
@@ -1,2 +1,2 @@
-# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import test_product_supplierinfo_for_customer_picking
