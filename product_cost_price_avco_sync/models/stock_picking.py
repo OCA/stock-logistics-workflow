09984714a9f5a84e177a2bf2d4e56312d43c1fd5
Sergio Teruel <sergio.teruel@tecnativa.com>
2019-10-29 11:47:28 +0100

    [ADD] product_cost_price_avco_sync: New module to sync product cost with stock move price unit

---
+++ product_cost_price_avco_sync/models/stock_picking.py
@@ -0,0 +1,12 @@
+# Copyright 2019 Carlos Dauden - Tecnativa <carlos.dauden@tecnativa.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    def action_done(self):
+        return super(StockPicking, self.with_context(
+            skip_cost_update=True)).action_done()
