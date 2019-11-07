0154126347fb0d57180bfbefc94d9dc1c2f29471
Carlos Dauden <carlos.dauden@tecnativa.com>
2019-11-07 12:13:09 +0100

    [IMP] product_cost_price_avco_sync: Cover more use cases

--- product_cost_price_avco_sync/models/__init__.py
+++ product_cost_price_avco_sync/models/__init__.py
@@ -1,5 +1,3 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from . import product
-from . import purchase_order
 from . import stock_move
 from . import stock_picking
--- product_cost_price_avco_sync/models/stock_move.py
+++ product_cost_price_avco_sync/models/stock_move.py
@@ -1,7 +1,9 @@
 # Copyright 2019 Carlos Dauden - Tecnativa <carlos.dauden@tecnativa.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from datetime import timedelta
+
+from odoo import fields, models
 from odoo.tools.float_utils import float_compare
 
 
@@ -9,46 +11,89 @@ class StockMove(models.Model):
     _inherit = 'stock.move'
 
     def write(self, vals):
+        """ Update cost price avco """
         res = super(StockMove, self).write(vals)
-        if ('price_unit' in vals and
-                not self.env.context.get('skip_cost_update')):
-            self.price_unit_update()
+        if (('price_unit' in vals or 'quantity_done' in vals) and
+                not self.env.context.get('skip_avco_sync')):
+            self.cost_price_avco_sync()
         return res
 
-    def price_unit_update(self):
-        procesed_moves = []
+    def _create_price_history(self, value):
+        PriceHistory = self.env['product.price.history']
+        for move in self:
+            PriceHistory.create({
+                'product_id': move.product_id.id,
+                'cost': value,
+                'datetime': move.date,
+                'company_id': move.company_id.id,
+            })
+
+    def _previous_instant_date(self):
+        """ Returns previous instant before move was done"""
+        self.ensure_one()
+        date_with_delta = fields.Datetime.from_string(
+            self.date) - timedelta(seconds=1)
+        return fields.Datetime.to_string(date_with_delta)
+
+    def _remove_after_history_price(self):
+        PriceHistory = self.env['product.price.history']
+        for move in self:
+            PriceHistory.search([
+                ('company_id', '=', move.company_id.id),
+                ('product_id', '=', move.product_id.id),
+                ('datetime', '>=', move.date),
+            ]).unlink()
+
+    def get_average_price(self, previous_qty, previous_price):
+        if self.quantity_done == 0.0:
+            price = previous_price
+        elif previous_qty < 0.0:
+            price = self.price_unit
+        else:
+            price = ((previous_qty * previous_price) +
+                     (self.quantity_done * self.price_unit)
+                     ) / (previous_qty + self.quantity_done)
+        return price
+
+    def cost_price_avco_sync(self):
+        procesed_moves = set()
         for move in self.sorted('date'):
             if (move.id in procesed_moves or move.state != 'done' or
                     not move._is_in() or
                     move.product_id.cost_method != 'average'):
                 continue
-            # not take in account move's quantities
+            previous_instant = move._previous_instant_date()
             previous_qty = move.product_id.with_context(
-                to_date=move.date
-            ).qty_available - move.quantity_done
-            price_history = self.product_id.get_cost_history_date(move.date)
-            price = move.get_average_price(previous_qty, price_history)
+                to_date=previous_instant
+            ).qty_available
+            previous_price = move.product_id.get_history_price(
+                move.company_id.id, previous_instant)
+            price = move.get_average_price(previous_qty, previous_price)
+            move._remove_after_history_price()
+            move._create_price_history(price)
             previous_qty += move.quantity_done
-            affected_moves = self.search([
+            affected_moves = self.with_context(skip_avco_sync=True).search([
                 ('product_id', '=', move.product_id.id),
                 ('date', '>', move.date),
             ], order='date')
             for af_move in affected_moves:
                 if af_move._is_in():
                     if af_move.inventory_id or af_move.move_orig_ids:
-                        af_move.with_context(
-                            skip_cost_update=True).price_unit = price
+                        af_move.write({
+                            'price_unit': price,
+                            'value': price * af_move.quantity_done,
+                        })
                     # Avoid reprocess move if included in self.write
-                    if af_move.id not in procesed_moves:
-                        procesed_moves.append(af_move.id)
+                    procesed_moves.add(af_move.id)
                     price = af_move.get_average_price(previous_qty, price)
+                    af_move._create_price_history(price)
                     previous_qty += af_move.quantity_done
                 elif af_move._is_out():
-                    af_move.with_context(
-                        skip_cost_update=True).price_unit = -price
+                    af_move.write({
+                        'price_unit': -price,
+                        'value': -price * af_move.quantity_done,
+                    })
                     previous_qty -= af_move.quantity_done
-                    if previous_qty < 0.0:
-                        previous_qty = 0.0
             if float_compare(move.product_id.standard_price, price,
                              precision_rounding=move.product_uom.rounding):
                 # Write the standard price, as SUPERUSER_ID because a warehouse
@@ -56,9 +101,3 @@ class StockMove(models.Model):
                 move.product_id.with_context(
                     force_company=move.company_id.id,
                 ).sudo().write({'standard_price': price})
-
-    def get_average_price(self, previous_qty, previous_price):
-        new_std_price = ((previous_qty * previous_price) + (
-            self.price_unit * self.quantity_done)) / (previous_qty +
-                                                      self.quantity_done)
-        return new_std_price
