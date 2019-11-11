18f6a825a39f6f2f229f9b9e00a418602958cc0b
Akim Juillerat <akim.juillerat@camptocamp.com>
2019-11-11 18:02:51 +0100

    # This is a combination of 3 commits. # This is the 1st commit message:
    Add module stock_quant_package_product_packaging
    
    # The commit message #2 will be skipped:
    
    # Add tests
    
    # The commit message #3 will be skipped:
    
    # Display single_product fields in debug mode

---
+++ stock_quant_package_product_packaging/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2019 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+{
+    "name": "Stock Quant Package Product Packaging",
+    "summary": "Use product packagings on packages",
+    "version": "12.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Warehouse Management",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_quant_package.xml",
+    ],
+    "demo": [
+    ],
+}
---
+++ stock_quant_package_product_packaging/models/stock_quant_package.py
@@ -0,0 +1,54 @@
+# Copyright 2019 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+from odoo import api, fields, models
+
+
+class StockQuantPackage(models.Model):
+
+    _inherit = "stock.quant.package"
+
+    # This is not the same thing as 'packaging_id':
+    # * packaging_id is the "Package type", packaging which have
+    #   no 'product_id' and used for the delivery (postal 2kg, ...)
+    # * product_packaging_id is the actual Product Packaging (usually
+    #   using a GTIN) used for the internal logistics/reception. It
+    #   has a product_id
+    product_packaging_id = fields.Many2one(
+        'product.packaging',
+        'Product Packaging',
+        index=True,
+        help="Packaging of the product, used for internal logistics"
+        "transfers, put-away rules, ..."
+    )
+    single_product_id = fields.Many2one(
+        'product.product', compute='_compute_single_product'
+    )
+    single_product_qty = fields.Float(compute='_compute_single_product')
+
+    @api.depends('quant_ids', 'quant_ids.product_id')
+    def _compute_single_product(self):
+        for pack in self:
+            pack_products = pack.quant_ids.mapped('product_id')
+            if len(pack_products) == 1:
+                pack.single_product_id = pack_products.id
+                # TODO handle uom
+                pack.single_product_qty = sum(pack.quant_ids.mapped('quantity'))
+            else:
+                pack.single_product_id = False
+                pack.single_product_qty = 0
+
+    def auto_assign_packaging(self):
+        for pack in self:
+            if (
+                not pack.product_packaging_id
+                and pack.single_product_id
+                and pack.single_product_qty
+            ):
+                packaging = self.env['product.packaging'].search(
+                    [
+                        ('product_id', '=', pack.single_product_id.id),
+                        ('qty', '=', pack.single_product_qty)
+                    ], limit=1
+                )
+                if packaging:
+                    pack.write({'product_packaging_id': packaging.id})
