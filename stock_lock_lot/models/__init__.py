2918ec16419c8a2ab35cb4565403948d41b967dd
ernestotejeda <ernesto.tejeda87@gmail.com>
2019-12-09 12:04:46 -0500

    [MIG] stock_lock_lot: Migration to 13.0

--- stock_lock_lot/models/__init__.py
+++ stock_lock_lot/models/__init__.py
@@ -1,11 +1,8 @@
-# © 2015 Serv. Tec. Avanzados - Pedro M. Baeza (http://www.serviciosbaeza.com)
-# © 2015 AvanzOsc (http://www.avanzosc.es)
+# Copyright 2015 Serv. Tec. Avanzados - Pedro M. Baeza (http://www.serviciosbaeza.com)
+# Copyright 2015 AvanzOsc (http://www.avanzosc.es)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import product_category
 from . import stock_production_lot
-from . import stock_quant
-from . import res_config
 from . import stock_location
-from . import stock_move
-from . import stock_inventory_line
+from . import stock_move_line
--- stock_lock_lot/wizard/__init__.py
+++
@@ -1,6 +0,0 @@
-# © 2015 Serv. Tec. Avanzados - Pedro M. Baeza (http://www.serviciosbaeza.com)
-# © 2015 AvanzOsc (http://www.avanzosc.es)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import wiz_lock_lot
-from . import stock_transfer_details
