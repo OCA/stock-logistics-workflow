fdf49d400bf90a6c426796e6b00a06c5110c67a0
ernestotejeda <ernesto.tejeda87@gmail.com>
2019-12-09 12:04:46 -0500

    [MIG] stock_lock_lot: Migration to 13.0

--- stock_lock_lot/__init__.py
+++ stock_lock_lot/__init__.py
@@ -1,6 +1,7 @@
-# © 2015 Serv. Tec. Avanzados - Pedro M. Baeza (http://www.serviciosbaeza.com)
-# © 2015 AvanzOsc (http://www.avanzosc.es)
+# Copyright 2015 Serv. Tec. Avanzados - Pedro M. Baeza (http://www.serviciosbaeza.com)
+# Copyright 2015 AvanzOsc (http://www.avanzosc.es)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import models
-from . import wizard
+
+# from . import wizard
--- stock_lock_lot/models/stock_location.py
+++ stock_lock_lot/models/stock_location.py
@@ -1,11 +1,10 @@
-# © 2016 AvanzOsc (http://www.avanzosc.es)
+# Copyright 2016 AvanzOsc (http://www.avanzosc.es)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class StockLocation(models.Model):
-
     _inherit = "stock.location"
 
     allow_locked = fields.Boolean(string="Allow Locked")
