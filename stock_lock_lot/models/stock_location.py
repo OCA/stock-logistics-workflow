23db842e4d89566a777164907338ef8b0a47dc29
ernestotejeda <ernesto.tejeda87@gmail.com>
2019-12-09 11:49:03 -0500

    [IMP] stock_lock_lot: black, isort

--- stock_lock_lot/models/stock_location.py
+++ stock_lock_lot/models/stock_location.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
 # © 2016 AvanzOsc (http://www.avanzosc.es)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from openerp import fields, models
 
 
 class StockLocation(models.Model):
 
-    _inherit = 'stock.location'
+    _inherit = "stock.location"
 
-    allow_locked = fields.Boolean(string='Allow Locked')
+    allow_locked = fields.Boolean(string="Allow Locked")
--- stock_lock_lot/wizard/wiz_lock_lot.py
+++ stock_lock_lot/wizard/wiz_lock_lot.py
@@ -1,22 +1,21 @@
-# -*- coding: utf-8 -*-
 # © 2015 Serv. Tec. Avanzados - Pedro M. Baeza (http://www.serviciosbaeza.com)
 # © 2015 AvanzOsc (http://www.avanzosc.es)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api
+from openerp import api, models
 
 
 class WizLockLot(models.TransientModel):
-    _name = 'wiz.lock.lot'
+    _name = "wiz.lock.lot"
 
     @api.multi
     def action_lock_lots(self):
-        lot_obj = self.env['stock.production.lot']
-        active_ids = self._context['active_ids']
+        lot_obj = self.env["stock.production.lot"]
+        active_ids = self._context["active_ids"]
         lot_obj.browse(active_ids).button_lock()
 
     @api.multi
     def action_unlock_lots(self):
-        lot_obj = self.env['stock.production.lot']
-        active_ids = self._context['active_ids']
+        lot_obj = self.env["stock.production.lot"]
+        active_ids = self._context["active_ids"]
         lot_obj.browse(active_ids).button_unlock()
