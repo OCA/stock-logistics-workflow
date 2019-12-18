4fb5c89342154b1409a5888dfe7e3410faca7239
Carlos Dauden <carlos.dauden@tecnativa.com>
2019-12-18 14:14:09 +0100

    [FIX] stock_picking_batch_extended: float_is_zero required parameters

--- stock_picking_batch_extended/models/stock_picking.py
+++ stock_picking_batch_extended/models/stock_picking.py
@@ -63,7 +63,10 @@ class StockPicking(models.Model):
                 for pack in pick.move_line_ids:
                     pack.qty_done = pack.product_uom_qty
             else:
-                if all(float_is_zero(pack.qty_done)
+                if all(
+                        float_is_zero(
+                            pack.qty_done,
+                            precision_rounding=pack.product_uom_id.rounding)
                         for pack in pick.move_line_ids):
                     # No qties to process, release out of the batch
                     pick.batch_id = False
