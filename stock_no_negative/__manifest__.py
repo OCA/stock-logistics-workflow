9eb27ce38ecabbd7488f2132f9ff431c21f1464d
Quentin <delcourte.quentin@hotmail.com>
2019-12-19 09:59:22 +0100

    [IMP] stock_no_negative: black, isort

--- stock_no_negative/__manifest__.py
+++ stock_no_negative/__manifest__.py
@@ -4,17 +4,14 @@
 
 
 {
-    'name': 'Stock Disallow Negative',
-    'version': '12.0.1.0.0',
-    'category': 'Inventory, Logistic, Storage',
-    'license': 'AGPL-3',
-    'summary': 'Disallow negative stock levels by default',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/stock-logistics-workflow',
-    'depends': ['stock'],
-    'data': [
-        'views/product_product_views.xml',
-        'views/stock_location_views.xml',
-    ],
-    'installable': True,
+    "name": "Stock Disallow Negative",
+    "version": "12.0.1.0.0",
+    "category": "Inventory, Logistic, Storage",
+    "license": "AGPL-3",
+    "summary": "Disallow negative stock levels by default",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "depends": ["stock"],
+    "data": ["views/product_product_views.xml", "views/stock_location_views.xml"],
+    "installable": True,
 }
