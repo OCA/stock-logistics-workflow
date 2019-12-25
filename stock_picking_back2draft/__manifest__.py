4f16f189facee471f3b84a3afbecf87d9be6193b
ps-tubtim <pimolnat.s@outlook.com>
2019-12-25 14:37:15 +0700

    [MIG] stock_picking_back2draft: Migration to 13.0

--- stock_picking_back2draft/__manifest__.py
+++ stock_picking_back2draft/__manifest__.py
@@ -5,7 +5,7 @@
     "summary": "Reopen cancelled pickings",
     "version": "13.0.1.0.0",
     "category": "Warehouse Management",
-    "website": "https://www.agilebg.com",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
     "author": "Agile Business Group, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
---
+++ stock_picking_back2draft/readme/CONTRIBUTORS.rst
@@ -0,0 +1,3 @@
+* Lorenzo Battistini <lorenzo.battistini@agilebg.com>
+* Iván Montagud <ivan@studio73.es>
+* Pimolnat Suntian <pimolnats@ecosoft.co.th>
