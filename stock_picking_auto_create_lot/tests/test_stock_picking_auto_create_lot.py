b9cfe965a01c6f42eee27deff5fd766989a5b21f
ps-tubtim <pimolnat.s@outlook.com>
2019-12-25 13:25:35 +0700

    [IMP] stock_picking_auto_create_lot: black, isort

--- stock_picking_auto_create_lot/tests/test_stock_picking_auto_create_lot.py
+++ stock_picking_auto_create_lot/tests/test_stock_picking_auto_create_lot.py
@@ -4,47 +4,52 @@ from odoo.tests import SavepointCase
 
 
 class TestStockPickingAutoCreateLot(SavepointCase):
-
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.warehouse = cls.env.ref('stock.warehouse0')
-        cls.picking_type_in = cls.env.ref('stock.picking_type_in')
-        cls.supplier_location = cls.env.ref('stock.stock_location_suppliers')
-        cls.supplier = cls.env['res.partner'].create({
-            'name': 'Supplier - test',
-            'supplier': True,
-        })
-        cls.product = cls.env['product.product'].create({
-            'name': 'test',
-            'type': 'product',
-            'tracking': 'lot',
-            'auto_create_lot': True,
-        })
-        cls.picking = cls.env['stock.picking'].with_context(
-            default_picking_type_id=cls.picking_type_in.id
-        ).create({
-            'partner_id': cls.supplier.id,
-            'picking_type_id': cls.picking_type_in.id,
-            'location_id': cls.supplier_location.id,
-        })
-        cls.move = cls.env['stock.move'].create({
-            'name': 'test-auto-lot',
-            'product_id': cls.product.id,
-            'picking_id': cls.picking.id,
-            'picking_type_id': cls.picking_type_in.id,
-            'product_uom_qty': 2.0,
-            'product_uom': cls.product.uom_id.id,
-            'location_id': cls.supplier_location.id,
-            'location_dest_id':
-                cls.picking_type_in.default_location_dest_id.id,
-        })
+        cls.warehouse = cls.env.ref("stock.warehouse0")
+        cls.picking_type_in = cls.env.ref("stock.picking_type_in")
+        cls.supplier_location = cls.env.ref("stock.stock_location_suppliers")
+        cls.supplier = cls.env["res.partner"].create(
+            {"name": "Supplier - test", "supplier": True}
+        )
+        cls.product = cls.env["product.product"].create(
+            {
+                "name": "test",
+                "type": "product",
+                "tracking": "lot",
+                "auto_create_lot": True,
+            }
+        )
+        cls.picking = (
+            cls.env["stock.picking"]
+            .with_context(default_picking_type_id=cls.picking_type_in.id)
+            .create(
+                {
+                    "partner_id": cls.supplier.id,
+                    "picking_type_id": cls.picking_type_in.id,
+                    "location_id": cls.supplier_location.id,
+                }
+            )
+        )
+        cls.move = cls.env["stock.move"].create(
+            {
+                "name": "test-auto-lot",
+                "product_id": cls.product.id,
+                "picking_id": cls.picking.id,
+                "picking_type_id": cls.picking_type_in.id,
+                "product_uom_qty": 2.0,
+                "product_uom": cls.product.uom_id.id,
+                "location_id": cls.supplier_location.id,
+                "location_dest_id": cls.picking_type_in.default_location_dest_id.id,
+            }
+        )
 
     def test_auto_create_lot(self):
         self.picking_type_in.auto_create_lot = True
         self.picking.action_assign()
         self.picking.button_validate()
-        lot = self.env['stock.production.lot'].search([
-            ('product_id', '=', self.product.id),
-        ])
+        lot = self.env["stock.production.lot"].search(
+            [("product_id", "=", self.product.id)]
+        )
         self.assertEqual(len(lot), 1)
