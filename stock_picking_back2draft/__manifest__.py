b9aed88fdf1fc5f73124c954b464889a3b9ac070
ps-tubtim <pimolnat.s@outlook.com>
2019-12-25 14:23:28 +0700

    [IMP] stock_picking_back2draft: black, isort

--- stock_picking_back2draft/__manifest__.py
+++ stock_picking_back2draft/__manifest__.py
@@ -3,18 +3,14 @@
 {
     "name": "Pickings back to draft",
     "summary": "Reopen cancelled pickings",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Warehouse Management",
     "website": "https://www.agilebg.com",
     "author": "Agile Business Group, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": [
-        "stock",
-    ],
-    "data": [
-        'views/picking_view.xml',
-    ],
-    'images': ['images/picking.png'],
+    "depends": ["stock"],
+    "data": ["views/picking_view.xml"],
+    "images": ["images/picking.png"],
 }
