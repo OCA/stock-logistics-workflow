ab14e773bf37173cc96b9026ee449815ae553042
ps-tubtim <pimolnat.s@outlook.com>
2019-12-25 14:37:15 +0700

    [MIG] stock_picking_back2draft: Migration to 13.0

--- stock_picking_back2draft/models/stock_move.py
+++ stock_picking_back2draft/models/stock_move.py
@@ -1,24 +1,14 @@
 # © 2016 Lorenzo Battistini - Agile Business Group
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, models
+from odoo import _, models
 from odoo.exceptions import UserError
 
 
 class StockMove(models.Model):
     _inherit = "stock.move"
 
-    @api.multi
     def action_back_to_draft(self):
         if self.filtered(lambda m: m.state != "cancel"):
             raise UserError(_("You can set to draft cancelled moves only"))
         self.write({"state": "draft"})
-
-
-class StockPicking(models.Model):
-    _inherit = "stock.picking"
-
-    @api.multi
-    def action_back_to_draft(self):
-        moves = self.mapped("move_lines")
-        moves.action_back_to_draft()
---
+++ stock_picking_back2draft/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module allows to bring cancelled pickings back to draft
