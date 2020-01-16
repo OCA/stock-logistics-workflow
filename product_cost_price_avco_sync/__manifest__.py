982071a1c81945a6e8f6add6bf41346a4e2d8cd5
Carlos Dauden <carlos.dauden@tecnativa.com>
2020-01-16 12:24:40 +0100

    [MIG] product_cost_price_avco_sync: Migration to 12.0

--- product_cost_price_avco_sync/__manifest__.py
+++ product_cost_price_avco_sync/__manifest__.py
@@ -1,13 +1,15 @@
-# Copyright 2018 Tecnativa - Carlos Dauden
-# Copyright 2018 Tecnativa - Sergio Teruel
+# Copyright 2020 Tecnativa - Carlos Dauden
+# Copyright 2020 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     'name': 'Product cost price avco sync',
     'summary': 'Set product cost price from updated moves',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
+    'development_status': 'Production/Stable',
     'category': 'Stock',
     'website': 'https://github.com/OCA/stock-logistics-workflow',
     'author': 'Tecnativa, Odoo Community Association (OCA)',
+    'maintainers': ['carlosdauden', 'sergio-teruel'],
     'license': 'AGPL-3',
     'installable': True,
     'depends': [
