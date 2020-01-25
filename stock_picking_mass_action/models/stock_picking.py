f148d680ddb8aebfb83c3f0eaf186cb5977b4256
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-01-25 09:28:35 +0100

    [IMP] stock_picking_mass_action: black, isort

--- stock_picking_mass_action/models/stock_picking.py
+++ stock_picking_mass_action/models/stock_picking.py
@@ -7,29 +7,29 @@ from odoo.models import Model
 
 
 class StockPicking(Model):
-    _inherit = 'stock.picking'
+    _inherit = "stock.picking"
 
     @api.model
     def check_assign_all(self):
         """ Try to assign confirmed pickings """
-        domain = [('picking_type_code', '=', 'outgoing'),
-                  ('state', '=', 'confirmed')]
-        records = self.search(domain, order='scheduled_date')
+        domain = [("picking_type_code", "=", "outgoing"), ("state", "=", "confirmed")]
+        records = self.search(domain, order="scheduled_date")
         records.action_assign()
 
     def action_immediate_transfer_wizard(self):
-        view = self.env.ref('stock.view_immediate_transfer')
-        wiz = self.env['stock.immediate.transfer'].create(
-            {'pick_ids': [(4, p.id) for p in self]})
+        view = self.env.ref("stock.view_immediate_transfer")
+        wiz = self.env["stock.immediate.transfer"].create(
+            {"pick_ids": [(4, p.id) for p in self]}
+        )
         return {
-            'name': _('Immediate Transfer?'),
-            'type': 'ir.actions.act_window',
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': 'stock.immediate.transfer',
-            'views': [(view.id, 'form')],
-            'view_id': view.id,
-            'target': 'new',
-            'res_id': wiz.id,
-            'context': self.env.context,
+            "name": _("Immediate Transfer?"),
+            "type": "ir.actions.act_window",
+            "view_type": "form",
+            "view_mode": "form",
+            "res_model": "stock.immediate.transfer",
+            "views": [(view.id, "form")],
+            "view_id": view.id,
+            "target": "new",
+            "res_id": wiz.id,
+            "context": self.env.context,
         }
