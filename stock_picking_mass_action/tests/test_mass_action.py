a4810e65b4f6ca8e301d123bcb5af6b18f3feffb
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-01-25 09:28:35 +0100

    [IMP] stock_picking_mass_action: black, isort

--- stock_picking_mass_action/tests/test_mass_action.py
+++ stock_picking_mass_action/tests/test_mass_action.py
@@ -9,106 +9,111 @@ class TestMassAction(common.SavepointCase):
     def setUpClass(cls):
         super().setUpClass()
 
-        partner = cls.env['res.partner'].create({
-            'name': 'Test Partner',
-        })
-        product = cls.env['product.product'].create({
-            'name': 'Product Test',
-            'type': 'product',
-        })
-        picking_type_out = cls.env.ref('stock.picking_type_out')
-        stock_location = cls.env.ref('stock.stock_location_stock')
-        customer_location = cls.env.ref('stock.stock_location_customers')
-        inventory = cls.env['stock.inventory'].create({
-            'name': 'Test Inventory',
-            'filter': 'product',
-            'product_id': product.id,
-            'location_id': stock_location.id,
-            'line_ids': [(0, 0, {
-                'product_qty': 600,
-                'location_id': stock_location.id,
-                'product_id': product.id,
-                'product_uom_id': product.uom_id.id,
-            })],
-        })
+        partner = cls.env["res.partner"].create({"name": "Test Partner"})
+        product = cls.env["product.product"].create(
+            {"name": "Product Test", "type": "product"}
+        )
+        picking_type_out = cls.env.ref("stock.picking_type_out")
+        stock_location = cls.env.ref("stock.stock_location_stock")
+        customer_location = cls.env.ref("stock.stock_location_customers")
+        inventory = cls.env["stock.inventory"].create(
+            {
+                "name": "Test Inventory",
+                "filter": "product",
+                "product_id": product.id,
+                "location_id": stock_location.id,
+                "line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_qty": 600,
+                            "location_id": stock_location.id,
+                            "product_id": product.id,
+                            "product_uom_id": product.uom_id.id,
+                        },
+                    )
+                ],
+            }
+        )
         inventory.action_validate()
         # We create a picking out
-        cls.picking = cls.env['stock.picking'].create({
-            'partner_id': partner.id,
-            'picking_type_id': picking_type_out.id,
-            'location_id': stock_location.id,
-            'location_dest_id': customer_location.id,
-            'move_lines': [(0, 0, {
-                'name': product.name,
-                'product_id': product.id,
-                'product_uom_qty': 200,
-                'product_uom': product.uom_id.id,
-                'location_id': stock_location.id,
-                'location_dest_id': customer_location.id,
-            })]
-        })
+        cls.picking = cls.env["stock.picking"].create(
+            {
+                "partner_id": partner.id,
+                "picking_type_id": picking_type_out.id,
+                "location_id": stock_location.id,
+                "location_dest_id": customer_location.id,
+                "move_lines": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": product.name,
+                            "product_id": product.id,
+                            "product_uom_qty": 200,
+                            "product_uom": product.uom_id.id,
+                            "location_id": stock_location.id,
+                            "location_dest_id": customer_location.id,
+                        },
+                    )
+                ],
+            }
+        )
 
     def test_mass_action(self):
-        self.assertEqual(self.picking.state, 'draft')
-        wiz = self.env['stock.picking.mass.action']
+        self.assertEqual(self.picking.state, "draft")
+        wiz = self.env["stock.picking.mass.action"]
         # We test confirming a picking
-        wiz_confirm = wiz.create({
-            "picking_ids": [(4, self.picking.id)],
-        })
+        wiz_confirm = wiz.create({"picking_ids": [(4, self.picking.id)]})
         wiz_confirm.confirm = True
         wiz_confirm.mass_action()
-        self.assertEqual(self.picking.state, 'confirmed')
+        self.assertEqual(self.picking.state, "confirmed")
         # We test checking availability
-        wiz_check = wiz.with_context(check_availability=True).create({
-            "picking_ids": [(4, self.picking.id)],
-        })
+        wiz_check = wiz.with_context(check_availability=True).create(
+            {"picking_ids": [(4, self.picking.id)]}
+        )
         wiz_check.confirm = True
         wiz_check.mass_action()
-        self.assertEqual(self.picking.state, 'assigned')
+        self.assertEqual(self.picking.state, "assigned")
         # We test transferring picking
-        wiz_tranfer = wiz.with_context(transfer=True).create({
-            "picking_ids": [(4, self.picking.id)],
-        })
+        wiz_tranfer = wiz.with_context(transfer=True).create(
+            {"picking_ids": [(4, self.picking.id)]}
+        )
         wiz_tranfer.confirm = True
         for line in self.picking.move_lines:
             line.quantity_done = line.product_uom_qty
         wiz_tranfer.mass_action()
-        self.assertEqual(self.picking.state, 'done')
+        self.assertEqual(self.picking.state, "done")
         # We test checking assign all
-        pickings = self.env['stock.picking']
+        pickings = self.env["stock.picking"]
         pick1 = self.picking.copy()
         pickings |= pick1
         pick2 = self.picking.copy()
         pickings |= pick2
-        self.assertEqual(pick1.state, 'draft')
-        self.assertEqual(pick2.state, 'draft')
-        wiz_confirm = wiz.create({
-            "picking_ids": [(6, 0, [pick1.id, pick2.id])],
-        })
+        self.assertEqual(pick1.state, "draft")
+        self.assertEqual(pick2.state, "draft")
+        wiz_confirm = wiz.create({"picking_ids": [(6, 0, [pick1.id, pick2.id])]})
         wiz_confirm.confirm = True
         wiz_confirm.mass_action()
-        self.assertEqual(pick1.state, 'confirmed')
-        self.assertEqual(pick2.state, 'confirmed')
+        self.assertEqual(pick1.state, "confirmed")
+        self.assertEqual(pick2.state, "confirmed")
         pickings.check_assign_all()
-        self.assertEqual(pick1.state, 'assigned')
-        self.assertEqual(pick2.state, 'assigned')
+        self.assertEqual(pick1.state, "assigned")
+        self.assertEqual(pick2.state, "assigned")
 
     def test_mass_action_inmediate_transfer(self):
-        wiz_tranfer = self.env['stock.picking.mass.action'].create({
-            'picking_ids': [(4, self.picking.id)],
-            'confirm': True,
-            'transfer': True,
-        })
+        wiz_tranfer = self.env["stock.picking.mass.action"].create(
+            {"picking_ids": [(4, self.picking.id)], "confirm": True, "transfer": True}
+        )
         res = wiz_tranfer.mass_action()
-        self.assertEqual(res['res_model'], 'stock.immediate.transfer')
+        self.assertEqual(res["res_model"], "stock.immediate.transfer")
 
     def test_mass_action_backorder(self):
-        wiz_tranfer = self.env['stock.picking.mass.action'].create({
-            'picking_ids': [(4, self.picking.id)],
-            'confirm': True,
-            'transfer': True,
-        })
+        wiz_tranfer = self.env["stock.picking.mass.action"].create(
+            {"picking_ids": [(4, self.picking.id)], "confirm": True, "transfer": True}
+        )
         self.picking.action_assign()
         self.picking.move_lines[0].quantity_done = 30
         res = wiz_tranfer.mass_action()
-        self.assertEqual(res['res_model'], 'stock.backorder.confirmation')
+        self.assertEqual(res["res_model"], "stock.backorder.confirmation")
