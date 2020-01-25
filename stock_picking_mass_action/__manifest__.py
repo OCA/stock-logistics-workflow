8319dc5a18691c9275539deec314497ead89b8b3
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-01-25 09:28:35 +0100

    [IMP] stock_picking_mass_action: black, isort

--- stock_picking_mass_action/__manifest__.py
+++ stock_picking_mass_action/__manifest__.py
@@ -3,20 +3,12 @@
 # Copyright 2019 Tecnativa - Carlos Dauden
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Stock Picking Mass Action',
-    'version': '12.0.1.1.0',
-    'author': 'Camptocamp, '
-              'GRAP,'
-              'Tecnativa,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/stock-logistics-workflow/',
-    'license': 'AGPL-3',
-    'category': 'Warehouse Management',
-    'depends': [
-        'stock_account',
-    ],
-    'data': [
-        'wizard/mass_action_view.xml',
-        'data/ir_cron.xml',
-    ],
+    "name": "Stock Picking Mass Action",
+    "version": "12.0.1.1.0",
+    "author": "Camptocamp, " "GRAP," "Tecnativa," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow/",
+    "license": "AGPL-3",
+    "category": "Warehouse Management",
+    "depends": ["stock_account"],
+    "data": ["wizard/mass_action_view.xml", "data/ir_cron.xml"],
 }
