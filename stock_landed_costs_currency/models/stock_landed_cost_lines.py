7a5a41d8ab3c0bedb3ffb8cbfbe8360034d127b2
ps-tubtim <pimolnats@ecosoft.co.th>
2020-01-27 16:28:43 +0700

    [IMP] stock_landed_costs_currency: black, isort

--- stock_landed_costs_currency/models/stock_landed_cost_lines.py
+++ stock_landed_costs_currency/models/stock_landed_cost_lines.py
@@ -4,14 +4,13 @@ from odoo import api, fields, models
 
 
 class LandedCostLine(models.Model):
-    _inherit = 'stock.landed.cost.lines'
+    _inherit = "stock.landed.cost.lines"
 
     price_unit = fields.Float(string="Cost in Company Currency")
     currency_id = fields.Many2one(related="cost_id.currency_id")
-    currency_price_unit = fields.Monetary(currency_field="currency_id",
-                                          string="Cost")
+    currency_price_unit = fields.Monetary(currency_field="currency_id", string="Cost")
 
-    @api.onchange('currency_price_unit')
+    @api.onchange("currency_price_unit")
     def _onchange_currency_price_unit(self):
         for rec in self:
             if rec.currency_price_unit:
@@ -19,12 +18,12 @@ class LandedCostLine(models.Model):
                 company = rec.cost_id.company_id
                 if rec.cost_id.currency_id != company.currency_id:
                     rec.price_unit = rec.cost_id.currency_id._convert(
-                        rec.currency_price_unit, company.currency_id,
-                        company, date)
+                        rec.currency_price_unit, company.currency_id, company, date
+                    )
                 else:
                     rec.price_unit = rec.currency_price_unit
 
-    @api.onchange('product_id')
+    @api.onchange("product_id")
     def onchange_product_id(self):
         res = super(LandedCostLine, self).onchange_product_id()
         self.currency_price_unit = self.price_unit
