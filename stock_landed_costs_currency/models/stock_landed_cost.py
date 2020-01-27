a5b8ebed4ad8d9a7e937d22260dc6a92288c3d10
ps-tubtim <pimolnats@ecosoft.co.th>
2020-01-27 16:28:43 +0700

    [IMP] stock_landed_costs_currency: black, isort

--- stock_landed_costs_currency/models/stock_landed_cost.py
+++ stock_landed_costs_currency/models/stock_landed_cost.py
@@ -4,19 +4,21 @@ from odoo import api, fields, models
 
 
 class LandedCost(models.Model):
-    _inherit = 'stock.landed.cost'
+    _inherit = "stock.landed.cost"
 
     currency_id = fields.Many2one(
-        'res.currency', required=True,
-        states={'done': [('readonly', True)]},
-        default=lambda self: self.env.user.company_id.currency_id)
+        "res.currency",
+        required=True,
+        states={"done": [("readonly", True)]},
+        default=lambda self: self.env.user.company_id.currency_id,
+    )
 
-    @api.onchange('account_journal_id')
+    @api.onchange("account_journal_id")
     def _onchange_account_journal_id(self):
         if self.account_journal_id and self.account_journal_id.currency_id:
             self.currency_id = self.account_journal_id.currency_id
 
-    @api.onchange('currency_id')
+    @api.onchange("currency_id")
     def _onchange_currency_id(self):
         if self.currency_id:
             self.cost_lines._onchange_currency_price_unit()
