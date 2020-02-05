65b47d1ad46dbd6da8aef0ebcb594e5ec1a2e860
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-02-05 20:53:47 +0100

    [IMP] stock_picking_whole_scrap: black, isort

--- stock_picking_whole_scrap/__manifest__.py
+++ stock_picking_whole_scrap/__manifest__.py
@@ -1,21 +1,16 @@
 # Copyright 2018 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Stock Picking Whole Scrap',
-    'summary': 'Create whole scrap from a picking for move lines',
-    'version': '12.0.1.0.0',
-    'development_status': 'Production/Stable',
-    'category': 'Warehouse',
-    'website': 'https://github.com/OCA/stock-logistics-workflow',
-    'author': 'Tecnativa, Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'installable': True,
-    'depends': [
-        'stock',
-    ],
-    'data': [
-        'wizards/stock_picking_whole_scrap.xml',
-        'views/stock_picking_views.xml',
-    ],
-    'maintainers': ['sergio-teruel'],
+    "name": "Stock Picking Whole Scrap",
+    "summary": "Create whole scrap from a picking for move lines",
+    "version": "12.0.1.0.0",
+    "development_status": "Production/Stable",
+    "category": "Warehouse",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["stock"],
+    "data": ["wizards/stock_picking_whole_scrap.xml", "views/stock_picking_views.xml"],
+    "maintainers": ["sergio-teruel"],
 }
