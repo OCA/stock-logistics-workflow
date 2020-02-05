a367bc77072f286b916063caea13154f8df37f26
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-02-05 20:53:47 +0100

    [IMP] stock_picking_whole_scrap: black, isort

--- stock_picking_whole_scrap/models/stock_picking.py
+++ stock_picking_whole_scrap/models/stock_picking.py
@@ -4,16 +4,16 @@ from odoo import _, models
 
 
 class StockPicking(models.Model):
-    _inherit = 'stock.picking'
+    _inherit = "stock.picking"
 
     def button_whole_scrap(self):
         self.ensure_one()
         return {
-            'name': _('Whole Scrap'),
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': 'wiz.stock.picking.scrap',
-            'type': 'ir.actions.act_window',
-            'context': {'default_picking_id': self.id},
-            'target': 'new',
+            "name": _("Whole Scrap"),
+            "view_type": "form",
+            "view_mode": "form",
+            "res_model": "wiz.stock.picking.scrap",
+            "type": "ir.actions.act_window",
+            "context": {"default_picking_id": self.id},
+            "target": "new",
         }
