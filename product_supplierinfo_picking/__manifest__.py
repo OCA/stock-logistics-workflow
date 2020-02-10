a96f4d27f190fefe30d06d60aee166b6a56d97bb
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2020-02-10 16:33:08 +0100

    [12.0][ADD] product_supplierinfo_picking

---
+++ product_supplierinfo_picking/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2020 ForgeFlow S.L.
+#     (<https://www.forgeflow.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Product Supplierinfo Picking",
+    "version": "12.0.1.0.0",
+    "author": "ForgeFlow, "
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "category": "Stock",
+    "summary": "This module makes the product supplier code visible "
+               "in the stock moves of a picking.",
+    "license": "AGPL-3",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_picking_view.xml",
+    ],
+    "installable": True,
+}
