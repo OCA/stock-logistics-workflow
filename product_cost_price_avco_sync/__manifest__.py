d478c8bc3dde63a013658a7bb4271cc36611e61e
Carlos Dauden <carlos.dauden@tecnativa.com>
2020-03-04 10:38:45 +0100

    [FIX] product_cost_price_avco_sync: Aleatory error because compare value before BD write.

--- product_cost_price_avco_sync/__manifest__.py
+++ product_cost_price_avco_sync/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Product cost price avco sync',
     'summary': 'Set product cost price from updated moves',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'development_status': 'Production/Stable',
     'category': 'Stock',
     'website': 'https://github.com/OCA/stock-logistics-workflow',
