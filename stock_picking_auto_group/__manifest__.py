5db1ff0eeb818668f4c66285d11996bd99210f59
Ryan Cole <admin@ryanc.me>
2020-03-06 00:29:56 +0000

    [ADD] stock_picking_auto_group: New module to automatically create procurement groups on individual stock pickings.

---
+++ stock_picking_auto_group/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020 Ryan Cole (www.ryanc.me)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+
+{
+    'name': 'Auto-Group Stock Pickings',
+    'summary': 'Automatically create procurement groups for individual stock pickings.',
+    'author': 'Ryan Cole, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/stock-logistics-workflow',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'category': 'Inventory',
+    'depends': [
+        'stock',
+    ],
+
+    'data': [
+        'views/views.xml',
+    ],
+    
+    'installable': True,
+    'application': False,
+}
