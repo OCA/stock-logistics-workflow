426cf5055d5abde32c7b2ab18dfc19ee2844745b
Ryan Cole <admin@ryanc.me>
2020-03-06 02:13:07 +0000

    [FIX] Fixed Flake8 errors in stock_picking_auto_group 2

--- stock_picking_auto_group/models/models.py
+++ stock_picking_auto_group/models/models.py
@@ -9,65 +9,69 @@ _logger = logging.getLogger(__name__)
 
 
 class StockPickingType(models.Model):
-	_inherit = 'stock.picking.type'
+    _inherit = 'stock.picking.type'
 
-	create_procurement_group = fields.Boolean(string='Create Group', help='Automatically create procurement groups for stock transfers originating from this picking type', default=False)
+    create_procurement_group = fields.Boolean(
+        string='Create Group',
+        help='Automatically create procurement groups for stock transfers originating \
+              from this picking type',
+        default=False)
 
 
 class StockPicking(models.Model):
-	_inherit = 'stock.picking'
+    _inherit = 'stock.picking'
 
-	@api.model
-	def create(self, values):
-		# copied from official Odoo code. we need the name field to generate a
+    @api.model
+    def create(self, values):
+        # copied from official Odoo code. we need the name field to generate a
         # procurement group, but the default create() is not easily overridable
         # in this case.
 
-		defaults = self.default_get(['name', 'picking_type_id'])
+        defaults = self.default_get(['name', 'picking_type_id'])
 
-		if (values.get('name', '/') == '/' and
-            defaults.get('name', '/') == '/' and
-            values.get('picking_type_id', defaults.get('picking_type_id'))):
+        if (values.get('name', '/') == '/' and
+           defaults.get('name', '/') == '/' and
+           values.get('picking_type_id', defaults.get('picking_type_id'))):
 
             picking_type = self.env['stock.picking.type'].browse(
                 values.get('picking_type_id', defaults.get('picking_type_id')))
 
-			values['name'] = picking_type.sequence_id.next_by_id()
+            values['name'] = picking_type.sequence_id.next_by_id()
 
-		picking_type_id = self.env['stock.picking.type'].browse(
+        picking_type_id = self.env['stock.picking.type'].browse(
             values['picking_type_id'])
 
-		if picking_type_id.create_procurement_group:
-			moves_nogroup = []
+        if picking_type_id.create_procurement_group:
+            moves_nogroup = []
 
-			# this might not be strictly necessary - it is very unlikely (impossible?)
-			# that _some_ moves have a group, and _some_ don't.
-			for stock_move_data in values.get('move_lines', []):
+            # this might not be strictly necessary - it is very unlikely (impossible?)
+            # that _some_ moves have a group, and _some_ don't.
+            for stock_move_data in values.get('move_lines', []):
 
-				# move lines come in the x2x-update format:
-				# e.g. tuple(0, 0, {values})
-				if not stock_move_data[2].get('group_id', False):
-					moves_nogroup.append(stock_move_data)
+                # move lines come in the x2x-update format:
+                # e.g. tuple(0, 0, {values})
+                if not stock_move_data[2].get('group_id', False):
+                    moves_nogroup.append(stock_move_data)
 
-			if len(moves_nogroup) > 0:
-				procurement_group = self.env['procurement.group'].create({
-                    'name': values.get('name', False), 
+            if len(moves_nogroup) > 0:
+                procurement_group = self.env['procurement.group'].create({
+                    'name': values.get('name', False),
                     'move_type': 'direct'})
 
-				# assign it
-				for move in moves_nogroup:
-					move[2]['group_id'] = procurement_group.id
+                # assign it
+                for move in moves_nogroup:
+                    move[2]['group_id'] = procurement_group.id
 
-		return super(StockPicking, self).create(values)
+        return super(StockPicking, self).create(values)
 
-	@api.multi
-	def write(self, values):
-		for record in self:
-			if record.picking_type_id.create_procurement_group:
+    @api.multi
+    def write(self, values):
+        for record in self:
+            if record.picking_type_id.create_procurement_group:
 
-				# ensure that new lines inherit the parent stock.picking's group
-				for stock_move_data in values.get('move_lines', []):
-					if not stock_move_data[2].get('group_id', False):
-						stock_move_data[2]['group_id'] = record.group_id.id
+                # ensure that new lines inherit the parent stock.picking's group
+                for stock_move_data in values.get('move_lines', []):
+                    if not stock_move_data[2].get('group_id', False):
+                        stock_move_data[2]['group_id'] = record.group_id.id
 
-		super(StockPicking, self).write(values)
+        super(StockPicking, self).write(values)
