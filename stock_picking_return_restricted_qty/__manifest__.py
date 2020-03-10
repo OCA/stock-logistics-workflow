ff1a2795ec5c73bba83d74c41519d1340348d9df
Carlos Roca <carlos.roca@tecnativa.com>
2020-03-10 11:28:23 +0100

    [ADD] stock_picking_return_restricted_qty New module

---
+++ stock_picking_return_restricted_qty/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Tecnativa - Carlos Roca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Picking Return Restricted Qty",
+    "summary": "Restrict the return to delivered quantity",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Tecnativa," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow/",
+    "depends": ["stock"],
+    "data": [],
+}
