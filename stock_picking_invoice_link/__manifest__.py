1a81e55e176bbc6bffbf719938aa50072fe950d5
Manuel Calero <manuel.calero@tecnativa.com>
2020-03-12 17:19:03 +0100

    [IMP] stock_picking_invoice_link: black, isort

--- stock_picking_invoice_link/__manifest__.py
+++ stock_picking_invoice_link/__manifest__.py
@@ -5,23 +5,18 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name': 'Stock Picking Invoice Link',
-    'version': '12.0.1.0.0',
-    'category': 'Warehouse Management',
-    'summary': 'Adds link between pickings and invoices',
-    'author': 'Agile Business Group, '
-              'Tecnativa, '
-              'BCIM sprl, '
-              'Softdil S.L, '
-              'Odoo Community Association (OCA)',
-    'website': 'http://github.com/OCA/stock-logistics-workflow',
-    'license': 'AGPL-3',
-    'depends': [
-        'sale_stock',
-    ],
-    'data': [
-        'views/stock_view.xml',
-        'views/account_invoice_view.xml',
-    ],
-    'installable': True,
+    "name": "Stock Picking Invoice Link",
+    "version": "12.0.1.0.0",
+    "category": "Warehouse Management",
+    "summary": "Adds link between pickings and invoices",
+    "author": "Agile Business Group, "
+    "Tecnativa, "
+    "BCIM sprl, "
+    "Softdil S.L, "
+    "Odoo Community Association (OCA)",
+    "website": "http://github.com/OCA/stock-logistics-workflow",
+    "license": "AGPL-3",
+    "depends": ["sale_stock"],
+    "data": ["views/stock_view.xml", "views/account_invoice_view.xml"],
+    "installable": True,
 }
--- stock_picking_invoice_link/models/stock_move.py
+++ stock_picking_invoice_link/models/stock_move.py
@@ -10,8 +10,8 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
     invoice_line_id = fields.Many2one(
-        comodel_name='account.invoice.line',
-        string='Invoice Line',
+        comodel_name="account.invoice.line",
+        string="Invoice Line",
         copy=False,
         readonly=True,
     )
