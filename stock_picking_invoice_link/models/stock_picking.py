0f0214dfe8fd3b1d5192dddff684b921d423ae8e
Manuel Calero <manuel.calero@tecnativa.com>
2020-03-12 18:00:13 +0100

    [MIG] stock_picking_invoice_link: Migration to 13.0

--- stock_picking_invoice_link/models/stock_picking.py
+++ stock_picking_invoice_link/models/stock_picking.py
@@ -4,17 +4,16 @@
 # Copyright 2017 Jacques-Etienne Baudoux <je@bcim.be>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     invoice_ids = fields.Many2many(
-        comodel_name="account.invoice", copy=False, string="Invoices", readonly=True
+        comodel_name="account.move", copy=False, string="Invoices", readonly=True
     )
 
-    @api.multi
     def action_view_invoice(self):
         """This function returns an action that display existing invoices
         of given stock pickings.
@@ -22,12 +21,13 @@ class StockPicking(models.Model):
         one invoice to show.
         """
         self.ensure_one()
-        action = self.env.ref("account.action_invoice_tree1")
+        form_view_name = "account.view_move_form"
+        action = self.env.ref("account.action_move_out_invoice_type")
         result = action.read()[0]
         if len(self.invoice_ids) > 1:
             result["domain"] = "[('id', 'in', %s)]" % self.invoice_ids.ids
         else:
-            form_view = self.env.ref("account.invoice_form")
+            form_view = self.env.ref(form_view_name)
             result["views"] = [(form_view.id, "form")]
             result["res_id"] = self.invoice_ids.id
         return result
