f19211149ffe07f99a4fad8e6b8a4d26a484fc8e
Manuel Calero <manuel.calero@tecnativa.com>
2020-03-12 18:00:13 +0100

    [MIG] stock_picking_invoice_link: Migration to 13.0

--- stock_picking_invoice_link/models/sale_order.py
+++ stock_picking_invoice_link/models/sale_order.py
@@ -1,38 +1,43 @@
 # Copyright 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
 # Copyright 2017 Jacques-Etienne Baudoux <je@bcim.be>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-from odoo import api, models
+from odoo import models
+from odoo.tools import float_compare
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    @api.multi
-    def invoice_line_create_vals(self, invoice_id, qty):
-        self.mapped("move_ids").filtered(
-            lambda x: x.state == "done"
-            and not x.invoice_line_id
-            and not x.location_dest_id.scrap_location
-            and x.location_dest_id.usage == "customer"
-        ).mapped("picking_id").write({"invoice_ids": [(4, invoice_id)]})
-        return super(SaleOrderLine, self).invoice_line_create_vals(invoice_id, qty)
-
-    @api.multi
-    def _prepare_invoice_line(self, qty):
-        vals = super(SaleOrderLine, self)._prepare_invoice_line(qty)
-        move_ids = (
-            self.mapped("move_ids")
-            .filtered(
-                lambda x: x.state == "done"
-                and not x.invoice_line_id
-                and not x.scrapped
+    def get_stock_moves_link_invoice(self):
+        return self.mapped("move_ids").filtered(
+            lambda mv: (
+                mv.state == "done"
+                and not (
+                    any(
+                        inv.state != "cancel"
+                        for inv in mv.invoice_line_ids.mapped("move_id")
+                    )
+                )
+                and not mv.scrapped
                 and (
-                    x.location_dest_id.usage == "customer"
-                    or (x.location_id.usage == "customer" and x.to_refund)
+                    mv.location_dest_id.usage == "customer"
+                    or (mv.location_id.usage == "customer" and mv.to_refund)
                 )
             )
-            .ids
         )
-        vals["move_line_ids"] = [(6, 0, move_ids)]
+
+    def _prepare_invoice_line(self):
+        vals = super()._prepare_invoice_line()
+        stock_moves = self.get_stock_moves_link_invoice()
+        # Invoice returned moves marked as to_refund
+        if (
+            float_compare(
+                self.qty_to_invoice, 0.0, precision_rounding=self.currency_id.rounding
+            )
+            < 0
+        ):
+            stock_moves = stock_moves.filtered(
+                lambda m: m.to_refund and not m.invoice_line_ids
+            )
+        vals["move_line_ids"] = [(4, m.id) for m in stock_moves]
         return vals
