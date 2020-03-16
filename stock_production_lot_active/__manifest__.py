c4ff5ca49c54b975b528c26823872dbd91a2716e
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2020-03-16 08:51:21 +0100

    [ADD] Stock Production Lot Active

---
+++ stock_production_lot_active/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Production Lot Active",
+    "summary": """
+        Allow to archive/unarchive a lot.""",
+    "version": "13.0.1.0.0",
+    "development_status": "Beta",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "maintainers": ["ThomasBinsfeld"],
+    "website": "https://github.com/oca/stock-logistics-workflow",
+    "depends": ["stock"],
+    "data": ["views/stock_production_lot.xml"],
+    "demo": [],
+}
