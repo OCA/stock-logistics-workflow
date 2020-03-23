ce2dc68dc3700e5abdefc67899a72f34e665194a
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2020-03-23 18:17:31 +0100

    [ADD] stock_picking_group_by_partner_by_carrier
    This module can be used if your customers expect that several different orders
    they passed will be shipped in a single delivery order.
    
    With this module installed, when a sale order is confirmed, the stock moves for
    the lines of the sale order can be placed in an existing delivery order that
    shares the same delivery address and carrier (or lack thereof).
    
    Sale orders with a Shipping Policy set to 'When all products are ready' always
    get their own shipping.

---
+++ stock_picking_group_by_partner_by_carrier/models/stock_move.py
@@ -0,0 +1,83 @@
+import re
+from collections import namedtuple
+
+from odoo import models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    def _assign_picking(self):
+        return super(
+            StockMove, self.with_context(picking_no_overwrite_partner_origin=1)
+        )._assign_picking()
+
+    def _assign_picking_post_process(self, new=False):
+        res = super()._assign_picking_post_process(new=new)
+        if not new:
+            picking = self.mapped("picking_id")
+            picking.ensure_one()
+            sales = self.mapped("sale_line_id.order_id")
+            for sale in sales:
+                pattern = r"\b%s\b" % sale.name
+                if not re.search(pattern, picking.origin):
+                    picking.origin += " " + sale.name
+                    picking.message_post_with_view(
+                        "mail.message_origin_link",
+                        values={"self": picking, "origin": sale},
+                        subtype_id=self.env.ref("mail.mt_note").id,
+                    )
+        return res
+
+    def _search_picking_for_assignation(self):
+        # totally reimplement this one to add a hook to change the domain
+        self.ensure_one()
+        picking = self.env["stock.picking"].search(
+            self._domain_search_picking_for_assignation(), limit=1
+        )
+        return picking
+
+    def _domain_search_picking_for_assignation(self):
+        states = ("draft", "confirmed", "waiting", "partially_available", "assigned")
+        if (
+            not self.picking_type_id.group_pickings
+            or self.sale_line_id.order_id.picking_policy == "one"
+        ):
+            # use the normal domain from the stock module
+            domain = [
+                ("group_id", "=", self.group_id.id),
+            ]
+        else:
+            domain = [
+                # same partner
+                ("partner_id", "=", self.group_id.partner_id.id),
+                # same carrier
+                ("carrier_id", "=", self.group_id.carrier_id.id),
+                # avoid mixing picking policies
+                ("move_type", "=", self.group_id.move_type),
+                # don't search on the procurement.group
+            ]
+        domain += [
+            ("location_id", "=", self.location_id.id),
+            ("location_dest_id", "=", self.location_dest_id.id),
+            ("picking_type_id", "=", self.picking_type_id.id),
+            ("printed", "=", False),
+            ("immediate_transfer", "=", False),
+            ("state", "in", states),
+        ]
+        if self.env.context.get("picking_no_copy_if_can_group"):
+            # we are in the context of the creation of a backorder:
+            # don't consider the current move's picking
+            domain.append(("id", "!=", self.picking_id.id))
+        return domain
+
+    def _key_assign_picking(self):
+        return (
+            self.sale_line_id.order_id.partner_shipping_id,
+            PickingPolicy(id=self.sale_line_id.order_id.picking_policy),
+        ) + super()._key_assign_picking()
+
+
+# we define a named tuple because the code in module stock expects the values in
+# the tuple returned by _key_assign_picking to be records with an id attribute
+PickingPolicy = namedtuple("PickingPolicy", ["id"])
