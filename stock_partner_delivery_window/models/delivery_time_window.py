16c1e8b649d0d23413cf1e21343aef310d4949e9
Akim Juillerat <akim.juillerat@camptocamp.com>
2020-04-02 16:30:49 +0200

    fixup! fixup! fixup! fixup! Create module stock_partner_delivery_window

--- stock_partner_delivery_window/models/delivery_time_window.py
+++ stock_partner_delivery_window/models/delivery_time_window.py
@@ -3,8 +3,9 @@
 from datetime import datetime
 import pytz
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 from odoo.addons.base.models.res_partner import _tz_get
+from odoo.addons.partner_tz.tools import tz_utils
 
 
 class DeliveryTimeWindow(models.Model):
@@ -21,6 +22,22 @@ class DeliveryTimeWindow(models.Model):
     tz = fields.Selection(
         _tz_get, related="partner_id.tz", readonly=True,
     )
+    tz_display_name = fields.Char(compute="_compute_tz_display_name")
+
+    @api.depends("start", "end", "weekday_ids", "tz")
+    def _compute_tz_display_name(self):
+        for record in self:
+            start_time = tz_utils.tz_to_tz_time(
+                record.tz, self.env.user.tz, record.get_start_time()
+            )
+            end_time = tz_utils.tz_to_tz_time(
+                record.tz, self.env.user.tz, record.get_end_time()
+            )
+            record.tz_display_name = _("{days}: From {start} to {end}").format(
+                days=", ".join(record.weekday_ids.mapped("display_name")),
+                start="%02d:%02d" % (start_time.hour, start_time.minute),
+                end="%02d:%02d" % (end_time.hour, end_time.minute),
+            )
 
     @api.constrains("partner_id")
     def check_window_no_overlaps(self):
--- stock_partner_delivery_window/models/stock_picking.py
+++ stock_partner_delivery_window/models/stock_picking.py
@@ -1,6 +1,7 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 from odoo import _, api, models
+from odoo.addons.partner_tz.tools import tz_utils
 
 
 class StockPicking(models.Model):
@@ -17,6 +18,8 @@ class StockPicking(models.Model):
             return
         p = self.partner_id
         if not p.is_in_delivery_window(self.scheduled_date):
+            user_tz = self.env.user.tz
+            tz_scheduled_date = tz_utils.utc_to_tz_naive_datetime(user_tz, self.scheduled_date)
             return {
                 "warning": {
                     "title": _(
@@ -27,11 +30,11 @@ class StockPicking(models.Model):
                         "The scheduled date is %s, but the partner is "
                         "set to prefer deliveries on following time windows:\n%s"
                         % (
-                            # TODO handle date format + tz + translation
-                            self.scheduled_date,
+                            # TODO handle date format
+                            tz_scheduled_date,
                             '\n'.join(
                                 [
-                                    "  * %s" % w.display_name
+                                    "  * %s" % w.tz_display_name
                                     for w
                                     in p.get_delivery_windows().get(p.id)
                                 ]
