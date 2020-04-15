36383b2f710b889df2cc9f84823a2965c02be353
Akim Juillerat <akim.juillerat@camptocamp.com>
2020-04-15 12:40:31 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Create module stock_partner_delivery_window

--- stock_partner_delivery_window/models/stock_picking.py
+++ stock_partner_delivery_window/models/stock_picking.py
@@ -1,5 +1,7 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+from datetime import timedelta
+
 from odoo import _, api, models
 from odoo.tools.misc import format_datetime
 
@@ -17,26 +19,36 @@ class StockPicking(models.Model):
         ):
             return
         p = self.partner_id
-        if not p.is_in_delivery_window(self.scheduled_date):
+        sec_lead_time = self.company_id.security_lead
+        delivery_date = self.scheduled_date + timedelta(days=sec_lead_time)
+        if not p.is_in_delivery_window(delivery_date):
+            message = _(
+                "The scheduled date is %s, but the partner is "
+                "set to prefer deliveries on following time windows:\n%s"
+                % (
+                    format_datetime(self.env, self.scheduled_date),
+                    '\n'.join(
+                        [
+                            "  * %s" % w.display_name
+                            for w
+                            in p.get_delivery_windows().get(p.id)
+                        ]
+                    ),
+                )
+            )
+            if sec_lead_time:
+                message += _(
+                    "\nConsidering the security lead time of %s days defined on "
+                    "the company, the delivery will not match the partner time"
+                    "windows preference."
+                    % sec_lead_time
+                )
             return {
                 "warning": {
                     "title": _(
-                        "Scheduled date does not match partner's Delivery time"
-                        " schedule preference."
+                        "Scheduled date does not match partner's Delivery window"
+                        " preference."
                     ),
-                    "message": _(
-                        "The scheduled date is %s, but the partner is "
-                        "set to prefer deliveries on following time windows:\n%s"
-                        % (
-                            format_datetime(self.env, self.scheduled_date),
-                            '\n'.join(
-                                [
-                                    "  * %s" % w.display_name
-                                    for w
-                                    in p.get_delivery_windows().get(p.id)
-                                ]
-                            ),
-                        )
-                    ),
+                    "message": message,
                 }
             }
