20b98763fa16716d635a822c78de9516c2a9505a
newtratip <tharathip.chaweewongphan@gmail.com>
2020-04-17 15:54:37 +0700

    [13.0][MIG] stock_picking_filter_lot

--- stock_picking_filter_lot/__manifest__.py
+++ stock_picking_filter_lot/__manifest__.py
@@ -2,19 +2,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Stock picking filter lot",
-    "summary": "In picking out lots' selection, " "filter lots based on their location",
-    "version": "13.0.1.1.0",
+    "summary": "In picking out lots' selection, filter lots based on their location",
+    "version": "13.0.1.0.0",
     "category": "Warehouse",
-    "website": "https://github.com/OCA/stock-logistics-workflow"
-    "12.0/stock_filter_lot",
-    "author": "Agile Business Group, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "author": "Agile Business Group, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
     "depends": ["stock"],
-    "data": [
-        "views/stock_move_line_view.xml",
-        "views/stock_picking_view.xml",
-        "views/stock_scrap_view.xml",
-    ],
+    "data": ["views/stock_move_line_view.xml", "views/stock_scrap_view.xml"],
 }
