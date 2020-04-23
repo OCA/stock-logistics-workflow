116c3bed0e51c706f8ea790755b0b3a8a46c3558
Lluís Rovira @PlanetaTIC <lrovira@planetatic.com>
2020-04-23 11:02:57 +0200

    [FIX] Add module description

--- stock_picking_show_date_done/__manifest__.py
+++ stock_picking_show_date_done/__manifest__.py
@@ -3,12 +3,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Stock Picking Show Date Done',
-    'summary': 'Show and group date_done of Stock Pickings',
     'version': '10.0.1.0.0',
     'category': 'Warehouse Management',
     'author': 'PlanetaTIC, Odoo Community Association (OCA)',
     'website': 'https://www.planetatic.com',
     'license': 'AGPL-3',
+    'description': '''
+        Show and group date_done of Stock Pickings.''',
     'application': False,
     'installable': True,
     'depends': [
