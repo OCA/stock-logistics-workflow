1ab29716e54cb51255ca52b15489a1cac0c40926
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-05-01 15:34:33 +0200

    Update stock_picking_type_shipping_policy_group_by/__manifest__.py
    Co-authored-by: Sébastien Alix <sebalix@users.noreply.github.com>

--- stock_picking_type_shipping_policy_group_by/__manifest__.py
+++ stock_picking_type_shipping_policy_group_by/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 {
-    "name": "Stock Picking Type Shipping Policy -" " Group By Partner and Carrier",
+    "name": "Stock Picking Type Shipping Policy - Group By Partner and Carrier",
     "summary": "Glue module for Picking Type Shipping Policy"
     " and Group Transfers by Partner and Carrier",
     "version": "13.0.1.0.0",
