0767a2484db541085292c981a344238c6a91237c
xavierpiernas <xavier.piernas@qubiq.es>
2020-05-18 15:35:23 +0200

    [MIG] stock_picking_send_by_mail: Pre-commit to 13.0

--- stock_picking_send_by_mail/__manifest__.py
+++ stock_picking_send_by_mail/__manifest__.py
@@ -5,20 +5,14 @@
 {
     "name": "Stock Picking by Mail",
     "summary": "Send stock picking by email",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "author": "Sandra Figueroa Varela, "
-              "Tecnativa, "
-              "Odoo Community Association (OCA)",
+    "Tecnativa, "
+    "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "category": "Warehouse Management",
     "license": "AGPL-3",
-    "depends": [
-        "stock",
-        "mail",
-    ],
-    "data": [
-        'data/stock_picking_mail.xml',
-        'views/stock_picking_view.xml',
-    ],
+    "depends": ["stock", "mail"],
+    "data": ["data/stock_picking_mail.xml", "views/stock_picking_view.xml"],
     "installable": True,
 }
