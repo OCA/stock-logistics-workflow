011cfc8fbb48f2e4035ab417513a4afd2103aa09
xavierpiernas <xavier.piernas@qubiq.es>
2020-05-19 15:39:56 +0200

    [MIG] stock_split_picking: Migration to 13.0

--- stock_split_picking/__manifest__.py
+++ stock_split_picking/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Split picking",
     "summary": "Split a picking in two not transferred pickings",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "category": "Inventory",
     "author": "Camptocamp, " "Tecnativa, " "Odoo Community Association (OCA),",
     "license": "AGPL-3",
