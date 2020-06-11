52732d8d54c297dbf63fe71a6551e2a2f40bc991
andrescalle <andres.calle@trescloud.com>
2020-06-11 09:45:44 -0500

    Correccion tabulaciones

--- stock_account_returns_price_difference/models/stock_quant.py
+++ stock_account_returns_price_difference/models/stock_quant.py
@@ -31,34 +31,34 @@ class StockMove(models.Model):
             if self.location_id.usage == 'customer' and self.origin_returned_move_id:
                 #debit_value = self.origin_returned_move_id.price_unit * qty -- Odoo core error commented
                 credit_value = self.company_id.currency_id.round(self.origin_returned_move_id.price_unit * qty)
-        #if credit_value distinct than debit_value:
-        if float_compare(credit_value, debit_value, precision_digits=self.company_id.currency_id.decimal_places) != 0:
-            #change the amount of original line to keep the journal entry balanced
-            for line in res:
-                if line[2]['credit']> 0.0:
-                    line[2]['credit'] = credit_value
-                elif line[2]['debit'] > 0.0:
-                    line[2]['debit'] = debit_value
-            #continue computing the difference line
-            partner_id = (self.picking_id.partner_id and self.env['res.partner']._find_accounting_partner(self.picking_id.partner_id).id) or False
-            # for supplier returns of product in average costing method, in anglo saxon mode
-            diff_amount = debit_value - credit_value
-            price_diff_account = self.product_id.property_account_creditor_price_difference
-            if not price_diff_account:
-                price_diff_account = self.product_id.categ_id.property_account_creditor_price_difference_categ
-            if not price_diff_account:
-                raise UserError(_('Configuration error. Please configure the price difference account on the product or its category to process this operation.'))
-            price_diff_line = {
-                'name': self.name,
-                'product_id': self.product_id.id,
-                'quantity': qty,
-                'product_uom_id': self.product_id.uom_id.id,
-                'ref': self.picking_id.name,
-                'partner_id': partner_id,
-                'credit': diff_amount > 0 and diff_amount or 0,
-                'debit': diff_amount < 0 and -diff_amount or 0,
-                'account_id': price_diff_account.id,
-            }
-            res.append((0, 0, price_diff_line))
+            #if credit_value distinct than debit_value:
+            if float_compare(credit_value, debit_value, precision_digits=self.company_id.currency_id.decimal_places) != 0:
+                #change the amount of original line to keep the journal entry balanced
+                for line in res:
+                    if line[2]['credit']> 0.0:
+                        line[2]['credit'] = credit_value
+                    elif line[2]['debit'] > 0.0:
+                        line[2]['debit'] = debit_value
+                #continue computing the difference line
+                partner_id = (self.picking_id.partner_id and self.env['res.partner']._find_accounting_partner(self.picking_id.partner_id).id) or False
+                # for supplier returns of product in average costing method, in anglo saxon mode
+                diff_amount = debit_value - credit_value
+                price_diff_account = self.product_id.property_account_creditor_price_difference
+                if not price_diff_account:
+                    price_diff_account = self.product_id.categ_id.property_account_creditor_price_difference_categ
+                if not price_diff_account:
+                    raise UserError(_('Configuration error. Please configure the price difference account on the product or its category to process this operation.'))
+                price_diff_line = {
+                    'name': self.name,
+                    'product_id': self.product_id.id,
+                    'quantity': qty,
+                    'product_uom_id': self.product_id.uom_id.id,
+                    'ref': self.picking_id.name,
+                    'partner_id': partner_id,
+                    'credit': diff_amount > 0 and diff_amount or 0,
+                    'debit': diff_amount < 0 and -diff_amount or 0,
+                    'account_id': price_diff_account.id,
+                }
+                res.append((0, 0, price_diff_line))
         return res
     
