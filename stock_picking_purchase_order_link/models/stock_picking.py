d32c78bf4c92d93ef2c530b600cb4b4c43e46200
mreficent <miquel.raich@eficent.com>
2020-06-15 12:41:33 +0200

    [MIG] stock_picking_purchase_order_link: Migration to 13.0

--- stock_picking_purchase_order_link/models/stock_picking.py
+++ stock_picking_purchase_order_link/models/stock_picking.py
@@ -1,13 +1,12 @@
-# Copyright 2019 Eficent Business and IT Consulting Services S.L.
+# Copyright 2019 ForgeFlow S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import models
 
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    @api.multi
     def action_view_purchase_order(self):
         """This function returns an action that display existing purchase order
         of given picking.
