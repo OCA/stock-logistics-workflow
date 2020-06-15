ea7b143c6819ad8de96cbd3969da2067c0008b02
mreficent <miquel.raich@eficent.com>
2020-06-15 12:41:33 +0200

    [MIG] stock_picking_purchase_order_link: Migration to 13.0

--- stock_picking_purchase_order_link/__manifest__.py
+++ stock_picking_purchase_order_link/__manifest__.py
@@ -1,14 +1,12 @@
-# Copyright 2019 Eficent Business and IT Consulting Services S.L.
+# Copyright 2019 ForgeFlow S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Stock Picking Purchase Order Link",
-    "summary": """
-        Link between picking and purchase order""",
-    "version": "12.0.1.0.0",
+    "summary": "Link between picking and purchase order",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Eficent Business and IT Consulting Services S.L.,"
-    "Odoo Community Association (OCA)",
+    "author": "ForgeFlow S.L., Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "application": False,
     "installable": True,
