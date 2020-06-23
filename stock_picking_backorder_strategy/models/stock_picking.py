b306d7ba14d44be0ac56b156b9a3f1686cb743ff
Arthur Cruz <arthur@raizs.com.br>
2020-06-23 15:42:45 -0300

    feat: added handler to cases where we only want to disable backorders when the picking is being validated as a whole

--- stock_picking_backorder_strategy/models/stock_picking.py
+++ stock_picking_backorder_strategy/models/stock_picking.py
@@ -4,9 +4,16 @@
 from odoo import api, models
 
 
+import logging
+_logger = logging.getLogger(__name__)
+
+
 class StockPicking(models.Model):
 
     _inherit = "stock.picking"
+
+    def action_done(self):
+        return super(StockPicking, self.with_context(on_picking_validation=True)).action_done()
 
     @api.multi
     def _check_backorder(self):
@@ -23,7 +30,13 @@ class StockPicking(models.Model):
         res = False
         # Do nothing with pickings 'no_create'
         pickings = self.filtered(
-            lambda p: p.picking_type_id.backorder_strategy != 'no_create')
+            #The pickings we want to keep creating a backorder are the ones
+            #with a backorder strategy set to anything other than 'no_create'
+            #or the ones which we only want to skip backorder if we are validating the picking
+            #as a whole, but we are not on such context (validating only a stock move, for example).
+            lambda p: (p.picking_type_id.backorder_strategy != 'no_create') or\
+                (p.picking_type_id.disable_backorder_only_on_picking_validation and not self._context.get('on_picking_validation'))
+        )
         pickings_no_create = self - pickings
         pickings_no_create.mapped('move_lines')._cancel_remaining_quantities()
         res = super(StockPicking, pickings)._create_backorder(
