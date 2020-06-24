a6fb0ba3e11ca28b801fe4f2d1bb8887cf6d79d7
Iryna Vushnevska <i.vyshnevska@mobilunity.com>
2020-06-24 18:30:02 +0300

    [ADD] stock_picking_return_lot: create

---
+++ stock_picking_return_lot/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2020 Camptocamp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Picking Return Lot",
+    "summary": "This module don't allows you to fill up return with serial lots"
+    "from initial picking.",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow/",
+    "depends": ["stock", "stock_account"],
+}
