c0da7151beb9481d8a700079ff4b53e38c83c7fb
Akim Juillerat <akim.juillerat@camptocamp.com>
2020-06-30 13:42:02 +0200

    Move consideration of security_lead to sale dependent module

--- stock_partner_delivery_window/models/stock_picking.py
+++ stock_partner_delivery_window/models/stock_picking.py
@@ -1,13 +1,14 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
-from datetime import timedelta
-
 from odoo import _, api, models
 from odoo.tools.misc import format_datetime
 
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
+
+    def _planned_delivery_date(self):
+        return self.scheduled_date
 
     @api.onchange("scheduled_date")
     def _onchange_scheduled_date(self):
@@ -19,9 +20,7 @@ class StockPicking(models.Model):
         ):
             return
         p = self.partner_id
-        sec_lead_time = self.company_id.security_lead
-        delivery_date = self.scheduled_date + timedelta(days=sec_lead_time)
-        if not p.is_in_delivery_window(delivery_date):
+        if not p.is_in_delivery_window(self._planned_delivery_date()):
             message = _(
                 "The scheduled date is %s, but the partner is "
                 "set to prefer deliveries on following time windows:\n%s"
@@ -35,12 +34,6 @@ class StockPicking(models.Model):
                     ),
                 )
             )
-            if sec_lead_time:
-                message += _(
-                    "\nConsidering the security lead time of %s days defined on "
-                    "the company, the delivery will not match the partner time"
-                    "windows preference." % sec_lead_time
-                )
             return {
                 "warning": {
                     "title": _(
