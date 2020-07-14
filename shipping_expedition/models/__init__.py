4c7979ae6f2c95144a2bd27a8c38e27ebd368c08
victoralmau <victoralmau@gmail.com>
2020-07-14 12:15:31 +0200

    remo pr

--- shipping_expedition/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from . import delivery_carrier
-from . import crm_lead
-from . import sale_order
-from . import stock_picking
-from . import shipping_expedition
-from . import res_partner
