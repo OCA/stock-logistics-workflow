cbcf002e46c0761e2459829b0f89b7accab18507
Akim Juillerat <akim.juillerat@camptocamp.com>
2020-07-27 15:06:12 +0200

    Create module delivery_package_default_shipping_weight

---
+++ delivery_package_default_shipping_weight/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2020 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+{
+    "name": "Delivery Package Default Shipping Weight",
+    "summary": "Set default package shipping weight according to packaging",
+    "version": "13.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Operations/Inventory/Delivery",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["delivery"],
+    "data": ["views/product_packaging.xml"],
+}
---
+++ delivery_package_default_shipping_weight/tests/__init__.py
@@ -0,0 +1,1 @@
+from . import test_delivery_package_default_shipping_weight
