53c571b16034d7abf6192fec32d10f9cbfdfafe5
Juventino Villegas <tino@pop-os.localdomain>
2020-08-10 11:12:32 -0500

    [ADD] stock_picking_filter_product: Add module to filter products that are available in origin location when the picking is created

---
+++ stock_picking_filter_product/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Alan Ramos - Jarsa
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "Stock picking filter proucts",
+    "summary": "Show only available products based on their location",
+    "version": "13.0.1.0.0",
+    "category": "Warehouse",
+    "website": "https://github.com/OCA/stock-logistics-workflow"
+    "13.0/stock_filter_product",
+    "author": "Jarsa, " "Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["stock"],
+    "data": [
+        "views/stock_picking_view.xml",
+        "views/stock_scrap_view.xml",
+        "views/stock_move_line_view.xml",
+    ],
+}
