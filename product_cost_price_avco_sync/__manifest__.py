cbadf819c92b8a5ebabef5b7e1bfff9509c3a9d7
sergio-teruel <sergio.teruel@tecnativa.com>
2020-09-07 17:40:42 +0200

    [IMP] product_cost_price_avco_sync: black, isort

--- product_cost_price_avco_sync/__manifest__.py
+++ product_cost_price_avco_sync/__manifest__.py
@@ -2,17 +2,15 @@
 # Copyright 2020 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Product cost price avco sync',
-    'summary': 'Set product cost price from updated moves',
-    'version': '12.0.1.0.3',
-    'development_status': 'Production/Stable',
-    'category': 'Stock',
-    'website': 'https://github.com/OCA/stock-logistics-workflow',
-    'author': 'Tecnativa, Odoo Community Association (OCA)',
-    'maintainers': ['carlosdauden', 'sergio-teruel'],
-    'license': 'AGPL-3',
-    'installable': True,
-    'depends': [
-        'stock_account',
-    ],
+    "name": "Product cost price avco sync",
+    "summary": "Set product cost price from updated moves",
+    "version": "12.0.1.0.3",
+    "development_status": "Production/Stable",
+    "category": "Stock",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "maintainers": ["carlosdauden", "sergio-teruel"],
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["stock_account",],
 }
