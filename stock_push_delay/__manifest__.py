bc4d65f6b4d5d9bccf66981387ccd84a8a22305b
AaronHForgeFlow <ahenriquez@eficent.com>
2020-09-30 09:05:43 +0200

    [ADD]stock_push_delay v11

---
+++ stock_push_delay/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2020 ForgeFlow
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Stock Push Delay',
+    'summary': 'Manual evalaution of Push rules',
+    'version': '11.0.1.0.0',
+    'category': 'Inventory',
+    'author': "ForgeFlow, "
+              "Odoo Community Association (OCA)",
+    'license': 'AGPL-3',
+    'website': 'https://github.com/OCA/stock-logistics-workflow',
+    'depends': [
+        'stock',
+    ],
+
+}
