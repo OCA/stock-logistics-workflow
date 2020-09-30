5fe8d92f096a9dc9c394e301a015a5259ca4432f
mreficent <miquel.raich@eficent.com>
2020-09-30 12:48:39 +0200

    [UPD] Eficent -> ForgeFlow

--- stock_no_negative/tests/__init__.py
+++ stock_no_negative/tests/__init__.py
@@ -1,7 +1,7 @@
 # ?? 2015-2016 Akretion (http://www.akretion.com)
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
-# ?? 2016 Eficent Business and IT Consulting Services S.L.
-#   (http://www.eficent.com)
+# ?? 2016 ForgeFlow S.L.
+#   (http://www.forgeflow.com)
 # ?? 2016 Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
