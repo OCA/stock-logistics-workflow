1bf4f8addd479a0c3587d91c08c85da5045b889d
Denis Roussel <denis.roussel@acsone.eu>
2020-11-13 12:55:36 +0100

    [IMP] stock_auto_move: black, isort, prettier

--- stock_auto_move/models/stock_auto_move.py
+++ stock_auto_move/models/stock_auto_move.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2014-2015 NDP Systèmes (<http://www.ndp-systemes.fr>)
 
 from odoo import api, fields, models
@@ -10,24 +9,24 @@ class StockMove(models.Model):
     auto_move = fields.Boolean(
         "Automatic move",
         help="If this option is selected, the move will be automatically "
-        "processed as soon as the products are available.")
+        "processed as soon as the products are available.",
+    )
 
     @api.multi
     def action_assign(self, no_prepare=False):
         super(StockMove, self).action_assign(no_prepare=no_prepare)
         # Transfer all pickings which have an auto move assigned
-        moves = self.filtered(lambda m: m.state == 'assigned' and m.auto_move)
-        todo_pickings = moves.mapped('picking_id')
+        moves = self.filtered(lambda m: m.state == "assigned" and m.auto_move)
+        todo_pickings = moves.mapped("picking_id")
         # We create packing operations to keep packing if any
         todo_pickings.do_prepare_partial()
         moves.action_done()
 
     @api.multi
     def _change_procurement_group(self):
-        automatic_group = self.env.ref('stock_auto_move.automatic_group')
-        moves = self.filtered(
-            lambda m: m.auto_move and m.group_id != automatic_group)
-        moves.write({'group_id': automatic_group.id})
+        automatic_group = self.env.ref("stock_auto_move.automatic_group")
+        moves = self.filtered(lambda m: m.auto_move and m.group_id != automatic_group)
+        moves.write({"group_id": automatic_group.id})
 
     @api.multi
     def action_confirm(self):
@@ -36,31 +35,32 @@ class StockMove(models.Model):
 
 
 class ProcurementRule(models.Model):
-    _inherit = 'procurement.rule'
+    _inherit = "procurement.rule"
 
     auto_move = fields.Boolean(
         "Automatic move",
         help="If this option is selected, the generated move will be "
         "automatically processed as soon as the products are available. "
         "This can be useful for situations with chained moves where we "
-        "do not want an operator action.")
+        "do not want an operator action.",
+    )
 
 
 class ProcurementOrder(models.Model):
-    _inherit = 'procurement.order'
+    _inherit = "procurement.order"
 
     def _get_stock_move_values(self):
         res = super(ProcurementOrder, self)._get_stock_move_values()
         if self.rule_id:
-            res.update({'auto_move': self.rule_id.auto_move})
+            res.update({"auto_move": self.rule_id.auto_move})
         return res
 
 
 class StockLocationPath(models.Model):
-    _inherit = 'stock.location.path'
+    _inherit = "stock.location.path"
 
     @api.model
     def _apply(self, move):
         """Set auto move to the new move created by push rule."""
-        move.auto_move = self.auto == 'transparent'
+        move.auto_move = self.auto == "transparent"
         return super(StockLocationPath, self)._apply(move)
