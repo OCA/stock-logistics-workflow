db11166f2b4347c9faea399a75130db1b995f93b
Denis Roussel <denis.roussel@acsone.eu>
2020-11-18 14:52:52 +0100

    [10.0][IMP] stock_auto_move: auto_confirm=>auto_move
    As in v>=12, stock.location.path and procurement.rule have been merged
    into stock.rule, use the same field name.

--- stock_auto_move/__manifest__.py
+++ stock_auto_move/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-# Copyright 2014 NDP Systèmes (<http://www.ndp-systemes.fr>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2014 NDP Systèmes (<https://www.ndp-systemes.fr>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Automatic Move Processing',
-    'version': '10.0.1.0.0',
+    'version': '10.0.2.0.0',
     'author': 'NDP Systèmes, Odoo Community Association (OCA)',
     'category': 'Warehouse',
-    'website': 'http://www.ndp-systemes.fr',
+    'website': 'https://github.com/OCA/stock-logistics-workflow',
     'depends': ['stock'],
     'data': [
         'data/stock_auto_move_data.xml',
