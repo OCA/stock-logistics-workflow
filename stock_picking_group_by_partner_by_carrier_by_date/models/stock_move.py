d01edcc3f259d2ebecdc8ac1c14781d83fc56c9e
Simone Orsi <simahawk@gmail.com>
2020-11-26 10:50:18 +0100

    Add stock_picking_group_by_partner_by_carrier_by_date

---
+++ stock_picking_group_by_partner_by_carrier_by_date/models/stock_move.py
@@ -0,0 +1,34 @@
+# Copyright 2020 Camptocamp (https://www.camptocamp.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    def _domain_search_picking_for_assignation(self):
+        domain = super()._domain_search_picking_for_assignation()
+        cutoff_domain = self._domain_search_picking_for_assignation_cutoff()
+        domain.extend(cutoff_domain)
+        return domain
+
+    def _skip_search_picking_for_assignation_cutoff(self):
+        return (
+            # TODO: consider making this configurable on the picking type
+            self.picking_type_id.code != "outgoing"
+            or not self.picking_type_id.group_pickings
+            or self.group_id.sale_id.picking_policy == "one"
+        )
+
+    def _domain_search_picking_for_assignation_cutoff(self):
+        domain = []
+        if self._skip_search_picking_for_assignation_cutoff():
+            return domain
+        date_planned = self.date_expected
+        date_planned_end = date_planned.replace(hour=23, minute=59, second=59)
+        domain = [
+            ("scheduled_date", "<=", date_planned_end),
+            ("scheduled_date", ">=", date_planned),
+        ]
+        return domain
