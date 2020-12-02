b44b094d024b1317ae1bf44e5a3db38e94cec715
Florian da Costa <florian.dacosta@akretion.com>
2020-12-02 18:31:32 +0100

    Add module stock_restrict_lot

---
+++ stock_restrict_lot/__manifest__.py
@@ -0,0 +1,14 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock Restrict Lot",
+    "summary": "Base module that add back the concept of restrict lot on stock move",
+    "version": "14.0.1.1.0",
+    "category": "Warehouse Management",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "maintainers": ["florian-dacosta"],
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["stock"],
+}
