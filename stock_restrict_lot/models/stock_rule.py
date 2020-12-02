dc9f6f87adfeee6f06f9db635a66d5a74d2f4a4a
Florian da Costa <florian.dacosta@akretion.com>
2020-12-02 18:31:32 +0100

    Add module stock_restrict_lot

---
+++ stock_restrict_lot/models/stock_rule.py
@@ -0,0 +1,12 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models
+
+
+class StockRule(models.Model):
+    _inherit = "stock.rule"
+
+    def _get_custom_move_fields(self):
+        fields = super()._get_custom_move_fields()
+        fields += ["restrict_lot_id"]
+        return fields
