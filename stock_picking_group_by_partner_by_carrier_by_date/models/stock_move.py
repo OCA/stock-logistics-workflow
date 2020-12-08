59447717ce02fead922791b7b96bc1902ce9c715
Simone Orsi <simahawk@gmail.com>
2020-12-08 14:59:43 +0100

    stock_picking_group_by_* rebase on stock_move_assign_picking_hook

--- stock_picking_group_by_partner_by_carrier_by_date/models/stock_move.py
+++ stock_picking_group_by_partner_by_carrier_by_date/models/stock_move.py
@@ -7,22 +7,22 @@ from odoo import models
 class StockMove(models.Model):
     _inherit = "stock.move"
 
-    def _domain_search_picking_for_assignation(self):
-        domain = super()._domain_search_picking_for_assignation()
-        by_date_domain = self._domain_search_picking_for_assignation_by_date()
+    def _assign_picking_group_domain(self):
+        domain = super()._assign_picking_group_domain()
+        by_date_domain = self._assign_picking_group_domain_by_date()
         domain.extend(by_date_domain)
         return domain
 
-    def _skip_search_picking_for_assignation_by_date(self):
+    def _skip_assign_picking_group_domain_by_date(self):
         return (
             not self.picking_type_id.group_pickings_by_date
             or not self.picking_type_id.group_pickings
             or self.group_id.sale_id.picking_policy == "one"
         )
 
-    def _domain_search_picking_for_assignation_by_date(self):
+    def _assign_picking_group_domain_by_date(self):
         domain = []
-        if self._skip_search_picking_for_assignation_by_date():
+        if self._skip_assign_picking_group_domain_by_date():
             return domain
         date_planned = self.date_expected
         date_planned_end = date_planned.replace(hour=23, minute=59, second=59)
