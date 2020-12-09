e72371928acc76ebd9855377130a6073f4bc48b3
sergio-teruel <sergio.teruel@tecnativa.com>
2020-12-09 22:32:55 +0100

    [IMP] stock_picking_invoice_link: Better link invoices to stock_moves process

--- stock_picking_invoice_link/models/sale_order.py
+++ stock_picking_invoice_link/models/sale_order.py
@@ -3,23 +3,32 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import models
-from odoo.tools import float_compare
+from odoo.tools import float_compare, float_is_zero
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     def get_stock_moves_link_invoice(self):
-        return self.mapped('move_ids').filtered(
-            lambda x: (
-                x.state == 'done' and not (any(
-                    inv.state != 'cancel' for inv in x.invoice_line_ids.mapped(
-                        'invoice_id'))) and not x.scrapped and (
-                    x.location_dest_id.usage == 'customer' or
-                    (x.location_id.usage == 'customer' and
-                     x.to_refund))
-            )
-        )
+        moves_linked = self.env["stock.move"]
+        for stock_move in self.move_ids:
+            if stock_move.state != 'done' or stock_move.scrapped or (
+                stock_move.location_dest_id.usage != "customer"
+                    and (stock_move.location_id.usage != "customer" or
+                         not stock_move.to_refund)):
+                continue
+            invoice_lines = stock_move.invoice_line_ids.filtered(
+                lambda invl: invl.invoice_id.state != "cancel")
+            invoiced_qty = 0
+            for inv_line in invoice_lines:
+                if inv_line.invoice_id.type == 'out_refund':
+                    invoiced_qty -= inv_line.quantity
+                else:
+                    invoiced_qty += inv_line.quantity
+            if float_is_zero(invoiced_qty,
+                             precision_rounding=self.product_uom.rounding):
+                moves_linked += stock_move
+        return moves_linked
 
     def invoice_line_create_vals(self, invoice_id, qty):
         stock_moves = self.get_stock_moves_link_invoice()
@@ -34,7 +43,6 @@ class SaleOrderLine(models.Model):
         # Invoice returned moves marked as to_refund
         if float_compare(
                 qty, 0.0, precision_rounding=self.currency_id.rounding) < 0:
-            stock_moves = stock_moves.filtered(
-                lambda m: m.to_refund and not m.invoice_line_ids)
+            stock_moves = stock_moves.filtered(lambda m: m.to_refund)
         vals['move_line_ids'] = [(4, m.id) for m in stock_moves]
         return vals
