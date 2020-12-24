7708be0a3684ccc31a1308494f6f721c731f4240
Carlos Roca <carlos.roca@tecnativa.com>
2020-12-24 08:27:28 +0100

    [MIG] stock_picking_product_assortment: Migration to v13.0

--- stock_picking_product_assortment/__manifest__.py
+++ stock_picking_product_assortment/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Stock Picking Product Assortment",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Warehouse Management",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "author": "Tecnativa, Odoo Community Association (OCA)",
