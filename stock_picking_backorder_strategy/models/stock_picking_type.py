48cf12ca25c354b7cb4d6d1d998f83b29a4a92e3
Sébastien Alix <sebastien.alix@camptocamp.com>
2021-01-06 13:27:53 +0100

    [IMP] stock_picking_backorder_strategy: black, isort, prettier

--- stock_picking_backorder_strategy/models/stock_picking_type.py
+++ stock_picking_backorder_strategy/models/stock_picking_type.py
@@ -5,13 +5,16 @@ from odoo import fields, models
 
 
 class StockPickingType(models.Model):
-    _inherit = 'stock.picking.type'
+    _inherit = "stock.picking.type"
 
     backorder_strategy = fields.Selection(
-        [('manual', 'Manual'), ('create', 'Create'),
-         ('no_create', 'No create'),
-         ('cancel', 'Cancel')],
-        default='manual',
+        [
+            ("manual", "Manual"),
+            ("create", "Create"),
+            ("no_create", "No create"),
+            ("cancel", "Cancel"),
+        ],
+        default="manual",
         help="Define what to do with backorder",
-        required=True
+        required=True,
     )
