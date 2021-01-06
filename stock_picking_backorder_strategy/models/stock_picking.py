1ecf114a6286bc59ddf8daed6a35f8da270a6a52
Sébastien Alix <sebastien.alix@camptocamp.com>
2021-01-06 13:49:19 +0100

    [MIG] stock_picking_backorder_strategy: Migration to 13.0

--- stock_picking_backorder_strategy/models/stock_picking.py
+++ stock_picking_backorder_strategy/models/stock_picking.py
@@ -1,14 +1,12 @@
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import models
 
 
 class StockPicking(models.Model):
-
     _inherit = "stock.picking"
 
-    @api.multi
     def _check_backorder(self):
         self.ensure_one()
         # If strategy == 'manual', let the normal process going on
@@ -16,20 +14,14 @@ class StockPicking(models.Model):
             return super(StockPicking, self)._check_backorder()
         return False
 
-    @api.multi
-    def _create_backorder(self, backorder_moves=None):
-        if backorder_moves is None:
-            backorder_moves = []
-        res = False
+    def _create_backorder(self):
         # Do nothing with pickings 'no_create'
         pickings = self.filtered(
             lambda p: p.picking_type_id.backorder_strategy != "no_create"
         )
         pickings_no_create = self - pickings
         pickings_no_create.mapped("move_lines")._cancel_remaining_quantities()
-        res = super(StockPicking, pickings)._create_backorder(
-            backorder_moves=backorder_moves
-        )
+        res = super(StockPicking, pickings)._create_backorder()
         to_cancel = res.filtered(
             lambda b: b.backorder_id.picking_type_id.backorder_strategy == "cancel"
         )
