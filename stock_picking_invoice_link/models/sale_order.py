47a724415bdfbcdda0674225e15f7488a691ae8e
João Marques <joao.marques@tecnativa.com>
2021-01-07 14:17:32 +0000

    [MIG] stock_picking_invoice_link: Migration to 14.0
    TT26455

--- stock_picking_invoice_link/models/sale_order.py
+++ stock_picking_invoice_link/models/sale_order.py
@@ -1,5 +1,6 @@
 # Copyright 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
 # Copyright 2017 Jacques-Etienne Baudoux <je@bcim.be>
+# Copyright 2021 Tecnativa - João Marques
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 from odoo import models
 from odoo.tools import float_compare
@@ -26,8 +27,8 @@ class SaleOrderLine(models.Model):
             )
         )
 
-    def _prepare_invoice_line(self):
-        vals = super()._prepare_invoice_line()
+    def _prepare_invoice_line(self, **optional_values):
+        vals = super()._prepare_invoice_line(**optional_values)
         stock_moves = self.get_stock_moves_link_invoice()
         # Invoice returned moves marked as to_refund
         if (
