f5676eea3d491795c66867f12ffb994d94d0e9a0
Denis Roussel <denis.roussel@acsone.eu>
2021-01-25 17:02:28 +0100

    [13.0][IMP] stock_picking_auto_create_lot: Allow multi recordset

--- stock_picking_auto_create_lot/models/stock_move_line.py
+++ stock_picking_auto_create_lot/models/stock_move_line.py
@@ -1,7 +1,6 @@
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import models
-from odoo.fields import first
 
 
 class StockMoveLine(models.Model):
@@ -22,12 +21,15 @@ class StockMoveLine(models.Model):
             products, we apply the lot with both different approaches.
         """
         values = []
+        lot_by_product = {}
         production_lot_obj = self.env["stock.production.lot"]
         for line in self:
             values.append(line._prepare_auto_lot_values())
         lots = production_lot_obj.create(values)
+        for lot in lots:
+            lot_by_product[lot.product_id.id] = lot
         for line in self:
-            lot = first(lots.filtered(lambda l: l.product_id == line.product_id))
+            lot = lot_by_product[line.product_id.id]
             line.lot_id = lot
             if lot.product_id.tracking == "serial":
                 lots -= lot
