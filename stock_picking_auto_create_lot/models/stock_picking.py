4aa57675c40b82e05e8269a6e7121143e31f337c
Denis Roussel <denis.roussel@acsone.eu>
2021-01-25 17:02:28 +0100

    [13.0][IMP] stock_picking_auto_create_lot: Allow multi recordset

--- stock_picking_auto_create_lot/models/stock_picking.py
+++ stock_picking_auto_create_lot/models/stock_picking.py
@@ -8,16 +8,19 @@ class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     def _set_auto_lot(self):
-        if self.picking_type_id.auto_create_lot:
-            lines = self.move_line_ids.filtered(
-                lambda x: (
-                    not x.lot_id
-                    and not x.lot_name
-                    and x.product_id.tracking != "none"
-                    and x.product_id.auto_create_lot
-                )
+        """
+            Allows to be called either by button or through code
+        """
+        pickings = self.filtered(lambda p: p.picking_type_id.auto_create_lot)
+        lines = pickings.mapped("move_line_ids").filtered(
+            lambda x: (
+                not x.lot_id
+                and not x.lot_name
+                and x.product_id.tracking != "none"
+                and x.product_id.auto_create_lot
             )
-            lines.set_lot_auto()
+        )
+        lines.set_lot_auto()
 
     def action_done(self):
         self._set_auto_lot()
@@ -25,4 +28,4 @@ class StockPicking(models.Model):
 
     def button_validate(self):
         self._set_auto_lot()
-        return super().action_done()
+        return super().button_validate()
