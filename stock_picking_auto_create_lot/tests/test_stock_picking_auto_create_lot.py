3e7423e3c69d7a8722965708347708d764cbc3fe
Denis Roussel <denis.roussel@acsone.eu>
2021-01-25 15:10:16 +0100

    [13.0][IMP] stock_picking_auto_create_lot: Allows to set lot on action_done
    As the picking could be validated by other means than button, allows
    to set lot anyway.
    
    Improve tests

--- stock_picking_auto_create_lot/tests/test_stock_picking_auto_create_lot.py
+++ stock_picking_auto_create_lot/tests/test_stock_picking_auto_create_lot.py
@@ -7,21 +7,24 @@ from .common import CommonStockPickingAutoCreateLot
 
 
 class TestStockPickingAutoCreateLot(CommonStockPickingAutoCreateLot, SavepointCase):
-    def test_auto_create_lot(self):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
         # Create 3 products with lot/serial and auto_create True/False
-        self.product = self._create_product()
-        self.product_serial = self._create_product(tracking="serial")
-        self.product_serial_not_auto = self._create_product(
-            tracking="serial", auto=False
-        )
-        self.picking_type_in.auto_create_lot = True
+        cls.product = cls._create_product()
+        cls.product_serial = cls._create_product(tracking="serial")
+        cls.product_serial_not_auto = cls._create_product(tracking="serial", auto=False)
+        cls.picking_type_in.auto_create_lot = True
 
-        self._create_picking()
-        self._create_move(product=self.product, qty=2.0)
-        self._create_move(product=self.product_serial, qty=3.0)
-        self._create_move(product=self.product_serial_not_auto, qty=4.0)
+        cls._create_picking()
+        cls._create_move(product=cls.product, qty=2.0)
+        cls._create_move(product=cls.product_serial, qty=3.0)
+        cls._create_move(product=cls.product_serial_not_auto, qty=4.0)
 
-        self.picking.action_assign()
+        cls.picking.action_assign()
+
+    def test_manual_lot(self):
+
         # Check the display field
         move = self.picking.move_lines.filtered(
             lambda m: m.product_id == self.product_serial
@@ -38,6 +41,56 @@ class TestStockPickingAutoCreateLot(CommonStockPickingAutoCreateLot, SavepointCa
             line.lot_id = self.lot_obj.create(line._prepare_auto_lot_values())
 
         self.picking.button_validate()
+        lot = self.env["stock.production.lot"].search(
+            [("product_id", "=", self.product.id)]
+        )
+        self.assertEqual(len(lot), 1)
+        # Search for serials
+        lot = self.env["stock.production.lot"].search(
+            [("product_id", "=", self.product_serial.id)]
+        )
+        self.assertEqual(len(lot), 3)
+
+    def test_auto_create_lot(self):
+        # Check the display field
+        move = self.picking.move_lines.filtered(
+            lambda m: m.product_id == self.product_serial
+        )
+        self.assertFalse(move.display_assign_serial)
+
+        move = self.picking.move_lines.filtered(
+            lambda m: m.product_id == self.product_serial_not_auto
+        )
+        self.assertTrue(move.display_assign_serial)
+
+        self.picking.action_done()
+        lot = self.env["stock.production.lot"].search(
+            [("product_id", "=", self.product.id)]
+        )
+        self.assertEqual(len(lot), 1)
+        # Search for serials
+        lot = self.env["stock.production.lot"].search(
+            [("product_id", "=", self.product_serial.id)]
+        )
+        self.assertEqual(len(lot), 3)
+
+    def test_auto_create_transfer_lot(self):
+        # Check the display field
+        move = self.picking.move_lines.filtered(
+            lambda m: m.product_id == self.product_serial
+        )
+        self.assertFalse(move.display_assign_serial)
+
+        moves = self.picking.move_lines.filtered(
+            lambda m: m.product_id == self.product_serial
+        )
+        for line in moves.mapped("move_line_ids"):
+            self.assertFalse(line.lot_id)
+
+        self.picking.button_validate()
+        for line in moves.mapped("move_line_ids"):
+            self.assertTrue(line.lot_id)
+
         lot = self.env["stock.production.lot"].search(
             [("product_id", "=", self.product.id)]
         )
