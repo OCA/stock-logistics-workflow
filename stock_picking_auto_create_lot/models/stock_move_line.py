f6cc855052f7f4c9a4ef0688a8b79373f60aa0a8
Denis Roussel <denis.roussel@acsone.eu>
2021-01-26 10:06:00 +0100

    [13.0][IMP] stock_picking_auto_create_lot: Fix serial generation, add tests

--- stock_picking_auto_create_lot/models/stock_move_line.py
+++ stock_picking_auto_create_lot/models/stock_move_line.py
@@ -1,6 +1,7 @@
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import models
+from odoo.fields import first
 
 
 class StockMoveLine(models.Model):
@@ -21,15 +22,18 @@ class StockMoveLine(models.Model):
             products, we apply the lot with both different approaches.
         """
         values = []
-        lot_by_product = {}
         production_lot_obj = self.env["stock.production.lot"]
+        lots_by_product = dict()
         for line in self:
             values.append(line._prepare_auto_lot_values())
         lots = production_lot_obj.create(values)
         for lot in lots:
-            lot_by_product[lot.product_id.id] = lot
+            if lot.product_id.id not in lots_by_product:
+                lots_by_product[lot.product_id.id] = lot
+            else:
+                lots_by_product[lot.product_id.id] += lot
         for line in self:
-            lot = lot_by_product[line.product_id.id]
+            lot = first(lots_by_product[line.product_id.id])
             line.lot_id = lot
             if lot.product_id.tracking == "serial":
-                lots -= lot
+                lots_by_product[line.product_id.id] -= lot
