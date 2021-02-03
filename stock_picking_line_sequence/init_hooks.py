981c48cb2cbd3d7f7584a35fa957453849bcd6da
Cecile Jallais <cjallais@archeti.ca>
2021-02-03 13:40:20 -0500

    [IMP] stock_picking_line_sequence: black, isort, prettier

--- stock_picking_line_sequence/init_hooks.py
+++ stock_picking_line_sequence/init_hooks.py
@@ -11,5 +11,5 @@ def post_init_hook(cr, pool):
     Fetches all the pickings and resets the sequence of the move lines
     """
     env = Environment(cr, SUPERUSER_ID, {})
-    stock = env['stock.picking'].search([])
+    stock = env["stock.picking"].search([])
     stock.with_context(skip_update_line_ids=True)._reset_sequence()
