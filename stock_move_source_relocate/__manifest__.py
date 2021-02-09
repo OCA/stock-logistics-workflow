cb518ba0ae6609b3cacad4307915c9e068f1bdb6
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2021-02-09 12:20:45 +0100

    stock_move_source_relocate: add dependency on stock_helper
    To remove the duplicate implementation of
    StockLocation.is_sublocation_of()

--- stock_move_source_relocate/__manifest__.py
+++ stock_move_source_relocate/__manifest__.py
@@ -3,14 +3,14 @@
 {
     "name": "Stock Move Source Relocation",
     "summary": "Change source location of unavailable moves",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.1.0",
     "development_status": "Alpha",
     "category": "Warehouse Management",
     "website": "https://github.com/OCA/wms",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
-    "installable": False,
-    "depends": ["stock"],
+    "installable": True,
+    "depends": ["stock", "stock_helper"],
     "data": ["views/stock_source_relocate_views.xml", "security/ir.model.access.csv"],
 }
--- stock_move_source_relocate/models/__init__.py
+++ stock_move_source_relocate/models/__init__.py
@@ -1,3 +1,2 @@
 from . import stock_move
-from . import stock_location
 from . import stock_source_relocate
