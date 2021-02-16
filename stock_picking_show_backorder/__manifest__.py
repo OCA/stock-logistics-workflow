7229e4e5d0f5616e954ac7a81fcf68e4bb50bbdf
Víctor Martínez <victor.martinez@tecnativa.com>
2021-02-16 13:11:21 +0100

    [MIG] stock_picking_show_backorder: Migration to 13.0

--- stock_picking_show_backorder/__manifest__.py
+++ stock_picking_show_backorder/__manifest__.py
@@ -3,14 +3,14 @@
 
 {
     "name": "Stock Picking Show Backorder",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Warehouse Management",
     "summary": "Provides a new field on stock pickings, allowing to display "
     "the corresponding backorders.",
     "author": "Eficent, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "depends": ["stock"],
-    "data": ["views/stock_view.xml",],
+    "data": ["views/stock_view.xml"],
     "installable": True,
     "license": "AGPL-3",
 }
