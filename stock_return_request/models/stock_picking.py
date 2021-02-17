2b3cc242f3c6ad4ff7956db6e8981ccfa490098c
sergio-teruel <sergio.teruel@tecnativa.com>
2021-02-17 22:24:40 +0100

    [IMP] stock_return_request: black, isort

--- stock_return_request/models/stock_picking.py
+++ stock_return_request/models/stock_picking.py
@@ -4,11 +4,9 @@ from odoo import fields, models
 
 
 class StockPicking(models.Model):
-    _inherit = 'stock.picking'
+    _inherit = "stock.picking"
 
-    stock_return_request_id = fields.Many2one(
-        comodel_name='stock.return.request',
-    )
+    stock_return_request_id = fields.Many2one(comodel_name="stock.return.request",)
 
     def _create_backorder(self, backorder_moves=None):
         """When we make a backorder of a picking in a return request, we
@@ -16,8 +14,9 @@ class StockPicking(models.Model):
         if backorder_moves is None:
             backorder_moves = []
         backorders = super()._create_backorder(backorder_moves)
-        rbo = backorders.filtered('backorder_id.stock_return_request_id')
+        rbo = backorders.filtered("backorder_id.stock_return_request_id")
         for backorder in rbo:
             backorder.stock_return_request_id = (
-                backorder.backorder_id.stock_return_request_id)
+                backorder.backorder_id.stock_return_request_id
+            )
         return backorders
