745a264e2b1e889a84d84cfe910fd4fd140713ab
xavierpiernas <xavier.piernas@qubiq.es>
2021-02-18 09:23:38 +0100

    [IMP] stock_picking_send_by_mail: black, isort, prettier

--- stock_picking_send_by_mail/__manifest__.py
+++ stock_picking_send_by_mail/__manifest__.py
@@ -7,18 +7,12 @@
     "summary": "Send stock picking by email",
     "version": "12.0.1.1.0",
     "author": "Sandra Figueroa Varela, "
-              "Tecnativa, "
-              "Odoo Community Association (OCA)",
+    "Tecnativa, "
+    "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "category": "Warehouse Management",
     "license": "AGPL-3",
-    "depends": [
-        "stock",
-        "mail",
-    ],
-    "data": [
-        'data/stock_picking_mail.xml',
-        'views/stock_picking_view.xml',
-    ],
+    "depends": ["stock", "mail",],
+    "data": ["data/stock_picking_mail.xml", "views/stock_picking_view.xml",],
     "installable": True,
 }
