64ad67c7b285432b4d43e428cc89b9773d9b0127
xavierpiernas <xavier.piernas@qubiq.es>
2021-02-18 09:23:38 +0100

    [IMP] stock_picking_send_by_mail: black, isort, prettier

--- stock_picking_send_by_mail/models/stock_picking.py
+++ stock_picking_send_by_mail/models/stock_picking.py
@@ -6,35 +6,31 @@ from odoo import _, api, models
 
 
 class StockPicking(models.Model):
-    _inherit = 'stock.picking'
+    _inherit = "stock.picking"
 
     @api.multi
     def action_picking_send(self):
         self.ensure_one()
         template = self.env.ref(
-            'stock_picking_send_by_mail.email_template_stock_picking',
-            False,
+            "stock_picking_send_by_mail.email_template_stock_picking", False,
         )
-        compose_form = self.env.ref(
-            'mail.email_compose_message_wizard_form',
-            False,
-        )
+        compose_form = self.env.ref("mail.email_compose_message_wizard_form", False,)
         ctx = dict(
-            default_model='stock.picking',
+            default_model="stock.picking",
             default_res_id=self.id,
             default_use_template=bool(template),
             default_template_id=template and template.id or False,
-            default_composition_mode='comment',
+            default_composition_mode="comment",
             user_id=self.env.user.id,
         )
         return {
-            'name': _('Compose Email'),
-            'type': 'ir.actions.act_window',
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': 'mail.compose.message',
-            'views': [(compose_form.id, 'form')],
-            'view_id': compose_form.id,
-            'target': 'new',
-            'context': ctx,
+            "name": _("Compose Email"),
+            "type": "ir.actions.act_window",
+            "view_type": "form",
+            "view_mode": "form",
+            "res_model": "mail.compose.message",
+            "views": [(compose_form.id, "form")],
+            "view_id": compose_form.id,
+            "target": "new",
+            "context": ctx,
         }
