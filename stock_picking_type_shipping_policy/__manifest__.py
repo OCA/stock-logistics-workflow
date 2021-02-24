b3fe779b63445045d7d3390d1e984aee6a0b57dd
Tran Thanh Phuc <phuc@trobz.com>
2021-02-24 23:40:53 +0700

    [MIG] stock_picking_type_shipping_policy: Migration to 14.0

--- stock_picking_type_shipping_policy/__manifest__.py
+++ stock_picking_type_shipping_policy/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Stock Picking Type Shipping Policy",
     "summary": "Define different shipping policies according to picking type",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "development_status": "Alpha",
     "category": "Warehouse Management",
     "website": "https://github.com/OCA/wms",
