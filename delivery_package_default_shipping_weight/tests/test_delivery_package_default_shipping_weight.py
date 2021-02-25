29d0bf9a63d9a3bd54e8451475142c53433920e7
dzung.tran <dungtd@trobz.com>
2021-02-25 19:25:36 +0700

    [MIG] delivery_package_default_shipping_weight: Migration to 14.0

--- delivery_package_default_shipping_weight/tests/test_delivery_package_default_shipping_weight.py
+++ delivery_package_default_shipping_weight/tests/test_delivery_package_default_shipping_weight.py
@@ -1,5 +1,5 @@
 # Copyright 2020 Camptocamp SA
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)shipping_weight
 from odoo import fields
 from odoo.exceptions import ValidationError
 from odoo.tests.common import SavepointCase
@@ -12,6 +12,9 @@ class TestDeliveryPackageDefaultShippingWeight(SavepointCase):
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
         cls.packaging = cls.env["product.packaging"].create(
             {"name": "Delivery package", "package_default_shipping_weight": 10.0}
+        )
+        cls.new_packaging = cls.env["product.packaging"].create(
+            {"name": "Delivery package", "package_default_shipping_weight": 12.0}
         )
         cls.picking_type_out = cls.env.ref("stock.picking_type_out")
 
@@ -29,10 +32,29 @@ class TestDeliveryPackageDefaultShippingWeight(SavepointCase):
         )
         move_line = fields.first(picking.move_line_ids_without_package)
         move_line.qty_done = move_line.product_uom_qty
-        picking.put_in_pack()
+        picking.action_put_in_pack()
         package = move_line.result_package_id
         self.assertAlmostEqual(package.shipping_weight, 0.0)
         package.packaging_id = self.packaging
         self.assertAlmostEqual(
             package.shipping_weight, self.packaging.package_default_shipping_weight
+        )
+
+    def test_onchange_packaging_id(self):
+        picking = self.env["stock.picking"].search(
+            [
+                ("picking_type_id", "=", self.picking_type_out.id),
+                ("state", "=", "assigned"),
+            ],
+            limit=1,
+        )
+        move_line = fields.first(picking.move_line_ids_without_package)
+        move_line.qty_done = move_line.product_uom_qty
+        picking.action_put_in_pack()
+        package = move_line.result_package_id
+        package.packaging_id = self.packaging
+        package.packaging_id = self.new_packaging
+        self.assertAlmostEqual(
+            package.shipping_weight,
+            package.packaging_id.package_default_shipping_weight,
         )
