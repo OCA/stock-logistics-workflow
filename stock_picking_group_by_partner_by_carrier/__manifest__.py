8985b9c8e3ae0c18a3b0aa58070fef53fbff1212
Tran Thanh Phuc <phuc@trobz.com>
2021-02-27 22:52:58 +0700

    [MIG] stock_picking_group_by_partner_by_carrier: Migration to 14.0

--- stock_picking_group_by_partner_by_carrier/__manifest__.py
+++ stock_picking_group_by_partner_by_carrier/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Picking: group by partner and carrier",
     "Summary": "Group sales deliveries moves in 1 picking per partner and carrier",
-    "version": "13.0.1.4.0",
+    "version": "14.0.1.0.0",
     "development_status": "Alpha",
     "author": "Camptocamp, BCIM, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-workflow",
@@ -17,6 +17,7 @@
         "report/report_delivery_slip.xml",
         "wizard/stock_picking_merge_wiz.xml",
         "wizard/stock_picking_merge_wiz_info_template.xml",
+        "security/ir.model.access.csv",
     ],
     "installable": True,
     "license": "AGPL-3",
