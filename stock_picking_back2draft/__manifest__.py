d187945c6c6ae7cdbb0928ef47a2b7a1d0efe441
Sébastien BEAU <sebastien.beau@akretion.com>
2021-03-02 00:17:34 +0100

    [MIG] stock_picking_back2draft: Migration to 14.0

--- stock_picking_back2draft/__manifest__.py
+++ stock_picking_back2draft/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Pickings back to draft",
     "summary": "Reopen cancelled pickings",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Warehouse Management",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "author": "Agile Business Group, Odoo Community Association (OCA)",
