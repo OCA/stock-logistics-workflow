eb3faf643420dd69e36e5d0dfbf1fa674b9ebc39
sergio-teruel <sergio.teruel@tecnativa.com>
2021-05-07 13:03:53 +0200

    [IMP] Improvements

--- product_cost_price_avco_sync/models/stock_valuation_layer.py
+++ product_cost_price_avco_sync/models/stock_valuation_layer.py
@@ -30,21 +30,23 @@ class StockValuationLayer(models.Model):
             .search(domain, order="create_date, id")
         )
 
-    def cost_price_avco_sync(self, vals):
+    def cost_price_avco_sync(self, vals):  # noqa: C901
         procesed_lines = set()
         for line in self.sorted(key=lambda l: (l.create_date, l.id)):
             if (
                 line.id in procesed_lines
                 or line.product_id.cost_method != "average"
-                or line.quantity <= 0.0
+                or line.quantity < 0.0
             ):
                 continue
             previous_price = previous_qty = 0.0
             # update_enabled determines if svl is after modified line to enable
             # write changes
             update_enabled = False
-
-            for svl in line.with_context(skip_avco_sync=True).get_svls_to_avco_sync():
+            svls_to_avco_sync = line.with_context(
+                skip_avco_sync=True
+            ).get_svls_to_avco_sync()
+            for svl in svls_to_avco_sync:
                 if svl.id == line.id:
                     qty = vals.get("quantity", line.quantity)
                     unit_cost = vals.get("unit_cost", line.unit_cost)
@@ -84,9 +86,50 @@ class StockValuationLayer(models.Model):
                             if total_qty
                             else unit_cost
                         )
+                    if previous_qty < 0:
+                        # Vacuum previous product outs without stock
+                        vacuum_qty = qty
+                        for svl_to_vacuum in svls_to_avco_sync.filtered(
+                            lambda ln: ln.remaining_qty < 0 and ln.quantity < 0.0
+                        ):
+                            if not svl_to_vacuum.quantity:
+                                continue
+                            if abs(svl_to_vacuum.remaining_qty) <= vacuum_qty:
+                                vacuum_qty += svl_to_vacuum.remaining_qty
+                                diff = -svl_to_vacuum.remaining_qty
+                                svl_to_vacuum.remaining_qty = 0.0
+                            else:
+                                svl_to_vacuum.remaining_qty += vacuum_qty
+                                diff = vacuum_qty
+                                vacuum_qty = 0.0
+                            # if svl.id != line.id:
+                            svl.remaining_qty = vacuum_qty
+                            svl.remaining_value = vacuum_qty * svl.unit_cost
+                            svl_to_vacuum.unit_cost = (
+                                (
+                                    svl_to_vacuum.unit_cost
+                                    * (abs(svl_to_vacuum.quantity + diff))
+                                )
+                                + previous_price * diff
+                            ) / abs(svl_to_vacuum.quantity)
+                            svl_to_vacuum.value = (
+                                svl_to_vacuum.quantity * svl_to_vacuum.unit_cost
+                            )
+                            svl_to_vacuum.remaining_value = (
+                                svl_to_vacuum.remaining_qty * svl_to_vacuum.unit_cost
+                            )
+                            if vacuum_qty == 0.0:
+                                break
                     previous_qty = total_qty
                 # Outgoing line in layer
                 elif f_compare < 0:
+                    if previous_qty < abs(qty):
+                        svl.remaining_qty = previous_qty + qty
+                    else:
+                        svl.remaining_qty = 0.0
+                    svl.remaining_value = line.currency_id.round(
+                        previous_price * svl.remaining_qty
+                    )
                     if update_enabled and float_compare(
                         unit_cost,
                         previous_price,
@@ -108,6 +151,8 @@ class StockValuationLayer(models.Model):
                     )
                     move_diff_price = move_diff / previous_qty
                     price = previous_price + old_diff + move_diff_price
+                    if self.env.context.get("get_price_from_description", True):
+                        price = float(svl.description.split(" ")[-1][:-1])
                     if update_enabled:
                         new_diff = price - previous_price
                         new_value = line.currency_id.round(new_diff * previous_qty)
@@ -134,19 +179,20 @@ class StockValuationLayer(models.Model):
                 ).sudo().write({"standard_price": previous_price})
             # Compute new values for layer line
             vals_unit_cost = vals.get("unit_cost", line.unit_cost)
-            if "quantity" in vals:
-                remaining_qty = line.remaining_qty - (line.quantity - vals["quantity"])
-            else:
-                remaining_qty = line.remaining_qty
+            # if "quantity" in vals:
+            #     remaining_qty = line.remaining_qty - (line.quantity -
+            #                                           vals["quantity"])
+            # else:
+            #     remaining_qty = line.remaining_qty
             vals.update(
                 {
                     "value": line.currency_id.round(
                         vals_unit_cost * vals.get("quantity", line.quantity)
                     ),
-                    "remaining_qty": remaining_qty,
-                    "remaining_value": line.currency_id.round(
-                        vals_unit_cost * remaining_qty
-                    ),
+                    # "remaining_qty": remaining_qty,
+                    # "remaining_value": line.currency_id.round(
+                    #     vals_unit_cost * remaining_qty
+                    # ),
                 }
             )
             # Update unit_cost for incoming stock moves
