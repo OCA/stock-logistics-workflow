ffd1610e8d374d2bc621c337a734db5af74cb3e1
Alexis de Lattre <alexis.delattre@akretion.com>
2021-06-21 16:55:30 +0200

    stock_cost_method_last: also update price when move is from production

--- stock_cost_method_last/models/stock_move.py
+++ stock_cost_method_last/models/stock_move.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016-2019 Akretion (http://www.akretion.com/)
+# Copyright 2016-2021 Akretion France (http://www.akretion.com/)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -13,7 +13,7 @@ class StockMove(models.Model):
     def product_price_update_before_done(self):
         super(StockMove, self).product_price_update_before_done()
         for move in self.filtered(
-                lambda move: move.location_id.usage == 'supplier' and
+                lambda move: move.location_id.usage in ('supplier', 'production') and
                 move.product_id.cost_method == 'last'):
             company_id = move.company_id.id
             move.product_id.with_context(force_company=company_id).write(
