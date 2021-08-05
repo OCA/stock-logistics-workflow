42c3d843196c4470b7eb75631ec02ae939fbc1a5
Sébastien Alix <sebastien.alix@camptocamp.com>
2021-08-05 09:46:05 +0200

    [MIG] stock_dynamic_routing_reserve_rule: Migration to 14.0

--- stock_dynamic_routing_reserve_rule/__manifest__.py
+++ stock_dynamic_routing_reserve_rule/__manifest__.py
@@ -5,7 +5,7 @@
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/wms",
     "category": "Warehouse Management",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
         "stock_dynamic_routing",  # OCA/wms
