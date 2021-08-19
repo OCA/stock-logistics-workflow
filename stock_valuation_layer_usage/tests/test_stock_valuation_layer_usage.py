179139b753e9043d08e3ea127823513030ad5da1
Simone Orsi <simahawk@gmail.com>
2021-08-19 10:32:36 +0200

    stock_valuation_layer_usage: boost tests

--- stock_valuation_layer_usage/tests/test_stock_valuation_layer_usage.py
+++ stock_valuation_layer_usage/tests/test_stock_valuation_layer_usage.py
@@ -3,98 +3,85 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import fields
-from odoo.tests.common import TransactionCase
+from odoo.tests.common import SavepointCase
 
 
-class TestStockValuationLayerUsage(TransactionCase):
-    def setUp(self):
-        super(TestStockValuationLayerUsage, self).setUp()
+class TestStockValuationLayerUsage(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
         # Get required Model
-        self.product_model = self.env["product.product"]
-        self.template_model = self.env["product.template"]
-        self.product_ctg_model = self.env["product.category"]
-        self.account_model = self.env["account.account"]
-        self.quant_model = self.env["stock.quant"]
-        self.layer_model = self.env["stock.valuation.layer"]
-        self.stock_location_model = self.env["stock.location"]
-        self.res_users_model = self.env["res.users"]
-        self.account_move_model = self.env["account.move"]
-        self.aml_model = self.env["account.move.line"]
-        self.journal_model = self.env["account.journal"]
+        cls.product_model = cls.env["product.product"]
+        cls.template_model = cls.env["product.template"]
+        cls.product_ctg_model = cls.env["product.category"]
+        cls.account_model = cls.env["account.account"]
+        cls.quant_model = cls.env["stock.quant"]
+        cls.layer_model = cls.env["stock.valuation.layer"]
+        cls.stock_location_model = cls.env["stock.location"]
+        cls.res_users_model = cls.env["res.users"]
+        cls.account_move_model = cls.env["account.move"]
+        cls.aml_model = cls.env["account.move.line"]
+        cls.journal_model = cls.env["account.journal"]
         # Get required Model data
-        self.product_uom = self.env.ref("uom.product_uom_unit")
-        self.company = self.env.ref("base.main_company")
-        self.stock_picking_type_out_id = self.ref("stock.picking_type_out")
-        self.stock_picking_type_in_id = self.ref("stock.picking_type_in")
-        self.stock_location_id = self.ref("stock.stock_location_stock")
-        self.stock_location_customer_id = self.ref("stock.stock_location_customers")
-        self.stock_location_supplier_id = self.ref("stock.stock_location_suppliers")
+        cls.product_uom = cls.env.ref("uom.product_uom_unit")
+        cls.company = cls.env.ref("base.main_company")
+        cls.stock_picking_type_out_id = cls.env.ref("stock.picking_type_out").id
+        cls.stock_picking_type_in_id = cls.env.ref("stock.picking_type_in").id
+        cls.stock_location_id = cls.env.ref("stock.stock_location_stock").id
+        cls.stock_location_customer_id = cls.env.ref(
+            "stock.stock_location_customers"
+        ).id
+        cls.stock_location_supplier_id = cls.env.ref(
+            "stock.stock_location_suppliers"
+        ).id
         # Account types
-        expense_type = self.env.ref("account.data_account_type_expenses")
-        equity_type = self.env.ref("account.data_account_type_equity")
-        asset_type = self.env.ref("account.data_account_type_fixed_assets")
+        expense_type = cls.env.ref("account.data_account_type_expenses")
+        equity_type = cls.env.ref("account.data_account_type_equity")
+        asset_type = cls.env.ref("account.data_account_type_fixed_assets")
         # Create account for Goods Received Not Invoiced
         name = "Goods Received Not Invoiced"
         code = "grni"
         acc_type = equity_type
-        self.account_grni = self._create_account(acc_type, name, code, self.company)
+        cls.account_grni = cls._create_account(acc_type, name, code, cls.company)
         # Create account for Cost of Goods Sold
         name = "Cost of Goods Sold"
         code = "cogs"
         acc_type = expense_type
-        self.account_cogs = self._create_account(acc_type, name, code, self.company)
+        cls.account_cogs = cls._create_account(acc_type, name, code, cls.company)
         # Create account for Goods Delivered Not Invoiced
         name = "Goods Delivered Not Invoiced"
         code = "gdni"
         acc_type = expense_type
-        self.account_gdni = self._create_account(acc_type, name, code, self.company)
+        cls.account_gdni = cls._create_account(acc_type, name, code, cls.company)
         # Create account for Inventory
         name = "Inventory"
         code = "inventory"
         acc_type = asset_type
-        self.account_inventory = self._create_account(
-            acc_type, name, code, self.company
-        )
+        cls.account_inventory = cls._create_account(acc_type, name, code, cls.company)
 
-        self.stock_journal = self.env["account.journal"].create(
+        cls.stock_journal = cls.env["account.journal"].create(
             {"name": "Stock journal", "type": "general", "code": "STK00"}
         )
         # Create product category
-        self.product_ctg = self._create_product_category()
+        cls.product_ctg = cls._create_product_category()
 
         # Create partners
-        self.supplier = self.env["res.partner"].create({"name": "Test supplier"})
-        self.customer = self.env["res.partner"].create({"name": "Test customer"})
+        cls.supplier = cls.env["res.partner"].create({"name": "Test supplier"})
+        cls.customer = cls.env["res.partner"].create({"name": "Test customer"})
 
         # Create a Product with real cost
         standard_price = 10.0
         list_price = 20.0
-        self.product = self._create_product(standard_price, False, list_price)
+        cls.product = cls._create_product(standard_price, False, list_price)
 
         # Create a vendor
-        self.vendor_partner = self.env["res.partner"].create(
-            {"name": "dropship vendor"}
-        )
+        cls.vendor_partner = cls.env["res.partner"].create({"name": "dropship vendor"})
 
-    def _create_user(self, login, groups, company):
-        """ Create a user."""
-        group_ids = [group.id for group in groups]
-        user = self.res_users_model.with_context({"no_reset_password": True}).create(
-            {
-                "name": "Test User",
-                "login": login,
-                "password": "demo",
-                "email": "test@yourcompany.com",
-                "company_id": company.id,
-                "company_ids": [(4, company.id)],
-                "groups_id": [(6, 0, group_ids)],
-            }
-        )
-        return user
-
-    def _create_account(self, acc_type, name, code, company):
+    @classmethod
+    def _create_account(cls, acc_type, name, code, company):
         """Create an account."""
-        account = self.account_model.create(
+        account = cls.account_model.create(
             {
                 "name": name,
                 "code": code,
@@ -104,49 +91,52 @@ class TestStockValuationLayerUsage(TransactionCase):
         )
         return account
 
-    def _create_product_category(self):
-        product_ctg = self.product_ctg_model.create(
+    @classmethod
+    def _create_product_category(cls):
+        product_ctg = cls.product_ctg_model.create(
             {
                 "name": "test_product_ctg",
-                "property_stock_valuation_account_id": self.account_inventory.id,
-                "property_stock_account_input_categ_id": self.account_grni.id,
-                "property_account_expense_categ_id": self.account_cogs.id,
-                "property_stock_account_output_categ_id": self.account_gdni.id,
+                "property_stock_valuation_account_id": cls.account_inventory.id,
+                "property_stock_account_input_categ_id": cls.account_grni.id,
+                "property_account_expense_categ_id": cls.account_cogs.id,
+                "property_stock_account_output_categ_id": cls.account_gdni.id,
                 "property_valuation": "real_time",
                 "property_cost_method": "fifo",
-                "property_stock_journal": self.stock_journal.id,
+                "property_stock_journal": cls.stock_journal.id,
             }
         )
         return product_ctg
 
-    def _create_product(self, standard_price, template, list_price):
+    @classmethod
+    def _create_product(cls, standard_price, template, list_price):
         """Create a Product variant."""
         if not template:
-            template = self.template_model.create(
+            template = cls.template_model.create(
                 {
                     "name": "test_product",
-                    "categ_id": self.product_ctg.id,
+                    "categ_id": cls.product_ctg.id,
                     "type": "product",
                     "standard_price": standard_price,
                     "valuation": "real_time",
                 }
             )
             return template.product_variant_ids[0]
-        product = self.product_model.create(
+        product = cls.product_model.create(
             {"product_tmpl_id": template.id, "list_price": list_price}
         )
         return product
 
+    @classmethod
     def _create_delivery(
-        self, product, qty,
+        cls, product, qty,
     ):
-        return self.env["stock.picking"].create(
+        return cls.env["stock.picking"].create(
             {
                 "name": product.name + "OUT",
-                "partner_id": self.customer.id,
-                "picking_type_id": self.stock_picking_type_out_id,
-                "location_id": self.stock_location_id,
-                "location_dest_id": self.stock_location_customer_id,
+                "partner_id": cls.customer.id,
+                "picking_type_id": cls.stock_picking_type_out_id,
+                "location_id": cls.stock_location_id,
+                "location_dest_id": cls.stock_location_customer_id,
                 "move_lines": [
                     (
                         0,
@@ -156,8 +146,8 @@ class TestStockValuationLayerUsage(TransactionCase):
                             "product_id": product.id,
                             "product_uom": product.uom_id.id,
                             "product_uom_qty": qty,
-                            "location_id": self.stock_location_id,
-                            "location_dest_id": self.stock_location_customer_id,
+                            "location_id": cls.stock_location_id,
+                            "location_dest_id": cls.stock_location_customer_id,
                             "procure_method": "make_to_stock",
                         },
                     )
