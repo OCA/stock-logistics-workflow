6f7c5542e486f12aca80e942022b3fc638ce8ecd
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2021-08-31 09:39:11 -0400

    [IMP] purchase_stock_picking_invoice_link: black, isort, prettier

--- purchase_stock_picking_invoice_link/__manifest__.py
+++ purchase_stock_picking_invoice_link/__manifest__.py
@@ -2,20 +2,14 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name': 'Purchase Stock Picking Invoice Link',
-    'version': '12.0.1.0.0',
-    'category': 'Warehouse Management',
-    'summary': 'Adds link between purchases, pickings and invoices',
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'website': 'http://github.com/OCA/stock-logistics-workflow',
-    'license': 'AGPL-3',
-    'depends': [
-        'stock_picking_invoice_link',
-        'purchase',
-    ],
-    'data': [
-        'views/account_invoice_views.xml',
-    ],
-    'installable': True,
+    "name": "Purchase Stock Picking Invoice Link",
+    "version": "12.0.1.0.0",
+    "category": "Warehouse Management",
+    "summary": "Adds link between purchases, pickings and invoices",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "website": "http://github.com/OCA/stock-logistics-workflow",
+    "license": "AGPL-3",
+    "depends": ["stock_picking_invoice_link", "purchase",],
+    "data": ["views/account_invoice_views.xml",],
+    "installable": True,
 }
