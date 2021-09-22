d2739d4145bdf95da021f26804291c01789e1976
Dũng (Trần Đình) <dungtd@trobz.com>
2021-09-22 11:32:33 +0700

    [MIG] stock_move_source_relocate: Migration to 14.0
    Update stock_move_source_relocate/models/stock_move.py
    
    Co-authored-by: Sébastien Alix <sebalix@users.noreply.github.com>

--- stock_move_source_relocate/models/stock_move.py
+++ stock_move_source_relocate/models/stock_move.py
@@ -60,9 +60,11 @@ class StockMove(models.Model):
             # A part of the quantity could be reserved in the original
             # location, so keep this part in the move and split the rest
             # in a new move, where will take the goods in the relocation
-            new_move_id = self._split(need)
-            # recheck first move which should now be available
-            new_move = self.browse(new_move_id)
+            new_move = self  # fallback if no split occurs
+            new_move_vals = self._split(need)
+            if new_move_vals:
+                new_move = self.create(new_move_vals)
+                new_move._action_confirm(merge=False)
             new_move.location_id = relocation.relocate_location_id
             self._action_assign()
             relocated = new_move
--- stock_move_source_relocate/readme/CONTRIBUTORS.rst
+++ stock_move_source_relocate/readme/CONTRIBUTORS.rst
@@ -1,1 +1,3 @@
 * Guewen Baconnier <guewen.baconnier@camptocamp.com>
+* `Trobz <https://trobz.com>`_:
+  * Dung Tran <dungtd@trobz.com>
