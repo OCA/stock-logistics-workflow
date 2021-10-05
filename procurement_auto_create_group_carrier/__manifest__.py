7f47d5aaabd268120663e642546cec69f9cc6820
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2021-10-05 08:16:48 +0200

    [13][ADD] procurement_auto_create_group_carrier
    This module is the glue between `procurement_auto_create_group` and
    `stock_picking_group_by_partner_by_carrier`.
    It stores the delivery carrier id into the procurement group.
    
    It needs this change
    
    * https://github.com/OCA/stock-logistics-warehouse/pull/1269
    
    to work as intended.

---
+++ procurement_auto_create_group_carrier/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Procurement Auto Create Group Carrier",
+    "Summary": """Adds the delivery carrier in the procurement group data.""",
+    "version": "13.0.1.0.0",
+    "development_status": "Alpha",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "category": "Warehouse Managemen",
+    "depends": [
+        "stock_picking_group_by_partner_by_carrier",
+        "procurement_auto_create_group",
+    ],
+    "installable": True,
+    "auto_install": True,
+    "license": "AGPL-3",
+}
