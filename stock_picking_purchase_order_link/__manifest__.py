16622e40f3be3bf23f9783a86465177899f44ace
Joan Mateu Jordi <joan.mateu@forgeflow.com>
2021-10-22 08:38:24 +0200

    [MIG] stock_picking_purchase_order_link: Migration to 15.0

--- stock_picking_purchase_order_link/__manifest__.py
+++ stock_picking_purchase_order_link/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2019 ForgeFlow S.L.
+# Copyright 2021 ForgeFlow S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Stock Picking Purchase Order Link",
     "summary": "Link between picking and purchase order",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "ForgeFlow S.L., Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-workflow",
