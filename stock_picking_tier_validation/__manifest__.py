e6eed263225d3aafde6bcd90beba892b5b5e37a1
ps-tubtim <pimolnats@ecosoft.co.th>
2021-10-24 00:15:12 +0700

    [14.0][ADD] stock_picking_tier_validation

---
+++ stock_picking_tier_validation/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Picking Tier Validation",
+    "summary": "Extends the functionality of Transfers to "
+    "support a tier validation process.",
+    "version": "14.0.1.0.0",
+    "category": "Warehouse Management",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["stock", "base_tier_validation"],
+    "data": ["views/stock_picking_views.xml"],
+    "installable": True,
+    "development_status": "Alpha",
+}
