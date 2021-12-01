fbfba9b6287351811b810f23f7d249ae720e9e5a
Jacques-Etienne Baudoux <je@bcim.be>
2021-12-01 14:32:13 +0100

    Add procurement_group_carrier. Extract feature from stock_picking_group_by_partner_by_carrier

--- stock_picking_group_by_partner_by_carrier/models/procurement_group.py
+++ stock_picking_group_by_partner_by_carrier/models/procurement_group.py
@@ -4,7 +4,6 @@ from odoo import fields, models
 class ProcurementGroup(models.Model):
     _inherit = "procurement.group"
 
-    carrier_id = fields.Many2one("delivery.carrier", string="Delivery Method")
     sale_ids = fields.Many2many(comodel_name="sale.order", copy=True)
     picking_ids = fields.One2many(
         comodel_name="stock.picking",
