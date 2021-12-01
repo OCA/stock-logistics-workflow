41eceed3157296d5de4b7aa4e40e623858d8c883
Jacques-Etienne Baudoux <je@bcim.be>
2021-12-01 14:32:13 +0100

    Add procurement_group_carrier. Extract feature from stock_picking_group_by_partner_by_carrier

--- procurement_auto_create_group_carrier/__manifest__.py
+++ procurement_auto_create_group_carrier/__manifest__.py
@@ -1,13 +1,13 @@
 {
     "name": "Procurement Auto Create Group Carrier",
     "Summary": """Adds the delivery carrier in the procurement group data.""",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.1.0",
     "development_status": "Alpha",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "category": "Warehouse Managemen",
     "depends": [
-        "stock_picking_group_by_partner_by_carrier",
+        "delivery_procurement_group_carrier",
         "procurement_auto_create_group",
     ],
     "installable": True,
