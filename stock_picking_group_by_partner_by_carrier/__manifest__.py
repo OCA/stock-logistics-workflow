0f6c1c88e67a7f0696cc83d9c43367b1513d4a7d
Jacques-Etienne Baudoux <je@bcim.be>
2021-12-01 14:32:13 +0100

    Add procurement_group_carrier. Extract feature from stock_picking_group_by_partner_by_carrier

--- stock_picking_group_by_partner_by_carrier/__manifest__.py
+++ stock_picking_group_by_partner_by_carrier/__manifest__.py
@@ -4,14 +4,16 @@
 {
     "name": "Stock Picking: group by partner and carrier",
     "Summary": "Group sales deliveries moves in 1 picking per partner and carrier",
-    "version": "14.0.1.3.0",
+    "version": "14.0.1.4.0",
     "development_status": "Beta",
     "author": "Camptocamp, BCIM, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "category": "Warehouse Management",
-    "depends": ["sale_stock", "delivery", "stock_move_assign_picking_hook"],
+    "depends": [
+        "delivery_procurement_group_carrier",
+        "stock_move_assign_picking_hook",
+    ],
     "data": [
-        "views/procurement_group.xml",
         "views/res_partner.xml",
         "views/stock_picking_type.xml",
         "views/stock_warehouse.xml",
