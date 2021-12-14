f4a4dff3a7b60099e51730fc3268e0cf9845a1ec
Joan Mateu Jordi <joan.mateu@forgeflow.com>
2021-12-14 12:41:15 +0100

    [IMP][15.0] stock_no_negative: black, isort, prettier

--- stock_no_negative/models/stock_location.py
+++ stock_no_negative/models/stock_location.py
@@ -9,7 +9,6 @@ class StockLocation(models.Model):
     _inherit = "stock.location"
 
     allow_negative_stock = fields.Boolean(
-        string="Allow Negative Stock",
         help="Allow negative stock levels for the stockable products "
         "attached to this location.",
     )
