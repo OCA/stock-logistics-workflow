2fa6257fefb019750bb022617db3b96968ddfe71
Joan Mateu Jordi <joan.mateu@forgeflow.com>
2021-12-14 13:46:38 +0100

    [15.0][MIG] stock_no_negative: Migration to 15.0

--- stock_no_negative/models/stock_location.py
+++ stock_no_negative/models/stock_location.py
@@ -1,4 +1,4 @@
-# ?? 2018 ForgeFlow (https://www.forgeflow.com)
+# Copyright 2018 ForgeFlow (https://www.forgeflow.com)
 # @author Jordi Ballester <jordi.ballester@forgeflow.com.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
