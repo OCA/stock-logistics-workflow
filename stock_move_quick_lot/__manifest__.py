f7589939d4191b7b80e3a5a08dc0f0ff74838370
Olga Marco <olga.marco@creublanca.es>
2022-01-05 11:51:12 +0100

    [IMP] stock_move_quick_lot: black, isort, prettier

--- stock_move_quick_lot/__manifest__.py
+++ stock_move_quick_lot/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Move Quick Lot",
     "summary": "Set lot name and end date directly on picking operations",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Stock",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "author": "Tecnativa, Odoo Community Association (OCA)",
