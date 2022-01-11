62edb88be6efbb4ad5eb41cabe3afa5f0cdc6373
sergio-teruel <sergio.teruel@tecnativa.com>
2022-01-11 09:05:29 +0100

    [IMP] sale_order_global_stock_route: black, isort

--- sale_order_global_stock_route/models/sale_order.py
+++ sale_order_global_stock_route/models/sale_order.py
@@ -1,38 +1,39 @@
 # Copyright 2020 Tecnativa - Carlos Roca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models, api
+from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     route_id = fields.Many2one(
-        comodel_name='stock.location.route',
-        string='Route',
-        domain=[('sale_selectable', '=', True)],
-        help='When you change this field all the lines will be changed.'
-             ' After use it you will be able to change each line.',
+        comodel_name="stock.location.route",
+        string="Route",
+        domain=[("sale_selectable", "=", True)],
+        help="When you change this field all the lines will be changed."
+        " After use it you will be able to change each line.",
     )
 
-    @api.onchange('route_id')
+    @api.onchange("route_id")
     def _onchange_route_id(self):
         for line in self.order_line:
             line.route_id = line.order_id.route_id
 
     def write(self, vals):
         res = super().write(vals)
-        if 'route_id' in vals:
-            lines = self.mapped('order_line').filtered(
-                lambda l: l.route_id.id != vals['route_id'])
-            lines.write({'route_id': vals['route_id']})
+        if "route_id" in vals:
+            lines = self.mapped("order_line").filtered(
+                lambda l: l.route_id.id != vals["route_id"]
+            )
+            lines.write({"route_id": vals["route_id"]})
         return res
 
 
 class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+    _inherit = "sale.order.line"
 
-    @api.onchange('product_id')
+    @api.onchange("product_id")
     def product_id_change(self):
         res = super().product_id_change()
         self.route_id = self.order_id.route_id
@@ -41,7 +42,7 @@ class SaleOrderLine(models.Model):
     @api.model_create_multi
     def create(self, vals_list):
         for vals in vals_list:
-            if not vals.get('route_id', False):
-                order = self.env['sale.order'].browse(vals['order_id'])
-                vals['route_id'] = order.route_id.id
+            if not vals.get("route_id", False):
+                order = self.env["sale.order"].browse(vals["order_id"])
+                vals["route_id"] = order.route_id.id
         return super().create(vals_list)
