f7ccec05da90dcfa01a7407b283f6623a0135207
sergio-teruel <sergio.teruel@tecnativa.com>
2022-01-11 09:05:30 +0100

    [MIG] sale_order_global_stock_route: Migration to v13.0

--- sale_order_global_stock_route/__manifest__.py
+++ sale_order_global_stock_route/__manifest__.py
@@ -4,13 +4,13 @@
 {
     "name": "Sale Order Global Stock Route",
     "summary": "Add the possibility to choose " "one warehouse path for an order",
-    "version": "12.0.1.0.1",
+    "version": "13.0.1.0.0",
     "development_status": "Beta",
     "category": "Warehouse",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
-    "depends": ["sale_stock",],
-    "data": ["views/sale_order_views.xml",],
+    "depends": ["sale_stock"],
+    "data": ["views/sale_order_views.xml"],
 }
--- sale_order_global_stock_route/readme/CONFIGURE.rst
+++ sale_order_global_stock_route/readme/CONFIGURE.rst
@@ -1,5 +1,2 @@
 For use this module you have to activate the option Settings > Inventory
 > Warehouse > Multi-Step Routes
-
-In the user profile you must select this option "Enable Route on
-Sales Order Line"
