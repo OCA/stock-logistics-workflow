7c96f1c042a688bef073287b2d6f66147c9f3dfb
Simone Orsi <simahawk@gmail.com>
2022-01-18 15:48:09 +0100

    stock_picking_type_shipping_policy: dev status -> Beta

--- stock_picking_type_shipping_policy/__manifest__.py
+++ stock_picking_type_shipping_policy/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Stock Picking Type Shipping Policy",
     "summary": "Define different shipping policies according to picking type",
     "version": "14.0.1.0.0",
-    "development_status": "Alpha",
+    "development_status": "Beta",
     "category": "Warehouse Management",
     "website": "https://github.com/OCA/wms",
     "author": "Camptocamp, Odoo Community Association (OCA)",
