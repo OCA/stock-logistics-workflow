15c542fb441401b649c1da34c0d45b14a8b4300d
Jordi Ballester <jordi.ballester@forgeflow.com>
2022-01-20 17:30:00 +0100

    [ADD] stock_landed_costs_mrp_subcontracting
    This module allows to add landed costs to your subcontracted products.

---
+++ stock_landed_costs_mrp_subcontracting/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 ForgeFlow, S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Landed Costs MRP Subcontracting",
+    "summary": "When using Subcontracting, use the incoming shipment in "
+               "landed costs",
+    "version": "14.0.1.0.0",
+    "category": "Warehouse Management",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["mrp_subcontracting", "stock_landed_costs"],
+}
