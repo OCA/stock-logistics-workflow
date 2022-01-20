aa350c0287de0db6ad14758626bfaaa25a2b4d38
Simone Orsi <simahawk@gmail.com>
2022-01-20 15:43:00 +0100

    s_picking_group_by_partner_by_carrier: dev status -> Beta

--- stock_picking_group_by_partner_by_carrier/__manifest__.py
+++ stock_picking_group_by_partner_by_carrier/__manifest__.py
@@ -4,8 +4,8 @@
 {
     "name": "Stock Picking: group by partner and carrier",
     "Summary": "Group sales deliveries moves in 1 picking per partner and carrier",
-    "version": "14.0.1.2.0",
-    "development_status": "Alpha",
+    "version": "14.0.1.3.0",
+    "development_status": "Beta",
     "author": "Camptocamp, BCIM, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "category": "Warehouse Management",
