983db6758abaca9d1acf8012cb126e18ceb62e51
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2022-03-03 11:44:23 -0500

    [MIG] stock_picking_batch_extended_account: Migration to 13.0

--- stock_picking_batch_extended_account/__manifest__.py
+++ stock_picking_batch_extended_account/__manifest__.py
@@ -4,14 +4,14 @@
 {
     "name": "Stock batch picking account",
     "summary": "Generates invoices when batch is set to Done state",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "maintainers": ["ernestotejeda"],
     "development_status": "Beta",
     "category": "Warehouse Management",
-    "depends": ["stock_picking_batch_extended",],
+    "depends": ["stock_picking_batch_extended"],
     "website": "https://github.com/OCA/stock-logistics-workflow",
-    "data": ["views/stock_batch_picking.xml", "views/res_partner_views.xml",],
+    "data": ["views/stock_batch_picking.xml", "views/res_partner_views.xml"],
     "installable": True,
     "license": "AGPL-3",
 }
