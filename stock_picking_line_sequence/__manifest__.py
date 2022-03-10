5615cc7b48819dc3b724c370cbfad79af5a5f4af
Sulivan Lominchar <slominchar@archeti.com>
2022-03-10 16:53:42 -0500

    [IMP] stock_picking_line_sequence: black, isort, prettier

--- stock_picking_line_sequence/__manifest__.py
+++ stock_picking_line_sequence/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Stock picking lines with sequence number",
     "summary": "Manages the order of stock moves by displaying its sequence",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Warehouse Management",
     "author": "Camptocamp, "
     "Eficent, "
