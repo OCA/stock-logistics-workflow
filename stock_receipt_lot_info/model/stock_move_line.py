dbbaf4f9fc58ba38ea9ad11d17177a5a8f1a1a67
Lois Rilo <lois.rilo@forgeflow.com>
2022-03-23 10:15:01 +0100

    [15.0][MIG] stock_receipt_lot_info

--- stock_receipt_lot_info/model/stock_move_line.py
+++ stock_receipt_lot_info/model/stock_move_line.py
@@ -7,14 +7,19 @@ from odoo import fields, models
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
-    lot_life_date = fields.Datetime(string="Lot/Serial End of Life Date")
+    lot_expiration_date = fields.Datetime(string="Lot/Serial Expiration Date")
     lot_use_date = fields.Datetime(string="Lot/Serial Best before Date")
     lot_removal_date = fields.Datetime(string="Lot/Serial Removal Date")
     lot_alert_date = fields.Datetime(string="Lot/Serial Alert Date")
 
     def _prepare_new_lot_vals(self):
         vals = super()._prepare_new_lot_vals()
-        creation_lot_fields = ["life_date", "use_date", "removal_date", "alert_date"]
+        creation_lot_fields = [
+            "expiration_date",
+            "use_date",
+            "removal_date",
+            "alert_date",
+        ]
         for field in creation_lot_fields:
             value = getattr(self, "lot_" + field)
             if value:
