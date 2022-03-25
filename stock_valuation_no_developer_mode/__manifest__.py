1319ade105a644b468836500fc34a4ea785e4df6
olveracuenca <jorge.olvera@jarsa.com>
2022-03-25 15:53:38 -0600

    [fix]add group to see the valuation view, without debugging

---
+++ stock_valuation_no_developer_mode/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Jarsa
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Valuation Layer no developer mode",
+    "summary": "Stock valuation layer no developer mode",
+    "version": "14.0.1.0.0",
+    "category": "Stock",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "author": "Jarsa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["stock_account"],
+    "data": [
+        "views/stock_valuation_layer.xml",
+    ],
+}
