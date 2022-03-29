284b979ba34434035285f81be476decbc31c79ce
Esteve Badia <badia.orive@gmail.com>
2022-03-29 13:29:20 +0200

    Backport to 12.0 from 13.0 version

---
+++ stock_scrap_cancel/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2021 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Scrap Cancel",
+    "version": "12.0.1.0.0",
+    "category": "Stock",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["stock"],
+    "installable": True,
+    "data": ["views/stock_scrap_view.xml"],
+    "maintainers": ["victoralmau"],
+}
