0d6181d915c459827c32809f1e2113b8d60de3c4
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-04-29 00:28:12 +0200

    [FIX] delivery_total_weight_from_packaging: Match license + development by dependencies

--- delivery_total_weight_from_packaging/__manifest__.py
+++ delivery_total_weight_from_packaging/__manifest__.py
@@ -1,14 +1,14 @@
 # Copyright 2020 Camptocamp SA
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl)
 {
     "name": "Delivery Total Weight From Packaging",
     "summary": "Include packaging weight on move, transfer and package.",
     "version": "13.0.1.1.1",
-    "development_status": "Beta",
+    "development_status": "Alpha",
     "category": "Inventory",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "author": "Camptocamp, Odoo Community Association (OCA)",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "application": False,
     "installable": True,
     "depends": [
