752556824c2da448f5688ff50221d910eebb4076
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-05-11 07:56:17 +0200

    [IMP] stock_picking_operation_quick_change: Allow to filter by product
    You can now also filter the lines to change by product.

--- stock_picking_operation_quick_change/wizards/stock_picking_wizard.py
+++ stock_picking_operation_quick_change/wizards/stock_picking_wizard.py
@@ -10,7 +10,10 @@ class StockPickingOperationWizard(models.TransientModel):
     _description = "Stock Picking Operation Wizard"
 
     def _prepare_default_values(self, picking):
-        return {"location_dest_id": picking.location_dest_id.id}
+        return {
+            "picking_id": picking.id,
+            "location_dest_id": picking.location_dest_id.id,
+        }
 
     @api.model
     def default_get(self, fields):
@@ -36,6 +39,7 @@ class StockPickingOperationWizard(models.TransientModel):
     def _get_allowed_picking_states(self):
         return ["assigned"]
 
+    picking_id = fields.Many2one(comodel_name="stock.picking")
     location_dest_id = fields.Many2one(
         comodel_name="stock.location",
         string="Actual destination location",
@@ -54,11 +58,24 @@ class StockPickingOperationWizard(models.TransientModel):
         required=True,
         domain=lambda self: self._get_allowed_location_domain(),
     )
+    allowed_product_ids = fields.Many2many(
+        comodel_name="product.product", compute="_compute_allowed_product_ids",
+    )
+    product_id = fields.Many2one(
+        comodel_name="product.product", domain="[('id', 'in', allowed_product_ids)]",
+    )
     change_all = fields.Boolean(
         string="Change All",
         help="Check if you want change all operations without filter "
         "by old location",
     )
+
+    @api.depends("picking_id")
+    def _compute_allowed_product_ids(self):
+        for record in self:
+            record.allowed_product_ids = record.picking_id.move_lines.mapped(
+                "product_id"
+            )
 
     def check_allowed_pickings(self, pickings):
         forbidden_pickings = pickings.filtered(
@@ -94,10 +111,17 @@ class StockPickingOperationWizard(models.TransientModel):
             # Write all operations destination location
             move_lines.write(vals)
         else:
+            matched_op = move_lines
             # Only write operations destination location if the location is
             # the same that old location value
-            matched_op = move_lines.filtered(
-                lambda x: x.location_dest_id == self.old_location_dest_id
-            )
+            if self.old_location_dest_id:
+                matched_op = matched_op.filtered(
+                    lambda x: x.location_dest_id == self.old_location_dest_id
+                )
+            # Only write operations destination location if the product is the same
+            if self.product_id:
+                matched_op = matched_op.filtered(
+                    lambda x: x.product_id == self.product_id
+                )
             matched_op.write(vals)
         return {"type": "ir.actions.act_window_close"}
