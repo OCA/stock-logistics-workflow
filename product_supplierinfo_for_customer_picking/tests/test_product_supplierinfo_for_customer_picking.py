1921a7b0e9385efe80ef1a35bb18e90aa19ffb8d
Janire Olagibel <janire.olaguibel@bdo.es>
2022-06-10 13:51:28 +0200

    [MIG] product_supplierinfo_for_customer_picking: improvements in tests

--- product_supplierinfo_for_customer_picking/tests/test_product_supplierinfo_for_customer_picking.py
+++ product_supplierinfo_for_customer_picking/tests/test_product_supplierinfo_for_customer_picking.py
@@ -7,16 +7,37 @@ from odoo.tests.common import TransactionCase
 class TestProductSupplierinfoForCustomerPicking(TransactionCase):
     def setUp(self):
         super(TestProductSupplierinfoForCustomerPicking, self).setUp()
-        self.computer_SC234 = self.browse_ref("product.product_product_3")
-        self.agrolait = self.browse_ref("base.res_partner_2")
-        self.computer_SC234.write(
+        self.src_location = self.env.ref("stock.stock_location_stock")
+        self.dest_location = self.env.ref("stock.stock_location_customers")
+        self.product = self.browse_ref("product.product_product_3")
+        self.partner = self.browse_ref("base.res_partner_2")
+        self.picking = self.env["stock.picking"].create(
+            {
+                "partner_id": self.partner.id,
+                "picking_type_id": self.env.ref("stock.picking_type_out").id,
+                "location_id": self.src_location.id,
+                "location_dest_id": self.dest_location.id,
+            }
+        )
+        self.move = self.env["stock.move"].create(
+            {
+                "name": self.product.partner_ref,
+                "picking_id": self.picking.id,
+                "product_id": self.product.id,
+                "product_uom": self.product.uom_id.id,
+                "location_id": self.src_location.id,
+                "location_dest_id": self.dest_location.id,
+                "product_uom_qty": 1.0,
+            }
+        )
+        self.product.write(
             {
                 "customer_ids": [
                     (
                         0,
                         0,
                         {
-                            "name": self.agrolait.id,
+                            "name": self.partner.id,
                             "product_code": "test_agrolait",
                         },
                     )
@@ -25,33 +46,6 @@ class TestProductSupplierinfoForCustomerPicking(TransactionCase):
         )
 
     def test_product_supplierinfo_for_customer_picking(self):
-        delivery_picking = self.env["stock.picking"].new(
-            {
-                "partner_id": self.agrolait.id,
-                "picking_type_id": self.env.ref("stock.picking_type_out").id,
-            }
-        )
-        delivery_picking.onchange_picking_type()
-        delivery_picking = self.env["stock.picking"].create(
-            {
-                "partner_id": delivery_picking.partner_id.id,
-                "picking_type_id": delivery_picking.picking_type_id.id,
-                "location_id": delivery_picking.location_id.id,
-                "location_dest_id": delivery_picking.location_dest_id.id,
-                "move_lines": [
-                    (
-                        0,
-                        0,
-                        {
-                            "name": self.computer_SC234.partner_ref,
-                            "product_id": self.computer_SC234.id,
-                            "product_uom": self.computer_SC234.uom_id.id,
-                            "product_uom_qty": 1.0,
-                        },
-                    )
-                ],
-            }
-        )
-        move = delivery_picking.move_lines[0]
+        move = self.picking.move_lines[0]
         move._compute_product_customer_code()
         self.assertEqual(move.product_customer_code, "test_agrolait")
