f52148f20c84f750377ed8f89eea674aabf04b66
Juany Davila <juanyadavilap@gmail.com>
2022-06-13 14:16:10 -0500

    [MIG] stock_picking_show_backorder: Migration to 15.0

--- stock_picking_show_backorder/__manifest__.py
+++ stock_picking_show_backorder/__manifest__.py
@@ -1,13 +1,13 @@
-# Copyright 2015-17 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2015-17 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Stock Picking Show Backorder",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Warehouse Management",
     "summary": "Provides a new field on stock pickings, allowing to display "
     "the corresponding backorders.",
-    "author": "Eficent, " "Odoo Community Association (OCA)",
+    "author": "ForgeFlow, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "depends": ["stock"],
     "data": ["views/stock_view.xml"],
