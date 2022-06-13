7c1ca31888801400f8b66883d524c36c9d8e55e4
Janire Olagibel <janire.olaguibel@bdo.es>
2022-06-13 10:14:42 +0200

    [MIG] stock_picking_customer_ref

--- stock_picking_customer_ref/__manifest__.py
+++ stock_picking_customer_ref/__manifest__.py
@@ -5,21 +5,14 @@
 # Copyright 2017 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Stock Picking Customer Reference',
-    'version': '12.0.1.0.0',
-    'category': 'Warehouse',
-    'author': 'AvanzOSC,'
-              'Tecnativa,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/stock-logistics-workflow',
-    'license': 'AGPL-3',
-    'summary': 'This module displays the sale reference/description in the '
-               'pickings',
-    'depends': [
-        'sale_stock'
-    ],
-    'data': [
-        'views/stock_picking_view.xml'
-    ],
-    'installable': True,
+    "name": "Stock Picking Customer Reference",
+    "version": "13.0.1.0.0",
+    "category": "Warehouse",
+    "author": "AvanzOSC," "Tecnativa," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "license": "AGPL-3",
+    "summary": "This module displays the sale reference/description in the " "pickings",
+    "depends": ["sale_stock"],
+    "data": ["views/stock_picking_view.xml"],
+    "installable": True,
 }
