43d4605864a888112d0135e057a18f821d88ac38
Jordi Ballester <jordi.ballester@forgeflow.com>
2022-06-14 19:20:54 +0200

    [FIX][stock_valuation_layer_usage]: look for true incoming moves In case that the out move is connected with an internal transfer, no valuation layer will be found, and then the system will create a layer with negative quantity. We should try to look for the true incoming move, if there is any.

--- stock_valuation_layer_usage/models/stock_move.py
+++ stock_valuation_layer_usage/models/stock_move.py
@@ -46,3 +46,12 @@ class StockMove(models.Model):
                 }
             )
         return layers
+
+    def _get_in_moves(self):
+        """We try to fetch the moves that originated"""
+        in_moves = self.env["stock.move"]
+        for move in self:
+            if move._is_in():
+                in_moves |= move
+            in_moves |= move.move_orig_ids._get_in_moves()
+        return in_moves
