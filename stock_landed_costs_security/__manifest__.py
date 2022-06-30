94b85215d4a7c8ec5d49edf984e6a4d3d0d0d3ec
Cesar Andres Sanchez <cesar.sanchez@tecnativa.com>
2022-06-30 10:36:04 +0200

    [IMP] stock_landed_costs_security: Restrict landed costs menu permission
    The top level menu should be only visible to this specific group.

--- stock_landed_costs_security/__manifest__.py
+++ stock_landed_costs_security/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Stock landed costs security",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.1.0",
     "category": "Inventory",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "author": "Tecnativa, Odoo Community Association (OCA)",
