2c06c2a43e1aebf43db204a84f721a7d7500af56
dhx <66140+dhx@users.noreply.github.com>
2022-07-06 12:36:52 +0200

    [IMP] stock_picking_warn_message: show blocking messages in red
     This allows the distinction between warn and block to be visible in the
     stock picking dialog by adding the additional picking_warn field to the
     stock.picking model containing what kind of message is to be shown.

--- stock_picking_warn_message/models/stock_picking.py
+++ stock_picking_warn_message/models/stock_picking.py
@@ -8,7 +8,27 @@ class StockPicking(models.Model):
 
     _inherit = "stock.picking"
 
+    picking_warn = fields.Text(compute="_compute_picking_warn")
     picking_warn_msg = fields.Text(compute="_compute_picking_warn_msg")
+
+    @api.depends(
+        "state",
+        "partner_id.picking_warn",
+        "partner_id.commercial_partner_id.picking_warn",
+    )
+    def _compute_picking_warn(self):
+        for rec in self:
+            picking_warn = "no-message"
+            if rec.state not in ["done", "cancel"] and rec.partner_id:
+                p = rec.partner_id.commercial_partner_id
+                if p.picking_warn == "block" or rec.partner_id.picking_warn == "block":
+                    picking_warn = "block"
+                elif (
+                    p.picking_warn == "warning"
+                    or rec.partner_id.picking_warn == "warning"
+                ):
+                    picking_warn = "warning"
+            rec.picking_warn = picking_warn
 
     @api.depends(
         "state",
@@ -21,9 +41,9 @@ class StockPicking(models.Model):
             if rec.state not in ["done", "cancel"] and rec.partner_id:
                 p = rec.partner_id.commercial_partner_id
                 separator = ""
-                if p.picking_warn == "warning":
+                if p.picking_warn != "no-message":
                     separator = "\n"
                     picking_warn_msg += p.picking_warn_msg
-                if p != rec.partner_id and rec.partner_id.picking_warn == "warning":
+                if p != rec.partner_id and rec.partner_id.picking_warn != "no-message":
                     picking_warn_msg += separator + rec.partner_id.picking_warn_msg
             rec.picking_warn_msg = picking_warn_msg or False
