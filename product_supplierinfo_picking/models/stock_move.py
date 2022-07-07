7413d4b69a5ed0b462570fa43c043d852136e07c
Héctor Villarreal Ortega <hector.villarreal@forgeflow.com>
2022-07-07 16:23:31 +0200

    [IMP] product_supplierinfo_picking: black, isort, prettier

--- product_supplierinfo_picking/models/stock_move.py
+++ product_supplierinfo_picking/models/stock_move.py
@@ -5,31 +5,34 @@ from odoo import api, fields, models
 
 
 class StockMove(models.Model):
-    _inherit = 'stock.move'
+    _inherit = "stock.move"
 
     @api.multi
-    @api.depends('picking_id.partner_id', 'product_id',
-                 'product_id.seller_ids.product_code')
+    @api.depends(
+        "picking_id.partner_id", "product_id", "product_id.seller_ids.product_code"
+    )
     def _compute_product_supplier_code(self):
-        for move in self.filtered(lambda m: m.picking_id and
-                                  m.picking_id.partner_id and
-                                  m.product_tmpl_id.seller_ids):
-            suppliers = \
-                move.product_tmpl_id.seller_ids.filtered(
-                    lambda m: m.name == move.picking_id.partner_id)
+        for move in self.filtered(
+            lambda m: m.picking_id
+            and m.picking_id.partner_id
+            and m.product_tmpl_id.seller_ids
+        ):
+            suppliers = move.product_tmpl_id.seller_ids.filtered(
+                lambda m: m.name == move.picking_id.partner_id
+            )
             if suppliers:
                 move.product_supplier_name = suppliers[0].product_name
                 move.product_supplier_code = suppliers[0].product_code
 
     product_supplier_name = fields.Char(
-        compute='_compute_product_supplier_code',
-        string='Product Supplier Name',
+        compute="_compute_product_supplier_code",
+        string="Product Supplier Name",
         store=True,
-        size=64
+        size=64,
     )
     product_supplier_code = fields.Char(
-        compute='_compute_product_supplier_code',
-        string='Product Supplier Code',
+        compute="_compute_product_supplier_code",
+        string="Product Supplier Code",
         store=True,
-        size=64
+        size=64,
     )
