14ac232f355d68d07f2601eab34ab34340eed0e4
ellbristow <elliott@glo.systems>
2022-07-13 14:38:57 +0200

    Fix pre-commit errors

--- stock_picking_group_by_partner_by_carrier/models/stock_picking_type.py
+++ stock_picking_group_by_partner_by_carrier/models/stock_picking_type.py
@@ -5,7 +5,6 @@ class StockPickingType(models.Model):
     _inherit = "stock.picking.type"
 
     group_pickings = fields.Boolean(
-        "Group pickings",
         help="Group pickings for the same partner and carrier. "
         "Pickings with shipping policy set to "
         "'When all products are ready' are never grouped.",
