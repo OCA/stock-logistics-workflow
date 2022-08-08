04b0c346b93569b093f2c3b96271d2db419e90ca
Denis Roussel <denis.roussel@acsone.eu>
2022-08-08 11:00:18 +0200

    [14.0][FIX] Align development statuses

--- stock_picking_group_by_partner_by_carrier/__manifest__.py
+++ stock_picking_group_by_partner_by_carrier/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Stock Picking: group by partner and carrier",
     "Summary": "Group sales deliveries moves in 1 picking per partner and carrier",
     "version": "14.0.1.4.2",
-    "development_status": "Beta",
+    "development_status": "Alpha",
     "author": "Camptocamp, BCIM, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "category": "Warehouse Management",
