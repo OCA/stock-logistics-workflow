5c892d361a3e34de7195797cd119dd73f0e0a25f
Simone Orsi <simahawk@gmail.com>
2022-08-08 10:20:32 +0200

    delivery_total_weight_from_packaging: re-license to AGPL

--- delivery_total_weight_from_packaging/models/stock_quant_package.py
+++ delivery_total_weight_from_packaging/models/stock_quant_package.py
@@ -1,5 +1,5 @@
 # Copyright 2021 Camptocamp SA
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import api, fields, models
 
--- delivery_total_weight_from_packaging/wizard/choose_delivery_package.py
+++ delivery_total_weight_from_packaging/wizard/choose_delivery_package.py
@@ -1,6 +1,6 @@
 # Copyright 2021 Camptocamp SA (http://www.camptocamp.com)
 # @author Simone Orsi <simahawk@gmail.com>
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, models
 from odoo.tools.float_utils import float_compare
