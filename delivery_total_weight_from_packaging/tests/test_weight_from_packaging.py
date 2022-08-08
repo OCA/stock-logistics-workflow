1b8666bc7a4829dd4cebd09263a65b4ece82f982
Simone Orsi <simahawk@gmail.com>
2022-08-08 10:20:32 +0200

    delivery_total_weight_from_packaging: re-license to AGPL

--- delivery_total_weight_from_packaging/tests/test_weight_from_packaging.py
+++ delivery_total_weight_from_packaging/tests/test_weight_from_packaging.py
@@ -1,5 +1,5 @@
 # Copyright 2020 Camptocamp SA (http://www.camptocamp.com)
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from .common import TestShippingWeightCommon
 
 
