375a7f1b3217a11f53df88f3b9b33a73b33f8262
Jasmin Solanki <jasmin.solanki@forgeflow.com>
2022-09-02 14:49:39 +0530

    [MIG] sale_line_returned_qty_mrp: Migration to 14.0

--- sale_line_returned_qty_mrp/tests/test_sale_line_returned_qty_mrp.py
+++ sale_line_returned_qty_mrp/tests/test_sale_line_returned_qty_mrp.py
@@ -10,6 +10,33 @@ class TestSaleLineReturnedQtyMrp(common.SavepointCase):
         cls.warehouse = cls.env.ref("stock.warehouse0")
         route_manufacture = cls.warehouse.manufacture_pull_id.route_id
         route_mto = cls.warehouse.mto_pull_id.route_id
+        route_buy = cls.warehouse.buy_pull_id.route_id
+        dropshipping_route = cls.env["stock.location.route"].create(
+            {
+                "name": "Dropship",
+                "sale_selectable": True,
+                "product_selectable": True,
+                "product_categ_selectable": True,
+            }
+        )
+        # Create dropship product
+        cls.dropship_product_main = cls.env["product.product"].create(
+            {"name": "Large Desk"}
+        )
+        vendor1 = cls.env["res.partner"].create({"name": "vendor1"})
+        seller1 = cls.env["product.supplierinfo"].create(
+            {
+                "name": vendor1.id,
+                "price": 8,
+            }
+        )
+        seller2 = cls.env["product.supplierinfo"].create(
+            {
+                "name": vendor1.id,
+                "price": 8,
+            }
+        )
+
         # Create Kit product
         cls.product = cls.env["product.product"].create(
             {
@@ -23,6 +50,26 @@ class TestSaleLineReturnedQtyMrp(common.SavepointCase):
         )
         cls.c2 = cls.env["product.product"].create(
             {"name": "test component 2", "type": "product"}
+        )
+        cls.c3 = cls.env["product.product"].create(
+            {
+                "name": "test component 3",
+                "type": "product",
+                "route_ids": [
+                    (6, 0, [dropshipping_route.id, route_mto.id, route_buy.id])
+                ],
+                "seller_ids": [(6, 0, [seller1.id])],
+            }
+        )
+        cls.c4 = cls.env["product.product"].create(
+            {
+                "name": "test component 4",
+                "type": "product",
+                "route_ids": [
+                    (6, 0, [dropshipping_route.id, route_mto.id, route_buy.id])
+                ],
+                "seller_ids": [(6, 0, [seller2.id])],
+            }
         )
         # Create BoM
         cls.bom = cls.env["mrp.bom"].create(
@@ -40,6 +87,24 @@ class TestSaleLineReturnedQtyMrp(common.SavepointCase):
         cls.env["mrp.bom.line"].create(
             {"bom_id": cls.bom.id, "product_id": cls.c2.id, "product_qty": 1}
         )
+
+        # Create dropshipping BoM
+        cls.dropship_bom = cls.env["mrp.bom"].create(
+            {
+                "product_tmpl_id": cls.dropship_product_main.product_tmpl_id.id,
+                "product_id": cls.dropship_product_main.id,
+                "product_qty": 1,
+                "type": "phantom",
+            }
+        )
+        # Create bom lines
+        cls.env["mrp.bom.line"].create(
+            {"bom_id": cls.dropship_bom.id, "product_id": cls.c3.id, "product_qty": 1}
+        )
+        cls.env["mrp.bom.line"].create(
+            {"bom_id": cls.dropship_bom.id, "product_id": cls.c4.id, "product_qty": 1}
+        )
+
         # Add components stock
         cls.env["stock.quant"].create(
             {
@@ -64,6 +129,15 @@ class TestSaleLineReturnedQtyMrp(common.SavepointCase):
             line_form.price_unit = 1000
         cls.order = order_form.save()
 
+        # create dropship order
+        dropship_order_form = Form(cls.env["sale.order"])
+        dropship_order_form.partner_id = cls.partner
+        with dropship_order_form.order_line.new() as line_form:
+            line_form.product_id = cls.dropship_product_main
+            line_form.product_uom_qty = 10
+            line_form.price_unit = 1000
+        cls.dropship_order = dropship_order_form.save()
+
     def _return_picking(self, picking, qty, to_refund=True):
         """Helper method to create a return of the original picking. It could
         be refundable or not"""
@@ -85,9 +159,9 @@ class TestSaleLineReturnedQtyMrp(common.SavepointCase):
         """Helper method to confirm the pickings"""
         for line in picking.move_lines:
             line.quantity_done = line.product_uom_qty
-        picking.action_done()
+        picking._action_done()
 
-    def test_returned_qty(self):
+    def test_01_returned_qty(self):
         self.order.action_confirm()
         so_line = self.order.order_line[0]
         self.assertEqual(so_line.qty_returned, 0.0)
@@ -98,5 +172,22 @@ class TestSaleLineReturnedQtyMrp(common.SavepointCase):
         self._validate_picking(picking)
         self.assertEqual(so_line.qty_returned, 0.0)
         # Make a return for 5 of the 10 units delivered
+        self._return_picking(picking, 5.0, to_refund=True)
+        self.assertEqual(so_line.qty_returned, 5.0)
+
+    def test_02_returned_qty(self):
+        self.dropship_order.action_confirm()
+        po = self.env["purchase.order"].search(
+            [("group_id", "=", self.dropship_order.procurement_group_id.id)]
+        )
+        po.button_confirm()
+        so_line = self.dropship_order.order_line[0]
+        self.assertEqual(so_line.qty_returned, 0.0)
+        # Deliver the order
+        picking = self.dropship_order.picking_ids
+        self.assertEqual(len(picking.move_lines), 2)
+        picking.action_assign()
+        self._validate_picking(picking)
+        self.assertEqual(so_line.qty_returned, 0.0)
         self._return_picking(picking, 5.0, to_refund=True)
         self.assertEqual(so_line.qty_returned, 5.0)
