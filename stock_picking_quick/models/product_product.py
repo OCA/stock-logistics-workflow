6ae00747b99a54904ad7a59c3374aee415ff6377
Kev-Roche <kevin.roche@akretion.com>
2022-09-26 21:32:31 +0200

    little fixes

--- stock_picking_quick/models/product_product.py
+++ stock_picking_quick/models/product_product.py
@@ -3,20 +3,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    move_ids = fields.One2many(
-        string="Stock Moves",
-        comodel_name="stock.move",
-        inverse_name="product_id",
-        help="Technical: used to compute quantities to pick.",
-    )
-
-    @api.depends("move_ids")
+    @api.depends("stock_move_ids")
+    @api.depends_context("parent_model")
     def _compute_process_qty(self):
         res = super(ProductProduct, self)._compute_process_qty()
         if self.env.context.get("parent_model", False) == "stock.picking":
@@ -46,8 +40,7 @@ class ProductProduct(models.Model):
                 self.env.context.get("parent_id")
             )
             if picking:
-                moves = self.env["stock.move"].search([("picking_id", "=", picking.id)])
-                args.append(("id", "in", moves.mapped("product_id").ids))
+                args.append(("stock_move_ids.picking_id", "=", picking.id))
         return super(ProductProduct, self).search(
             args, offset=offset, limit=limit, order=order, count=count
         )
