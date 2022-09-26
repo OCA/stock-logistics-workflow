77c06e89f95aae5e58914973fc6aeba2c46ecbfe
Kev-Roche <kevin.roche@akretion.com>
2022-09-26 21:32:31 +0200

    little fixes

--- stock_picking_quick/tests/test_stock_picking_quick.py
+++ stock_picking_quick/tests/test_stock_picking_quick.py
@@ -1,10 +1,10 @@
 # @author Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo.tests.common import Form, TransactionCase
+from odoo.tests.common import Form, SavepointCase
 
 
-class TestQuickPicking(TransactionCase):
+class TestQuickPicking(SavepointCase):
     def setUp(self):
         super(TestQuickPicking, self).setUp()
         # Useful models
