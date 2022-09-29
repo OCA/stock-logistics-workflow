bc30c8f10e4b51122b1a308a3a3be1819855ae17
Akim Juillerat <akim.juillerat@camptocamp.com>
2022-09-29 15:44:31 +0200

    Add module stock_lot_on_hand_first

---
+++ stock_lot_on_hand_first/models/stock_move.py
@@ -0,0 +1,14 @@
+# Copyright 2022 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
+from odoo import fields, models
+
+
+class StockMove(models.Model):
+
+    _inherit = "stock.move"
+
+    display_lots_on_hand_first = fields.Boolean(
+        related="picking_type_id.display_lots_on_hand_first",
+        readonly=True,
+    )
---
+++ stock_lot_on_hand_first/readme/CONTRIBUTORS.rst
@@ -0,0 +1,2 @@
+* Akim Juillerat <akim.juillerat@camptocamp.com>
+* Ricardo Almeida Soares <ricardo.almeidasoares@camptocamp.com>
