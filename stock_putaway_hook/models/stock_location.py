66fb7b09175e68661950b68742c5415f7d1c0cdd
Denis Roussel <denis.roussel@acsone.eu>
2022-10-05 09:11:54 +0200

    [MIG] stock_putaway_hook: Migration to 16.0

--- stock_putaway_hook/models/stock_location.py
+++ stock_putaway_hook/models/stock_location.py
@@ -1,5 +1,5 @@
 # Copyright 2020 Camptocamp
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import models
 
