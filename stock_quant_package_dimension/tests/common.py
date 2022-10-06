377be6bfe00820eb2f2d29ae4dd85494ab1ab330
Denis Roussel <denis.roussel@acsone.eu>
2022-10-06 09:28:10 +0200

    [MIG] stock_quant_package_dimension: Migration to 16.0

--- stock_quant_package_dimension/tests/common.py
+++ stock_quant_package_dimension/tests/common.py
@@ -1,10 +1,10 @@
 # Copyright 2021 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo.tests import SavepointCase
+from odoo.tests import TransactionCase
 
 
-class TestStockQuantPackageCommon(SavepointCase):
+class TestStockQuantPackageCommon(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
