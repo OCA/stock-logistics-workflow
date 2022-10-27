5c6e68368053d20f1f2a46b3e0ada2324dda7237
Alexis de Lattre <alexis.delattre@akretion.com>
2022-10-27 09:21:05 +0200

    [MIG] product_expiry_simple to v16

---
+++ product_expiry_simple/models/product_template.py
@@ -0,0 +1,27 @@
+# Copyright 2022 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    use_expiry_date = fields.Boolean()
+
+    @api.constrains("tracking", "use_expiry_date")
+    def _check_use_expiry_date(self):
+        for ptemplate in self:
+            if ptemplate.use_expiry_date and ptemplate.tracking not in (
+                "lot",
+                "serial",
+            ):
+                raise ValidationError(
+                    _(
+                        "You cannot set 'Use Expiry Date' on product '%s' because "
+                        "it is not tracked by lot nor by serial number."
+                    )
+                    % ptemplate.display_name
+                )
