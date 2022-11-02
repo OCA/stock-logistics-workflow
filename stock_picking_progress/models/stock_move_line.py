7db308929ab855edcf9d513b50a2d43ca27c65f2
MmeQuignon <matthieu.mequignon@camptocamp.com>
2022-11-02 17:37:44 +0100

    Add stock_picking_progress
    [UPD] Update stock_picking_progress.pot
    
    [UPD] README.rst
    
    [ADD] icon.png

---
+++ stock_picking_progress/models/stock_move_line.py
@@ -0,0 +1,27 @@
+# Copyright 2022 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import api, fields, models
+from odoo.tools.float_utils import float_is_zero
+
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    progress = fields.Float(
+        compute="_compute_progress", store=True, group_operator="avg"
+    )
+
+    @api.depends(
+        "product_uom_qty", "product_uom_id", "product_uom_id.rounding", "qty_done"
+    )
+    def _compute_progress(self):
+        for record in self:
+            if record.state == "done":
+                record.progress = 100
+                continue
+            rounding = record.product_uom_id.rounding
+            if float_is_zero(record.product_uom_qty, precision_rounding=rounding):
+                record.progress = 0
+            else:
+                record.progress = (record.qty_done / record.product_uom_qty) * 100
