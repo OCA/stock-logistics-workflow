273037c60aa7e5d8dffb9c33e8d2e40fe91cb513
otmanelagy <otman.elagy@usmba.ac.ma>
2022-11-08 10:57:19 +0100

    [MIG] stock_picking_back2draft: Migration to 16.0

--- stock_picking_back2draft/models/stock_picking.py
+++ stock_picking_back2draft/models/stock_picking.py
@@ -8,5 +8,5 @@ class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     def action_back_to_draft(self):
-        moves = self.mapped("move_lines")
+        moves = self.mapped("move_ids")
         moves.action_back_to_draft()
