3b0b944b49f5bb3201f24ff4b108489e1c85acd5
Florian da Costa <florian.dacosta@akretion.com>
2022-11-22 22:36:08 +0100

    [MIG] Migrate stock_restrict_lot to v16

--- stock_restrict_lot/__manifest__.py
+++ stock_restrict_lot/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Stock Restrict Lot",
     "summary": "Base module that add back the concept of restrict lot on stock move",
-    "version": "15.0.0.0.2",
+    "version": "16.0.1.0.0",
     "category": "Warehouse Management",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "author": "Akretion, Odoo Community Association (OCA)",
