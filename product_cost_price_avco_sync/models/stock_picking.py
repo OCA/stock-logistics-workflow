af8f441cec17f3ae552e51d2267e0a01c14422fc
CarlosRoca13 <carlos.roca@tecnativa.com>
2022-11-23 12:41:47 +0100

    [IMP] product_cost_price_avco_sync: black, isort, prettier

--- product_cost_price_avco_sync/models/stock_picking.py
+++ product_cost_price_avco_sync/models/stock_picking.py
@@ -8,5 +8,5 @@ class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     def action_done(self):
-        """ Avoid AVCO cost price recomputation when validating picking """
+        """Avoid AVCO cost price recomputation when validating picking"""
         return super(StockPicking, self.with_context(skip_avco_sync=True)).action_done()
