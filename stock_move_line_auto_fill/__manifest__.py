cc39b87b51ffe471b7c439ba2d2c42b36dba0e16
syera bonneaux <syera.bonneaux@akretion.com>
2022-12-01 16:59:59 +0100

    [MIG] stock_move_line_auto_fill : Migration to 16.0

--- stock_move_line_auto_fill/__manifest__.py
+++ stock_move_line_auto_fill/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Stock Move Line Auto Fill",
     "summary": "Stock Move Line auto fill",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV, Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-workflow",
