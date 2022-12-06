3793c603365adb776bcac6f7a2f39001b13e39e0
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2022-12-06 17:36:25 +0100

    [IMP] stock_picking_product_link: Return a list of product templates when product variants are not enabled
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

--- stock_picking_product_link/models/stock_picking.py
+++ stock_picking_product_link/models/stock_picking.py
@@ -12,9 +12,16 @@ class StockPicking(models.Model):
         comodel_name="product.product",
         compute="_compute_product_ids",
     )
+    product_template_ids = fields.Many2many(
+        string="Products",
+        comodel_name="product.template",
+        compute="_compute_product_ids",
+    )
     product_count = fields.Integer(
         string="Number of Products",
         compute="_compute_product_ids",
+        help="""This displays the amount of products (variants) in the stock transfer.
+            The amount of product templates may be lower."""
     )
 
     @api.depends("move_line_ids", "move_line_ids.product_id")
@@ -28,14 +35,28 @@ class StockPicking(models.Model):
             products = move_products + move_wo_package_products
             picking.product_count = len(set(products.ids))
             picking.product_ids = products
+            picking.product_template_ids = products.mapped("product_tmpl_id")
 
     @api.multi
     def action_view_products(self):
         self.ensure_one()
-        return {
-            "name": _("Products"),
-            "view_mode": "tree,form",
-            "res_model": "product.product",
-            "type": "ir.actions.act_window",
-            "domain": [("id", "in", self.product_ids.ids)],
-        }
+        if self.env.user.has_group("product.group_product_variant"):
+            return {
+                "name": _("Products"),
+                "view_mode": "tree,form",
+                "res_model": "product.product",
+                "type": "ir.actions.act_window",
+                "domain": [("id", "in", self.product_ids.ids)],
+            }
+        # The use case for returning a list of product templates here is that
+        # clients who have not enabled product variants are not
+        # functionality-wise familiar with product variants. The fields and
+        # available interactions are slightly different.
+        else:
+            return {
+                "name": _("Products"),
+                "view_mode": "tree,form",
+                "res_model": "product.template",
+                "type": "ir.actions.act_window",
+                "domain": [("id", "in", self.product_template_ids.ids)],
+            }
