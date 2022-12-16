699752f7a3afd4343c7d386a3eac06c73a3f28b6
Sébastien Alix <sebastien.alix@camptocamp.com>
2022-12-16 12:00:22 +0100

    Add stock_lot_production_date

---
+++ stock_lot_production_date/models/stock_lot.py
@@ -0,0 +1,12 @@
+# Copyright 2022 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import fields, models
+
+
+class StockProductionLot(models.Model):
+    _inherit = "stock.production.lot"
+
+    production_date = fields.Datetime(
+        help="This is the date when the goods with this lot/serial number have been produced."
+    )
