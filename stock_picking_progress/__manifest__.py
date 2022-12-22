0e9a038486b0f0c350a9b98fc2a5f0a96c4d7887
JuMiSanAr <jumisan134@gmail.com>
2022-12-22 16:37:13 +0100

    stock_picking_progress: compute progress for moves
    [UPD] Update stock_picking_progress.pot
    
    [UPD] README.rst

--- stock_picking_progress/__manifest__.py
+++ stock_picking_progress/__manifest__.py
@@ -4,14 +4,15 @@
 {
     "name": "Stock Picking Progress",
     "summary": "Compute the stock.picking progression",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.1.0",
     "category": "Inventory",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "author": "mmequignon, Camptocamp SA, Odoo Community Association (OCA)",
-    "maintainers": ["mmequignon"],
+    "maintainers": ["mmequignon", "JuMiSanAr"],
     "license": "AGPL-3",
     "installable": True,
     "depends": ["stock"],
     "data": ["views/stock_picking.xml"],
     "pre_init_hook": "pre_init_hook",
+    "post_init_hook": "post_init_hook",
 }
