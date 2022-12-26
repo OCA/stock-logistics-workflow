71cfa903090d42d94a9ebc9a126f0529144e8301
Ivàn Todorovich <ivan.todorovich@gmail.com>
2022-12-26 16:07:34 -0300

    [MIG] stock_production_lot_traceability: Migration to 15.0

--- stock_production_lot_traceability/__manifest__.py
+++ stock_production_lot_traceability/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Stock Production Lot Traceability",
     "summary": "Drill down/up the lots produced or consumed",
-    "version": "12.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "maintainers": ["ivantodorovich"],
     "website": "https://github.com/OCA/stock-logistics-workflow",
