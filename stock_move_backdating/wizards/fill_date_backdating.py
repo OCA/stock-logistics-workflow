200363fc1e422163540e6142d03d11462e866787
Simone Rubino <sir@takobi.online>
2023-01-11 17:17:21 +0100

    [MIG] stock_move_backdating: Migration to 12.0

--- stock_move_backdating/wizards/fill_date_backdating.py
+++ stock_move_backdating/wizards/fill_date_backdating.py
@@ -1,25 +1,37 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Alex Comba - Agile Business Group
+# Copyright 2023 Simone Rubino - TAKOBI
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models
-from ..models.stock_pack_operation import check_date
+from odoo.exceptions import UserError, ValidationError
+from ..models.stock_move_line import check_date
 
 
 class FillDateBackdating(models.TransientModel):
     _name = "fill.date.backdating"
+    _description = "Fill Actual Movement Date of all operations"
 
-    date_backdating = fields.Datetime(string='Actual Movement Date')
+    date_backdating = fields.Datetime(
+        string='Actual Movement Date',
+    )
 
-    @api.onchange('date_backdating')
-    def onchange_date_backdating(self):
-        check_date(self.date_backdating)
+    @api.constrains(
+        'date_backdating',
+    )
+    def constrain_date_backdating(self):
+        try:
+            check_date(self.date_backdating)
+        except UserError as ue:
+            raise ValidationError(ue.args[0]) from ue
 
-    @api.multi
     def fill_date_backdating(self):
         """ Fill the Actual Movement Date on all pack operations. """
         self.ensure_one()
-        picking = self.env['stock.picking'].browse(self._context['active_id'])
-        picking.pack_operation_product_ids.write(
-            {'date_backdating': self.date_backdating})
-        return {'type': 'ir.actions.act_window_close'}
+        picking_id = self.env.context['active_id']
+        picking = self.env['stock.picking'].browse(picking_id)
+        picking.move_line_ids.update({
+            'date_backdating': self.date_backdating,
+        })
+        return {
+            'type': 'ir.actions.act_window_close',
+        }
