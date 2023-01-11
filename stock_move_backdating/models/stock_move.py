1285edad7cb66e30ba9a0c7b5e3f0097f3968bd7
Simone Rubino <sir@takobi.online>
2023-01-11 17:17:21 +0100

    [MIG] stock_move_backdating: Migration to 12.0

--- stock_move_backdating/models/stock_move.py
+++ stock_move_backdating/models/stock_move.py
@@ -1,28 +1,46 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015-2016 Agile Business Group (<http://www.agilebg.com>)
 # Copyright 2016 BREMSKERL-REIBBELAGWERKE EMMERLING GmbH & Co. KG
 #    Author Marco Dieckhoff
 # Copyright 2018 Alex Comba - Agile Business Group
+# Copyright 2023 Simone Rubino - TAKOBI
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, models
-from ..models.stock_pack_operation import check_date
+from odoo.fields import first
+from .stock_move_line import check_date
 
 
 class StockMove(models.Model):
     _inherit = "stock.move"
 
-    @api.multi
-    def action_done(self):
-        # do actual processing
-        result = super(StockMove, self).action_done()
-        # overwrite date field where applicable
+    def _backdating_action_done(self, moves_todo):
+        """Process the moves one by one, backdating the ones that need to."""
         for move in self:
-            if move.linked_move_operation_ids:
-                operation = move.linked_move_operation_ids[0]
-                if operation.operation_id.date_backdating:
-                    move.date = operation.operation_id.date_backdating
-                    if move.quant_ids:
-                        check_date(move.date)
-                        move.quant_ids.sudo().write({'in_date': move.date})
-        return result
+            move_line = first(move.move_line_ids)
+            date_backdating = move_line.date_backdating
+            if date_backdating:
+                move = move.with_context(
+                    date_backdating=date_backdating,
+                )
+
+            moves_todo |= super(StockMove, move)._action_done()
+
+            # overwrite date field where applicable
+            date_backdating = move_line.date_backdating
+            if date_backdating:
+                check_date(date_backdating)
+                move.date = date_backdating
+                move.move_line_ids.update({
+                    'date': date_backdating,
+                })
+        return moves_todo
+
+    @api.multi
+    def _action_done(self):
+        moves_todo = self.env['stock.move']
+        has_move_lines_to_backdate = any(self.mapped('move_line_ids.date_backdating'))
+        if not has_move_lines_to_backdate:
+            moves_todo |= super()._action_done()
+        else:
+            moves_todo = self._backdating_action_done(moves_todo)
+        return moves_todo
