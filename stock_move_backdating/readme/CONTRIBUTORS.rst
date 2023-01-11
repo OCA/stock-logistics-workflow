294a63e5e6ad56834124ccdf7072dd94b8622b07
Simone Rubino <sir@takobi.online>
2023-01-11 17:17:21 +0100

    [MIG] stock_move_backdating: Migration to 12.0

---
+++ stock_move_backdating/readme/CONTRIBUTORS.rst
@@ -0,0 +1,7 @@
+* Alex Comba <alex.comba@agilebg.com>
+* Marco Dieckhoff < marco.dieckhoff@bremskerl.de>
+* Lorenzo Battistini <lorenzo.battistini@agilebg.com>
+* Alessio Gerace <alessio.gerace@agilebg.com>
+* `TAKOBI <https://takobi.online>`_:
+
+  * Simone Rubino <sir@takobi.online>
--- stock_move_backdating/tests/__init__.py
+++ stock_move_backdating/tests/__init__.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright 2015-2016 Agile Business Group (<http://www.agilebg.com>)
-# Copyright 2015 BREMSKERL-REIBBELAGWERKE EMMERLING GmbH & Co. KG
-#    Author Marco Dieckhoff
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import test_stock_move_backdating
