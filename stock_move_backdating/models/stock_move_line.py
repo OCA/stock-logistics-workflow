bad5bca978cd89d8041eab7483d46ec4771f8e2a
Simone Rubino <sir@takobi.online>
2023-01-11 17:17:21 +0100

    [MIG] stock_move_backdating: Migration to 12.0

--- stock_move_backdating/models/stock_move_line.py
+++ stock_move_backdating/models/stock_move_line.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Alex Comba - Agile Business Group
+# Copyright 2023 Simone Rubino - TAKOBI
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models, _
@@ -14,11 +14,16 @@ def check_date(date):
               "movement date in the future."))
 
 
-class PackOperation(models.Model):
-    _inherit = "stock.pack.operation"
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
 
-    date_backdating = fields.Datetime(string='Actual Movement Date')
+    date_backdating = fields.Datetime(
+        string='Actual Movement Date',
+    )
 
-    @api.onchange('date_backdating')
+    @api.onchange(
+        'date_backdating',
+    )
     def onchange_date_backdating(self):
+        self.ensure_one()
         check_date(self.date_backdating)
