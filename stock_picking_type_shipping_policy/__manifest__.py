678a6ce00e75e45a0dda0db69ad6dc20d0f5dd7d
Lindsay <lindsay.marion@acsone.eu>
2023-01-13 13:27:44 +0100

    [IMP] stock_picking_type_shipping_policy: black, isort, prettier

--- stock_picking_type_shipping_policy/__manifest__.py
+++ stock_picking_type_shipping_policy/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Stock Picking Type Shipping Policy",
     "summary": "Define different shipping policies according to picking type",
-    "version": "14.0.1.1.0",
+    "version": "16.0.1.0.0",
     "development_status": "Beta",
     "category": "Warehouse Management",
     "website": "https://github.com/OCA/wms",
