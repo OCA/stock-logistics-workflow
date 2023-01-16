64d02d46e7a56c638c2c7712c71cd337ba95cd87
Simone Rubino <sir@takobi.online>
2023-01-16 10:25:41 +0100

    [IMP] stock_move_backdating: Update source quant date

--- stock_move_backdating/models/stock_quant.py
+++ stock_move_backdating/models/stock_quant.py
@@ -12,8 +12,11 @@ class StockQuant (models.Model):
         lot_id=None, package_id=None, owner_id=None, in_date=None,
     ):
         date_backdating = self.env.context.get('date_backdating')
-        if date_backdating is not None and in_date is not None:
-            in_date = min(date_backdating, in_date)
+        if date_backdating is not None:
+            if in_date is not None:
+                in_date = min(date_backdating, in_date)
+            else:
+                in_date = date_backdating
         return super()._update_available_quantity(
             product_id, location_id, quantity,
             lot_id=lot_id, package_id=package_id, owner_id=owner_id, in_date=in_date,
