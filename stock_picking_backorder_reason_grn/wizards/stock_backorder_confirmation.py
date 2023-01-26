385cf7dc6eb9ce7c43ca7e0f697d513c4cd6891b
Denis Roussel <denis.roussel@acsone.eu>
2023-01-26 14:20:29 +0100

    [IMP] stock_picking_backorder_reason_grn: Simplified filtered function

--- stock_picking_backorder_reason_grn/wizards/stock_backorder_confirmation.py
+++ stock_picking_backorder_reason_grn/wizards/stock_backorder_confirmation.py
@@ -11,11 +11,9 @@ class StockBackorderConfirmation(models.TransientModel):
         res = super().process()
         # We keep only pickings that were eligible for backorders creation and those
         # where backorder reason is configured to keep the GRN
-        pickings_for_grn = (
-            self.backorder_confirmation_line_ids.filtered("to_backorder")
-            .filtered("keep_grn")
-            .mapped("picking_id")
-        )
+        pickings_for_grn = self.backorder_confirmation_line_ids.filtered(
+            lambda line: line.to_backorder and line.keep_grn
+        ).mapped("picking_id")
         # Do one search before the loop
         backorders = self.env["stock.picking"].search(
             [("backorder_id", "in", pickings_for_grn.ids)]
