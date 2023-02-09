628e706c2091922d6e04df8b772817b4f20f23d5
Víctor Martínez <victor.martinez@tecnativa.com>
2023-02-09 08:43:20 +0100

    [FIX] stock_owner_restriction: Change SavepointCase to TransactionCase

--- stock_owner_restriction/tests/test_stock_owner_restriction.py
+++ stock_owner_restriction/tests/test_stock_owner_restriction.py
@@ -1,10 +1,10 @@
 # Copyright 2020 Carlos Dauden - Tecnativa
 # Copyright 2020 Sergio Teruel - Tecnativa
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestStockOwnerRestriction(SavepointCase):
+class TestStockOwnerRestriction(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
