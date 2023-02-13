52ccc5797fad5455b2aa311262df47347abf08e5
Stefan <stefan.ungureanu@tecnativa.com>
2023-02-13 10:13:40 +0100

    [MIG] sale_order_global_stock_route: Migration to 15.0

--- sale_order_global_stock_route/__manifest__.py
+++ sale_order_global_stock_route/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Order Global Stock Route",
     "summary": "Add the possibility to choose one warehouse path for an order",
-    "version": "14.0.1.0.1",
+    "version": "15.0.1.0.0",
     "development_status": "Beta",
     "category": "Warehouse",
     "website": "https://github.com/OCA/stock-logistics-workflow",
