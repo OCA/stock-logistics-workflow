c0e6263e401c6b0d69bfb12778fdd00898930c31
Luis Ernesto García Medina <ernesto.garcia@tecnativa.com>
2023-02-15 01:47:53 -0600

    [MIG] stock_picking_backorder_strategy: Migration to 15.0

--- stock_picking_backorder_strategy/tests/test_backorder_strategy.py
+++ stock_picking_backorder_strategy/tests/test_backorder_strategy.py
@@ -1,10 +1,10 @@
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests import common
+from odoo.tests import Form, TransactionCase
 
 
-class TestBackorderStrategy(common.SavepointCase):
+class TestBackorderStrategy(TransactionCase):
     @classmethod
     def setUpClass(cls):
         """Create the picking."""
@@ -42,6 +42,7 @@ class TestBackorderStrategy(common.SavepointCase):
 
     def _process_picking(self):
         """Receive partially the picking"""
+        self.picking.move_line_ids.qty_done = 1.0
         res = self.picking.button_validate()
         return res
 
@@ -53,8 +54,7 @@ class TestBackorderStrategy(common.SavepointCase):
         self.picking_type.backorder_strategy = "create"
         self._process_picking()
         backorder = self.picking_obj.search([("backorder_id", "=", self.picking.id)])
-        if len(backorder):
-            self.assertTrue(backorder)
+        self.assertTrue(backorder)
 
     def test_backorder_strategy_no_create(self):
         """Set strategy for stock.picking_type_in to no_create
@@ -65,17 +65,16 @@ class TestBackorderStrategy(common.SavepointCase):
         self.picking_type.backorder_strategy = "no_create"
         self._process_picking()
         backorder = self.picking_obj.search([("backorder_id", "=", self.picking.id)])
-        if len(backorder):
-            self.assertFalse(backorder)
-            states = list(set(self.picking.move_lines.mapped("state")))
-            self.assertEqual(
-                "done",
-                self.picking.state,
-            )
-            self.assertListEqual(
-                ["cancel", "done"],
-                states,
-            )
+        self.assertFalse(backorder)
+        states = list(set(self.picking.move_lines.mapped("state")))
+        self.assertEqual(
+            "done",
+            self.picking.state,
+        )
+        self.assertListEqual(
+            ["cancel", "done"],
+            sorted(states),
+        )
 
     def test_backorder_strategy_cancel(self):
         """Set strategy for stock.picking_type_in to cancel
@@ -85,9 +84,8 @@ class TestBackorderStrategy(common.SavepointCase):
         self.picking_type.backorder_strategy = "cancel"
         self._process_picking()
         backorder = self.picking_obj.search([("backorder_id", "=", self.picking.id)])
-        if len(backorder):
-            self.assertTrue(backorder)
-            self.assertEqual("cancel", backorder[0].state)
+        self.assertTrue(backorder)
+        self.assertEqual("cancel", backorder.state)
 
     def test_backorder_strategy_manual(self):
         """Set strategy for stock.picking_type_in to manual
@@ -95,10 +93,16 @@ class TestBackorderStrategy(common.SavepointCase):
         Check the backorder wizard is launched
         """
         self.assertEqual("manual", self.picking_type.backorder_strategy)
-        old_wizards = self.env["stock.backorder.confirmation"].search([])
-        self._process_picking()
-        if len(old_wizards):
-            backorder_obj = self.env["stock.backorder.confirmation"]
-            new_wizards = backorder_obj.search([]) - old_wizards
-            if len(new_wizards):
-                self.assertTrue(new_wizards)
+        res_dict = self._process_picking()
+        backorder_wizard = Form(
+            self.env["stock.backorder.confirmation"].with_context(**res_dict["context"])
+        ).save()
+        backorder_wizard.process()
+        self.assertEqual(
+            "done",
+            self.picking.state,
+        )
+        backorder_exist = self.picking_obj.search(
+            [("backorder_id", "=", self.picking.id)]
+        )
+        self.assertEqual(len(backorder_exist), 1, "Back order should be created.")
