d63ca714d87fa16ed9a207b8902020ed68957fbc
Luis Ernesto García Medina <ernesto.garcia@tecnativa.com>
2023-02-15 01:47:53 -0600

    [MIG] stock_picking_backorder_strategy: Migration to 15.0

--- stock_picking_backorder_strategy/tests/test_backorder_strategy.py
+++ stock_picking_backorder_strategy/tests/test_backorder_strategy.py
@@ -1,10 +1,10 @@
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests import common
+from odoo.tests.common import TransactionCase
 
 
-class TestBackorderStrategy(common.SavepointCase):
+class TestBackorderStrategy(TransactionCase):
     @classmethod
     def setUpClass(cls):
         """Create the picking."""
