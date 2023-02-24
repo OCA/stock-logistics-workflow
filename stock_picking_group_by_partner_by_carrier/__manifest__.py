d82230ad827d5303af20ea73b747427410761568
Pere Martínez <pere@aquarian.tech>
2023-02-24 18:23:39 +0100

    replacing replaces

--- stock_picking_group_by_partner_by_carrier/__manifest__.py
+++ stock_picking_group_by_partner_by_carrier/__manifest__.py
@@ -16,7 +16,7 @@
         "views/res_partner.xml",
         "views/stock_picking_type.xml",
         "views/stock_warehouse.xml",
-        "report/assets.xml",
+        # "report/assets.xml",
         "report/report_delivery_slip.xml",
         "wizard/stock_picking_merge_wiz.xml",
         "wizard/stock_picking_merge_wiz_info_template.xml",
