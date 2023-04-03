df17f00dd40ab872976facb723ee006d8f081f9e
Urvisha-OSI <udesai@opensourceintegrators.com>
2023-04-03 11:46:31 +0530

    [IMP] stock_picking_backorder_strategy: pre-commit stuff

--- stock_picking_backorder_strategy/__manifest__.py
+++ stock_picking_backorder_strategy/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Picking backordering strategies",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "development_status": "Production/Stable",
     "author": """ACSONE SA/NV,
                  Odoo Community Association (OCA)""",
