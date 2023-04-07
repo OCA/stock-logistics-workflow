c32138fafc066b5a7472ab5648179f275763636d
moaadbourhim <moaad.bourhim@gmail.com>
2023-04-07 03:46:45 +0000

    [IMP] stock_picking_send_by_mail: pre-commit stuff

--- stock_picking_send_by_mail/tests/test_stock_picking_send_by_mail.py
+++ stock_picking_send_by_mail/tests/test_stock_picking_send_by_mail.py
@@ -8,6 +8,16 @@ class TestStockPickingSendByMail(common.TransactionCase):
     @classmethod
     def setUpClass(cls):
         super(TestStockPickingSendByMail, cls).setUpClass()
+        # Remove this variable in v16 and put instead:
+        # from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+        DISABLED_MAIL_CONTEXT = {
+            "tracking_disable": True,
+            "mail_create_nolog": True,
+            "mail_create_nosubscribe": True,
+            "mail_notrack": True,
+            "no_reset_password": True,
+        }
+        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
         cls.product = cls.env["product.product"].create({"name": "Test product"})
         cls.location_id = cls.env.ref("stock.stock_location_stock")
         cls.location_destination_id = cls.env.ref("stock.stock_location_customers")
