d31ff95b941b0ea8ad81916a2d41321a2ac4689d
Guillermo Bonet <gbonet@puntsistemes.es>
2023-06-05 13:12:49 +0200

    [MIG] : Migration to 16.0

--- stock_production_lot_active/__manifest__.py
+++ stock_production_lot_active/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Stock Production Lot Active",
     "summary": """
         Allow to archive/unarchive a lot.""",
-    "version": "15.0.1.0.2",
+    "version": "16.0.1.0.0",
     "development_status": "Beta",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
