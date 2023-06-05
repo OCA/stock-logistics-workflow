736efe662b442f04b33f2045eb64204b3c88da51
Guillermo Bonet <gbonet@puntsistemes.es>
2023-06-05 17:52:52 +0200

    pre-commit

--- stock_production_lot_active/models/stock_production_lot.py
+++ stock_production_lot_active/models/stock_production_lot.py
@@ -10,9 +10,9 @@ class StockProductionLot(models.Model):
 
     def write(self, vals):
         for record in self:
-            if record.active and record.product_qty and vals.get('active') == False:
+            if record.active and record.product_qty and not vals.get("active"):
                 raise UserError(_("This product has stock: %s", record.product_id.name))
-            super(StockProductionLot, self).write(vals)
+            return super(StockProductionLot, self).write(vals)
 
     active = fields.Boolean(default=True)
 
