c292b18f373dc0c10acb619fb590c15c455b1a72
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2023-06-23 19:13:16 +0200

    [MIG] stock_picking_line_sequence: Migration to 16.0

--- stock_picking_line_sequence/__manifest__.py
+++ stock_picking_line_sequence/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Stock picking lines with sequence number",
     "summary": "Manages the order of stock moves by displaying its sequence",
-    "version": "15.0.1.0.2",
+    "version": "16.0.1.0.0",
     "category": "Warehouse Management",
     "author": "Camptocamp, "
     "Eficent, "
