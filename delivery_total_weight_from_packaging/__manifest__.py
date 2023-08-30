5cc5cc358966b0104a7d9bec372b6ce81987d738
duongtq <duongtq@trobz.com>
2023-08-30 09:50:39 +0700

    [MIG] delivery_total_weight_from_packaging: Migration to 16.0

--- delivery_total_weight_from_packaging/__manifest__.py
+++ delivery_total_weight_from_packaging/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Delivery Total Weight From Packaging",
     "summary": "Include packaging weight on move, transfer and package.",
-    "version": "14.0.1.0.1",
+    "version": "16.0.1.0.0",
     "development_status": "Beta",
     "category": "Inventory",
     "website": "https://github.com/OCA/stock-logistics-workflow",
--- delivery_total_weight_from_packaging/readme/CREDITS.rst
+++ delivery_total_weight_from_packaging/readme/CREDITS.rst
@@ -3,4 +3,4 @@ The development of this module has been financially supported by:
 * Camptocamp
 * Cosanum
 
-The migration of this module from 13.0 to 14.0 was financially supported by Camptocamp
+The migration of this module from 13.0 to 16.0 was financially supported by Camptocamp
