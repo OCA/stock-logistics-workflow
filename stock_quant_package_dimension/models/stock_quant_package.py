b166f3db80481d8c300ac5e07d57456442ce168c
Denis Roussel <denis.roussel@acsone.eu>
2023-09-27 07:47:14 +0200

    [FIX] stock_quant_package_dimension: Don't change field type with a default
    As the original field is a computed one with a default, Odoo generates
    warning about that default if defined on a related field.
    
    So, override the compute method instead.

--- stock_quant_package_dimension/models/stock_quant_package.py
+++ stock_quant_package_dimension/models/stock_quant_package.py
@@ -61,7 +61,6 @@ class StockQuantPackage(models.Model):
     weight_uom_name = fields.Char(
         # Same as product.packing
         string="Weight unit of measure label",
-        related="weight_uom_id.name",
         readonly=True,
     )
 
@@ -84,6 +83,13 @@ class StockQuantPackage(models.Model):
         related="volume_uom_id.name",
         readonly=True,
     )
+
+    @api.depends("weight_uom_id.name")
+    def _compute_weight_uom_name(self):
+        # Don't use a related here as the original default value
+        # causes warnings (Redundant default on related fields are not wanted)
+        for package in self:
+            package.weight_uom_name = package.weight_uom_id.name
 
     @api.depends("pack_length", "width", "height")
     def _compute_volume(self):
