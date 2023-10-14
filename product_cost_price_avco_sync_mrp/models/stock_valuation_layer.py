d2fa53d421ea4ce3c9b08bec9dfe67d969279097
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-10-14 11:56:24 +0200

    [IMP] product_cost_price_avco_sync_mrp: Simplify code

--- product_cost_price_avco_sync_mrp/models/stock_valuation_layer.py
+++ product_cost_price_avco_sync_mrp/models/stock_valuation_layer.py
@@ -1,4 +1,5 @@
 # Copyright 2023 Tecnativa - Carlos Roca
+# Copyright 2023 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, models
@@ -8,48 +9,35 @@ class StockValuationLayer(models.Model):
     _inherit = "stock.valuation.layer"
 
     @api.model
-    def _cal_price_mrp(self, production, finished_move, consumed_values_list, svls_dic):
+    def _cal_price_mrp(self, quantity, svls_dic):
+        """Get the cost price for the production order linked to this SVL.
+        NOTE: This is adaptation of the method _cal_price in mrp_account.
+        """
+        finished_move = self.stock_move_id
+        production = finished_move.production_id
+        cost = 0
+        for cons_move in finished_move.move_line_ids.consume_line_ids.move_id:
+            svl = cons_move.stock_valuation_layer_ids[:1]
+            cost -= svls_dic.get(svl, {"value": svl.value})["value"]
         work_center_cost = 0
         for work_order in production.workorder_ids:
             time_lines = work_order.time_ids.filtered(
                 lambda x: x.date_end and not x.cost_already_recorded
             )
             duration = sum(time_lines.mapped("duration"))
-            time_lines.write({"cost_already_recorded": True})
             work_center_cost += (duration / 60.0) * work_order.workcenter_id.costs_hour
         svl = finished_move.stock_valuation_layer_ids[:1]
         currency = svl.currency_id
-        qty = (
-            svls_dic[svl]["quantity"]
-            if svl in svls_dic
-            else finished_move.quantity_done
-        )
         qty_done = finished_move.product_uom._compute_quantity(
-            qty, finished_move.product_id.uom_id
+            quantity, finished_move.product_id.uom_id
         )
         extra_cost = production.extra_cost * qty_done
-        return {
-            "unit_cost": currency.round(
-                (sum(consumed_values_list) + work_center_cost + extra_cost) / qty_done
-            )
-        }
-
-    @api.model
-    def _get_consumed_values(self, finished_move, svls_dic):
-        consumed_values_list = []
-        cosumed_moves = finished_move.move_line_ids.consume_line_ids.move_id
-        for cons_move in cosumed_moves:
-            if cons_move.stock_valuation_layer_ids[:1] in svls_dic:
-                consumed_values_list.append(
-                    abs(svls_dic[cons_move.stock_valuation_layer_ids[:1]]["value"])
-                )
-            else:
-                consumed_values_list.append(
-                    abs(cons_move.stock_valuation_layer_ids[:1]["value"])
-                )
-        return consumed_values_list
+        return currency.round((cost + work_center_cost + extra_cost) / qty_done)
 
     def _postprocess_rest_svl_to_sync(self, svls_dic, postprocess_svl_dic):
+        """If this SVL is the consumption of a raw material, we should affect the cost
+        of the finished products for such production order.
+        """
         if self.stock_move_id.raw_material_production_id:
             production = self.stock_move_id.raw_material_production_id
             for move in production.move_finished_ids.filtered(
@@ -58,26 +46,20 @@ class StockValuationLayer(models.Model):
                 and x.product_id.cost_method == "average"
             ):
                 svl = move.stock_valuation_layer_ids[:1]
-                consumed_values_list = self._get_consumed_values(move, svls_dic)
-                dict_price = self._cal_price_mrp(
-                    production, move, consumed_values_list, svls_dic
-                )
-                qty = svls_dic[svl]["quantity"] if svl in svls_dic else svl.quantity
-                dict_price["value"] = dict_price["unit_cost"] * qty
-                postprocess_svl_dic[svl] = dict_price
+                unit_cost = svl._cal_price_mrp(svl.quantity, svls_dic)
+                postprocess_svl_dic[svl] = {
+                    "unit_cost": unit_cost,
+                    "value": unit_cost * svl.quantity,
+                }
+                return True
         return super()._postprocess_rest_svl_to_sync(svls_dic, postprocess_svl_dic)
 
     def _preprocess_rest_svl_to_sync(self, svls_dic, preprocess_svl_dic):
+        """If this SVL is coming from a move of a production order, we get the unit cost
+        from the consumed materials, not doing anything more with it.
+        """
         if self.stock_move_id.production_id:
-            production = self.stock_move_id.production_id
-            svl = self.stock_move_id.stock_valuation_layer_ids[:1]
-            consumed_values_list = self._get_consumed_values(
-                self.stock_move_id, svls_dic
-            )
-            svls_dic[svl].update(
-                self._cal_price_mrp(
-                    production, self.stock_move_id, consumed_values_list, svls_dic
-                )
-            )
+            svl_dic = svls_dic[self]
+            svl_dic["unit_cost"] = self._cal_price_mrp(svl_dic["quantity"], svls_dic)
             return True
         return super()._preprocess_rest_svl_to_sync(svls_dic, preprocess_svl_dic)
