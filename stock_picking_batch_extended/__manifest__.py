77d7d5f0c532b9657d62714cdd701a208d4af52d
Rodrigo <rodrigo.bonilla@factorlibre.com>
2023-10-19 14:35:31 +0200

    [REF] stock_picking_batch_extended: readme is changed for better compression

--- stock_picking_batch_extended/__manifest__.py
+++ stock_picking_batch_extended/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Stock batch picking extended",
     "summary": "Allows manage a lot of pickings in batch",
-    "version": "16.0.1.0.1",
+    "version": "16.0.1.0.2",
     "author": "Camptocamp, " "Tecnativa, " "Odoo Community Association (OCA)",
     "development_status": "Mature",
     "maintainers": ["gurneyalex", "carlosdauden", "i-vyshnevska"],
