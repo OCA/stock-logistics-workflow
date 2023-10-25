7e853268b8796c278d833b39afd2e2989483dcc6
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-25 16:38:46 +0200

    [IMP] stock_picking_product_availability_inline: test performance improvement - Include context keys for avoiding mail operations overhead.

--- stock_picking_product_availability_inline/__manifest__.py
+++ stock_picking_product_availability_inline/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Stock Picking Product Availability Inline",
     "summary": "Show product availability in product drop-down in stock picking form view.",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.0.1",
     "development_status": "Beta",
     "category": "Warehouse Management",
     "website": "https://github.com/OCA/stock-logistics-workflow",
