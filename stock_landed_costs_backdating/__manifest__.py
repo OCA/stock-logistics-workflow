d6ea809f0c900c9e37f77a0bae1863c1e3813403
TheerayutEncoder <theerayutattajak@gmail.com>
2023-10-25 15:56:14 +0700

    [15.0][ADD] stock_landed_costs_backdating

---
+++ stock_landed_costs_backdating/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2023 Ecosoft Co., Ltd (https://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Landed Cost Backdating",
+    "summary": """
+        This module allows to fill Landed Cost date to backdate (date != now),
+    Stock valuation date will following to accouting date in Journal Entry on Landed Cost.""",
+    "version": "15.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Ecosoft Co., Ltd, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "depends": ["stock_landed_costs"],
+}
