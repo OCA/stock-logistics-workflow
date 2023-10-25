ad94a4d63868535af770ed0c68de092ee6819570
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-25 16:41:23 +0200

    [IMP] stock_picking_return_restricted_qty: test performance improvement - Switch to setUpClass for avoiding repeat the same setup for each test. - Include context keys for avoiding mail operations overhead.

--- stock_picking_return_restricted_qty/tests/test_stock_picking_return_restricted_qty.py
+++ stock_picking_return_restricted_qty/tests/test_stock_picking_return_restricted_qty.py
@@ -6,16 +6,27 @@ from odoo.tests import Form, common
 
 
 class StockPickingReturnRestrictedQtyTest(common.TransactionCase):
-    def setUp(self):
-        super().setUp()
-        partner = self.env["res.partner"].create({"name": "Test"})
-        product = self.env["product.product"].create(
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        # Remove this variable in v16 and put instead:
+        # from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+        DISABLED_MAIL_CONTEXT = {
+            "tracking_disable": True,
+            "mail_create_nolog": True,
+            "mail_create_nosubscribe": True,
+            "mail_notrack": True,
+            "no_reset_password": True,
+        }
+        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
+        partner = cls.env["res.partner"].create({"name": "Test"})
+        product = cls.env["product.product"].create(
             {"name": "test_product", "type": "product"}
         )
-        picking_type_out = self.env.ref("stock.picking_type_out")
-        stock_location = self.env.ref("stock.stock_location_stock")
-        customer_location = self.env.ref("stock.stock_location_customers")
-        self.picking = self.env["stock.picking"].create(
+        picking_type_out = cls.env.ref("stock.picking_type_out")
+        stock_location = cls.env.ref("stock.stock_location_stock")
+        customer_location = cls.env.ref("stock.stock_location_customers")
+        cls.picking = cls.env["stock.picking"].create(
             {
                 "partner_id": partner.id,
                 "picking_type_id": picking_type_out.id,
@@ -37,10 +48,10 @@ class StockPickingReturnRestrictedQtyTest(common.TransactionCase):
                 ],
             }
         )
-        self.picking.action_confirm()
-        self.picking.action_assign()
-        self.picking.move_lines[:1].quantity_done = 20
-        self.picking.button_validate()
+        cls.picking.action_confirm()
+        cls.picking.action_assign()
+        cls.picking.move_lines[:1].quantity_done = 20
+        cls.picking.button_validate()
 
     def get_return_picking_wizard(self, picking):
         stock_return_picking_form = Form(
