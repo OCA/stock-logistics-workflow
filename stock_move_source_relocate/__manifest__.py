c7024afe8063755715b1cf644001604d53484004
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2023-10-30 15:53:32 +0100

    [IMP] manually fix linting

--- stock_move_source_relocate/__manifest__.py
+++ stock_move_source_relocate/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Stock Move Source Relocation",
     "summary": "Change source location of unavailable moves",
-    "version": "14.0.1.3.0",
+    "version": "14.0.1.3.1",
     "development_status": "Beta",
     "category": "Warehouse Management",
     "website": "https://github.com/OCA/wms",
