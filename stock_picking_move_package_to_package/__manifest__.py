af7c21b404a0b37310f633928c10248befee6752
angelinaanaki <a.anakievskaa@gmail.com>
2023-11-08 23:23:13 +0100

    [16.0][ADD] stock_picking_move_package_to_package

---
+++ stock_picking_move_package_to_package/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2023 Cetmix OÜ
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Picking Move Package to Another Package",
+    "version": "16.0.1.0.0",
+    "category": "Inventory/Inventory",
+    "summary": "Move entire package to another package",
+    "depends": ["stock"],
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "author": "Cetmix, Odoo Community Association (OCA)",
+    "installable": True,
+    "data": [
+        "views/stock_package_level_view.xml",
+    ],
+    "license": "AGPL-3",
+}
