a9e5097f146058bad87c4d66cd4e98945d1168e4
Víctor Martínez <victor.martinez@tecnativa.com>
2023-11-08 13:43:10 +0100

    [IMP] stock_landed_costs_purchase_auto: Allow create stock.landed.cost without advanced permissions
    TT45656
    
    [UPD] Update stock_landed_costs_purchase_auto.pot
    
    [BOT] post-merge updates
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: stock-logistics-workflow-15.0/stock-logistics-workflow-15.0-stock_landed_costs_purchase_auto
    Translate-URL: https://translation.odoo-community.org/projects/stock-logistics-workflow-15-0/stock-logistics-workflow-15-0-stock_landed_costs_purchase_auto/

--- stock_landed_costs_purchase_auto/__manifest__.py
+++ stock_landed_costs_purchase_auto/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Stock landed costs purchase auto",
-    "version": "15.0.1.0.0",
+    "version": "15.0.2.0.0",
     "category": "Inventory",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "author": "Tecnativa, Odoo Community Association (OCA)",
