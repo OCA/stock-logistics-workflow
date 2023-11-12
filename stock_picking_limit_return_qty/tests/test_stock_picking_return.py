ca569c45cd4fb0284f68637f6fc50a854fa9e9a6
Demchuk Mykola <kolya233445@gmail.com>
2023-11-12 16:43:25 +0200

    [IMP]stock_picking_limit_return_qty: added tests to check the quantity_max field

--- stock_picking_limit_return_qty/tests/test_stock_picking_return.py
+++ stock_picking_limit_return_qty/tests/test_stock_picking_return.py
@@ -1,21 +1,26 @@
 # Copyright 2023 Cetmix OU
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from odoo.exceptions import ValidationError
 from odoo.tests.common import TransactionCase
 
 
 class TestStockReturnPicking(TransactionCase):
+    post_install = True
+    at_install = True
+
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
 
         cls.supplier_location = cls.env.ref("stock.stock_location_suppliers")
-        cls.customer_location = cls.env.ref("stock.stock_location_customers")
         cls.stock_location = cls.env.ref("stock.stock_location_stock")
-        cls.output_location = cls.env.ref("stock.stock_location_output")
         cls.picking_type_in = cls.env.ref("stock.picking_type_in")
-        cls.picking_type_out = cls.env.ref("stock.picking_type_out")
+        cls.picking_return = cls.env["stock.return.picking"]
+        cls.picking_return_line = cls.env["stock.return.picking.line"]
+        cls.stock_move = cls.env["stock.move"]
+        cls.product_product = cls.env["product.product"]
 
     @classmethod
     def _create_picking(cls, location, destination_location, picking_type):
@@ -28,46 +33,54 @@ class TestStockReturnPicking(TransactionCase):
         )
 
     def test_00(self):
-        """
-        Data:
-            4 stock location: customer, supplier, stock and stock_output
-        Case:
-            Create as much pickings as picking_type combinations to verify the compute
-        Result:
-            The picking_kind is correct for the combination of locations:
-                supplier-stock: supplier_in
-                stock-supplier: supplier_return
-                customer_stock: customer_return
-                stock-customer: customer_out
-                supplier-customer: drop_shipping
-                customer-supplier: drop_shipping_return
-                stock-output: False
-        """
         picking = self._create_picking(
             self.supplier_location, self.stock_location, self.picking_type_in
         )
-        self.assertEqual(picking.picking_kind, "supplier_in")
-        picking = self._create_picking(
-            self.stock_location, self.supplier_location, self.picking_type_out
+        product_a = self.product_product.create(
+            {"name": "Product A", "type": "product"}
         )
-        self.assertEqual(picking.picking_kind, "supplier_return")
-        picking = self._create_picking(
-            self.customer_location, self.stock_location, self.picking_type_in
+        product_b = self.product_product.create(
+            {"name": "Product B", "type": "product"}
         )
-        self.assertEqual(picking.picking_kind, "customer_return")
-        picking = self._create_picking(
-            self.stock_location, self.customer_location, self.picking_type_out
+        move_1 = self.stock_move.create(
+            {
+                "name": product_a.name,
+                "product_id": product_a.id,
+                "product_uom_qty": 2,
+                "picking_id": picking.id,
+                "location_id": picking.location_id.id,
+                "location_dest_id": picking.location_dest_id.id,
+            }
         )
-        self.assertEqual(picking.picking_kind, "customer_out")
-        picking = self._create_picking(
-            self.supplier_location, self.customer_location, self.picking_type_out
+        move_2 = self.stock_move.create(
+            {
+                "name": product_b.name,
+                "product_id": product_b.id,
+                "product_uom_qty": 1,
+                "picking_id": picking.id,
+                "location_id": picking.location_id.id,
+                "location_dest_id": picking.location_dest_id.id,
+            }
         )
-        self.assertEqual(picking.picking_kind, "drop_shipping")
-        picking = self._create_picking(
-            self.customer_location, self.supplier_location, self.picking_type_in
-        )
-        self.assertEqual(picking.picking_kind, "drop_shipping_return")
-        picking = self._create_picking(
-            self.stock_location, self.output_location, self.picking_type_out
-        )
-        self.assertFalse(picking.picking_kind)
+        picking.action_confirm()
+        picking.action_assign()
+        move_1.quantity_done = 2
+        move_2.quantity_done = 1
+        picking.button_validate()
+        return_wizard = self.picking_return.with_context(
+            active_id=picking.id, active_model="stock.picking"
+        ).create({})
+        return_wizard._onchange_picking_id()
+        for line in return_wizard.product_return_moves:
+            self.assertEqual(line.quantity, line.quantity_max)
+            error = "Error"
+            msg = ""
+            try:
+                line.quantity = line.quantity + 1
+            except ValidationError as e:
+                error = str(e)
+                msg = "You can return only %s of %s" % (
+                    line.quantity_max,
+                    line.product_id.name,
+                )
+            self.assertEqual(error, msg)
