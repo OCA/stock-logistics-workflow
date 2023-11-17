79f246a63ef60edbac1c08fd0bd2275819c99f59
Alessio Renda <renda855@gmail.com>
2023-11-17 10:56:50 +0100

    [IMP] stock_picking_inter_warehouse: restrict internal transfers type
    Make boolean 'Use for Inter-warehouse transfer' on picking type available only for configured warehouses

--- stock_picking_inter_warehouse/models/stock_picking_type.py
+++ stock_picking_inter_warehouse/models/stock_picking_type.py
@@ -7,6 +7,9 @@ from odoo import fields, models
 class StockPickingType(models.Model):
     _inherit = "stock.picking.type"
 
+    warehouse_inter_warehouse_transfers = fields.Boolean(
+        related="warehouse_id.inter_warehouse_transfers"
+    )
     inter_warehouse_transfer = fields.Boolean(string="Inter-Warehouse Transfer")
     disable_merge_picking_moves = fields.Boolean(
         string="Disable Merge Picking Moves",
