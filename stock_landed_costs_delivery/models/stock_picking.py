4c7c36b5a86ee923540308765be63fa119b7449e
Víctor Martínez <victor.martinez@tecnativa.com>
2024-01-16 16:20:22 +0100

    [FIX] stock_landed_costs_delivery: Filtering the correct Landed Cost
    It is necessary to filter the correct Landed Cost (from the correct picking)
    to set the cost lines.
    
    Example use case:
    - PO without carrier.
    - Picking A without carrier done > LC A (draft) without cost lines.
    - Picking B with carrier done > LC B (done) with cost lines.
    
    TT45656

--- stock_landed_costs_delivery/models/stock_picking.py
+++ stock_landed_costs_delivery/models/stock_picking.py
@@ -1,4 +1,4 @@
-# Copyright 2023 Tecnativa - Víctor Martínez
+# Copyright 2023-2024 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import models
 from odoo.tools import config
@@ -6,6 +6,12 @@ from odoo.tools import config
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
+
+    def _add_delivery_cost_to_po(self):
+        """We add the context to identify in _create_delivery_line() method the picking."""
+        self.ensure_one()
+        self = self.with_context(from_picking=self)
+        super()._add_delivery_cost_to_po()
 
     def _action_done(self):
         """Validate Landed costs linked to the purchase and picking."""
