67880541e1347f3102a1594bad5eb3127baeb760
Eduardo De Miguel <edu@moduon.team>
2024-02-07 13:47:35 +0100

    [FIX] stock_move_line_expiration_date_required: Remove required from field definition
    Using the required on the field definition causes the database to set "not nullable" on expiry_date column on stock.move.line.
    Moved the required to the views.

--- stock_move_line_expiration_date_required/__manifest__.py
+++ stock_move_line_expiration_date_required/__manifest__.py
@@ -3,12 +3,15 @@
 {
     "name": "Stock Move Line Expiration Date Required",
     "Summary": "Expiration date is required to enter manually on Move Lines.",
-    "version": "16.0.1.0.1",
+    "version": "16.0.1.0.2",
     "author": "Moduon, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "category": "Warehouse Management",
     "depends": ["product_expiry"],
-    "data": [],
+    "data": [
+        "views/stock_move_view.xml",
+        "views/stock_move_line_view.xml",
+    ],
     "installable": True,
     "license": "LGPL-3",
 }
