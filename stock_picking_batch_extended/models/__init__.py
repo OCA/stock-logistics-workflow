889b7f30c686aa01e305f6cdc5802156815b5f84
sergio-teruel <sergio.teruel@tecnativa.com>
2024-02-14 18:59:42 +0100

    [IMP] stock_picking_batch_extended: Open wave after creation

--- stock_picking_batch_extended/models/__init__.py
+++ stock_picking_batch_extended/models/__init__.py
@@ -1,7 +1,8 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
+from . import product_product
 from . import res_config_settings
 from . import stock_batch_picking
 from . import stock_move
-from . import product_product
+from . import stock_move_line
 from . import stock_warehouse
