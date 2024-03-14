e8013958296eef39d703e3ccc646839a3706641c
Telmo Santos <telmo.santos@camptocamp.com>
2024-03-14 11:57:15 +0100

    [IMP] stock_picking_partner_note check note already in use

---
+++ stock_picking_partner_note/models/res_company.py
@@ -0,0 +1,13 @@
+# Copyright 2024 Camptocamp (<https://www.camptocamp.com>).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    check_note_already_in_use = fields.Boolean(
+        string="Check picking note already in use",
+        default=False,
+    )
