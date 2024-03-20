d06be906d458f8a46e1c27697b4cd42804d4383a
Aungkokolin1997 <lin@quartile.co>
2024-03-20 18:18:08 +0700

    [ADD] stock_account_owner_valued

---
+++ stock_account_owner_valued/models/stock_move_line.py
@@ -0,0 +1,14 @@
+# Copyright 2024 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    @api.model
+    def _should_exclude_for_valuation(self):
+        if self.owner_id and self.owner_id.value_owner_inventory:
+            return False
+        return super()._should_exclude_for_valuation()
---
+++ stock_account_owner_valued/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module enables inventory valuation for owners whose value_owner_inventory is set to True.
