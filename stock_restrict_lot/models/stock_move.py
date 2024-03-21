f7c0b8cc761d6faa119882e36f1c39ce0f710498
hembert iregui <hiregui@anditel.com>
2024-03-21 11:21:48 -0500

    [MIG] stock_restrict_lot: Migration to 17.0

--- stock_restrict_lot/models/stock_move.py
+++ stock_restrict_lot/models/stock_move.py
@@ -63,20 +63,20 @@ class StockMove(models.Model):
     def _update_reserved_quantity(
         self,
         need,
-        available_quantity,
         location_id,
+        quant_ids=None,
         lot_id=None,
         package_id=None,
         owner_id=None,
-        strict=True,
+        strict=True
     ):
         self.ensure_one()
         if self.restrict_lot_id:
             lot_id = self.restrict_lot_id
         return super()._update_reserved_quantity(
             need,
-            available_quantity,
             location_id,
+            quant_ids=quant_ids,
             lot_id=lot_id,
             package_id=package_id,
             owner_id=owner_id,
