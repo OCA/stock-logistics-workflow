7d210a0610553b7b7c6877d0256c20b77ba885c8
Deriman-Alonso <d.alonso@binhex.cloud>
2024-04-09 10:36:53 +0100

    [IMP] stock_push_delay: pre-commit stuff

--- stock_push_delay/__manifest__.py
+++ stock_push_delay/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Stock Push Delay",
     "summary": "Manual evaluation of Push rules",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Inventory",
     "author": "ForgeFlow, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
