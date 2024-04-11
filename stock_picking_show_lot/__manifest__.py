f32c342621d146d0f44b2749462ff7d86391a97d
pilarvargas-tecnativa <pilar.vargas@tecnativa.com>
2024-04-11 17:02:01 +0200

    [ADD] stock_picking_show_lot: New module
    TT48693

---
+++ stock_picking_show_lot/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2024 Tecnativa - Pilar Vargas
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Picking Show Lot",
+    "version": "15.0.1.0.0",
+    "summary": "",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "depends": ["stock"],
+    "data": ["views/stock_picking_views.xml"],
+    "installable": True,
+    "license": "AGPL-3",
+}
---
+++ stock_picking_show_lot/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module adds a button to the "stock.picking" form if you have the option
+"Create New Lots/Serial Numbers" activated in the operation type that allows you to display the
+created "stock.production.lot".
