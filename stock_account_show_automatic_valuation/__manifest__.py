6d64586b99ee15c53783ad0b2a6f7aaeef5ff5ed
Cyril VINH-TUNG <cyril@invitu.com>
2024-06-01 10:14:24 -1000

    [ADD] Add Stock Account Show Automatic Valuation Module

---
+++ stock_account_show_automatic_valuation/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 INVITU
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Show Automatic Valuation for Stock Moves in CE",
+    "summary": "Allow automatic valuation for stock moves in community edition",
+    "version": "17.0.1.0.0",
+    "category": "Inventory",
+    "author": "INVITU," "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "depends": ["stock_account"],
+    "data": [
+        "views/res_config_settings_views.xml",
+    ],
+}
---
+++ stock_account_show_automatic_valuation/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+This module shows the "Automatic Valuation" setup for stock that has been hidden by Odoo SA in Community Edition
