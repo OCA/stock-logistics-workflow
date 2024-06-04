518bc1c321201f2e432ba527b20cc9586205c87e
Eduardo De Miguel <edu@moduon.team>
2024-06-04 16:16:48 +0200

    [ADD] stock_picking_date_deadline_syncs_scheduled_date

---
+++ stock_picking_date_deadline_syncs_scheduled_date/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Moduon Team S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0)
+
+{
+    "name": "Stock Picking Date Deadline syncs Scheduled Date",
+    "summary": "Sync Scheduled Date with Date Deadline in Stock Picking",
+    "version": "16.0.1.0.1",
+    "development_status": "Alpha",
+    "category": "Inventory/Inventory",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "author": "Moduon, Odoo Community Association (OCA)",
+    "maintainers": ["Shide"],
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["stock"],
+}
