a52e8a5fc8db0c9becb034b5e423616a877a2d6f
ferran-S73 <ferran@studio73.es>
2024-06-10 16:21:27 +0200

    [17.0][MIG] stock_quant_package_product_packaging

--- stock_quant_package_product_packaging/__manifest__.py
+++ stock_quant_package_product_packaging/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Stock Quant Package Product Packaging",
     "summary": "Use product packagings on packages",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.0",
     "development_status": "Beta",
     "category": "Warehouse Management",
     "website": "https://github.com/OCA/stock-logistics-workflow",
