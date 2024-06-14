5a5e9e0dc735387c3c08647d4b61977cae5e758d
anaïs <anais.lopez@forgeflow.com>
2024-06-14 14:35:36 +0200

    [MIG] stock_picking_origin_reference: Migration to 17.0

--- stock_picking_origin_reference/models/stock_picking.py
+++ stock_picking_origin_reference/models/stock_picking.py
@@ -1,4 +1,4 @@
-# Copyright 2023 ForgeFlow S.L. (https://www.forgeflow.com)
+# Copyright 2023-2024 ForgeFlow S.L. (https://www.forgeflow.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import api, fields, models
 
@@ -25,5 +25,5 @@ class StockPicking(models.Model):
             origin_reference = False
             rel_picking = self.search([("name", "=", picking.origin)], limit=1)
             if rel_picking:
-                origin_reference = "%s,%s" % (self._name, rel_picking.id)
+                origin_reference = f"{self._name},{rel_picking.id}"
             picking.origin_reference = origin_reference
