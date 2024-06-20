d33f6b1e0db6383fe05ece6c66a6022acc1db013
Mathieu <mathieu.delva@hotmail.com>
2024-06-20 16:15:32 +0200

    [ADD] stock_move_force_reservation

---
+++ stock_move_force_reservation/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2024 Akretion France (http://www.akretion.com/)
+# @author: Mathieu Delva <mathieu.delva@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Move Force Reservation",
+    "summary": "",
+    "version": "14.0.1.0.0",
+    "category": "Inventory",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "",
+    "depends": ["stock"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/stock_picking_views.xml",
+        "wizard/stock_move_force_reservation.xml",
+    ],
+    "installable": True,
+}
