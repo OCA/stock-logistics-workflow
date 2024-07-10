6908aaec7646a288789e3fb0f61e87b71745c4d7
Christian-RB <c.ramos@binhex.es>
2024-07-10 12:03:02 +0100

    [FIX] stock_picking_invoice_link: fix reverse_moves on refunds

--- stock_picking_invoice_link/models/stock_picking.py
+++ stock_picking_invoice_link/models/stock_picking.py
@@ -4,7 +4,7 @@
 # Copyright 2017 Jacques-Etienne Baudoux <je@bcim.be>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockPicking(models.Model):
@@ -12,6 +12,9 @@ class StockPicking(models.Model):
 
     invoice_ids = fields.Many2many(
         comodel_name="account.move", copy=False, string="Invoices", readonly=True
+    )
+    invoice_count = fields.Integer(
+        string="Number of Invoices", compute="_compute_invoice_count"
     )
 
     def action_view_invoice(self):
@@ -22,12 +25,18 @@ class StockPicking(models.Model):
         """
         self.ensure_one()
         form_view_name = "account.view_move_form"
-        xmlid = "account.action_move_out_invoice_type"
-        action = self.env["ir.actions.act_window"]._for_xml_id(xmlid)
+        result = self.env["ir.actions.act_window"]._for_xml_id(
+            "account.action_move_out_invoice_type"
+        )
         if len(self.invoice_ids) > 1:
-            action["domain"] = "[('id', 'in', %s)]" % self.invoice_ids.ids
+            result["domain"] = f"[('id', 'in', {self.invoice_ids.ids})]"
         else:
             form_view = self.env.ref(form_view_name)
-            action["views"] = [(form_view.id, "form")]
-            action["res_id"] = self.invoice_ids.id
-        return action
+            result["views"] = [(form_view.id, "form")]
+            result["res_id"] = self.invoice_ids.id
+        return result
+
+    @api.depends("invoice_ids")
+    def _compute_invoice_count(self):
+        for order in self:
+            order.invoice_count = len(order.invoice_ids)
