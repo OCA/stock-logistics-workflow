5e8f06111a73577fd0560caa74b652b4a20ea319
Juan Ignacio Carreras <jc@adhoc.com.ar>
2024-07-10 11:31:26 -0300

    [MIG] stock_restrict_lot: Migration to 17.0

--- stock_restrict_lot/models/stock_move.py
+++ stock_restrict_lot/models/stock_move.py
@@ -63,8 +63,8 @@ class StockMove(models.Model):
     def _update_reserved_quantity(
         self,
         need,
-        available_quantity,
         location_id,
+        quant_ids=None,
         lot_id=None,
         package_id=None,
         owner_id=None,
@@ -75,8 +75,8 @@ class StockMove(models.Model):
             lot_id = self.restrict_lot_id
         return super()._update_reserved_quantity(
             need,
-            available_quantity,
             location_id,
+            quant_ids=quant_ids,
             lot_id=lot_id,
             package_id=package_id,
             owner_id=owner_id,
