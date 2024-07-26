e5dd098e35d18ced184cc660ab678bbc04dd1edb
Mathieu <mathieu.delva@hotmail.com>
2024-07-26 10:21:17 +0200

    [ADD] readme [FIX] pylint

--- stock_move_force_reservation/models/stock_move.py
+++ stock_move_force_reservation/models/stock_move.py
@@ -10,14 +10,18 @@ class StockMove(models.Model):
 
     show_force_quantity = fields.Boolean(compute="_compute_show_force_quantity")
     qty_in_stock = fields.Float(compute="_compute_qty_in_stock")
-    picking_state = fields.Selection([
-        ('draft', 'Draft'),
-        ('waiting', 'Waiting Another Operation'),
-        ('confirmed', 'Waiting'),
-        ('assigned', 'Ready'),
-        ('done', 'Done'),
-        ('cancel', 'Cancelled'),
-    ], related='picking_id.state', string="Picking Status")
+    picking_state = fields.Selection(
+        [
+            ("draft", "Draft"),
+            ("waiting", "Waiting Another Operation"),
+            ("confirmed", "Waiting"),
+            ("assigned", "Ready"),
+            ("done", "Done"),
+            ("cancel", "Cancelled"),
+        ],
+        related="picking_id.state",
+        string="Picking Status",
+    )
 
     def _compute_show_force_quantity(self):
         for record in self:
@@ -27,20 +31,22 @@ class StockMove(models.Model):
 
     def _compute_qty_in_stock(self):
         for record in self:
-            record.qty_in_stock = record.product_id.with_context(warehouse=record.location_id.get_warehouse().id).qty_available
-    
+            record.qty_in_stock = record.product_id.with_context(
+                warehouse=record.location_id.get_warehouse().id
+            ).qty_available
+
     def force_quantity(self):
         self.ensure_one()
-        
+
         action = self.env["ir.actions.act_window"]._for_xml_id(
             "stock_move_force_reservation.stock_move_force_reservation_action"
         )
         self.env.context = dict(self.env.context)
         self.env.context.update({"product_id": self.product_id})
         action.update(
-            {   
+            {
                 "active_id": self.id,
-                "active_model": self._name,       
+                "active_model": self._name,
             }
         )
         return action
---
+++ stock_move_force_reservation/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+When a product is not available, the module adds a wizard on picking to display all the other pickings which have not yet been done and which contain this product
+We can then unreserve the pickings we want to make the quantities available on the picking we are on.
