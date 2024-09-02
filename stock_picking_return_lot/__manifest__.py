4f8c3f5ec491bd1c0e4feca8770ffdef21d617de
sbejaoui <souheil.bejaoui@acsone.eu>
2024-09-02 13:10:29 +0200

    [IMP] stock_picking_return_lot: Add a lot field to the return wizard
    - Add a lot field to the return wizard and create a return line for each lot/product combination.
    - Use Stock Restrict Lot to track the accepted lot on the return picking.

--- stock_picking_return_lot/__manifest__.py
+++ stock_picking_return_lot/__manifest__.py
@@ -1,4 +1,5 @@
 # Copyright 2020 Camptocamp
+# Copyright 2024 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
@@ -6,7 +7,8 @@
     "summary": "Propagate SN/lots from origin picking to return picking.",
     "version": "16.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "author": "Camptocamp, ACSONE SA/NV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-workflow",
-    "depends": ["stock", "stock_account"],
+    "depends": ["stock", "stock_restrict_lot"],
+    "data": ["wizards/stock_return_picking.xml"],
 }
