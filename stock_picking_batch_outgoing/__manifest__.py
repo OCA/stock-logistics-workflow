e07c2048e90539bba305088de334b5196b322ec2
sergio-teruel <sergio.teruel@tecnativa.com>
2024-09-18 13:53:10 +0200

    [IMP] stock_picking_batch_outgoing: Allow group pickings by outgoing batch picking TT50892

--- stock_picking_batch_outgoing/__manifest__.py
+++ stock_picking_batch_outgoing/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Stock picking batch outgoing",
     "summary": "Allows set on pickings the batch picking from last picking (out)",
-    "version": "15.0.1.1.0",
+    "version": "15.0.1.2.0",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "development_status": "Beta",
     "maintainers": ["sergio-teruel"],
