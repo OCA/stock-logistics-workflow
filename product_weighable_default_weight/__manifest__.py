a5583ea4de3660ad8469725ee65f05fa14494aec
Sylvain LE GAL <sylvain.legal@grap.coop>
2024-10-20 10:48:59 +0200

    [MIG] product_weighable_default_weight: Migration to 16.0 (from 12.0)

--- product_weighable_default_weight/__manifest__.py
+++ product_weighable_default_weight/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Weighable Product - Default Weight",
-    "version": "12.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Point Of Sale",
     "summary": "Set default weight on weighable product,"
     " the weight is guessed from the ratio of the unit of mesure",
