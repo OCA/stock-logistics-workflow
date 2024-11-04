fdae4eb6f6032330832a8aeb2badce5de17b727a
LauraCForgeFlow <laura.cazorla@forgeflow.com>
2024-11-04 15:18:47 +0100

    [ADD] stock_landed_costs_priority

---
+++ stock_landed_costs_priority/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2024 ForgeFlow S.L. (https://www.forgeflow.com)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from . import models
---
+++ stock_landed_costs_priority/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module adds priority to landed costs. Priorities can be used to rank
+landed costs by their level of importance, urgency or other criteria.
