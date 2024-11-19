73341daf07ad237a497fa52a5029fb9e59c50ae4
Frédéric GRALL <grall.f@hotmail.fr>
2024-11-19 17:13:15 +0100

    [MIG] stock_picking_send_by_mail: Migration to 18.0
    [MIG] stock_picking_send_by_mail: Migration to 18.0

--- stock_picking_send_by_mail/__manifest__.py
+++ stock_picking_send_by_mail/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Stock Picking by Mail",
     "summary": "Send stock picking by email",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Sandra Figueroa Varela, "
     "Tecnativa, "
     "Odoo Community Association (OCA), "
--- stock_picking_send_by_mail/tests/test_stock_picking_send_by_mail.py
+++ stock_picking_send_by_mail/tests/test_stock_picking_send_by_mail.py
@@ -9,14 +9,8 @@ class TestStockPickingSendByMail(common.TransactionCase):
     def setUpClass(cls):
         super().setUpClass()
         # Remove this variable in v16 and put instead:
-        # from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
-        DISABLED_MAIL_CONTEXT = {
-            "tracking_disable": True,
-            "mail_create_nolog": True,
-            "mail_create_nosubscribe": True,
-            "mail_notrack": True,
-            "no_reset_password": True,
-        }
+        from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+
         cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
         cls.product = cls.env["product.product"].create({"name": "Test product"})
         cls.location_id = cls.env.ref("stock.stock_location_stock")
