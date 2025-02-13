43c317be1f120e03180d5ea22f4cccf5c40fb487
ricardoMC <rmaldonado@spearhead.global>
2025-02-13 19:10:55 -0400

    [MIG] stock_picking_tier_validation: Migration to version 18.0

--- stock_picking_tier_validation/tests/test_tier_validation.py
+++ stock_picking_tier_validation/tests/test_tier_validation.py
@@ -1,15 +1,14 @@
 # Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo.tests import common
-from odoo.tests.common import tagged
+from odoo.tests.common import TransactionCase, tagged
 
 from .common import setup_test_model, teardown_test_model
 from .tier_validation_tester import TierValidationTester
 
 
 @tagged("post_install", "-at_install")
-class TestStockPickingTierValidation(common.SavepointCase):
+class TestStockPickingTierValidation(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
