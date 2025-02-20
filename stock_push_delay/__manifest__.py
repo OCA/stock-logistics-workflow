1f74dbdd93daa3f54a1fc121c9c9d1e093611ac6
DavidJForgeFlow <david.jimenez@forgeflow.com>
2025-02-20 16:21:31 +0100

    [MIG] stock_push_delay: Migration to 17.0

--- stock_push_delay/__manifest__.py
+++ stock_push_delay/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2020 ForgeFlow S.L.
+# Copyright 2020-25 ForgeFlow S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Stock Push Delay",
     "summary": "Manual evaluation of Push rules",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Inventory",
     "author": "ForgeFlow, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
