8a0b3ec933e9e4323987e87d8d7324b72303b38c
DavidJForgeFlow <david.jimenez@forgeflow.com>
2025-02-26 09:23:52 +0100

    [IMP] stock_push_delay
    Allows to create stock.moves from procurements and take the destination location of the resulting moves to be the one defined in the picking type. Also adds final location on the stock moves to be able to follow the same logic as v18.

--- stock_push_delay/__manifest__.py
+++ stock_push_delay/__manifest__.py
@@ -1,13 +1,14 @@
 # Copyright 2020-25 ForgeFlow S.L.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Stock Push Delay",
     "summary": "Manual evaluation of Push rules",
     "version": "17.0.1.0.0",
     "category": "Inventory",
     "author": "ForgeFlow, " "Odoo Community Association (OCA)",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "website": "https://github.com/OCA/stock-logistics-workflow",
+    "data": ["views/stock_move_views.xml", "views/stock_rule_views.xml"],
     "depends": ["stock"],
     "development_status": "Alpha",
 }
