05f84c1007da984abe39e320153c58ed99574856
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2025-04-17 19:34:23 +0200

    [IMP] stock_move_backdating: pre-commit auto fixes

--- stock_move_backdating/tests/test_picking.py
+++ stock_move_backdating/tests/test_picking.py
@@ -4,7 +4,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
-from odoo.exceptions import UserError, ValidationError
 from odoo.tests import tagged
 
 from .common import TestCommon
