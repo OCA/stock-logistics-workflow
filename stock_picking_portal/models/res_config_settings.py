d7b7685711d28cdce5649c963ef1b2378a1dac5f
Mohajiro <lavrovuriy@gmail.com>
2025-04-22 09:27:06 +0200

    [FIX] stock_picking_portal: optimize set_values writes
    Only update types whose portal_visible really changes to cut redundant writes.
    
    Task: 3612

--- stock_picking_portal/models/res_config_settings.py
+++ stock_picking_portal/models/res_config_settings.py
@@ -12,16 +12,38 @@ class ResConfigSettings(models.TransientModel):
         string="Portal Visible Operations",
     )
 
+    def set_values(self):
+        """Save the M2M into the boolean field on stock.picking.type,
+        updating only the records whose flag have changed."""
+        res = super().set_values()
+        selected = self.portal_visible_operation_ids
+        currently_visible = self.env["stock.picking.type"].search(
+            [
+                ("portal_visible", "=", True),
+            ]
+        )
+
+        to_invisible = currently_visible - selected
+        to_visible = selected - currently_visible
+
+        if to_invisible:
+            to_invisible.write({"portal_visible": False})
+        if to_visible:
+            to_visible.write({"portal_visible": True})
+
+        return res
+
     @api.model
     def get_values(self):
         res = super().get_values()
-        visible = self.env["stock.picking.type"].search([("portal_visible", "=", True)])
-        res.update(portal_visible_operation_ids=visible.ids)
-        return res
-
-    def set_values(self):
-        res = super().set_values()
-        all_types = self.env["stock.picking.type"].search([])
-        all_types.write({"portal_visible": False})
-        self.portal_visible_operation_ids.write({"portal_visible": True})
+        visible_ids = (
+            self.env["stock.picking.type"]
+            .search(
+                [
+                    ("portal_visible", "=", True),
+                ]
+            )
+            .ids
+        )
+        res.update(portal_visible_operation_ids=visible_ids)
         return res
