e874716fde2153c4a75e897495ffbab1276da41c
Sébastien Alix <sebastien.alix@camptocamp.com>
2025-06-06 16:41:44 +0200

    [IMP] stock_dynamic_routing_checkout_sync: pre-commit auto fixes

--- stock_dynamic_routing_checkout_sync/__manifest__.py
+++ stock_dynamic_routing_checkout_sync/__manifest__.py
@@ -4,7 +4,7 @@
     "summary": "Glue module for tests when dynamic routing and"
     " checkout sync are used ",
     "author": "Camptocamp, Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/wms",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
     "category": "Warehouse Management",
     "version": "14.0.1.0.0",
     "license": "AGPL-3",
