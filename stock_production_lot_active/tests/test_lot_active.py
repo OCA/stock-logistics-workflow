c64461a757e385ff68b13b5122173444c72437dc
AEstLo <palestebanez1@hotmail.com>
2025-07-22 09:47:57 +0200

    [MIG] stock_production_lot_active: Migration to 18.0

--- stock_production_lot_active/tests/test_lot_active.py
+++ stock_production_lot_active/tests/test_lot_active.py
@@ -1,10 +1,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo.exceptions import ValidationError
-from odoo.tests.common import TransactionCase
+
+from odoo.addons.base.tests.common import BaseCommon
 
 
-class TestLotActive(TransactionCase):
+class TestLotActive(BaseCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
