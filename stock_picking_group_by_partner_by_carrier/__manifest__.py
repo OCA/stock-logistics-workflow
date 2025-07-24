3fa961f9a84fe49f6147e7859beaae3ffabd1232
PolComas <pcomasro93@alumnes.ub.edu>
2025-07-24 11:49:50 +0200

    [MIG] Set maturity to alpha in manifest

--- stock_picking_group_by_partner_by_carrier/__manifest__.py
+++ stock_picking_group_by_partner_by_carrier/__manifest__.py
@@ -5,6 +5,7 @@
     "name": "Stock Picking: group by partner and carrier",
     "Summary": "Group sales deliveries moves in 1 picking per partner and carrier",
     "version": "15.0.1.0.0",
+    "development_status": "Alpha",
     "author": "Camptocamp, BCIM, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "category": "Warehouse",
@@ -28,5 +29,4 @@
     },
     "installable": True,
     "license": "AGPL-3",
-    "maturity": "Alpha",
 }
