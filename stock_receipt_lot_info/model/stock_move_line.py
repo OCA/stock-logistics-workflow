4b8fb11ecdd1c9c918c201b55ce6d266c66c211d
Arnau <arnau.cruz@forgeflow.com>
2025-08-13 17:27:33 +0200

    [MIG] stock_receipt_lot_info: Migration to 17.0

--- stock_receipt_lot_info/model/stock_move_line.py
+++ stock_receipt_lot_info/model/stock_move_line.py
@@ -10,9 +10,10 @@ class StockMoveLine(models.Model):
     lot_use_date = fields.Datetime(string="Lot/Serial Best before Date")
     lot_removal_date = fields.Datetime(string="Lot/Serial Removal Date")
     lot_alert_date = fields.Datetime(string="Lot/Serial Alert Date")
+    show_lots_text = fields.Boolean(related="picking_id.show_lots_text", readonly=True)
 
-    def _get_value_production_lot(self):
-        vals = super()._get_value_production_lot()
+    def _prepare_new_lot_vals(self):
+        vals = super()._prepare_new_lot_vals()
         creation_lot_fields = [
             "use_date",
             "removal_date",
