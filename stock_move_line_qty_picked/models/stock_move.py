3b778713056b91b1231403ce427814224f64ede5
Akim Juillerat <akim.juillerat@camptocamp.com>
2025-08-18 20:18:39 +0200

    [FIX] stock_move_line_qty_picked: Align move quantity with reserved
    In the previous version of this module the computation of
    stock.move.line.quantity was modified to reflect the picked quantity.
    
    However, stock.move.line.quantity must reflect the reserved quantity
    on the move lines since there is an inverse function that is going
    to increase or decrease the quantity on stock.move.line.quantity
    accordingly.
    
    The stock.move.quantity must only be updated to reflect the sum
    of stock.move.line.qty_picked when stock.move._action_done is called,
    to avoid the creation of a backorder.

--- stock_move_line_qty_picked/models/stock_move.py
+++ stock_move_line_qty_picked/models/stock_move.py
@@ -1,8 +1,8 @@
 # Copyright 2025 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
-from collections import defaultdict
 
-from odoo import api, models
+from odoo import models
+from odoo.tools import float_compare
 
 
 class StockMove(models.Model):
@@ -13,45 +13,25 @@ class StockMove(models.Model):
             return self._sum_ml_qty_picked()
         return super()._get_picked_quantity()
 
-    @api.depends("move_line_ids.picked", "move_line_ids.qty_picked")
-    def _compute_quantity(self):
-        picked_moves_ids = []
-        for move in self:
-            if move.picked and any(ml.qty_picked for ml in move.move_line_ids):
-                picked_moves_ids.append(move.id)
-        picked_moves_ids = self.browse(picked_moves_ids)
-
-        # Copied from odoo and adapted for qty_picked
-        data = self.env["stock.move.line"]._read_group(
-            [("id", "in", picked_moves_ids.move_line_ids.ids)],
-            ["move_id", "product_uom_id"],
-            ["qty_picked:sum"],
-        )
-        sum_qty = defaultdict(float)
-        for move, product_uom, qty_sum in data:
-            uom = move.product_uom
-            sum_qty[move.id] += product_uom._compute_quantity(qty_sum, uom, round=False)
-
-        for move in picked_moves_ids:
-            move.quantity = sum_qty[move.id]
-
-        not_picked_moves = self - picked_moves_ids
-        return super(StockMove, not_picked_moves)._compute_quantity()
-
-    def _quantity_sml(self):
-        if self._ml_has_qty_picked():
-            return self._sum_ml_qty_picked()
-        return super()._quantity_sml()
-
     def _ml_has_qty_picked(self):
-        self.ensure_one()
         return self.picked and any(ml.qty_picked for ml in self.move_line_ids)
 
     def _sum_ml_qty_picked(self):
         self.ensure_one()
         quantity = 0
-        for move_line in self.move_line_ids:
+        for move_line in self.move_line_ids.filtered("picked"):
             quantity += move_line.product_uom_id._compute_quantity(
                 move_line.qty_picked, self.product_uom, round=False
             )
         return quantity
+
+    def _action_done(self, cancel_backorder=False):
+        for move in self:
+            for line in move.move_line_ids:
+                if line.picked and float_compare(
+                    line.qty_picked,
+                    line.quantity,
+                    precision_rounding=line.product_uom_id.rounding,
+                ):
+                    line.quantity = line.qty_picked
+        return super()._action_done(cancel_backorder=cancel_backorder)
