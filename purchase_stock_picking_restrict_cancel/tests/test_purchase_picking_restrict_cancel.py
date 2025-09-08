3148d641f361b20daf4ede0a9d6b47041f9ba7b6
MeritxellA <meritxell.abellan@forgeflow.com>
2025-09-08 09:51:08 +0200

    [MIG] purchase_stock_picking_restrict_cancel: Migration to 18.0

--- purchase_stock_picking_restrict_cancel/tests/test_purchase_picking_restrict_cancel.py
+++ purchase_stock_picking_restrict_cancel/tests/test_purchase_picking_restrict_cancel.py
@@ -1,10 +1,13 @@
 # Copyright 2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import Command
 from odoo.exceptions import UserError
-from odoo.tests import Form, SavepointCase
+from odoo.tests import Form
+
+from odoo.addons.base.tests.common import BaseCommon
 
 
-class TestRestrictCancelStockMove(SavepointCase):
+class TestRestrictCancelStockMove(BaseCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -15,38 +18,75 @@ class TestRestrictCancelStockMove(SavepointCase):
         cls.qc_loc = cls.warehouse.wh_qc_stock_loc_id
         cls.internal_pt = cls.warehouse.int_type_id
         cls.internal_pt.active = True
+        cls.internal_pt.restrict_cancel_with_orig_move = True
         # Create a vendor
         partner = cls.env["res.partner"].create({"name": "Smith"})
         # Create product and set the default vendor
         product_form = Form(cls.env["product.product"])
         product_form.name = "Product A"
-        product_form.type = "product"
+        product_form.type = "consu"
+        product_form.is_storable = "True"
         product_form.purchase_ok = True
         with product_form.seller_ids.new() as seller:
-            seller.name = partner
+            seller.partner_id = partner
         product_form.route_ids.add(cls.env.ref("purchase_stock.route_warehouse0_buy"))
         cls.dummy_product = product_form.save()
         # Create product reordering rule
-        orderpoint_form = Form(cls.env["stock.warehouse.orderpoint"])
-        orderpoint_form.warehouse_id = cls.warehouse
-        orderpoint_form.location_id = cls.warehouse.lot_stock_id
-        orderpoint_form.product_id = cls.dummy_product
-        orderpoint_form.product_min_qty = 0.000
-        orderpoint_form.product_max_qty = 10.000
-        cls.order_point = orderpoint_form.save()
+        cls.order_point = cls.env["stock.warehouse.orderpoint"].create(
+            {
+                "name": f"OP-{cls.dummy_product.name}",
+                "warehouse_id": cls.warehouse.id,
+                "location_id": cls.stock_loc.id,
+                "product_id": cls.dummy_product.id,
+                "product_min_qty": 1.0,
+                "product_max_qty": 10.0,
+            }
+        )
 
     def test_restrict(self):
-        # Run scheduler, this should create new PO and Stock Move (QC -> Stock)
+        # Run scheduler, this should create a new RFQ
         self.env["procurement.group"].run_scheduler()
-        # Find newly created move from QC -> Stock
-        qc_to_stock_move = self.env["stock.move"].search(
+        rfqs = (
+            self.env["purchase.order.line"]
+            .search([("product_id", "=", self.dummy_product.id)])
+            .order_id.filtered(lambda po: po.state in ("draft", "sent", "to approve"))
+        )
+        rfqs.button_confirm()
+        receipt_move = self.env["stock.move"].search(
             [
                 ("product_id", "=", self.dummy_product.id),
-                ("location_id", "=", self.qc_loc.id),
-                ("location_dest_id", "=", self.stock_loc.id),
+                ("location_dest_id", "=", self.input_loc.id),
             ]
         )
-        qc_to_stock_picking = qc_to_stock_move.picking_id
-        self.assertNotEqual(qc_to_stock_move.state, "cancel")
-        with self.assertRaises(UserError):
-            qc_to_stock_picking.action_cancel()
+        self.assertNotEqual(receipt_move.state, "cancel")
+        # Manually create the next leg (Input -> QC), linked to that receipt
+        # move as its origin, to test cancelling it while the PO receipt is
+        # not done yet.
+        input_to_qc_picking = self.env["stock.picking"].create(
+            {
+                "picking_type_id": self.internal_pt.id,
+                "location_id": self.input_loc.id,
+                "location_dest_id": self.qc_loc.id,
+                "move_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": self.dummy_product.name,
+                            "product_id": self.dummy_product.id,
+                            "product_uom": self.dummy_product.uom_id.id,
+                            "product_uom_qty": 10,
+                            "location_id": self.input_loc.id,
+                            "location_dest_id": self.qc_loc.id,
+                        },
+                    )
+                ],
+            }
+        )
+        input_to_qc_picking.action_confirm()
+        input_to_qc_picking.move_ids.move_orig_ids |= receipt_move
+        receipt_move.created_purchase_line_ids = [Command.link(rfqs.order_line.id)]
+        self.assertNotEqual(input_to_qc_picking.move_ids.state, "cancel")
+        with self.assertRaises(UserError) as cm:
+            input_to_qc_picking.action_cancel()
+        self.assertIn(rfqs.order_line.name, str(cm.exception))
