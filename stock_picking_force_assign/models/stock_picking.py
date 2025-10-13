87f28f2766537b8e0d1eab6d45a83b816a4110db
AaronHForgeFlow <aaron.henriquez@forgeflow.com>
2025-10-13 16:57:31 +0200

    [IMP] stock_picking_force_assign: pre-commit auto fixes

--- stock_picking_force_assign/models/stock_picking.py
+++ stock_picking_force_assign/models/stock_picking.py
@@ -1,6 +1,7 @@
 # Copyright 2021 Hunki Enterprises BV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 import functools
+
 from odoo import _, exceptions, models, tools
 
 
@@ -8,55 +9,57 @@ class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     def action_force_assign_pickings(self):
-        """ Unreserve other pickings in order to reserve pickings in self """
+        """Unreserve other pickings in order to reserve pickings in self"""
         link_template = '<a href="#" data-oe-model="%s" data-oe-id="%d">%s</a>'
         to_unreserve = self._force_assign_find_moves()
         to_unreserve._do_unreserve()
-        self.message_post(body=_(
-            'Unreserved picking(s) %s in order to assign this one'
-        ) % ', '.join(to_unreserve.mapped('picking_id').mapped(
-            lambda x: link_template % (x._name, x.id, x.name)
-        )))
-        to_unreserve.mapped('picking_id').message_post(body=_(
-            'Unreserved this picking in order to assign %s'
-        ) % ', '.join(self.mapped(
-            lambda x: link_template % (x._name, x.id, x.name)
-        )))
+        self.message_post(
+            body=_("Unreserved picking(s) %s in order to assign this one")
+            % ", ".join(
+                to_unreserve.mapped("picking_id").mapped(
+                    lambda x: link_template % (x._name, x.id, x.name)
+                )
+            )
+        )
+        to_unreserve.mapped("picking_id").message_post(
+            body=_("Unreserved this picking in order to assign %s")
+            % ", ".join(self.mapped(lambda x: link_template % (x._name, x.id, x.name)))
+        )
         return self.action_assign()
 
     def _force_assign_allow_partial(self):
         """Indicate if partial forced assignments should be allowed"""
         return tools.misc.str2bool(
-            self.env["ir.config_parameter"].sudo().get_param(
-                "stock_picking_force_assign.allow_partial"
-            ),
+            self.env["ir.config_parameter"]
+            .sudo()
+            .get_param("stock_picking_force_assign.allow_partial"),
             default=False,
         )
 
     def _force_assign_find_moves(self):
-        """ Return moves to unreserve in order to reserve pickings in self """
-        location = self.mapped('location_id')
-        result = self.env['stock.move']
-        assert len(location) == 1, 'Pickings need to be from the same location'
+        """Return moves to unreserve in order to reserve pickings in self"""
+        location = self.mapped("location_id")
+        result = self.env["stock.move"]
+        assert len(location) == 1, "Pickings need to be from the same location"
         float_compare = functools.partial(
             tools.float_compare,
-            precision_digits=result._fields['product_qty'].digits,
+            precision_digits=result._fields["product_qty"].digits,
         )
 
-        for move in self.mapped('move_lines'):
-            demand = (
-                move.product_qty - move.reserved_availability -
-                move.availability
-            )
+        for move in self.mapped("move_lines"):
+            demand = move.product_qty - move.reserved_availability - move.availability
             if float_compare(demand, 0) <= 0:
                 continue
-            candidates = self.env['stock.move'].search([
-                ('id', 'not in', result.ids),
-                ('picking_id', 'not in', self.ids),
-                ('location_id', '=', location.id),
-                ('product_id', '=', move.product_id.id),
-                ('state', 'in', ('partially_available', 'assigned')),
-            ], order='write_date asc')
+            candidates = self.env["stock.move"].search(
+                [
+                    ("id", "not in", result.ids),
+                    ("picking_id", "not in", self.ids),
+                    ("location_id", "=", location.id),
+                    ("product_id", "=", move.product_id.id),
+                    ("state", "in", ("partially_available", "assigned")),
+                ],
+                order="write_date asc",
+            )
 
             for candidate in candidates:
                 if float_compare(demand, 0) > 0:
@@ -65,11 +68,9 @@ class StockPicking(models.Model):
                 else:
                     break
 
-            if (float_compare(demand, 0) > 0
-                    and not self._force_assign_allow_partial()):
+            if float_compare(demand, 0) > 0 and not self._force_assign_allow_partial():
                 raise exceptions.UserError(
-                    _('Cannot unreserve enough %s, missing quantity is %d') % (
-                        move.product_id.name, demand
-                    )
+                    _("Cannot unreserve enough %s, missing quantity is %d")
+                    % (move.product_id.name, demand)
                 )
         return result
--- stock_picking_force_assign/tests/test_stock_picking_force_assign.py
+++ stock_picking_force_assign/tests/test_stock_picking_force_assign.py
@@ -1,72 +1,85 @@
 # Copyright 2021 Hunki Enterprises BV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import TransactionCase
 from odoo.exceptions import UserError
+from odoo.tests.common import TransactionCase
 
 
 class TestStockPickingForceAssign(TransactionCase):
     def setUp(self, *args, **kwargs):
         super().setUp(*args, **kwargs)
-        self.product = self.env['product.product'].create({
-            'name': 'stockable',
-            'type': 'product',
-        })
-        self.assigned_picking = self.env['stock.picking'].create({
-            'name': 'Picking to unassign',
-            'picking_type_id': self.env.ref('stock.picking_type_out').id,
-            'location_id': self.env.ref('stock.stock_location_stock').id,
-            'location_dest_id':
-            self.env.ref('stock.stock_location_customers').id,
-            'move_lines': [(0, 0, {
-                'name': self.product.name,
-                'product_id': self.product.id,
-                'product_uom': self.product.uom_id.id,
-                'product_uom_qty': 42,
-            })],
-        })
-        self.env['stock.quant'].create({
-            'location_id': self.assigned_picking.location_id.id,
-            'product_id': self.product.id,
-            'quantity': 42,
-        })
+        self.product = self.env["product.product"].create(
+            {
+                "name": "stockable",
+                "type": "product",
+            }
+        )
+        self.assigned_picking = self.env["stock.picking"].create(
+            {
+                "name": "Picking to unassign",
+                "picking_type_id": self.env.ref("stock.picking_type_out").id,
+                "location_id": self.env.ref("stock.stock_location_stock").id,
+                "location_dest_id": self.env.ref("stock.stock_location_customers").id,
+                "move_lines": [
+                    (
+                        0,
+                        0,
+                        {
+                            "name": self.product.name,
+                            "product_id": self.product.id,
+                            "product_uom": self.product.uom_id.id,
+                            "product_uom_qty": 42,
+                        },
+                    )
+                ],
+            }
+        )
+        self.env["stock.quant"].create(
+            {
+                "location_id": self.assigned_picking.location_id.id,
+                "product_id": self.product.id,
+                "quantity": 42,
+            }
+        )
         self.assigned_picking.action_assign()
-        self.assertEqual(self.assigned_picking.state, 'assigned')
-        self.waiting_picking = self.assigned_picking.copy({
-            'name': 'Picking to assign',
-        })
+        self.assertEqual(self.assigned_picking.state, "assigned")
+        self.waiting_picking = self.assigned_picking.copy(
+            {
+                "name": "Picking to assign",
+            }
+        )
 
     def test_happy_flow(self):
-        """ Test unreservation works """
+        """Test unreservation works"""
         self.waiting_picking.action_assign()
-        self.assertEqual(self.waiting_picking.state, 'confirmed')
+        self.assertEqual(self.waiting_picking.state, "confirmed")
         self.waiting_picking.action_force_assign_pickings()
-        self.assertEqual(self.waiting_picking.state, 'assigned')
-        self.assertEqual(self.assigned_picking.state, 'confirmed')
+        self.assertEqual(self.waiting_picking.state, "assigned")
+        self.assertEqual(self.assigned_picking.state, "confirmed")
 
     def test_on_done_picking(self):
-        """ Test force assigning a done picking """
+        """Test force assigning a done picking"""
         self.waiting_picking.action_assign()
         self.assigned_picking.move_line_ids.qty_done = 42
         self.assigned_picking.action_done()
-        self.assertEqual(self.assigned_picking.state, 'done')
+        self.assertEqual(self.assigned_picking.state, "done")
         with self.assertRaises(UserError):
             self.waiting_picking.action_force_assign_pickings()
 
     def test_partial(self):
-        """ Test partial force assignment controlled by configuration parameter
-        """
+        """Test partial force assignment controlled by configuration parameter"""
         self.waiting_picking.move_lines.product_uom_qty = 43
         self.waiting_picking.action_assign()
         self.env["ir.config_parameter"].set_param(
-            "stock_picking_force_assign.allow_partial", "False")
+            "stock_picking_force_assign.allow_partial", "False"
+        )
         with self.assertRaises(UserError):
             with self.env.cr.savepoint():
                 self.waiting_picking.action_force_assign_pickings()
         # Alter the configuration
         self.env["ir.config_parameter"].set_param(
-            "stock_picking_force_assign.allow_partial", "True")
+            "stock_picking_force_assign.allow_partial", "True"
+        )
         # Partial force assignment is now possible
         self.waiting_picking.action_force_assign_pickings()
-        self.assertEqual(
-            self.waiting_picking.move_lines.reserved_availability, 42)
+        self.assertEqual(self.waiting_picking.move_lines.reserved_availability, 42)
