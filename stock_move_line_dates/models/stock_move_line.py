3d210c098456468b00066181d8479d2105e1cd9d
Andrii Kompaniiets <andrii@moduon.team>
2025-11-19 17:31:01 +0100

    [MIG] stock_move_line_dates: Migration to 18.0
    In v18 there is already the field scheduled_date (related move_id.date) stock.move.line
    Set the scheduled_date column storable and readonly.
    
    MT-11841

--- stock_move_line_dates/models/stock_move_line.py
+++ stock_move_line_dates/models/stock_move_line.py
@@ -7,15 +7,10 @@ from odoo import fields, models
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
-    date_schedule = fields.Datetime(
-        related="move_id.date",
-        string="Date Scheduled",
-        store=True,
-        readonly=True,
-    )
     date_deadline = fields.Datetime(
         related="move_id.date_deadline",
         string="Deadline",
         store=True,
         readonly=True,
     )
+    scheduled_date = fields.Datetime(store=True, readonly=True)
