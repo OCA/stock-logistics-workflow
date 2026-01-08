06936af6dc64ba23e44e1de159ba6ca6b69fa01a
Sabrina Martín <sabrina@moduon.team>
2026-01-08 16:03:27 +0000

    [MIG] stock_warn_option: Migration to 18.0

--- stock_warn_option/__manifest__.py
+++ stock_warn_option/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Warn Option",
     "summary": "Add Options to Stock Warn Messages",
-    "version": "15.0.1.0.0",
+    "version": "18.0.1.0.0",
     "development_status": "Alpha",
     "category": "Inventory/Inventory",
     "website": "https://github.com/OCA/stock-logistics-workflow",
--- stock_warn_option/tests/test_warn_options.py
+++ stock_warn_option/tests/test_warn_options.py
@@ -1,7 +1,6 @@
 # Copyright 2024 Moduon Team S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0)
 
-from odoo.tests import Form
 from odoo.tests.common import TransactionCase
 
 
@@ -31,10 +30,15 @@ class TestWarnOptions(TransactionCase):
 
     def test_partner_warn_options(self):
         """Test Warn Options on Partner Form"""
-        with Form(self.partner) as partner_f:
-            partner_f.picking_warn = "warning"
-            partner_f.picking_warn_option = self.partner_picking_warn_warning
-            self.assertEqual(partner_f.picking_warn_msg, "warning")
-            partner_f.picking_warn = "block"
-            partner_f.picking_warn_option = self.partner_picking_warn_blocking
-            self.assertEqual(partner_f.picking_warn_msg, "block")
+        partner_f = self.env["res.partner"].new(
+            {
+                "picking_warn": "warning",
+                "picking_warn_option": self.partner_picking_warn_warning.id,
+            }
+        )
+        partner_f._onchange_picking_warn_option()
+        self.assertEqual(partner_f.picking_warn_msg, "warning")
+        partner_f.picking_warn = "block"
+        partner_f.picking_warn_option = self.partner_picking_warn_blocking.id
+        partner_f._onchange_picking_warn_option()
+        self.assertEqual(partner_f.picking_warn_msg, "block")
