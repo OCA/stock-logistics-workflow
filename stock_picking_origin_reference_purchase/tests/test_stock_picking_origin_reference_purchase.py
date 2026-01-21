40d956e5e4fb4612507609978697993625a338a4
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2026-01-21 08:47:03 +0000

    [MIG] stock_picking_origin_reference_purchase: Migration to 19.0

--- stock_picking_origin_reference_purchase/tests/test_stock_picking_origin_reference_purchase.py
+++ stock_picking_origin_reference_purchase/tests/test_stock_picking_origin_reference_purchase.py
@@ -27,9 +27,9 @@ class TestStockPickingOriginReferencePurchase(
                             "name": product.name,
                             "product_id": product.id,
                             "product_qty": 5,
-                            "product_uom": product.uom_id.id,
+                            "product_uom_id": product.uom_id.id,
                             "price_unit": 500,
-                            "date_planned": fields.datetime.now(),
+                            "date_planned": fields.Datetime.now(),
                         },
                     )
                 ],
