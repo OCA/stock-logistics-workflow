f51f0faccbb971ef6700d205c4ae7edb01862ba0
Yann Papouin <ypa@decgroupe.com>
2026-01-29 09:17:02 +0100

    [MIG] product_supplierinfo_picking: Migration to 18.0

--- product_supplierinfo_picking/models/stock_move.py
+++ product_supplierinfo_picking/models/stock_move.py
@@ -7,31 +7,32 @@ from odoo import api, fields, models
 class StockMove(models.Model):
     _inherit = "stock.move"
 
-    @api.depends(
-        "picking_id.partner_id", "product_id", "product_id.seller_ids.product_code"
+    product_supplier_name = fields.Char(
+        compute="_compute_product_supplierinfo",
+        string="Vendor Product Name",
+        store=True,
     )
-    def _compute_product_supplier_code(self):
+    product_supplier_code = fields.Char(
+        compute="_compute_product_supplierinfo",
+        string="Vendor Product Code",
+        store=True,
+    )
+
+    @api.depends(
+        "picking_id.partner_id",
+        "product_id",
+        "product_id.seller_ids.product_name",
+        "product_id.seller_ids.product_code",
+    )
+    def _compute_product_supplierinfo(self):
         for move in self.filtered(
             lambda m: m.picking_id
             and m.picking_id.partner_id
             and m.product_tmpl_id.seller_ids
         ):
             suppliers = move.product_tmpl_id.seller_ids.filtered(
-                lambda m: m.name == move.picking_id.partner_id
+                lambda s, move=move: s.partner_id == move.picking_id.partner_id
             )
             if suppliers:
                 move.product_supplier_name = suppliers[0].product_name
                 move.product_supplier_code = suppliers[0].product_code
-
-    product_supplier_name = fields.Char(
-        compute="_compute_product_supplier_code",
-        string="Product Supplier Name",
-        store=True,
-        size=64,
-    )
-    product_supplier_code = fields.Char(
-        compute="_compute_product_supplier_code",
-        string="Product Supplier Code",
-        store=True,
-        size=64,
-    )
