a86e16da2e56d16a5f9f67e2de8ca31b0d6e06a3
Marcos Mendez <mendez.foto@gmail.com>
2026-03-11 11:27:21 -0300

    [ADD] stock_move_line_analytic_account: show, search and group move lines by analytic account

---
+++ stock_move_line_analytic_account/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Stock Move Line Analytic Account",
+    "summary": "Show, search and group stock move lines by analytic account",
+    "version": "16.0.1.0.0",
+    "category": "Inventory/Inventory",
+    "author": "PopSolutions, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "license": "AGPL-3",
+    "depends": ["stock_analytic"],
+    "data": [
+        "views/stock_move_line_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
