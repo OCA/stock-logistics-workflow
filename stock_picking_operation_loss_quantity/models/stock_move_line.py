48dea45d6c2da111b0c7c252565f2360cb5dafc5
Nicolas Delbovier <nicolas.delbovier@acsone.eu>
2026-03-16 12:19:48 +0100

    [REF] stock_picking_operation_loss_quantity: get rid of the useless wizards + clean code
    - Relocate the loss declaration code from external wizard to the stock.move.line model
    - Remove useless grouping logic (the code is always called on a single line through UI)
    - Rename variables in for better clarity
    - Remove code that was never reached in unit tests and that seemed dead
    - Clean and improve unit tests (add quants verifications)

--- stock_picking_operation_loss_quantity/models/stock_move_line.py
+++ stock_picking_operation_loss_quantity/models/stock_move_line.py
@@ -2,137 +2,36 @@
 # Copyright 2018 Okia SPRL <sylvain@okia.be>
 # Copyright 2023 ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from collections import defaultdict, namedtuple
-
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError, ValidationError
-from odoo.fields import Command, float_compare, float_round
-
-MoveLineGroupKey = namedtuple(
-    "MoveLineGroupKey",
-    ["location_id", "product_id", "package_id", "lot_id", "owner_id"],
-)
+from odoo.fields import Command, float_compare
 
 
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
-    is_action_loss_qty_allowed = fields.Boolean(
-        compute="_compute_is_action_loss_qty_allowed"
+    is_action_lose_quantity_allowed = fields.Boolean(
+        compute="_compute_is_action_lose_quantity_allowed"
     )
 
     @api.depends("reserved_qty", "qty_done", "picking_id.picking_type_code")
-    def _compute_is_action_loss_qty_allowed(self):
+    def _compute_is_action_lose_quantity_allowed(self):
         for rec in self:
-            rec.is_action_loss_qty_allowed = (
+            rec.is_action_lose_quantity_allowed = (
                 rec.location_id.warehouse_id.use_loss_picking
                 and (rec.qty_done - rec.reserved_qty < 0)
                 and rec.state not in ("done", "draft")
                 and rec.picking_id.picking_type_code != "incoming"
             )
 
-    def _check_is_action_loss_qty_allowed(self):
-        if any(not rec.is_action_loss_qty_allowed for rec in self):
+    def action_lose_quantity(self):
+        if any(not rec.is_action_lose_quantity_allowed for rec in self):
             raise UserError(_("You are not allowed to declare loss quantities"))
+        return self._lose_quantity()
 
-    def action_loss_quantity(self):
-        """This action process the operation and makes the remaining qty no more
-        available into the stock at the same time of creating a picking to
-        search for the qty loss. At the end, we try to recompute a new move line
-        to find qty into an other place to complete the move.
-        """
-        self._check_is_action_loss_qty_allowed()
-        wizard = self.env["stock.picking.operation.loss.quantity"].create(
-            {
-                "move_line_ids": [Command.set(self.ids)],
-            }
-        )
-        return wizard.action_lose_quantity()
-
-    def _group_move_lines_for_loss(self):
-        """
-        Return a list of move lines by MoveLineGroupKey
-        """
-        ret = defaultdict(list)
-        for rec in self:
-            key = MoveLineGroupKey(
-                location_id=rec.location_id,
-                product_id=rec.product_id,
-                package_id=rec.package_id,
-                lot_id=rec.lot_id,
-                owner_id=rec.owner_id,
-            )
-            ret[key].append(rec.id)
-
-        return {k: self.browse(v) for k, v in ret.items()}
-
-    def _prepare_values_for_loss_movement(self, group_key):
-        """
-            Careful: Lines are here for reference only as values are grouped in
-                     group key
-
-        :param group_key: _description_
-        :type group_key: _type_
-        :param quantity: _description_
-        :type quantity: _type_
-        :return: _description_
-        :rtype: _type_
-        """
+    def _create_loss_picking(self, unprocessed_qty: float):
         self.ensure_one()
-        pick_type_id = group_key.location_id.warehouse_id.loss_type_id
-
-        quants_quantity = self.env["stock.quant"]._get_available_quantity(
-            group_key.product_id,
-            group_key.location_id,
-            lot_id=group_key.lot_id,
-            package_id=group_key.package_id,
-            owner_id=group_key.owner_id,
-        )
-        loss_quantity = float_round(
-            quants_quantity - self.qty_done,
-            precision_rounding=self.product_uom_id.rounding,
-            rounding_method="HALF-UP",
-        )
-
-        self.env["stock.quant"]._update_reserved_quantity(
-            group_key.product_id,
-            group_key.location_id,
-            loss_quantity,
-            group_key.lot_id,
-            group_key.package_id,
-            group_key.owner_id,
-        )
-        move_line = {
-            "reserved_uom_qty": loss_quantity,
-            "product_id": group_key.product_id.id,
-            "picking_type_id": pick_type_id.id,
-            "package_id": group_key.package_id.id,
-            "location_id": group_key.location_id.id,
-            "location_dest_id": pick_type_id.default_location_dest_id.id,
-            "lot_id": group_key.lot_id.id,
-            "owner_id": group_key.owner_id.id,
-            "origin": "Operator: %s / Pickings: %s"
-            % (
-                self.env.user.name,
-                ", ".join(self.mapped("picking_id.name")),
-            ),
-        }
-        return move_line
-
-    def _create_loss_picking(self, group_key):
-        loss_line_values = []
-        for line in self:
-            # Free the reserved quantity with the done one in order to be able to reserve
-            # the quant for the loss movement
-            line.reserved_uom_qty = line.qty_done
-            loss_line_values.append(
-                Command.create(line._prepare_values_for_loss_movement(group_key))
-            )
-        if not loss_line_values:
-            raise ValidationError(
-                _("You try to create a Loss picking without any loss movement!")
-            )
-        pick_type_id = group_key.location_id.warehouse_id.loss_type_id
+        pick_type_id = self.location_id.warehouse_id.loss_type_id
         if not pick_type_id:
             raise ValidationError(
                 _(
@@ -147,54 +46,80 @@ class StockMoveLine(models.Model):
                     "You don't have any default destination set on your Loss picking type!"
                 )
             )
+        if (
+            float_compare(
+                unprocessed_qty, 0, precision_rounding=self.product_uom_id.rounding
+            )
+            <= 0
+        ):
+            raise ValidationError(
+                _("You try to create a Loss picking without any loss quantity!")
+            )
+
         loss_picking = self.env["stock.picking"].create(
             {
                 "picking_type_id": pick_type_id.id,
-                "location_id": group_key.location_id.id,
+                "location_id": self.location_id.id,
                 "location_dest_id": pick_type_id.default_location_dest_id.id,
-                "move_line_ids": loss_line_values,
+                "move_ids": [
+                    Command.create(
+                        {
+                            "name": self.product_id.display_name,
+                            "product_id": self.product_id.id,
+                            "product_uom_qty": unprocessed_qty,  # This is the "demand"
+                            "product_uom": self.product_uom_id.id,
+                            "location_id": self.location_id.id,
+                            "location_dest_id": pick_type_id.default_location_dest_id.id,
+                            "lot_ids": [Command.set(self.lot_id.ids)]
+                            if self.lot_id
+                            else False,
+                        }
+                    )
+                ],
             }
         )
+        loss_picking.action_confirm()
+        loss_picking.action_assign()
         return loss_picking
 
-    def _get_saved_operations_for_loss(self, group_key, operations_by_move):
-        """
-            Save already begun stock operations
-        :param operations_by_move: _description_
-        :type operations_by_move: defaultdict(dict)
-        """
-        for line in self:
-            if line.qty_done:
-                # In order to not lose any information, copy the recordset to
-                # a memory one
-                values = line._convert_to_write(line._cache).copy()
-                operations_by_move[group_key][line.id] = values
+    def _release_unprocessed_qty(self) -> float:
+        self.ensure_one()
+        unprocessed_qty = self.reserved_uom_qty - self.qty_done
+        # Free the quantity that the operator was not able to do
+        self.reserved_uom_qty = self.qty_done
+        return unprocessed_qty
 
-    def _split_for_loss(self) -> dict:
+    def _lose_quantity(self):
         """
-        This method imitates the _put_in_pack() method as it
-        copies the existing line into a new one and set
+        This is the main function to call in order to declare a loss.
+
+        It will check if operation is in progress (if operator has found the
+        whole quantity, do not allow to declare a loss).
+
+        Then, lock the quant that should be reserved by the loss picking and
+        create that loss picking.
         """
-        splitted_lines_result = dict()
-        for line in self:
-            done_to_keep = line.qty_done
+        for line in self.filtered(lambda line: line.progress != 100.0):
+            # Lock quants until the end of the transaction to avoid furter reservations
+            quants = self.env["stock.quant"]._gather(
+                product_id=line.product_id,
+                location_id=line.location_id,
+                lot_id=line.lot_id,
+                package_id=line.package_id,
+                owner_id=line.owner_id,
+            )
+            quants._lock_quants_for_loss()
+
+            unprocessed_qty = line._release_unprocessed_qty()
+            loss_picking = line._create_loss_picking(unprocessed_qty)
+            loss_picking._schedule_loss_activity()
+
             if (
                 float_compare(
-                    done_to_keep, 0, precision_rounding=line.product_uom_id.rounding
+                    line.reserved_uom_qty,
+                    0,
+                    precision_rounding=self.product_uom_id.rounding,
                 )
                 <= 0
             ):
-                continue
-            # quantity_left_todo = float_round(
-            #     line.reserved_uom_qty - done_to_keep,
-            #     precision_rounding=line.product_uom_id.rounding,
-            #     rounding_method="HALF-UP",
-            # )
-            new_move_line = line.copy(
-                default={"reserved_uom_qty": 0, "qty_done": line.qty_done}
-            )
-            vals = {"reserved_uom_qty": 0.0, "qty_done": 0.0}
-            line.write(vals)
-            new_move_line.write({"reserved_uom_qty": done_to_keep})
-            splitted_lines_result[line] = new_move_line
-        return splitted_lines_result
+                line.unlink()
--- stock_picking_operation_loss_quantity/tests/common.py
+++ stock_picking_operation_loss_quantity/tests/common.py
@@ -3,8 +3,10 @@
 # Copyright 2023 ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+from odoo.tests import TransactionCase
 
-class OperationLossQuantityCommon:
+
+class OperationLossQuantityCommon(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -13,40 +15,40 @@ class OperationLossQuantityCommon:
 
         cls.product_1 = cls.env["product.product"].create(
             {
-                "name": "Product loss quantity",
+                "name": "Product 1",
                 "type": "product",
                 "uom_id": cls.env.ref("uom.product_uom_unit").id,
                 "uom_po_id": cls.env.ref("uom.product_uom_unit").id,
-                "default_code": "Code product loss",
+                "default_code": "Tracking Lot",
                 "tracking": "lot",
             }
         )
         cls.product_2 = cls.env["product.product"].create(
             {
-                "name": "Product 2 loss quantity no tracking",
+                "name": "Product 2",
                 "type": "product",
                 "uom_id": cls.env.ref("uom.product_uom_unit").id,
                 "uom_po_id": cls.env.ref("uom.product_uom_unit").id,
-                "default_code": "Code product lot_loss no tracking 2",
+                "default_code": "No Tracking",
                 "tracking": "none",
             }
         )
         cls.product_3 = cls.env["product.product"].create(
             {
-                "name": "Product 3 loss quantity no tracking",
+                "name": "Product 3",
                 "type": "product",
                 "uom_id": cls.env.ref("uom.product_uom_unit").id,
                 "uom_po_id": cls.env.ref("uom.product_uom_unit").id,
-                "default_code": "Code product lot_loss no tracking 3",
+                "default_code": "No Tracking",
                 "tracking": "none",
             }
         )
         wh = cls.env["stock.warehouse"].search([], limit=1)
-        cls.location = wh.lot_stock_id
+        cls.loc_stock = wh.lot_stock_id
 
         cls.loc_customer = cls.env.ref("stock.stock_location_customers")
 
-        cls.pick_type = cls.env.ref("stock.picking_type_out")
+        cls.pick_type_out = cls.env.ref("stock.picking_type_out")
         cls.warehouse = wh
         cls.warehouse.use_loss_picking = True
 
@@ -69,37 +71,24 @@ class OperationLossQuantityCommon:
         # Create picking 1
         cls.picking_1 = cls.env["stock.picking"].create(
             {
-                "picking_type_id": cls.pick_type.id,
-                "location_id": cls.location.id,
+                "picking_type_id": cls.pick_type_out.id,
+                "location_id": cls.loc_stock.id,
                 "location_dest_id": cls.loc_customer.id,
             }
         )
-        cls.move_1a = cls.env["stock.move"].create(
+        cls.move_1 = cls.env["stock.move"].create(
             {
                 "picking_id": cls.picking_1.id,
-                "name": "Test move 1a",
+                "name": "Test move",
                 "product_id": cls.product_1.id,
                 "product_uom": cls.product_1.uom_id.id,
-                "product_uom_qty": 6,
-                "location_id": cls.location.id,
+                "product_uom_qty": 7,
+                "location_id": cls.loc_stock.id,
                 "location_dest_id": cls.loc_customer.id,
                 "date": "2018-01-01 00:00:00",
             }
         )
-        cls.move_1a._action_confirm()
-        cls.move_1b = cls.env["stock.move"].create(
-            {
-                "picking_id": cls.picking_1.id,
-                "name": "Test move 1b",
-                "product_id": cls.product_1.id,
-                "product_uom": cls.product_1.uom_id.id,
-                "product_uom_qty": 1,
-                "location_id": cls.location.id,
-                "location_dest_id": cls.loc_customer.id,
-                "date": "2018-01-01 00:00:00",
-            }
-        )
-        cls.move_1b._action_confirm()
+        cls.move_1._action_confirm()
 
         # Put product in stock
         # LotA: 3
@@ -107,13 +96,15 @@ class OperationLossQuantityCommon:
         cls._create_quantities(cls.product_1, 3.0, lot=cls.product_1_lotA)
         cls._create_quantities(cls.product_1, 5.0, lot=cls.product_1_lotB)
 
+        cls.picking_1.action_assign()
+
     @classmethod
     def initiate_values_no_tracking(cls):
         # Create picking 2
         cls.picking_2 = cls.env["stock.picking"].create(
             {
-                "picking_type_id": cls.pick_type.id,
-                "location_id": cls.location.id,
+                "picking_type_id": cls.pick_type_out.id,
+                "location_id": cls.loc_stock.id,
                 "location_dest_id": cls.loc_customer.id,
             }
         )
@@ -124,7 +115,7 @@ class OperationLossQuantityCommon:
                 "product_id": cls.product_2.id,
                 "product_uom": cls.product_2.uom_id.id,
                 "product_uom_qty": 6,
-                "location_id": cls.location.id,
+                "location_id": cls.loc_stock.id,
                 "location_dest_id": cls.loc_customer.id,
                 "date": "2018-01-01 00:00:00",
             }
@@ -136,8 +127,8 @@ class OperationLossQuantityCommon:
                 "name": "Test move 3",
                 "product_id": cls.product_3.id,
                 "product_uom": cls.product_3.uom_id.id,
-                "product_uom_qty": 1,
-                "location_id": cls.location.id,
+                "product_uom_qty": 2,
+                "location_id": cls.loc_stock.id,
                 "location_dest_id": cls.loc_customer.id,
                 "date": "2018-01-01 00:00:00",
             }
@@ -145,10 +136,10 @@ class OperationLossQuantityCommon:
         cls.move_3._action_confirm()
 
         # Put product in stock
-        # Product2: 3
-        # Product3: 5
-        cls._create_quantities(cls.product_2, 3.0)
-        cls._create_quantities(cls.product_3, 5.0)
+        cls._create_quantities(cls.product_2, 10.0)
+        cls._create_quantities(cls.product_3, 10.0)
+
+        cls.picking_2.action_assign()
 
     @classmethod
     def _create_quantities(
@@ -158,7 +149,7 @@ class OperationLossQuantityCommon:
             {
                 "product_id": product.id,
                 "inventory_quantity": quantity,
-                "location_id": location.id if location else cls.location.id,
+                "location_id": location.id if location else cls.loc_stock.id,
                 "lot_id": lot.id if lot else False,
                 "package_id": package.id if package else False,
             }
@@ -178,3 +169,13 @@ class OperationLossQuantityCommon:
             self.env["stock.picking"].search(self._loss_pickings_domain())
             - self.loss_pickings_before
         )
+
+    def _get_quants_available_qty(self, line):
+        quants_available_quantity = self.env["stock.quant"]._get_available_quantity(
+            product_id=line.product_id,
+            location_id=line.location_id,
+            lot_id=line.lot_id,
+            package_id=line.package_id,
+            owner_id=line.owner_id,
+        )
+        return quants_available_quantity
