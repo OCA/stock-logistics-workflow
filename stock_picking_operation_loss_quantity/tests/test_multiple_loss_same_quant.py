0f6ca59ed4559569bfa266f4b9675c49d1d0f8a8
Nicolas Delbovier <nicolas.delbovier@acsone.eu>
2026-03-17 16:22:32 +0100

    [IMP] stock_picking_operation_loss_quantity: link similar loss move lines to same loss picking

---
+++ stock_picking_operation_loss_quantity/tests/test_multiple_loss_same_quant.py
@@ -0,0 +1,52 @@
+# Copyright 2026 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from .common import OperationLossQuantityCommon
+
+
+class TestMultipleLossSameQuant(OperationLossQuantityCommon):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls._create_quantities(cls.product_2, 100.0)
+
+        cls.pickings = []
+        for _ in range(3):
+            picking = cls.env["stock.picking"].create(
+                {
+                    "picking_type_id": cls.pick_type_out.id,
+                    "location_id": cls.loc_stock.id,
+                    "location_dest_id": cls.loc_customer.id,
+                }
+            )
+            move = cls.env["stock.move"].create(
+                {
+                    "picking_id": picking.id,
+                    "name": "Test move 2",
+                    "product_id": cls.product_2.id,
+                    "product_uom": cls.product_2.uom_id.id,
+                    "product_uom_qty": 5,
+                    "location_id": cls.loc_stock.id,
+                    "location_dest_id": cls.loc_customer.id,
+                    "date": "2018-01-01 00:00:00",
+                }
+            )
+            move._action_confirm()
+
+            picking.action_assign()
+
+            cls.pickings.append(picking)
+
+    def test_one_single_loss_picking_for_same_quant(self):
+        for picking in self.pickings:
+            line = picking.move_line_ids
+            line.qty_done = 1
+            line.action_lose_quantity()
+            quant_available_qty = self._get_quants_available_qty(line)
+            self.assertEqual(
+                quant_available_qty, 85
+            )  # 100 (total) - 3*5 (from move lines)
+
+        loss_picking = self._get_loss_pickings()
+        self.assertEqual(len(loss_picking), 1)
