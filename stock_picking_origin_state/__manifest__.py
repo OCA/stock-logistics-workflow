d402e1824e179a172081f3feac8652eb2ecc24f1
Andreu Orensanz <andreu.orensanz@forgeflow.com>
2026-04-30 16:13:32 +0200

    [ADD] stock_picking_origin_state

---
+++ stock_picking_origin_state/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2026 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Picking Origin State",
+    "summary": "Expose the aggregated state of the origin pickings on a transfer",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ForgeFlow,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "depends": ["stock"],
+    "data": [
+        "views/stock_picking_views.xml",
+    ],
+}
