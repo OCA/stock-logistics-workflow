dfbeec37fa624016a040cc2dab5e21422f5e3ffe
Eduardo de Miguel <edu@moduon.team>
2026-05-13 14:19:54 +0200

    [MIG] stock_picking_move_line_no_print: Migration to 19.0

--- stock_picking_move_line_no_print/models/stock_move.py
+++ stock_picking_move_line_no_print/models/stock_move.py
@@ -1,6 +1,6 @@
 # Copyright 2025 Moduon Team S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0)
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockMove(models.Model):
@@ -14,6 +14,15 @@ class StockMove(models.Model):
         # It's need for hide in delivery slip report merged lines
         # if a least one has display_in_report False
         vals = super()._merge_moves_fields()
-        if not any(self.mapped("display_in_report")):
+        if self.filtered_domain([("display_in_report", "=", False)]):
             vals.update({"display_in_report": False})
         return vals
+
+    @api.model
+    @api.depends_context("display_in_report")
+    def _search(self, domain, *args, **kwargs):
+        if self.env.context.get("display_in_report"):
+            domain = fields.Domain.AND(
+                [domain, fields.Domain("display_in_report", "=", True)]
+            )
+        return super()._search(domain, *args, **kwargs)
--- stock_picking_move_line_no_print/tests/test_no_print_picking_move_line.py
+++ stock_picking_move_line_no_print/tests/test_no_print_picking_move_line.py
@@ -1,7 +1,6 @@
 # Copyright 2026 Moduon Team S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0)
 from odoo.tests import Form, common
-from odoo.tools import html2plaintext
 
 
 class TestStockPickingReportCustomName(common.TransactionCase):
@@ -11,35 +10,49 @@ class TestStockPickingReportCustomName(common.TransactionCase):
         cls.customer = cls.env["res.partner"].create({"name": "Mr. Odoo"})
         cls.product = cls.env["product.product"].create(
             {
-                "name": "Test Name",
-                "default_code": "SPN",
+                "name": "Test Name P1",
+                "default_code": "SPN1",
                 "type": "consu",
             }
         )
         cls.product_2 = cls.env["product.product"].create(
             {
-                "name": "Test Name For Print",
-                "default_code": "SPN",
+                "name": "Test Name P2",
+                "default_code": "SPN2",
                 "type": "consu",
             }
         )
         cls.delivery_type = cls.env.ref("stock.picking_type_out")
         cls.picking_form = Form(cls.env["stock.picking"])
         cls.picking_form.picking_type_id = cls.env.ref("stock.picking_type_out")
-        with cls.picking_form.move_ids_without_package.new() as move_form:
+        with cls.picking_form.move_ids.new() as move_form:
             move_form.product_id = cls.product
             move_form.quantity = 1
             move_form.product_uom_qty = 1
+            move_form.display_in_report = False
+        with cls.picking_form.move_ids.new() as move_form:
+            move_form.product_id = cls.product_2
+            move_form.quantity = 1
+            move_form.product_uom_qty = 1
+            move_form.display_in_report = True
         cls.picking = cls.picking_form.save()
 
     def _get_report_in_plain_text(self):
-        html, _ = self.env["ir.actions.report"]._render_qweb_html(
-            "stock.action_report_delivery", self.picking.ids
+        txt, _ = self.env["ir.actions.report"]._render_qweb_text(
+            "stock.report_deliveryslip", self.picking.ids
         )
-        return html2plaintext(html)
+        return txt.decode("utf-8")
+
+    def test_delivery_slip_without_move(self):
+        self.assertNotEqual(self.picking.state, "done")
+        text = self._get_report_in_plain_text()
+        self.assertFalse("Test Name P1" in text)
+        self.assertTrue("Test Name P2" in text)
 
     def test_delivery_slip_without_move_line(self):
-        self.picking.move_ids[0].display_in_report = False
+        self.picking.button_validate()
+        self.assertEqual(self.picking.state, "done")
         text = self._get_report_in_plain_text()
-        self.assertFalse("Test Name" in text)
-        self.assertFalse("Test Name For Print" in text)
+        # print(text)
+        self.assertFalse("Test Name P1" in text)
+        self.assertTrue("Test Name P2" in text)
