c2ca905ee6b90973d68a3c9bd2434986d4527bbb
bosd <ebo@stefcy.com>
2026-07-23 08:45:11 +0200

    [ADD] stock_move_backdating: regression tests for multi-move _get_price_unit
    Cover the crash fixed in the previous commit: _get_price_unit called on
    a multi-move recordset (as sale_stock_margin does via
    _get_price_unit_delivery) must not raise 'Expected singleton' and must
    return a scalar, never a {lot: price} dict.

--- stock_move_backdating/tests/__init__.py
+++ stock_move_backdating/tests/__init__.py
@@ -1,5 +1,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from . import test_get_price_unit
 from . import test_move
 from . import test_move_line
 from . import test_picking
