945f5f317a755d0b3b130ab2ec2fc40a89d7defb
Abraham Jesús <febresaj@gmail.com>
2026-08-19 08:47:07 +0200

    [ADD] stock_picking_portal_owner: add module Stock Picking Portal Owner.

--- stock_picking_portal_owner/controllers/portal.py
+++ stock_picking_portal_owner/controllers/portal.py
@@ -1,99 +1,41 @@
-# Copyright (C) 2024 Cetmix OÜ
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-import binascii
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import _, fields, http
-from odoo.exceptions import AccessDenied, AccessError, MissingError, UserError
+from odoo.exceptions import AccessDenied, AccessError, MissingError
 from odoo.http import request
 
-from odoo.addons.portal.controllers import portal
 from odoo.addons.portal.controllers.mail import _message_post_helper
 from odoo.addons.portal.controllers.portal import pager as portal_pager
+from odoo.addons.stock_picking_portal.controllers.portal import (
+    CustomerPortal as StockPickingPortal,
+)
 
 
-class CustomerPortal(portal.CustomerPortal):
-    def _get_prepared_operation_domain(self, partner):
-        """Returns a domain to search for operations for the given partner.
-
-        Args:
-            partner (res.partner): partner to search operations for.
-
-        Returns:
-            list: domain to search for operations for the given partner.
-
-        """
+class StockPickingPortalOwner(StockPickingPortal):
+    def _get_prepared_owner_operation_domain(self, partner):
         portal_visible_operation_ids = (
             request.env["stock.picking.type"].sudo()._get_available_operations()
         )
         return [
-            ("partner_id", "=", partner.id),
+            ("owner_id", "=", partner.id),
             ("picking_type_id", "in", portal_visible_operation_ids),
         ]
 
     def _prepare_home_portal_values(self, counters):
-        """
-        Values for /my & /my/home routes template rendering.
-
-        Includes the record count for the displayed badges.
-        where 'counters' is the list of the displayed badges
-        and so the list to compute.
-        """
         values = super()._prepare_home_portal_values(counters)
-        if "stock_operations_count" in counters:
-            partner = request.env.user.partner_id
-            domain = self._get_prepared_operation_domain(partner)
-            stock_operations_count = request.env["stock.picking"].search_count(domain)
-            values["stock_operations_count"] = (
-                stock_operations_count if stock_operations_count > 0 else "0"
+        if "owner_stock_operations_count" in counters:
+            domain = self._get_prepared_owner_operation_domain(
+                request.env.user.partner_id
+            )
+            owner_stock_operations_count = request.env["stock.picking"].search_count(
+                domain
+            )
+            values["owner_stock_operations_count"] = (
+                owner_stock_operations_count and owner_stock_operations_count or "0"
             )
         return values
 
-    def _get_stock_operations_base_url(self):
-        return "/my/stock_operations"
-
-    @http.route(
-        ["/my/stock_operations", "/my/stock_operations/page/<int:page>"],
-        type="http",
-        auth="user",
-        website=True,
-    )
-    def portal_my_stock_operations(self, **kwargs):
-        """
-        Prepares the values required for rendering the stock operations
-        portal view using the `_prepare_stock_operations_portal_rendering_values`
-        method
-        """
-        values = self._prepare_stock_operations_portal_rendering_values(**kwargs)
-        request.session["my_operation_history"] = values["stock_operation_ids"].ids[
-            :100
-        ]
-        return request.render("stock_picking_portal.portal_my_stock_operations", values)
-
-    def _get_stock_operations_searchbar_sortings(self):
-        return {
-            "date": {"label": _("Order Date"), "order": "scheduled_date desc"},
-            "name": {"label": _("Reference"), "order": "name"},
-            "state": {"label": _("State"), "order": "state"},
-        }
-
-    def _get_stock_operations_searchbar_filters(self):
-        return {
-            "all": {
-                "label": _("All"),
-                "domain": [("picking_type_id.code", "in", ("outgoing", "incoming"))],
-            },
-            "outgoing": {
-                "label": _("Delivery"),
-                "domain": [("picking_type_id.code", "=", "outgoing")],
-            },
-            "incoming": {
-                "label": _("Receipt"),
-                "domain": [("picking_type_id.code", "=", "incoming")],
-            },
-        }
-
-    def _prepare_stock_operations_portal_rendering_values(
+    def _prepare_stock_operations_portal_owner_rendering_values(
         self,
         page=1,
         date_begin=None,
@@ -125,8 +67,8 @@ class CustomerPortal(portal.CustomerPortal):
         """
         partner = request.env.user.partner_id
         StockPicking = request.env["stock.picking"]
-        url = "/my/stock_operations"
-        domain = self._get_prepared_operation_domain(partner)
+        url = "/my/stock_operations/owner"
+        domain = self._get_prepared_owner_operation_domain(partner)
         if not sortby:
             sortby = "date"
         if not filterby:
@@ -165,18 +107,34 @@ class CustomerPortal(portal.CustomerPortal):
                 "searchbar_sortings": searchbar_sortings,
                 "sortby": sortby,
                 "searchbar_filters": searchbar_filters,
-                "page_name": "stock_operations",
+                "page_name": "owner_stock_operations",
             }
         )
         return values
 
     @http.route(
-        ["/my/stock_operations/<int:operation_id>"],
+        [
+            "/my/stock_operations/owner",
+            "/my/stock_operations/owner/page/<int:page>",
+        ],
+        type="http",
+        auth="user",
+        website=True,
+    )
+    def portal_my_owner_stock_operations(self, **kwargs):
+        values = self._prepare_stock_operations_portal_owner_rendering_values(**kwargs)
+        request.session["my_owner_operation_history"] = values[
+            "stock_operation_ids"
+        ].ids[:100]
+        return request.render("stock_picking_portal.portal_my_stock_operations", values)
+
+    @http.route(
+        ["/my/stock_operations/owner/<int:operation_id>"],
         type="http",
         auth="public",
         website=True,
     )
-    def portal_stock_operation_page(
+    def portal_owner_stock_operation_page(
         self,
         operation_id,
         report_type=None,
@@ -249,80 +207,15 @@ class CustomerPortal(portal.CustomerPortal):
                     partner_ids=operation_sudo.user_id.sudo().partner_id.ids,
                 )
         values = {
-            "stock_operations": operation_sudo,
+            "owner_stock_operations": operation_sudo,
             "res_company": operation_sudo.company_id,
-            "page_name": "stock_operations",
+            "page_name": "owner_stock_operations",
             "report_type": "html",
             "message": message,
         }
         values = self._get_page_view_values(
-            operation_sudo, access_token, values, "my_operation_history", False
+            operation_sudo, access_token, values, "my_owner_operation_history", False
         )
         return request.render(
             "stock_picking_portal.stock_operation_portal_template", values
         )
-
-    @http.route(
-        ["/my/stock_operations/<int:operation_id>/accept"],
-        type="json",
-        auth="public",
-        website=True,
-    )
-    def portal_stock_operations_accept(
-        self, operation_id, access_token=None, name=None, signature=None
-    ):
-        """
-        Acceptance of the warehouse operation by the user signing the document.
-
-        Args:
-            operation_id (int): The ID of the stock operation to accept.
-            access_token (str, optional): The access token for the portal user.
-                If not provided, it will be retrieved from the query string.
-            name (str, optional): The name of the user accepting the operation.
-            signature (str, optional): The signature of the user accepting
-            the operation.
-
-        Returns:
-            dict: A dictionary containing the URL to redirect the user
-            to after accepting the operation, and a flag to indicate
-            whether to force a refresh of the page."""
-        access_token = access_token or request.httprequest.args.get("access_token")
-        try:
-            operation_sudo = self._document_check_access(
-                "stock.picking", operation_id, access_token=access_token
-            )
-        except (AccessError, MissingError):
-            return {"error": _("Invalid Stock Operation.")}
-
-        if not signature:
-            return {"error": _("Signature is missing.")}
-
-        try:
-            operation_sudo.write(
-                {
-                    "signed_by": name,
-                    "signed_on": fields.Datetime.now(),
-                    "signature": signature,
-                }
-            )
-        except (TypeError, binascii.Error, UserError):
-            return {"error": _("Invalid signature data.")}
-
-        pdf = (
-            request.env["ir.actions.report"]
-            .sudo()
-            ._render_qweb_pdf("stock.action_report_delivery", [operation_sudo.id])[0]
-        )
-
-        _message_post_helper(
-            "stock.picking",
-            operation_sudo.id,
-            _("Stock Operation signed by %s", name),
-            attachments=[("%s.pdf" % operation_sudo.name, pdf)],
-            token=access_token,
-        )
-        query_string = "&message=sign_ok"
-        return {
-            "force_refresh": True,
-            "redirect_url": operation_sudo.get_portal_url(query_string=query_string),
-        }
